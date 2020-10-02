radareorg/radare2

Rename `asm.filter` to `asm.sub.names` ##cons (#17737)
* Rename `asm.filter` to `asm.sub.names`
* Rename tests for `asm.filter`

libr/asm/asm.c
@@ -219,7 +219,7 @@ R_API bool r_asm_setup(RAsm *a, const char *arch, int bits, int big_endian) {
 }
 
 // TODO: spagueti
-R_API int r_asm_filter_input(RAsm *a, const char *f) {
+R_API int r_asm_sub_names_input(RAsm *a, const char *f) {
 	r_return_val_if_fail (a && f, false);
 	if (!a->ifilter) {
 		a->ifilter = r_parse_new ();
@@ -232,7 +232,7 @@ R_API int r_asm_filter_input(RAsm *a, const char *f) {
 	return true;
 }
 
-R_API int r_asm_filter_output(RAsm *a, const char *f) {
+R_API int r_asm_sub_names_output(RAsm *a, const char *f) {
 	if (!a->ofilter) {
 		a->ofilter = r_parse_new ();
 	}

libr/core/cmd_search.c
@@ -2260,7 +2260,7 @@ static void do_asm_search(RCore *core, struct search_parameters *param, const ch
 	}
 
 	maxhits = (int) r_config_get_i (core->config, "search.maxhits");
-	filter = (int) r_config_get_i (core->config, "asm.filter");
+	filter = (int) r_config_get_i (core->config, "asm.sub.names");
 	if (param->outmode == R_MODE_JSON) {
 		r_cons_print ("[");
 	}

libr/core/disasm.c
@@ -88,7 +88,7 @@ typedef struct {
 	int atabsoff;
 	int decode;
 	bool pseudo;
-	int filter;
+	int subnames;
 	int interactive;
 	bool subjmp;
 	bool subvar;
@@ -630,7 +630,7 @@ static RDisasmState * ds_init(RCore *core) {
 	if (ds->pseudo) {
 		ds->atabs = 0;
 	}
-	ds->filter = r_config_get_i (core->config, "asm.filter");
+	ds->subnames = r_config_get_i (core->config, "asm.sub.names");
 	ds->interactive = r_cons_is_interactive ();
 	ds->subjmp = r_config_get_i (core->config, "asm.sub.jmp");
 	ds->subvar = r_config_get_i (core->config, "asm.sub.var");
@@ -1060,7 +1060,7 @@ static void ds_build_op_str(RDisasmState *ds, bool print_color) {
 		free (ds->opstr);
 		ds->opstr = strdup (ds->hint->opcode);
 	}
-	if (ds->filter) {
+	if (ds->subnames) {
 		RSpace *ofs = core->parser->flagspace;
 		RSpace *fs = ds->flagspace_ports;
 		if (ds->analop.type == R_ANAL_OP_TYPE_IO) {
@@ -3371,7 +3371,7 @@ static void ds_print_fcn_name(RDisasmState *ds) {
 		return;
 	}
 	RAnalFunction *f = fcnIn (ds, ds->analop.jump, R_ANAL_FCN_TYPE_NULL);
-	if (!f && ds->core->flags && (!ds->core->vmode || (!ds->subjmp && !ds->filter))) {
+	if (!f && ds->core->flags && (!ds->core->vmode || (!ds->subjmp && !ds->subnames))) {
 		const char *arch;
 		RFlagItem *flag = r_flag_get_by_spaces (ds->core->flags, ds->analop.jump,
 		                                        R_FLAGS_FS_CLASSES, R_FLAGS_FS_SYMBOLS, NULL);
@@ -3410,7 +3410,7 @@ static void ds_print_fcn_name(RDisasmState *ds) {
 		} else if (delta < 0) {
 			ds_begin_comment (ds);
 			ds_comment (ds, true, "; %s-0x%x", f->name, -delta);
-		} else if ((!ds->core->vmode || (!ds->subjmp && !ds->filter))
+		} else if ((!ds->core->vmode || (!ds->subjmp && !ds->subnames))
 			   && (!ds->opstr || !strstr (ds->opstr, f->name))) {
 			RFlagItem *flag_sym;
 			if (ds->core->vmode && ds->asm_demangle
@@ -5770,7 +5770,7 @@ toro:
 					free (ds->opstr);
 					ds->opstr = strdup (R_STRBUF_SAFEGET (&ds->analop.esil));
 				}
-			} else if (ds->filter) {
+			} else if (ds->subnames) {
 				char *asm_str;
 				RSpace *ofs = core->parser->flagspace;
 				RSpace *fs = ds->flagspace_ports;
@@ -6331,7 +6331,7 @@ R_API int r_core_disasm_pdi_with_buf(RCore *core, ut64 address, ut8 *buf, ut32 n
 	bool show_offset = r_config_get_i (core->config, "asm.offset");
 	bool show_bytes = r_config_get_i (core->config, "asm.bytes");
 	int decode = r_config_get_i (core->config, "asm.decode");
-	int filter = r_config_get_i (core->config, "asm.filter");
+	int subnames = r_config_get_i (core->config, "asm.sub.names");
 	int show_color = r_config_get_i (core->config, "scr.color");
 	bool asm_ucase = r_config_get_i (core->config, "asm.ucase");
 	bool asm_instr = r_config_get_i (core->config, "asm.instr");
@@ -6531,7 +6531,7 @@ toro:
 				if (asm_immtrim) {
 					r_parse_immtrim (asm_str);
 				}
-				if (filter) {
+				if (subnames) {
 					RAnalHint *hint = r_anal_hint_get (core->anal, at);
 					r_parse_filter (core->parser, at, core->flags, hint,
 						asm_str, opstr, sizeof (opstr) - 1, core->print->big_endian);

libr/core/pseudo.c
@@ -191,7 +191,7 @@ R_API int r_core_pseudo_code(RCore *core, const char *input) {
 	}
 	r_config_hold_i (hc, "asm.pseudo", "asm.decode", "asm.lines", "asm.bytes", "asm.stackptr", NULL);
 	r_config_hold_i (hc, "asm.offset", "asm.flags", "asm.lines.fcn", "asm.comments", NULL);
-	r_config_hold_i (hc, "asm.functions", "asm.section", "asm.cmt.col", "asm.filter", NULL);
+	r_config_hold_i (hc, "asm.functions", "asm.section", "asm.cmt.col", "asm.sub.names", NULL);
 	r_config_hold_i (hc, "scr.color", "emu.str", "asm.emu", "emu.write", NULL);
 	r_config_hold_i (hc, "io.cache", NULL);
 	if (!fcn) {
@@ -203,7 +203,7 @@ R_API int r_core_pseudo_code(RCore *core, const char *input) {
 	r_config_set_i (core->config, "asm.stackptr", 0);
 	r_config_set_i (core->config, "asm.pseudo", 1);
 	r_config_set_i (core->config, "asm.decode", 0);
-	r_config_set_i (core->config, "asm.filter", 1);
+	r_config_set_i (core->config, "asm.sub.names", 1);
 	r_config_set_i (core->config, "asm.lines", 0);
 	r_config_set_i (core->config, "asm.bytes", 0);
 	r_config_set_i (core->config, "asm.offset", 0);

rizinorg/rizin

librz/asm/asm.c
@@ -219,7 +219,7 @@ RZ_API bool rz_asm_setup(RzAsm *a, const char *arch, int bits, int big_endian) {
 }
 
 // TODO: spagueti
-RZ_API int rz_asm_filter_input(RzAsm *a, const char *f) {
+RZ_API int rz_asm_sub_names_input(RzAsm *a, const char *f) {
 	rz_return_val_if_fail (a && f, false);
 	if (!a->ifilter) {
 		a->ifilter = rz_parse_new ();
@@ -232,7 +232,7 @@ RZ_API int rz_asm_filter_input(RzAsm *a, const char *f) {
 	return true;
 }
 
-RZ_API int rz_asm_filter_output(RzAsm *a, const char *f) {
+RZ_API int rz_asm_sub_names_output(RzAsm *a, const char *f) {
 	if (!a->ofilter) {
 		a->ofilter = rz_parse_new ();
 	}

librz/core/cmd_search.c
@@ -2259,7 +2259,7 @@ static void do_asm_search(RzCore *core, struct search_parameters *param, const c
 	}
 
 	maxhits = (int) rz_config_get_i (core->config, "search.maxhits");
-	filter = (int) rz_config_get_i (core->config, "asm.filter");
+	filter = (int) rz_config_get_i (core->config, "asm.sub.names");
 	if (param->outmode == RZ_MODE_JSON) {
 		rz_cons_print ("[");
 	}

librz/core/disasm.c
@@ -88,7 +88,7 @@ typedef struct {
 	int atabsoff;
 	int decode;
 	bool pseudo;
-	int filter;
+	int subnames;
 	int interactive;
 	bool subjmp;
 	bool subvar;
@@ -630,7 +630,7 @@ static RDisasmState * ds_init(RzCore *core) {
 	if (ds->pseudo) {
 		ds->atabs = 0;
 	}
-	ds->filter = rz_config_get_i (core->config, "asm.filter");
+	ds->subnames = rz_config_get_i (core->config, "asm.sub.names");
 	ds->interactive = rz_cons_is_interactive ();
 	ds->subjmp = rz_config_get_i (core->config, "asm.sub.jmp");
 	ds->subvar = rz_config_get_i (core->config, "asm.sub.var");
@@ -1060,7 +1060,7 @@ static void ds_build_op_str(RDisasmState *ds, bool print_color) {
 		free (ds->opstr);
 		ds->opstr = strdup (ds->hint->opcode);
 	}
-	if (ds->filter) {
+	if (ds->subnames) {
 		RSpace *ofs = core->parser->flagspace;
 		RSpace *fs = ds->flagspace_ports;
 		if (ds->analop.type == RZ_ANAL_OP_TYPE_IO) {
@@ -3371,7 +3371,7 @@ static void ds_print_fcn_name(RDisasmState *ds) {
 		return;
 	}
 	RzAnalFunction *f = fcnIn (ds, ds->analop.jump, RZ_ANAL_FCN_TYPE_NULL);
-	if (!f && ds->core->flags && (!ds->core->vmode || (!ds->subjmp && !ds->filter))) {
+	if (!f && ds->core->flags && (!ds->core->vmode || (!ds->subjmp && !ds->subnames))) {
 		const char *arch;
 		RzFlagItem *flag = rz_flag_get_by_spaces (ds->core->flags, ds->analop.jump,
 		                                        RZ_FLAGS_FS_CLASSES, RZ_FLAGS_FS_SYMBOLS, NULL);
@@ -3410,7 +3410,7 @@ static void ds_print_fcn_name(RDisasmState *ds) {
 		} else if (delta < 0) {
 			ds_begin_comment (ds);
 			ds_comment (ds, true, "; %s-0x%x", f->name, -delta);
-		} else if ((!ds->core->vmode || (!ds->subjmp && !ds->filter))
+		} else if ((!ds->core->vmode || (!ds->subjmp && !ds->subnames))
 			   && (!ds->opstr || !strstr (ds->opstr, f->name))) {
 			RzFlagItem *flag_sym;
 			if (ds->core->vmode && ds->asm_demangle
@@ -5770,7 +5770,7 @@ toro:
 					free (ds->opstr);
 					ds->opstr = strdup (RZ_STRBUF_SAFEGET (&ds->analop.esil));
 				}
-			} else if (ds->filter) {
+			} else if (ds->subnames) {
 				char *asm_str;
 				RSpace *ofs = core->parser->flagspace;
 				RSpace *fs = ds->flagspace_ports;
@@ -6331,7 +6331,7 @@ RZ_API int rz_core_disasm_pdi_with_buf(RzCore *core, ut64 address, ut8 *buf, ut3
 	bool show_offset = rz_config_get_i (core->config, "asm.offset");
 	bool show_bytes = rz_config_get_i (core->config, "asm.bytes");
 	int decode = rz_config_get_i (core->config, "asm.decode");
-	int filter = rz_config_get_i (core->config, "asm.filter");
+	int subnames = rz_config_get_i (core->config, "asm.sub.names");
 	int show_color = rz_config_get_i (core->config, "scr.color");
 	bool asm_ucase = rz_config_get_i (core->config, "asm.ucase");
 	bool asm_instr = rz_config_get_i (core->config, "asm.instr");
@@ -6531,7 +6531,7 @@ toro:
 				if (asm_immtrim) {
 					rz_parse_immtrim (asm_str);
 				}
-				if (filter) {
+				if (subnames) {
 					RzAnalHint *hint = rz_anal_hint_get (core->anal, at);
 					rz_parse_filter (core->parser, at, core->flags, hint,
 						asm_str, opstr, sizeof (opstr) - 1, core->print->big_endian);

librz/core/pseudo.c
@@ -191,7 +191,7 @@ RZ_API int rz_core_pseudo_code(RzCore *core, const char *input) {
 	}
 	rz_config_hold_i (hc, "asm.pseudo", "asm.decode", "asm.lines", "asm.bytes", "asm.stackptr", NULL);
 	rz_config_hold_i (hc, "asm.offset", "asm.flags", "asm.lines.fcn", "asm.comments", NULL);
-	rz_config_hold_i (hc, "asm.functions", "asm.section", "asm.cmt.col", "asm.filter", NULL);
+	rz_config_hold_i (hc, "asm.functions", "asm.section", "asm.cmt.col", "asm.sub.names", NULL);
 	rz_config_hold_i (hc, "scr.color", "emu.str", "asm.emu", "emu.write", NULL);
 	rz_config_hold_i (hc, "io.cache", NULL);
 	if (!fcn) {
@@ -203,7 +203,7 @@ RZ_API int rz_core_pseudo_code(RzCore *core, const char *input) {
 	rz_config_set_i (core->config, "asm.stackptr", 0);
 	rz_config_set_i (core->config, "asm.pseudo", 1);
 	rz_config_set_i (core->config, "asm.decode", 0);
-	rz_config_set_i (core->config, "asm.filter", 1);
+	rz_config_set_i (core->config, "asm.sub.names", 1);
 	rz_config_set_i (core->config, "asm.lines", 0);
 	rz_config_set_i (core->config, "asm.bytes", 0);
 	rz_config_set_i (core->config, "asm.offset", 0);