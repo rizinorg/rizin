20edd0bc91b7defa84b137c5ec5d8f3fbbd036c4
yossizap <yossizap@gmail.com>
2020-10-07 07:43:41 +0000

    Fix dp 0 not showing other processes on freebsd ##debug

--- librz/debug/p/native/bsd/bsd_debug.h
+++ librz/debug/p/native/bsd/bsd_debug.h
@@ -9,7 +9,7 @@
 int bsd_handle_signals(RzDebug *dbg);
 int bsd_reg_write(RzDebug *dbg, int type, const ut8 *buf, int size);
 RzDebugInfo *bsd_info(RzDebug *dbg, const char *arg);
-RzList *bsd_pid_list(RzDebug *dbg, RzList *list);
+RzList *bsd_pid_list(RzDebug *dbg, int pid, RzList *list);
 RzList *bsd_native_sysctl_map(RzDebug *dbg);
 RzList *bsd_desc_list(int pid);
 RzList *bsd_thread_list(RzDebug *dbg, int pid, RzList *list);
