radareorg/radare2

Graph refactor ##refactor (#17631)
* Support more output modes for class inheritence graph.
* Refactor codexrefs and importxrefs graphs.
* Add ag_w for the commands using new mechanism.
* Separate drawable graph from r_graph.

libr/core/canal.c
@@ -6,6 +6,7 @@
 #include <r_core.h>
 #include <r_bin.h>
 #include <ht_uu.h>
+#include <r_util/r_graph_drawable.h>
 
 #include <string.h>
 
@@ -2280,43 +2281,64 @@ R_API void r_core_anal_coderefs(RCore *core, ut64 addr) {
 	}
 }
 
-R_API void r_core_anal_importxrefs(RCore *core) {
-	RBinInfo *info = r_bin_get_info (core->bin);
-	RBinObject *obj = r_bin_cur_object (core->bin);
-	bool lit = info ? info->has_lit: false;
-	bool va = core->io->va || core->bin->is_debugger;
-
-	RListIter *iter;
-	RBinImport *imp;
-	if (!obj) {
-		return;
-	}
-	r_list_foreach (obj->imports, iter, imp) {
-		ut64 addr = lit ? r_core_bin_impaddr (core->bin, va, imp->name): 0;
-		if (addr) {
-			r_core_anal_codexrefs (core, addr);
-		} else {
-			r_cons_printf ("agn %s\n", imp->name);
-		}
-	}
-}
-
-R_API void r_core_anal_codexrefs(RCore *core, ut64 addr) {
+static void add_single_addr_xrefs(RCore *core, ut64 addr, RGraph *graph) {
+	r_return_if_fail (graph);
 	RFlagItem *f = r_flag_get_at (core->flags, addr, false);
 	char *me = (f && f->offset == addr)
-		? r_str_new (f->name) : r_str_newf ("0x%"PFMT64x, addr);
-	r_cons_printf ("agn %s\n", me);
+		? r_str_new (f->name)
+		: r_str_newf ("0x%" PFMT64x, addr);
+
+	RGraphNode *curr_node = r_graph_add_node_info (graph, me, NULL, addr);
+	R_FREE (me);
+	if (!curr_node) {
+		return;
+	}
 	RListIter *iter;
 	RAnalRef *ref;
 	RList *list = r_anal_xrefs_get (core->anal, addr);
 	r_list_foreach (list, iter, ref) {
 		RFlagItem *item = r_flag_get_i (core->flags, ref->addr);
-		const char *src = item? item->name: sdb_fmt ("0x%08"PFMT64x, ref->addr);
-		r_cons_printf ("agn %s\n", src);
-		r_cons_printf ("age %s %s\n", src, me);
+		char *src = item? r_str_new (item->name): r_str_newf ("0x%08" PFMT64x, ref->addr);
+		RGraphNode *reference_from = r_graph_add_node_info (graph, src, NULL, ref->addr);
+		free (src);
+		r_graph_add_edge (graph, reference_from, curr_node);
 	}
 	r_list_free (list);
-	free (me);
+}
+
+R_API RGraph *r_core_anal_importxrefs(RCore *core) {
+	RBinInfo *info = r_bin_get_info (core->bin);
+	RBinObject *obj = r_bin_cur_object (core->bin);
+	bool lit = info? info->has_lit: false;
+	bool va = core->io->va || core->bin->is_debugger;
+
+	RListIter *iter;
+	RBinImport *imp;
+	if (!obj) {
+		return NULL;
+	}
+	RGraph *graph = r_graph_new ();
+	if (!graph) {
+		return NULL;
+	}
+	r_list_foreach (obj->imports, iter, imp) {
+		ut64 addr = lit ? r_core_bin_impaddr (core->bin, va, imp->name): 0;
+		if (addr) {
+			add_single_addr_xrefs (core, addr, graph);
+		} else {
+			r_graph_add_node_info (graph, imp->name, NULL, 0);
+		}
+	}
+	return graph;
+}
+
+R_API RGraph *r_core_anal_codexrefs(RCore *core, ut64 addr) {
+	RGraph *graph = r_graph_new ();
+	if (!graph) {
+		return NULL;
+	}
+	add_single_addr_xrefs (core, addr, graph);
+	return graph;
 }
 
 static int RAnalRef_cmp(const RAnalRef* ref1, const RAnalRef* ref2) {

libr/util/graph_drawable.c
@@ -1,0 +1,104 @@
+#include <r_core.h>
+#include <r_util/r_graph_drawable.h>
+
+R_API void r_graph_free_node_info(void *ptr) {
+	if (!ptr) {
+		return;
+	}
+	RGraphNodeInfo *info = ptr;
+	free (info->body);
+	free (info->title);
+	free (info);
+}
+
+R_API RGraphNodeInfo *r_graph_create_node_info(const char *title, const char *body, ut64 offset) {
+	RGraphNodeInfo *data = R_NEW0 (RGraphNodeInfo);
+	if (data) {
+		data->title = R_STR_DUP (title);
+		data->body = R_STR_DUP (body);
+		data->offset = offset;
+	}
+	return data;
+}
+
+R_API RGraphNode *r_graph_add_node_info(RGraph *graph, const char *title, const char *body, ut64 offset) {
+	r_return_val_if_fail (graph, NULL);
+	RGraphNodeInfo *data = r_graph_create_node_info (title, body, offset);
+	if (!data) {
+		return NULL;
+	}
+	RGraphNode *node = r_graph_add_nodef (graph, data, r_graph_free_node_info);
+	if (!node) {
+		r_graph_free_node_info (data);
+	}
+	return node;
+}
+
+R_API char *r_graph_drawable_to_dot(RGraph /*RGraphNodeInfo*/ *graph, const char *node_properties, const char *edge_properties) {
+	RList *nodes = graph->nodes;
+	RListIter *it, *itt;
+	RGraphNode *node = NULL, *target = NULL;
+	RStrBuf buf;
+	r_strbuf_init (&buf);
+	r_strbuf_appendf (&buf,
+		"digraph code {\nrankdir=LR;\noutputorder=edgesfirst\ngraph [bgcolor=azure];\n"
+		"edge [arrowhead=normal, color=\"#3030c0\" style=bold weight=2 %s];\n"
+		"node [fillcolor=white, style=filled shape=box "
+		"fontsize=\"8\" %s];\n",
+		edge_properties? edge_properties: "",
+		node_properties? node_properties: "");
+
+	r_list_foreach (nodes, it, node) {
+		RGraphNodeInfo *print_node = (RGraphNodeInfo *)node->data;
+		const char *body = print_node->body;
+		if (!body || !*body) {
+			r_strbuf_appendf (&buf, "%d [URL=\"%s\", color=\"lightgray\", label=\"%s\"]\n",
+				node->idx, print_node->title, print_node->title);
+		} else {
+			r_strbuf_appendf (&buf, "%d [URL=\"%s\", color=\"lightgray\", label=\"%s\\n%s\"]\n",
+				node->idx, print_node->title, print_node->title, body);
+		}
+		r_list_foreach (node->out_nodes, itt, target) {
+			r_strbuf_appendf (&buf, "%d -> %d\n", node->idx, target->idx);
+		}
+	}
+	r_strbuf_append (&buf, "}\n");
+	return r_strbuf_drain_nofree (&buf);
+}
+
+R_API void r_graph_drawable_to_json(RGraph /*RGraphNodeInfo*/ *graph, PJ *pj, bool use_offset) {
+	RList *nodes = graph->nodes, *neighbours = NULL;
+	RListIter *it, *itt;
+	RGraphNode *node = NULL, *neighbour = NULL;
+	if (!pj) {
+		return;
+	}
+	pj_o (pj);
+	pj_k (pj, "nodes");
+	pj_a (pj);
+
+	r_list_foreach (nodes, it, node) {
+		RGraphNodeInfo *print_node = (RGraphNodeInfo *)node->data;
+		pj_o (pj);
+		pj_ki (pj, "id", node->idx);
+		if (print_node->title) {
+			pj_ks (pj, "title", print_node->title);
+		}
+		if (print_node->body) {
+			pj_ks (pj, "body", print_node->body);
+		}
+		if (use_offset) {
+			pj_kn (pj, "offset", print_node->offset);
+		}
+		pj_k (pj, "out_nodes");
+		pj_a (pj);
+		neighbours = node->out_nodes;
+		r_list_foreach (neighbours, itt, neighbour) {
+			pj_i (pj, neighbour->idx);
+		}
+		pj_end (pj);
+		pj_end (pj);
+	}
+	pj_end (pj);
+	pj_end (pj);
+}

rizinorg/rizin

librz/core/canal.c
@@ -6,6 +6,7 @@
 #include <rz_core.h>
 #include <rz_bin.h>
 #include <ht_uu.h>
+#include <rz_util/rz_graph_drawable.h>
 
 #include <string.h>
 
@@ -2280,43 +2281,64 @@ RZ_API void rz_core_anal_coderefs(RzCore *core, ut64 addr) {
 	}
 }
 
-RZ_API void rz_core_anal_importxrefs(RzCore *core) {
-	RBinInfo *info = rz_bin_get_info (core->bin);
-	RBinObject *obj = rz_bin_cur_object (core->bin);
-	bool lit = info ? info->has_lit: false;
-	bool va = core->io->va || core->bin->is_debugger;
-
-	RzListIter *iter;
-	RBinImport *imp;
-	if (!obj) {
-		return;
-	}
-	rz_list_foreach (obj->imports, iter, imp) {
-		ut64 addr = lit ? rz_core_bin_impaddr (core->bin, va, imp->name): 0;
-		if (addr) {
-			rz_core_anal_codexrefs (core, addr);
-		} else {
-			rz_cons_printf ("agn %s\n", imp->name);
-		}
-	}
-}
-
-RZ_API void rz_core_anal_codexrefs(RzCore *core, ut64 addr) {
+static void add_single_addr_xrefs(RzCore *core, ut64 addr, RzGraph *graph) {
+	rz_return_if_fail (graph);
 	RzFlagItem *f = rz_flag_get_at (core->flags, addr, false);
 	char *me = (f && f->offset == addr)
-		? rz_str_new (f->name) : rz_str_newf ("0x%"PFMT64x, addr);
-	rz_cons_printf ("agn %s\n", me);
+		? rz_str_new (f->name)
+		: rz_str_newf ("0x%" PFMT64x, addr);
+
+	RzGraphNode *curr_node = rz_graph_add_node_info (graph, me, NULL, addr);
+	RZ_FREE (me);
+	if (!curr_node) {
+		return;
+	}
 	RzListIter *iter;
 	RzAnalRef *ref;
 	RzList *list = rz_anal_xrefs_get (core->anal, addr);
 	rz_list_foreach (list, iter, ref) {
 		RzFlagItem *item = rz_flag_get_i (core->flags, ref->addr);
-		const char *src = item? item->name: sdb_fmt ("0x%08"PFMT64x, ref->addr);
-		rz_cons_printf ("agn %s\n", src);
-		rz_cons_printf ("age %s %s\n", src, me);
+		char *src = item? rz_str_new (item->name): rz_str_newf ("0x%08" PFMT64x, ref->addr);
+		RzGraphNode *reference_from = rz_graph_add_node_info (graph, src, NULL, ref->addr);
+		free (src);
+		rz_graph_add_edge (graph, reference_from, curr_node);
 	}
 	rz_list_free (list);
-	free (me);
+}
+
+RZ_API RzGraph *rz_core_anal_importxrefs(RzCore *core) {
+	RBinInfo *info = rz_bin_get_info (core->bin);
+	RBinObject *obj = rz_bin_cur_object (core->bin);
+	bool lit = info? info->has_lit: false;
+	bool va = core->io->va || core->bin->is_debugger;
+
+	RzListIter *iter;
+	RBinImport *imp;
+	if (!obj) {
+		return NULL;
+	}
+	RzGraph *graph = rz_graph_new ();
+	if (!graph) {
+		return NULL;
+	}
+	rz_list_foreach (obj->imports, iter, imp) {
+		ut64 addr = lit ? rz_core_bin_impaddr (core->bin, va, imp->name): 0;
+		if (addr) {
+			add_single_addr_xrefs (core, addr, graph);
+		} else {
+			rz_graph_add_node_info (graph, imp->name, NULL, 0);
+		}
+	}
+	return graph;
+}
+
+RZ_API RzGraph *rz_core_anal_codexrefs(RzCore *core, ut64 addr) {
+	RzGraph *graph = rz_graph_new ();
+	if (!graph) {
+		return NULL;
+	}
+	add_single_addr_xrefs (core, addr, graph);
+	return graph;
 }
 
 static int RzAnalRef_cmp(const RzAnalRef* ref1, const RzAnalRef* ref2) {
@@ -5930,7 +5952,7 @@ kontinue:
 RZ_API void rz_core_anal_esil_graph(RzCore *core, const char *expr) {
 	RzAnalEsilDFG * edf = rz_anal_esil_dfg_expr(core->anal, NULL, expr);
 	RzListIter *iter, *ator;
-	RGraphNode *node, *edon;
+	RzGraphNode *node, *edon;
 	RStrBuf *buf = rz_strbuf_new ("");
 	rz_cons_printf ("ag-\n");
 	rz_list_foreach (rz_graph_get_nodes (edf->flow), iter, node) {

librz/util/graph_drawable.c
@@ -1,0 +1,104 @@
+#include <rz_core.h>
+#include <rz_util/rz_graph_drawable.h>
+
+RZ_API void rz_graph_free_node_info(void *ptr) {
+	if (!ptr) {
+		return;
+	}
+	RzGraphNodeInfo *info = ptr;
+	free (info->body);
+	free (info->title);
+	free (info);
+}
+
+RZ_API RzGraphNodeInfo *rz_graph_create_node_info(const char *title, const char *body, ut64 offset) {
+	RzGraphNodeInfo *data = RZ_NEW0 (RzGraphNodeInfo);
+	if (data) {
+		data->title = RZ_STR_DUP (title);
+		data->body = RZ_STR_DUP (body);
+		data->offset = offset;
+	}
+	return data;
+}
+
+RZ_API RzGraphNode *rz_graph_add_node_info(RzGraph *graph, const char *title, const char *body, ut64 offset) {
+	rz_return_val_if_fail (graph, NULL);
+	RzGraphNodeInfo *data = rz_graph_create_node_info (title, body, offset);
+	if (!data) {
+		return NULL;
+	}
+	RzGraphNode *node = rz_graph_add_nodef (graph, data, rz_graph_free_node_info);
+	if (!node) {
+		rz_graph_free_node_info (data);
+	}
+	return node;
+}
+
+RZ_API char *rz_graph_drawable_to_dot(RzGraph /*RzGraphNodeInfo*/ *graph, const char *node_properties, const char *edge_properties) {
+	RzList *nodes = graph->nodes;
+	RzListIter *it, *itt;
+	RzGraphNode *node = NULL, *target = NULL;
+	RStrBuf buf;
+	rz_strbuf_init (&buf);
+	rz_strbuf_appendf (&buf,
+		"digraph code {\nrankdir=LR;\noutputorder=edgesfirst\ngraph [bgcolor=azure];\n"
+		"edge [arrowhead=normal, color=\"#3030c0\" style=bold weight=2 %s];\n"
+		"node [fillcolor=white, style=filled shape=box "
+		"fontsize=\"8\" %s];\n",
+		edge_properties? edge_properties: "",
+		node_properties? node_properties: "");
+
+	rz_list_foreach (nodes, it, node) {
+		RzGraphNodeInfo *print_node = (RzGraphNodeInfo *)node->data;
+		const char *body = print_node->body;
+		if (!body || !*body) {
+			rz_strbuf_appendf (&buf, "%d [URL=\"%s\", color=\"lightgray\", label=\"%s\"]\n",
+				node->idx, print_node->title, print_node->title);
+		} else {
+			rz_strbuf_appendf (&buf, "%d [URL=\"%s\", color=\"lightgray\", label=\"%s\\n%s\"]\n",
+				node->idx, print_node->title, print_node->title, body);
+		}
+		rz_list_foreach (node->out_nodes, itt, target) {
+			rz_strbuf_appendf (&buf, "%d -> %d\n", node->idx, target->idx);
+		}
+	}
+	rz_strbuf_append (&buf, "}\n");
+	return rz_strbuf_drain_nofree (&buf);
+}
+
+RZ_API void rz_graph_drawable_to_json(RzGraph /*RzGraphNodeInfo*/ *graph, PJ *pj, bool use_offset) {
+	RzList *nodes = graph->nodes, *neighbours = NULL;
+	RzListIter *it, *itt;
+	RzGraphNode *node = NULL, *neighbour = NULL;
+	if (!pj) {
+		return;
+	}
+	pj_o (pj);
+	pj_k (pj, "nodes");
+	pj_a (pj);
+
+	rz_list_foreach (nodes, it, node) {
+		RzGraphNodeInfo *print_node = (RzGraphNodeInfo *)node->data;
+		pj_o (pj);
+		pj_ki (pj, "id", node->idx);
+		if (print_node->title) {
+			pj_ks (pj, "title", print_node->title);
+		}
+		if (print_node->body) {
+			pj_ks (pj, "body", print_node->body);
+		}
+		if (use_offset) {
+			pj_kn (pj, "offset", print_node->offset);
+		}
+		pj_k (pj, "out_nodes");
+		pj_a (pj);
+		neighbours = node->out_nodes;
+		rz_list_foreach (neighbours, itt, neighbour) {
+			pj_i (pj, neighbour->idx);
+		}
+		pj_end (pj);
+		pj_end (pj);
+	}
+	pj_end (pj);
+	pj_end (pj);
+}