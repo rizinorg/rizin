radareorg/radare2

Fix incorrect uses of printf-like functions ##refactor (#17648)

libr/anal/p/anal_gb.c
@@ -188,11 +188,11 @@ static void gb_anal_mov_imm (RReg *reg, RAnalOp *op, const ut8 *data) {
 	if (data[0] & 1) {
 		op->dst->reg = r_reg_get (reg, regs_16[data[0]>>4], R_REG_TYPE_GPR);
 		op->src[0]->imm = GB_SOFTCAST (data[1], data[2]);
-		r_strbuf_setf (&op->esil, "0x%04x,%s,=", op->src[0]->imm, regs_16[data[0]>>4]);
+		r_strbuf_setf (&op->esil, "0x%04" PFMT64x ",%s,=", op->src[0]->imm, regs_16[data[0]>>4]);
 	} else {
 		op->dst->reg = r_reg_get (reg, regs_8[data[0]>>3], R_REG_TYPE_GPR);
 		op->src[0]->imm = data[1];
-		r_strbuf_setf (&op->esil, "0x%02x,%s,=", op->src[0]->imm, regs_8[data[0]>>3]);
+		r_strbuf_setf (&op->esil, "0x%02" PFMT64x ",%s,=", op->src[0]->imm, regs_8[data[0]>>3]);
 	}
 	op->src[0]->absolute = true;
 	op->val = op->src[0]->imm;
@@ -310,9 +310,9 @@ static inline void gb_anal_and_res (RAnal *anal, RAnalOp *op, const ut8 data) {
 	op->dst->memref = ((data & 7) == 6);
 	op->dst->reg = r_reg_get (anal->reg, regs_x[data & 7], R_REG_TYPE_GPR);
 	if (op->dst->memref) {
-		r_strbuf_setf (&op->esil, "0x%02x,%s,[1],&,%s,=[1]", op->src[0]->imm, regs_x[data & 7], regs_x[data & 7]);
+		r_strbuf_setf (&op->esil, "0x%02" PFMT64x ",%s,[1],&,%s,=[1]", op->src[0]->imm, regs_x[data & 7], regs_x[data & 7]);
 	} else {
-		r_strbuf_setf (&op->esil, "0x%02x,%s,&=", op->src[0]->imm, regs_x[data & 7]);
+		r_strbuf_setf (&op->esil, "0x%02" PFMT64x ",%s,&=", op->src[0]->imm, regs_x[data & 7]);
 	}
 }
 
@@ -323,9 +323,9 @@ static inline void gb_anal_and_bit (RReg *reg, RAnalOp *op, const ut8 data) {
 	op->dst->memref = ((data & 7) == 6);
 	op->dst->reg = r_reg_get (reg, regs_x[data & 7], R_REG_TYPE_GPR);
 	if (op->dst->memref) {
-		r_strbuf_setf (&op->esil, "%i,%s,[1],&,0,==,$z,Z,:=,0,N,:=,1,H,:=", op->src[0]->imm, regs_x[data & 7]);
+		r_strbuf_setf (&op->esil, "%" PFMT64d ",%s,[1],&,0,==,$z,Z,:=,0,N,:=,1,H,:=", op->src[0]->imm, regs_x[data & 7]);
 	} else {
-		r_strbuf_setf (&op->esil, "%i,%s,&,0,==,$z,Z,:=,0,N,:=,1,H,:=", op->src[0]->imm, regs_x[data & 7]);
+		r_strbuf_setf (&op->esil, "%" PFMT64d ",%s,&,0,==,$z,Z,:=,0,N,:=,1,H,:=", op->src[0]->imm, regs_x[data & 7]);
 	}
 }
 
@@ -336,9 +336,9 @@ static inline void gb_anal_or_set (RAnal *anal, RAnalOp *op, const ut8 data) {
 	op->dst->memref = ((data & 7) == 6);
 	op->dst->reg = r_reg_get (anal->reg, regs_x[data & 7], R_REG_TYPE_GPR);
 	if (op->dst->memref) {
-		r_strbuf_setf (&op->esil, "0x%02x,%s,[1],|,%s,=[1]", op->src[0]->imm, regs_x[data & 7], regs_x[data & 7], op->src[0]->imm);
+		r_strbuf_setf (&op->esil, "0x%02" PFMT64x ",%s,[1],|,%s,=[1]", op->src[0]->imm, regs_x[data & 7], regs_x[data & 7]);
 	} else {
-		r_strbuf_setf (&op->esil, "0x%02x,%s,|=", op->src[0]->imm, regs_x[data & 7]);
+		r_strbuf_setf (&op->esil, "0x%02" PFMT64x ",%s,|=", op->src[0]->imm, regs_x[data & 7]);
 	}
 }
 
@@ -489,7 +489,7 @@ static inline void gb_anal_load (RReg *reg, RAnalOp *op, const ut8 *data) {
 	switch (data[0]) {
 	case 0xf0:
 		op->src[0]->base = 0xff00 + data[1];
-		r_strbuf_setf (&op->esil, "0x%04x,[1],a,=", op->src[0]->base);
+		r_strbuf_setf (&op->esil, "0x%04" PFMT64x ",[1],a,=", op->src[0]->base);
 		break;
 	case 0xf2:
 		op->src[0]->base = 0xff00;
@@ -505,7 +505,7 @@ static inline void gb_anal_load (RReg *reg, RAnalOp *op, const ut8 *data) {
 				op->ptr = op->src[0]->base + (op->addr & 0xffffffffffff0000LL);
 			}
 		}
-		r_strbuf_setf (&op->esil, "0x%04x,[1],a,=", op->src[0]->base);
+		r_strbuf_setf (&op->esil, "0x%04" PFMT64x ",[1],a,=", op->src[0]->base);
 		break;
 	default:
 		op->src[0]->reg = r_reg_get (reg, regs_16[(data[0] & 0xf0) >> 4], R_REG_TYPE_GPR);
@@ -546,11 +546,11 @@ static void gb_anal_store (RReg *reg, RAnalOp *op, const ut8 *data)
 			op->dst->memref = 2;
 			op->dst->base = GB_SOFTCAST (data[1], data[2]);
 			op->src[0]->reg = r_reg_get (reg, "sp", R_REG_TYPE_GPR);
-			r_strbuf_setf (&op->esil, "sp,0x%04x,=[2]", op->dst->base);
+			r_strbuf_setf (&op->esil, "sp,0x%04" PFMT64x ",=[2]", op->dst->base);
 			break;
 		case 0xe0:
 			op->dst->base = 0xff00 + data[1];
-			r_strbuf_setf (&op->esil, "a,0x%04x,=[1]", op->dst->base);
+			r_strbuf_setf (&op->esil, "a,0x%04" PFMT64x ",=[1]", op->dst->base);
 			break;
 		case 0xe2:
 			op->dst->base = 0xff00;
@@ -559,7 +559,7 @@ static void gb_anal_store (RReg *reg, RAnalOp *op, const ut8 *data)
 			break;
 		case 0xea:
 			op->dst->base = GB_SOFTCAST (data[1], data[2]);
-			r_strbuf_setf (&op->esil, "a,0x%04x,=[1]", op->dst->base);
+			r_strbuf_setf (&op->esil, "a,0x%04" PFMT64x ",=[1]", op->dst->base);
 			break;
 		default:
 			op->dst->reg = r_reg_get (reg, regs_16[(data[0] & 0xf0)>>4], R_REG_TYPE_GPR);

libr/anal/p/anal_sysz.c
@@ -32,7 +32,7 @@ static void opex(RStrBuf *buf, csh handle, cs_insn *insn) {
 			break;
 		case SYSZ_OP_IMM:
 			r_strbuf_append (buf, "\"type\":\"imm\"");
-			r_strbuf_appendf (buf, ",\"value\":%"PFMT64d, op->imm);
+			r_strbuf_appendf (buf, ",\"value\":%" PFMT64d, (st64)op->imm);
 			break;
 		case SYSZ_OP_MEM:
 			r_strbuf_append (buf, "\"type\":\"mem\"");

libr/anal/p/anal_wasm.c
@@ -332,7 +332,7 @@ static int wasm_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int len
 			break;
 		case WASM_OP_NOP:
 			op->type = R_ANAL_OP_TYPE_NOP;
-			r_strbuf_setf (&op->esil, "");
+			r_strbuf_setf (&op->esil, "%s", "");
 			break;
 		case WASM_OP_CALL:
 		case WASM_OP_CALLINDIRECT:

libr/anal/p/anal_xtensa.c
@@ -1791,7 +1791,7 @@ static void analop_esil (xtensa_isa isa, xtensa_opcode opcode, xtensa_format for
 		break;
 	case 0:  /* excw */
 	case 34: /* nop.n */
-		r_strbuf_setf (&op->esil, "");
+		r_strbuf_setf (&op->esil, "%s", "");
 		break;
 	// TODO: s32cli (s32c1i) is conditional (CAS)
 	// should it be handled here?
@@ -1873,7 +1873,7 @@ static void analop_esil (xtensa_isa isa, xtensa_opcode opcode, xtensa_format for
 		esil_extract_unsigned (isa, opcode, format, i, slot_buffer, op);
 		break;
 	case 79: /* ill */
-		r_strbuf_setf (&op->esil, "");
+		r_strbuf_setf (&op->esil, "%s", "");
 		break;
 	// TODO: windowed calls?
 	case 7: /* call4 */

libr/anal/rtti_itanium.c
@@ -364,7 +364,7 @@ static void rtti_itanium_print_vmi_class_type_info(vmi_class_type_info *vmi_cti,
 	int i;
 	for (i = 0; i < vmi_cti->vmi_base_count; i++) {
 		r_cons_printf ("%s    Base class type descriptor address: 0x%08" PFMT64x "\n"
-			       "%s    Base class flags: 0x%x"
+			       "%s    Base class flags: 0x%" PFMT64x
 			       "\n",
 			prefix, vmi_cti->vmi_bases[i].base_class_addr,
 			prefix, vmi_cti->vmi_bases[i].flags);
@@ -767,7 +767,7 @@ static void recovery_apply_vtable(RVTableContext *context, const char *class_nam
 		RAnalMethod meth;
 		meth.addr = vmeth->addr;
 		meth.vtable_offset = vmeth->vtable_offset;
-		meth.name = r_str_newf ("virtual_%d", meth.vtable_offset);
+		meth.name = r_str_newf ("virtual_%" PFMT64d, meth.vtable_offset);
 		r_anal_class_method_set (context->anal, class_name, &meth);
 		r_anal_class_method_fini (&meth);
 	}

libr/bin/p/bin_mdmp.c
@@ -148,7 +148,7 @@ static RList* libs(RBinFile *bf) {
 			return ret;
 		}
 		for (i = 0; !libs[i].last; i++) {
-			ptr = r_str_newf ("[0x%.08x] - %s", pe32_bin->vaddr, libs[i].name);
+			ptr = r_str_newf ("[0x%.08" PFMT64x "] - %s", pe32_bin->vaddr, libs[i].name);
 			r_list_append (ret, ptr);
 		}
 		free (libs);

libr/core/cmd.c
@@ -5069,7 +5069,7 @@ DEFINE_HANDLE_TS_FCN_AND_SYMBOL(repeat_command) {
 		return R_CMD_STATUS_INVALID;
 	}
 	if (rep > INTERACTIVE_MAX_REP && r_cons_is_interactive ()) {
-		if (!r_cons_yesno ('n', "Are you sure to repeat this %" PFMT64d " times? (y/N)", rep)) {
+		if (!r_cons_yesno ('n', "Are you sure to repeat this %d times? (y/N)", rep)) {
 			return R_CMD_STATUS_INVALID;
 		}
 	}

libr/core/cmd_print.c
@@ -2438,7 +2438,7 @@ static int cmd_print_pxA(RCore *core, int len, const char *input) {
 		i += opsz;
 		r_anal_op_fini (&op);
 	}
-	r_cons_printf ("  %d\n", i - oi);
+	r_cons_printf ("  %" PFMT64d "\n", i - oi);
 	if (bgcolor_in_heap) {
 		free (bgcolor);
 	}
@@ -3007,13 +3007,13 @@ static void cmd_print_pv(RCore *core, const char *input, bool useBytes) {
 				ut8 *b = block + (i * n);
 				  switch (n) {
 				  case 1:
-					  r_cons_printf ("f pval.0x%08"PFMT64x"=%"PFMT64d"\n", at, r_read_ble8 (b));
+					  r_cons_printf ("f pval.0x%08"PFMT64x"=%d\n", at, r_read_ble8 (b));
 					  break;
 				  case 2:
-					  r_cons_printf ("f pval.0x%08"PFMT64x"=%"PFMT64d"\n", at, r_read_ble16 (b, be));
+					  r_cons_printf ("f pval.0x%08"PFMT64x"=%d\n", at, r_read_ble16 (b, be));
 					  break;
 				  case 4:
-					  r_cons_printf ("f pval.0x%08"PFMT64x"=%"PFMT64d"\n", at, r_read_ble32 (b, be));
+					  r_cons_printf ("f pval.0x%08"PFMT64x"=%d\n", at, r_read_ble32 (b, be));
 					  break;
 				  case 8:
 				default:
@@ -3673,7 +3673,7 @@ static void cmd_print_bars(RCore *core, const char *input) {
 				ut64 word64 = word[i] + ST16_MAX;
 				r_cons_printf ("0x%08"PFMT64x" %8d  ", core->offset + (i *2), word[i]);
 				r_print_progressbar (core->print, word64 * 100 / UT16_MAX, 60);
-				r_cons_printf (" %d", word64 - oldword);
+				r_cons_printf (" %" PFMT64d, word64 - oldword);
 				oldword = word64;
 				r_cons_newline ();
 				i += step;
@@ -3986,7 +3986,7 @@ static void __printPattern(RCore *core, const char *_input) {
 		{
 			int min = (core->offset & 0xff);
 			for (i = 0; i < len; i++) {
-				r_cons_printf ("%02x", i + min);
+				r_cons_printf ("%02zx", i + min);
 			}
 			r_cons_newline ();
 		}
@@ -3997,7 +3997,7 @@ static void __printPattern(RCore *core, const char *_input) {
 			// TODO: honor cfg.bigendian
 			int min = (core->offset & 0xffff);
 			for (i = 0; i < len; i++) {
-				r_cons_printf ("%04x", i + min);
+				r_cons_printf ("%04zx", i + min);
 			}
 			r_cons_newline ();
 		}
@@ -4008,7 +4008,7 @@ static void __printPattern(RCore *core, const char *_input) {
 			// TODO: honor cfg.bigendian
 			int min = (core->offset & UT32_MAX);
 			for (i = 0; i < len; i++) {
-				r_cons_printf ("%08x", i + min);
+				r_cons_printf ("%08zx", i + min);
 			}
 			r_cons_newline ();
 		}
@@ -6171,7 +6171,7 @@ l = use_blocksize;
 							if (v == f->offset) {
 								fn = strdup (f->name);
 							} else {
-								fn = r_str_newf ("%s+%d",
+								fn = r_str_newf ("%s+%" PFMT64d,
 									f->name, v - f->offset);
 							}
 						}
@@ -6335,7 +6335,7 @@ l = use_blocksize;
 							if (v == f->offset) {
 								fn = strdup (f->name);
 							} else {
-								fn = r_str_newf ("%s+%d", f->name, v - f->offset);
+								fn = r_str_newf ("%s+%" PFMT64d, f->name, v - f->offset);
 							}
 						}
 					}

rizinorg/rizin

librz/anal/p/anal_gb.c
@@ -188,11 +188,11 @@ static void gb_anal_mov_imm (RzReg *reg, RzAnalOp *op, const ut8 *data) {
 	if (data[0] & 1) {
 		op->dst->reg = rz_reg_get (reg, regs_16[data[0]>>4], RZ_REG_TYPE_GPR);
 		op->src[0]->imm = GB_SOFTCAST (data[1], data[2]);
-		rz_strbuf_setf (&op->esil, "0x%04x,%s,=", op->src[0]->imm, regs_16[data[0]>>4]);
+		rz_strbuf_setf (&op->esil, "0x%04" PFMT64x ",%s,=", op->src[0]->imm, regs_16[data[0]>>4]);
 	} else {
 		op->dst->reg = rz_reg_get (reg, regs_8[data[0]>>3], RZ_REG_TYPE_GPR);
 		op->src[0]->imm = data[1];
-		rz_strbuf_setf (&op->esil, "0x%02x,%s,=", op->src[0]->imm, regs_8[data[0]>>3]);
+		rz_strbuf_setf (&op->esil, "0x%02" PFMT64x ",%s,=", op->src[0]->imm, regs_8[data[0]>>3]);
 	}
 	op->src[0]->absolute = true;
 	op->val = op->src[0]->imm;
@@ -310,9 +310,9 @@ static inline void gb_anal_and_res (RzAnal *anal, RzAnalOp *op, const ut8 data) 
 	op->dst->memref = ((data & 7) == 6);
 	op->dst->reg = rz_reg_get (anal->reg, regs_x[data & 7], RZ_REG_TYPE_GPR);
 	if (op->dst->memref) {
-		rz_strbuf_setf (&op->esil, "0x%02x,%s,[1],&,%s,=[1]", op->src[0]->imm, regs_x[data & 7], regs_x[data & 7]);
+		rz_strbuf_setf (&op->esil, "0x%02" PFMT64x ",%s,[1],&,%s,=[1]", op->src[0]->imm, regs_x[data & 7], regs_x[data & 7]);
 	} else {
-		rz_strbuf_setf (&op->esil, "0x%02x,%s,&=", op->src[0]->imm, regs_x[data & 7]);
+		rz_strbuf_setf (&op->esil, "0x%02" PFMT64x ",%s,&=", op->src[0]->imm, regs_x[data & 7]);
 	}
 }
 
@@ -323,9 +323,9 @@ static inline void gb_anal_and_bit (RzReg *reg, RzAnalOp *op, const ut8 data) {
 	op->dst->memref = ((data & 7) == 6);
 	op->dst->reg = rz_reg_get (reg, regs_x[data & 7], RZ_REG_TYPE_GPR);
 	if (op->dst->memref) {
-		rz_strbuf_setf (&op->esil, "%i,%s,[1],&,0,==,$z,Z,:=,0,N,:=,1,H,:=", op->src[0]->imm, regs_x[data & 7]);
+		rz_strbuf_setf (&op->esil, "%" PFMT64d ",%s,[1],&,0,==,$z,Z,:=,0,N,:=,1,H,:=", op->src[0]->imm, regs_x[data & 7]);
 	} else {
-		rz_strbuf_setf (&op->esil, "%i,%s,&,0,==,$z,Z,:=,0,N,:=,1,H,:=", op->src[0]->imm, regs_x[data & 7]);
+		rz_strbuf_setf (&op->esil, "%" PFMT64d ",%s,&,0,==,$z,Z,:=,0,N,:=,1,H,:=", op->src[0]->imm, regs_x[data & 7]);
 	}
 }
 
@@ -336,9 +336,9 @@ static inline void gb_anal_or_set (RzAnal *anal, RzAnalOp *op, const ut8 data) {
 	op->dst->memref = ((data & 7) == 6);
 	op->dst->reg = rz_reg_get (anal->reg, regs_x[data & 7], RZ_REG_TYPE_GPR);
 	if (op->dst->memref) {
-		rz_strbuf_setf (&op->esil, "0x%02x,%s,[1],|,%s,=[1]", op->src[0]->imm, regs_x[data & 7], regs_x[data & 7], op->src[0]->imm);
+		rz_strbuf_setf (&op->esil, "0x%02" PFMT64x ",%s,[1],|,%s,=[1]", op->src[0]->imm, regs_x[data & 7], regs_x[data & 7], op->src[0]->imm);
 	} else {
-		rz_strbuf_setf (&op->esil, "0x%02x,%s,|=", op->src[0]->imm, regs_x[data & 7]);
+		rz_strbuf_setf (&op->esil, "0x%02" PFMT64x ",%s,|=", op->src[0]->imm, regs_x[data & 7]);
 	}
 }
 
@@ -489,7 +489,7 @@ static inline void gb_anal_load (RzReg *reg, RzAnalOp *op, const ut8 *data) {
 	switch (data[0]) {
 	case 0xf0:
 		op->src[0]->base = 0xff00 + data[1];
-		rz_strbuf_setf (&op->esil, "0x%04x,[1],a,=", op->src[0]->base);
+		rz_strbuf_setf (&op->esil, "0x%04" PFMT64x ",[1],a,=", op->src[0]->base);
 		break;
 	case 0xf2:
 		op->src[0]->base = 0xff00;
@@ -505,7 +505,7 @@ static inline void gb_anal_load (RzReg *reg, RzAnalOp *op, const ut8 *data) {
 				op->ptr = op->src[0]->base + (op->addr & 0xffffffffffff0000LL);
 			}
 		}
-		rz_strbuf_setf (&op->esil, "0x%04x,[1],a,=", op->src[0]->base);
+		rz_strbuf_setf (&op->esil, "0x%04" PFMT64x ",[1],a,=", op->src[0]->base);
 		break;
 	default:
 		op->src[0]->reg = rz_reg_get (reg, regs_16[(data[0] & 0xf0) >> 4], RZ_REG_TYPE_GPR);
@@ -546,11 +546,11 @@ static void gb_anal_store (RzReg *reg, RzAnalOp *op, const ut8 *data)
 			op->dst->memref = 2;
 			op->dst->base = GB_SOFTCAST (data[1], data[2]);
 			op->src[0]->reg = rz_reg_get (reg, "sp", RZ_REG_TYPE_GPR);
-			rz_strbuf_setf (&op->esil, "sp,0x%04x,=[2]", op->dst->base);
+			rz_strbuf_setf (&op->esil, "sp,0x%04" PFMT64x ",=[2]", op->dst->base);
 			break;
 		case 0xe0:
 			op->dst->base = 0xff00 + data[1];
-			rz_strbuf_setf (&op->esil, "a,0x%04x,=[1]", op->dst->base);
+			rz_strbuf_setf (&op->esil, "a,0x%04" PFMT64x ",=[1]", op->dst->base);
 			break;
 		case 0xe2:
 			op->dst->base = 0xff00;
@@ -559,7 +559,7 @@ static void gb_anal_store (RzReg *reg, RzAnalOp *op, const ut8 *data)
 			break;
 		case 0xea:
 			op->dst->base = GB_SOFTCAST (data[1], data[2]);
-			rz_strbuf_setf (&op->esil, "a,0x%04x,=[1]", op->dst->base);
+			rz_strbuf_setf (&op->esil, "a,0x%04" PFMT64x ",=[1]", op->dst->base);
 			break;
 		default:
 			op->dst->reg = rz_reg_get (reg, regs_16[(data[0] & 0xf0)>>4], RZ_REG_TYPE_GPR);

librz/anal/p/anal_sysz.c
@@ -32,7 +32,7 @@ static void opex(RzStrBuf *buf, csh handle, cs_insn *insn) {
 			break;
 		case SYSZ_OP_IMM:
 			rz_strbuf_append (buf, "\"type\":\"imm\"");
-			rz_strbuf_appendf (buf, ",\"value\":%"PFMT64d, op->imm);
+			rz_strbuf_appendf (buf, ",\"value\":%" PFMT64d, (st64)op->imm);
 			break;
 		case SYSZ_OP_MEM:
 			rz_strbuf_append (buf, "\"type\":\"mem\"");

librz/anal/p/anal_wasm.c
@@ -332,7 +332,7 @@ static int wasm_op(RzAnal *anal, RzAnalOp *op, ut64 addr, const ut8 *data, int l
 			break;
 		case WASM_OP_NOP:
 			op->type = RZ_ANAL_OP_TYPE_NOP;
-			rz_strbuf_setf (&op->esil, "");
+			rz_strbuf_setf (&op->esil, "%s", "");
 			break;
 		case WASM_OP_CALL:
 		case WASM_OP_CALLINDIRECT:

librz/anal/p/anal_xtensa.c
@@ -1791,7 +1791,7 @@ static void analop_esil (xtensa_isa isa, xtensa_opcode opcode, xtensa_format for
 		break;
 	case 0:  /* excw */
 	case 34: /* nop.n */
-		rz_strbuf_setf (&op->esil, "");
+		rz_strbuf_setf (&op->esil, "%s", "");
 		break;
 	// TODO: s32cli (s32c1i) is conditional (CAS)
 	// should it be handled here?
@@ -1873,7 +1873,7 @@ static void analop_esil (xtensa_isa isa, xtensa_opcode opcode, xtensa_format for
 		esil_extract_unsigned (isa, opcode, format, i, slot_buffer, op);
 		break;
 	case 79: /* ill */
-		rz_strbuf_setf (&op->esil, "");
+		rz_strbuf_setf (&op->esil, "%s", "");
 		break;
 	// TODO: windowed calls?
 	case 7: /* call4 */

librz/anal/rtti_itanium.c
@@ -364,7 +364,7 @@ static void rtti_itanium_print_vmi_class_type_info(vmi_class_type_info *vmi_cti,
 	int i;
 	for (i = 0; i < vmi_cti->vmi_base_count; i++) {
 		rz_cons_printf ("%s    Base class type descriptor address: 0x%08" PFMT64x "\n"
-			       "%s    Base class flags: 0x%x"
+			       "%s    Base class flags: 0x%" PFMT64x
 			       "\n",
 			prefix, vmi_cti->vmi_bases[i].base_class_addr,
 			prefix, vmi_cti->vmi_bases[i].flags);
@@ -767,7 +767,7 @@ static void recovery_apply_vtable(RVTableContext *context, const char *class_nam
 		RzAnalMethod meth;
 		meth.addr = vmeth->addr;
 		meth.vtable_offset = vmeth->vtable_offset;
-		meth.name = rz_str_newf ("virtual_%d", meth.vtable_offset);
+		meth.name = rz_str_newf ("virtual_%" PFMT64d, meth.vtable_offset);
 		rz_anal_class_method_set (context->anal, class_name, &meth);
 		rz_anal_class_method_fini (&meth);
 	}

librz/bin/p/bin_mdmp.c
@@ -148,7 +148,7 @@ static RzList* libs(RzBinFile *bf) {
 			return ret;
 		}
 		for (i = 0; !libs[i].last; i++) {
-			ptr = rz_str_newf ("[0x%.08x] - %s", pe32_bin->vaddr, libs[i].name);
+			ptr = rz_str_newf ("[0x%.08" PFMT64x "] - %s", pe32_bin->vaddr, libs[i].name);
 			rz_list_append (ret, ptr);
 		}
 		free (libs);

librz/core/cmd.c
@@ -5088,7 +5088,7 @@ DEFINE_HANDLE_TS_FCN_AND_SYMBOL(repeat_command) {
 		return RZ_CMD_STATUS_INVALID;
 	}
 	if (rep > INTERACTIVE_MAX_REP && rz_cons_is_interactive ()) {
-		if (!rz_cons_yesno ('n', "Are you sure to repeat this %" PFMT64d " times? (y/N)", rep)) {
+		if (!rz_cons_yesno ('n', "Are you sure to repeat this %d times? (y/N)", rep)) {
 			return RZ_CMD_STATUS_INVALID;
 		}
 	}

librz/core/cmd_print.c
@@ -2438,7 +2438,7 @@ static int cmd_print_pxA(RzCore *core, int len, const char *input) {
 		i += opsz;
 		rz_anal_op_fini (&op);
 	}
-	rz_cons_printf ("  %d\n", i - oi);
+	rz_cons_printf ("  %" PFMT64d "\n", i - oi);
 	if (bgcolor_in_heap) {
 		free (bgcolor);
 	}
@@ -3007,13 +3007,13 @@ static void cmd_print_pv(RzCore *core, const char *input, bool useBytes) {
 				ut8 *b = block + (i * n);
 				  switch (n) {
 				  case 1:
-					  rz_cons_printf ("f pval.0x%08"PFMT64x"=%"PFMT64d"\n", at, rz_read_ble8 (b));
+					  rz_cons_printf ("f pval.0x%08"PFMT64x"=%d\n", at, rz_read_ble8 (b));
 					  break;
 				  case 2:
-					  rz_cons_printf ("f pval.0x%08"PFMT64x"=%"PFMT64d"\n", at, rz_read_ble16 (b, be));
+					  rz_cons_printf ("f pval.0x%08"PFMT64x"=%d\n", at, rz_read_ble16 (b, be));
 					  break;
 				  case 4:
-					  rz_cons_printf ("f pval.0x%08"PFMT64x"=%"PFMT64d"\n", at, rz_read_ble32 (b, be));
+					  rz_cons_printf ("f pval.0x%08"PFMT64x"=%d\n", at, rz_read_ble32 (b, be));
 					  break;
 				  case 8:
 				default:
@@ -3673,7 +3673,7 @@ static void cmd_print_bars(RzCore *core, const char *input) {
 				ut64 word64 = word[i] + ST16_MAX;
 				rz_cons_printf ("0x%08"PFMT64x" %8d  ", core->offset + (i *2), word[i]);
 				rz_print_progressbar (core->print, word64 * 100 / UT16_MAX, 60);
-				rz_cons_printf (" %d", word64 - oldword);
+				rz_cons_printf (" %" PFMT64d, word64 - oldword);
 				oldword = word64;
 				rz_cons_newline ();
 				i += step;
@@ -3986,7 +3986,7 @@ static void __printPattern(RzCore *core, const char *_input) {
 		{
 			int min = (core->offset & 0xff);
 			for (i = 0; i < len; i++) {
-				rz_cons_printf ("%02x", i + min);
+				rz_cons_printf ("%02zx", i + min);
 			}
 			rz_cons_newline ();
 		}
@@ -3997,7 +3997,7 @@ static void __printPattern(RzCore *core, const char *_input) {
 			// TODO: honor cfg.bigendian
 			int min = (core->offset & 0xffff);
 			for (i = 0; i < len; i++) {
-				rz_cons_printf ("%04x", i + min);
+				rz_cons_printf ("%04zx", i + min);
 			}
 			rz_cons_newline ();
 		}
@@ -4008,7 +4008,7 @@ static void __printPattern(RzCore *core, const char *_input) {
 			// TODO: honor cfg.bigendian
 			int min = (core->offset & UT32_MAX);
 			for (i = 0; i < len; i++) {
-				rz_cons_printf ("%08x", i + min);
+				rz_cons_printf ("%08zx", i + min);
 			}
 			rz_cons_newline ();
 		}
@@ -6171,7 +6171,7 @@ l = use_blocksize;
 							if (v == f->offset) {
 								fn = strdup (f->name);
 							} else {
-								fn = rz_str_newf ("%s+%d",
+								fn = rz_str_newf ("%s+%" PFMT64d,
 									f->name, v - f->offset);
 							}
 						}
@@ -6335,7 +6335,7 @@ l = use_blocksize;
 							if (v == f->offset) {
 								fn = strdup (f->name);
 							} else {
-								fn = rz_str_newf ("%s+%d", f->name, v - f->offset);
+								fn = rz_str_newf ("%s+%" PFMT64d, f->name, v - f->offset);
 							}
 						}
 					}