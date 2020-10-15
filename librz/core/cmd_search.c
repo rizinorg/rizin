radareorg/radare2

Speedup r2pipe-spawn: r2 -0 sets scr.prompt=0 do not autocomplete or log ##r2pipe (#17767)

libr/core/cmd_search.c
@@ -2070,7 +2070,7 @@ static bool do_anal_search(RCore *core, struct search_parameters *param, const c
 			}
 			at = from + i;
 			ut8 bufop[32];
-			r_io_read_at (core->io, at, bufop, sizeof(bufop));
+			r_io_read_at (core->io, at, bufop, sizeof (bufop));
 			ret = r_anal_op (core->anal, &aop, at, bufop, sizeof(bufop), R_ANAL_OP_MASK_BASIC | R_ANAL_OP_MASK_DISASM);
 			if (ret) {
 				bool match = false;

libr/core/core.c
@@ -1944,19 +1944,25 @@ static int autocomplete(RLineCompletion *completion, RLineBuffer *buf, RLineProm
 }
 
 R_API int r_core_fgets(char *buf, int len) {
-	const char *ptr;
-	RLine *rli = r_line_singleton ();
+	RCons *cons = r_cons_singleton ();
+	RLine *rli = cons->line;
+	bool prompt = cons->context->is_interactive;
 	buf[0] = '\0';
-	r_line_completion_set (&rli->completion, radare_argc, radare_argv);
- 	rli->completion.run = autocomplete;
- 	rli->completion.run_user = rli->user;
-	ptr = r_line_readline ();
+	if (prompt) {
+		r_line_completion_set (&rli->completion, radare_argc, radare_argv);
+		rli->completion.run = autocomplete;
+		rli->completion.run_user = rli->user;
+	} else {
+		rli->history.data = NULL;
+		r_line_completion_set (&rli->completion, 0, NULL);
+		rli->completion.run = NULL;
+		rli->completion.run_user = NULL;
+	}
+	const char *ptr = r_line_readline ();
 	if (!ptr) {
 		return -1;
 	}
-	strncpy (buf, ptr, len - 1);
-	buf[len - 1] = 0;
-	return strlen (buf);
+	return r_str_ncpy (buf, ptr, len - 1);
 }
 
 static const char *r_core_print_offname(void *p, ut64 addr) {

libr/util/str.c
@@ -735,12 +735,12 @@ R_API char *r_str_newf(const char *fmt, ...) {
 }
 
 // Secure string copy with null terminator (like strlcpy or strscpy but ours
-R_API void r_str_ncpy(char *dst, const char *src, size_t n) {
-	int i;
+R_API size_t r_str_ncpy(char *dst, const char *src, size_t n) {
+	size_t i;
 
 	// do not do anything if n is 0
 	if (n == 0) {
-		return;
+		return 0;
 	}
 
 	n--;
@@ -748,6 +748,7 @@ R_API void r_str_ncpy(char *dst, const char *src, size_t n) {
 		dst[i] = src[i];
 	}
 	dst[i] = 0;
+	return i;
 }
 
 /* memccmp("foo.bar", "foo.cow, '.') == 0 */

rizinorg/rizin

librz/core/cmd_search.c
@@ -2069,7 +2069,7 @@ static bool do_anal_search(RzCore *core, struct search_parameters *param, const 
 			}
 			at = from + i;
 			ut8 bufop[32];
-			rz_io_read_at (core->io, at, bufop, sizeof(bufop));
+			rz_io_read_at (core->io, at, bufop, sizeof (bufop));
 			ret = rz_anal_op (core->anal, &aop, at, bufop, sizeof(bufop), RZ_ANAL_OP_MASK_BASIC | RZ_ANAL_OP_MASK_DISASM);
 			if (ret) {
 				bool match = false;

librz/core/core.c
@@ -1837,19 +1837,25 @@ static int autocomplete(RLineCompletion *completion, RLineBuffer *buf, RLineProm
 }
 
 RZ_API int rz_core_fgets(char *buf, int len) {
-	const char *ptr;
-	RLine *rli = rz_line_singleton ();
+	RzCons *cons = rz_cons_singleton ();
+	RLine *rzli = cons->line;
+	bool prompt = cons->context->is_interactive;
 	buf[0] = '\0';
-	rz_line_completion_set (&rli->completion, rizin_argc, rizin_argv);
- 	rli->completion.run = autocomplete;
- 	rli->completion.run_user = rli->user;
-	ptr = rz_line_readline ();
+	if (prompt) {
+		rz_line_completion_set (&rzli->completion, rizin_argc, rizin_argv);
+		rzli->completion.run = autocomplete;
+		rzli->completion.run_user = rzli->user;
+	} else {
+		rzli->history.data = NULL;
+		rz_line_completion_set (&rzli->completion, 0, NULL);
+		rzli->completion.run = NULL;
+		rzli->completion.run_user = NULL;
+	}
+	const char *ptr = rz_line_readline ();
 	if (!ptr) {
 		return -1;
 	}
-	strncpy (buf, ptr, len - 1);
-	buf[len - 1] = 0;
-	return strlen (buf);
+	return rz_str_ncpy (buf, ptr, len - 1);
 }
 
 static const char *rz_core_print_offname(void *p, ut64 addr) {

librz/util/str.c
@@ -735,12 +735,12 @@ RZ_API char *rz_str_newf(const char *fmt, ...) {
 }
 
 // Secure string copy with null terminator (like strlcpy or strscpy but ours
-RZ_API void rz_str_ncpy(char *dst, const char *src, size_t n) {
-	int i;
+RZ_API size_t rz_str_ncpy(char *dst, const char *src, size_t n) {
+	size_t i;
 
 	// do not do anything if n is 0
 	if (n == 0) {
-		return;
+		return 0;
 	}
 
 	n--;
@@ -748,6 +748,7 @@ RZ_API void rz_str_ncpy(char *dst, const char *src, size_t n) {
 		dst[i] = src[i];
 	}
 	dst[i] = 0;
+	return i;
 }
 
 /* memccmp("foo.bar", "foo.cow, '.') == 0 */