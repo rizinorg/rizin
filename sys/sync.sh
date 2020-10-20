7416ad971840c3b29a9e24c9bd6c4342685a508b
Anton Kochkov <anton.kochkov@gmail.com>
2020-10-20 19:31:39 +0800

    Remove outdated and unused files (#35)
    * Remove outdated and unused files
    
    * fix

--- sys/sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if ! git remote | grep upstream > /dev/null
-then 
-    git remote add upstream https://github.com/rizinorg/rizin.git
-fi
-test -d rizin && git fetch upstream && git rebase --onto master upstream/master
