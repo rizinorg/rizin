radareorg/radare2

Fix format string warnings

libr/anal/p/anal_arm_cs.c
@@ -534,7 +534,7 @@ static void opex64(RStrBuf *buf, csh handle, cs_insn *insn) {
 			break;
 		case ARM64_OP_IMM:
 			r_strbuf_append (buf, "\"type\":\"imm\"");
-			r_strbuf_appendf (buf, ",\"value\":%" PFMT64d, op->imm);
+			r_strbuf_appendf (buf, ",\"value\":%" PFMT64d, (st64)op->imm);
 			break;
 		case ARM64_OP_MEM:
 			r_strbuf_append (buf, "\"type\":\"mem\"");
@@ -552,7 +552,7 @@ static void opex64(RStrBuf *buf, csh handle, cs_insn *insn) {
 			break;
 		case ARM64_OP_CIMM:
 			r_strbuf_append (buf, "\"type\":\"cimm\"");
-			r_strbuf_appendf (buf, ",\"value\":%" PFMT64d, op->imm);
+			r_strbuf_appendf (buf, ",\"value\":%" PFMT64d, (st64)op->imm);
 			break;
 		case ARM64_OP_PSTATE:
 			r_strbuf_append (buf, "\"type\":\"pstate\"");
@@ -2044,7 +2044,7 @@ PUSH { r4, r5, r6, r7, lr }
 		}
 		break;
 	case ARM_INS_VSTMIA:
-		r_strbuf_setf (&op->esil, "");
+		r_strbuf_set (&op->esil, "");
 		width = 0;
 		for (i = 1; i < insn->detail->arm.op_count; i++) {
 			r_strbuf_appendf (&op->esil, "%s,%d,%s,+,=[%d],",
@@ -2057,7 +2057,7 @@ PUSH { r4, r5, r6, r7, lr }
 		}
 		break;
 	case ARM_INS_VSTMDB:
-		r_strbuf_setf (&op->esil, "");
+		r_strbuf_set (&op->esil, "");
 		width = 0;
 		for (i = insn->detail->arm.op_count - 1; i > 0; i--) {
 			width += REGSIZE32(i);
@@ -2068,7 +2068,7 @@ PUSH { r4, r5, r6, r7, lr }
 		r_strbuf_appendf (&op->esil, "%d,%s,-=,", width, ARG (0));
 		break;
 	case ARM_INS_VLDMIA:
-		r_strbuf_setf (&op->esil, "");
+		r_strbuf_set (&op->esil, "");
 		width = 0;
 		for (i = 1; i < insn->detail->arm.op_count; i++) {
 			r_strbuf_appendf (&op->esil, "%d,%s,+,[%d],%s,=,",
@@ -2081,7 +2081,7 @@ PUSH { r4, r5, r6, r7, lr }
 		}
 		break;
 	case ARM_INS_VLDMDB:
-		r_strbuf_setf (&op->esil, "");
+		r_strbuf_set (&op->esil, "");
 		width = 0;
 		for (i = insn->detail->arm.op_count - 1; i > 0; i--) {
 			width += REGSIZE32(i);

libr/anal/rtti_itanium.c
@@ -336,7 +336,7 @@ static void rtti_itanium_print_class_type_info_json(class_type_info *cti) {
 	pj_kb (pj, "name_unique", cti->name_unique);
 	pj_end (pj);
 
-	r_cons_printf (pj_string (pj));
+	r_cons_print (pj_string (pj));
 	pj_free (pj);
 }
 
@@ -398,7 +398,7 @@ static void rtti_itanium_print_vmi_class_type_info_json(vmi_class_type_info *vmi
 	pj_end (pj);
 	pj_end (pj);
 
-	r_cons_printf (pj_string (pj));
+	r_cons_print (pj_string (pj));
 	pj_free (pj);
 }
 
@@ -438,7 +438,7 @@ static void rtti_itanium_print_si_class_type_info_json(si_class_type_info *si_ct
 	pj_kn (pj, "ref_to_parent_type", si_cti->base_class_addr);
 	pj_end (pj);
 
-	r_cons_printf (pj_string (pj));
+	r_cons_print (pj_string (pj));
 	pj_free (pj);
 }
 

libr/anal/sign.c
@@ -129,7 +129,7 @@ R_API RList *r_sign_fcn_xrefs(RAnal *a, RAnalFunction *fcn) {
 		if (refi->type == R_ANAL_REF_TYPE_CODE || refi->type == R_ANAL_REF_TYPE_CALL) {
 			const char *flag = getRealRef (core, refi->addr);
 			if (flag) {
-				r_list_append (ret, r_str_newf (flag));
+				r_list_append (ret, r_str_new (flag));
 			}
 		}
 	}
@@ -155,7 +155,7 @@ R_API RList *r_sign_fcn_refs(RAnal *a, RAnalFunction *fcn) {
 		if (refi->type == R_ANAL_REF_TYPE_CODE || refi->type == R_ANAL_REF_TYPE_CALL) {
 			const char *flag = getRealRef (core, refi->addr);
 			if (flag) {
-				r_list_append (ret, r_str_newf (flag));
+				r_list_append (ret, r_str_new (flag));
 			}
 		}
 	}

libr/bin/pdb/pdb.c
@@ -854,7 +854,7 @@ static int build_member_format(STypeInfo *type_info, RStrBuf *format, RStrBuf *n
 		}
 		snprintf (tmp_format, 5, "p%d", size);
 		member_format = tmp_format;
-		r_strbuf_appendf (names, name);
+		r_strbuf_append (names, name);
 	} break;
 	case eLF_CLASS:
 	case eLF_UNION:
@@ -901,7 +901,7 @@ static int build_member_format(STypeInfo *type_info, RStrBuf *format, RStrBuf *n
 		r_warn_if_reached (); // Unhandled type format
 		goto error;
 	}
-	r_strbuf_appendf (format, "%s", member_format);
+	r_strbuf_append (format, member_format);
 skip: // shortcut for unknown types where we only skip the bytes
 	free (name);
 	return 0;

libr/core/canal.c
@@ -1539,7 +1539,7 @@ static int core_anal_graph_construct_edges (RCore *core, RAnalFunction *fcn, int
                                         if (is_star) {
                                                 char *from = get_title (bbi->addr);
                                                 char *to = get_title (bbi->fail);
-                                                r_cons_printf ("%age %s %s\n", from, to);
+                                                r_cons_printf ("age %s %s\n", from, to);
                                                 free(from);
                                                 free(to);
                                         } else {
@@ -2450,12 +2450,12 @@ repeat:
 		case R_GRAPH_FORMAT_JSON:
 			if (usenames) {
 				r_cons_printf ("%s{\"name\":\"%s\", "
-						"\"size\":%d,\"imports\":[",
+						"\"size\":%" PFMT64u ",\"imports\":[",
 						first ? "," : "", fcni->name,
 						r_anal_function_linear_size (fcni));
 			} else {
 				r_cons_printf ("%s{\"name\":\"0x%08" PFMT64x
-						"\", \"size\":%d,\"imports\":[",
+						"\", \"size\":%" PFMT64u ",\"imports\":[",
 						first ? "," : "", fcni->addr,
 						r_anal_function_linear_size (fcni));
 			}
@@ -2512,7 +2512,7 @@ repeat:
 					r_cons_printf ("%s\"%s\"", first2?",":"", fcnr_name);
 				}
 				else {
-					r_cons_printf ("%s\"0x%08"PFMT64x"\"", first2?",":"", fcnr_name);
+					r_cons_printf ("%s\"0x%08"PFMT64x"\"", first2? ",":"", fcnr->addr);
 				}
 				break;
 			default:
@@ -2561,7 +2561,7 @@ static void fcn_list_bbs(RAnalFunction *fcn) {
 	RListIter *iter;
 
 	r_list_foreach (fcn->bbs, iter, bbi) {
-		r_cons_printf ("afb+ 0x%08" PFMT64x " 0x%08" PFMT64x " %d ",
+		r_cons_printf ("afb+ 0x%08" PFMT64x " 0x%08" PFMT64x " %" PFMT64u " ",
 				   fcn->addr, bbi->addr, bbi->size);
 		r_cons_printf ("0x%08"PFMT64x" ", bbi->jump);
 		r_cons_printf ("0x%08"PFMT64x, bbi->fail);
@@ -3062,7 +3062,7 @@ static int fcn_print_detail(RCore *core, RAnalFunction *fcn) {
 	}
 	r_list_free (refs);
 	/*Saving Function stack frame*/
-	r_cons_printf ("afS %"PFMT64d" @ 0x%"PFMT64x"\n", fcn->maxstack, fcn->addr);
+	r_cons_printf ("afS %d @ 0x%"PFMT64x"\n", fcn->maxstack, fcn->addr);
 	free (name);
 	return 0;
 }
@@ -3093,7 +3093,7 @@ static int fcn_print_legacy(RCore *core, RAnalFunction *fcn) {
 	r_cons_printf ("#\noffset: 0x%08"PFMT64x"\nname: %s\nsize: %"PFMT64u,
 			fcn->addr, name, r_anal_function_linear_size (fcn));
 	r_cons_printf ("\nis-pure: %s", r_str_bool (r_anal_function_purity (fcn)));
-	r_cons_printf ("\nrealsz: %d", r_anal_function_realsize (fcn));
+	r_cons_printf ("\nrealsz: %" PFMT64d, r_anal_function_realsize (fcn));
 	r_cons_printf ("\nstackframe: %d", fcn->maxstack);
 	if (fcn->cc) {
 		r_cons_printf ("\ncall-convention: %s", fcn->cc);

libr/core/casm.c
@@ -167,7 +167,7 @@ R_API RList *r_core_asm_strsearch(RCore *core, const char *input, ut64 from, ut6
 					}
 					r_asm_disassemble (core->rasm, &op, buf + addrbytes * idx,
 					      core->blocksize - addrbytes * idx);
-					hit->code = r_str_newf (r_strbuf_get (&op.buf_asm));
+					hit->code = r_str_new (r_strbuf_get (&op.buf_asm));
 					idx = (matchcount)? tidx + 1: idx + 1;
 					matchcount = 0;
 					r_list_append (hits, hit);

libr/core/cmd_print.c
@@ -2227,7 +2227,7 @@ static int cmd_print_pxA(RCore *core, int len, const char *input) {
 	for (oi = i = c = 0; i < len; c++) {
 		if (i && (cols != 0) && !(c % cols)) {
 			show_offset = true;
-			r_cons_printf ("  %d\n", i - oi);
+			r_cons_printf ("  %" PFMT64u "\n", i - oi);
 			oi = i;
 		}
 		if (show_offset && hex_offset) {

rizinorg/rizin

librz/anal/p/anal_arm_cs.c
@@ -534,7 +534,7 @@ static void opex64(RzStrBuf *buf, csh handle, cs_insn *insn) {
 			break;
 		case ARM64_OP_IMM:
 			rz_strbuf_append (buf, "\"type\":\"imm\"");
-			rz_strbuf_appendf (buf, ",\"value\":%" PFMT64d, op->imm);
+			rz_strbuf_appendf (buf, ",\"value\":%" PFMT64d, (st64)op->imm);
 			break;
 		case ARM64_OP_MEM:
 			rz_strbuf_append (buf, "\"type\":\"mem\"");
@@ -552,7 +552,7 @@ static void opex64(RzStrBuf *buf, csh handle, cs_insn *insn) {
 			break;
 		case ARM64_OP_CIMM:
 			rz_strbuf_append (buf, "\"type\":\"cimm\"");
-			rz_strbuf_appendf (buf, ",\"value\":%" PFMT64d, op->imm);
+			rz_strbuf_appendf (buf, ",\"value\":%" PFMT64d, (st64)op->imm);
 			break;
 		case ARM64_OP_PSTATE:
 			rz_strbuf_append (buf, "\"type\":\"pstate\"");
@@ -2044,7 +2044,7 @@ PUSH { r4, r5, r6, r7, lr }
 		}
 		break;
 	case ARM_INS_VSTMIA:
-		rz_strbuf_setf (&op->esil, "");
+		rz_strbuf_set (&op->esil, "");
 		width = 0;
 		for (i = 1; i < insn->detail->arm.op_count; i++) {
 			rz_strbuf_appendf (&op->esil, "%s,%d,%s,+,=[%d],",
@@ -2057,7 +2057,7 @@ PUSH { r4, r5, r6, r7, lr }
 		}
 		break;
 	case ARM_INS_VSTMDB:
-		rz_strbuf_setf (&op->esil, "");
+		rz_strbuf_set (&op->esil, "");
 		width = 0;
 		for (i = insn->detail->arm.op_count - 1; i > 0; i--) {
 			width += REGSIZE32(i);
@@ -2068,7 +2068,7 @@ PUSH { r4, r5, r6, r7, lr }
 		rz_strbuf_appendf (&op->esil, "%d,%s,-=,", width, ARG (0));
 		break;
 	case ARM_INS_VLDMIA:
-		rz_strbuf_setf (&op->esil, "");
+		rz_strbuf_set (&op->esil, "");
 		width = 0;
 		for (i = 1; i < insn->detail->arm.op_count; i++) {
 			rz_strbuf_appendf (&op->esil, "%d,%s,+,[%d],%s,=,",
@@ -2081,7 +2081,7 @@ PUSH { r4, r5, r6, r7, lr }
 		}
 		break;
 	case ARM_INS_VLDMDB:
-		rz_strbuf_setf (&op->esil, "");
+		rz_strbuf_set (&op->esil, "");
 		width = 0;
 		for (i = insn->detail->arm.op_count - 1; i > 0; i--) {
 			width += REGSIZE32(i);

librz/anal/rtti_itanium.c
@@ -336,7 +336,7 @@ static void rtti_itanium_print_class_type_info_json(class_type_info *cti) {
 	pj_kb (pj, "name_unique", cti->name_unique);
 	pj_end (pj);
 
-	rz_cons_printf (pj_string (pj));
+	rz_cons_print (pj_string (pj));
 	pj_free (pj);
 }
 
@@ -398,7 +398,7 @@ static void rtti_itanium_print_vmi_class_type_info_json(vmi_class_type_info *vmi
 	pj_end (pj);
 	pj_end (pj);
 
-	rz_cons_printf (pj_string (pj));
+	rz_cons_print (pj_string (pj));
 	pj_free (pj);
 }
 
@@ -438,7 +438,7 @@ static void rtti_itanium_print_si_class_type_info_json(si_class_type_info *si_ct
 	pj_kn (pj, "ref_to_parent_type", si_cti->base_class_addr);
 	pj_end (pj);
 
-	rz_cons_printf (pj_string (pj));
+	rz_cons_print (pj_string (pj));
 	pj_free (pj);
 }
 

librz/anal/sign.c
@@ -129,7 +129,7 @@ RZ_API RzList *rz_sign_fcn_xrefs(RzAnal *a, RzAnalFunction *fcn) {
 		if (refi->type == RZ_ANAL_REF_TYPE_CODE || refi->type == RZ_ANAL_REF_TYPE_CALL) {
 			const char *flag = getRealRef (core, refi->addr);
 			if (flag) {
-				rz_list_append (ret, rz_str_newf (flag));
+				rz_list_append (ret, rz_str_new (flag));
 			}
 		}
 	}
@@ -155,7 +155,7 @@ RZ_API RzList *rz_sign_fcn_refs(RzAnal *a, RzAnalFunction *fcn) {
 		if (refi->type == RZ_ANAL_REF_TYPE_CODE || refi->type == RZ_ANAL_REF_TYPE_CALL) {
 			const char *flag = getRealRef (core, refi->addr);
 			if (flag) {
-				rz_list_append (ret, rz_str_newf (flag));
+				rz_list_append (ret, rz_str_new (flag));
 			}
 		}
 	}

librz/bin/pdb/pdb.c
@@ -854,7 +854,7 @@ static int build_member_format(STypeInfo *type_info, RzStrBuf *format, RzStrBuf 
 		}
 		snprintf (tmp_format, 5, "p%d", size);
 		member_format = tmp_format;
-		rz_strbuf_appendf (names, name);
+		rz_strbuf_append (names, name);
 	} break;
 	case eLF_CLASS:
 	case eLF_UNION:
@@ -901,7 +901,7 @@ static int build_member_format(STypeInfo *type_info, RzStrBuf *format, RzStrBuf 
 		rz_warn_if_reached (); // Unhandled type format
 		goto error;
 	}
-	rz_strbuf_appendf (format, "%s", member_format);
+	rz_strbuf_append (format, member_format);
 skip: // shortcut for unknown types where we only skip the bytes
 	free (name);
 	return 0;

librz/core/canal.c
@@ -1539,7 +1539,7 @@ static int core_anal_graph_construct_edges (RzCore *core, RzAnalFunction *fcn, i
                                         if (is_star) {
                                                 char *from = get_title (bbi->addr);
                                                 char *to = get_title (bbi->fail);
-                                                rz_cons_printf ("%age %s %s\n", from, to);
+                                                rz_cons_printf ("age %s %s\n", from, to);
                                                 free(from);
                                                 free(to);
                                         } else {
@@ -2450,12 +2450,12 @@ repeat:
 		case RZ_GRAPH_FORMAT_JSON:
 			if (usenames) {
 				rz_cons_printf ("%s{\"name\":\"%s\", "
-						"\"size\":%d,\"imports\":[",
+						"\"size\":%" PFMT64u ",\"imports\":[",
 						first ? "," : "", fcni->name,
 						rz_anal_function_linear_size (fcni));
 			} else {
 				rz_cons_printf ("%s{\"name\":\"0x%08" PFMT64x
-						"\", \"size\":%d,\"imports\":[",
+						"\", \"size\":%" PFMT64u ",\"imports\":[",
 						first ? "," : "", fcni->addr,
 						rz_anal_function_linear_size (fcni));
 			}
@@ -2512,7 +2512,7 @@ repeat:
 					rz_cons_printf ("%s\"%s\"", first2?",":"", fcnr_name);
 				}
 				else {
-					rz_cons_printf ("%s\"0x%08"PFMT64x"\"", first2?",":"", fcnr_name);
+					rz_cons_printf ("%s\"0x%08"PFMT64x"\"", first2?",":"", fcnr->addr);
 				}
 				break;
 			default:
@@ -2561,7 +2561,7 @@ static void fcn_list_bbs(RzAnalFunction *fcn) {
 	RzListIter *iter;
 
 	rz_list_foreach (fcn->bbs, iter, bbi) {
-		rz_cons_printf ("afb+ 0x%08" PFMT64x " 0x%08" PFMT64x " %d ",
+		rz_cons_printf ("afb+ 0x%08" PFMT64x " 0x%08" PFMT64x " %" PFMT64u " ",
 				   fcn->addr, bbi->addr, bbi->size);
 		rz_cons_printf ("0x%08"PFMT64x" ", bbi->jump);
 		rz_cons_printf ("0x%08"PFMT64x, bbi->fail);
@@ -3062,7 +3062,7 @@ static int fcn_print_detail(RzCore *core, RzAnalFunction *fcn) {
 	}
 	rz_list_free (refs);
 	/*Saving Function stack frame*/
-	rz_cons_printf ("afS %"PFMT64d" @ 0x%"PFMT64x"\n", fcn->maxstack, fcn->addr);
+	rz_cons_printf ("afS %d @ 0x%"PFMT64x"\n", fcn->maxstack, fcn->addr);
 	free (name);
 	return 0;
 }
@@ -3093,7 +3093,7 @@ static int fcn_print_legacy(RzCore *core, RzAnalFunction *fcn) {
 	rz_cons_printf ("#\noffset: 0x%08"PFMT64x"\nname: %s\nsize: %"PFMT64u,
 			fcn->addr, name, rz_anal_function_linear_size (fcn));
 	rz_cons_printf ("\nis-pure: %s", rz_str_bool (rz_anal_function_purity (fcn)));
-	rz_cons_printf ("\nrealsz: %d", rz_anal_function_realsize (fcn));
+	rz_cons_printf ("\nrealsz: %" PFMT64d, rz_anal_function_realsize (fcn));
 	rz_cons_printf ("\nstackframe: %d", fcn->maxstack);
 	if (fcn->cc) {
 		rz_cons_printf ("\ncall-convention: %s", fcn->cc);

librz/core/casm.c
@@ -167,7 +167,7 @@ RZ_API RzList *rz_core_asm_strsearch(RzCore *core, const char *input, ut64 from,
 					}
 					rz_asm_disassemble (core->rasm, &op, buf + addrbytes * idx,
 					      core->blocksize - addrbytes * idx);
-					hit->code = rz_str_newf (rz_strbuf_get (&op.buf_asm));
+					hit->code = rz_str_new (rz_strbuf_get (&op.buf_asm));
 					idx = (matchcount)? tidx + 1: idx + 1;
 					matchcount = 0;
 					rz_list_append (hits, hit);

librz/core/cmd_print.c
@@ -2227,7 +2227,7 @@ static int cmd_print_pxA(RzCore *core, int len, const char *input) {
 	for (oi = i = c = 0; i < len; c++) {
 		if (i && (cols != 0) && !(c % cols)) {
 			show_offset = true;
-			rz_cons_printf ("  %d\n", i - oi);
+			rz_cons_printf ("  %" PFMT64u "\n", i - oi);
 			oi = i;
 		}
 		if (show_offset && hex_offset) {