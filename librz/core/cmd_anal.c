radareorg/radare2

Fix format warnings in libr/core

libr/core/cmd_anal.c
@@ -1599,7 +1599,7 @@ static void print_trampolines(RCore *core, ut64 a, ut64 b, size_t element_size) 
 			if (element_size == 4) {
 				r_cons_printf ("f trampoline.%x @ 0x%" PFMT64x "\n", n, core->offset + i);
 			} else {
-				r_cons_printf ("f trampoline.%" PFMT64x " @ 0x%" PFMT64x "\n", n, core->offset + i);
+				r_cons_printf ("f trampoline.%" PFMT32x " @ 0x%" PFMT64x "\n", n, core->offset + i);
 			}
 			r_cons_printf ("Cd %zu @ 0x%" PFMT64x ":%zu\n", element_size, core->offset + i, element_size);
 			// TODO: add data xrefs
@@ -7835,7 +7835,7 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 					// TODO: implement multi-line comments
 					r_list_foreach (list, iter, ref) {
 						r_cons_printf ("CCa 0x%" PFMT64x " \"XREF from 0x%u \n",
-								ref->at, ref->type, r_asm_op_get_asm (&asmop), iter->n? ",": "");
+								ref->at, ref->type);
 					}
 				} else { // "axf"
 					char str[512];

libr/core/cmd_print.c
@@ -1771,7 +1771,7 @@ static void annotated_hexdump(RCore *core, const char *str, int len) {
 			// collect comments
 			const char *comment = r_meta_get_string (core->anal, R_META_TYPE_COMMENT, addr + j);
 			if (comment) {
-				note[j] = r_str_newf (";", comment);
+				note[j] = r_str_newf (";%s", comment);
 				marks = true;
 			}
 

libr/core/p/core_java.c
@@ -357,17 +357,17 @@ static const char *r_cmd_java_strtok(const char *str1, const char b, size_t len)
 	if (len == (size_t)-1) {
 		len = strlen (str1);
 	}
-	IFDBG r_cons_printf ("Looking for char (%c) in (%s) up to %d\n", b, p, len);
+	IFDBG r_cons_printf ("Looking for char (%c) in (%s) up to %zu\n", b, p, len);
 	for (; i < len; i++, p++) {
 		if (*p == b) {
-			IFDBG r_cons_printf ("Found? for char (%c) @ %d: (%s)\n", b, i, p);
+			IFDBG r_cons_printf ("Found? for char (%c) @ %zu: (%s)\n", b, i, p);
 			break;
 		}
 	}
 	if (i == len) {
 		p = NULL;
 	}
-	IFDBG r_cons_printf ("Found? for char (%c) @ %d: (%s)\n", b, len, p);
+	IFDBG r_cons_printf ("Found? for char (%c) @ %zu: (%s)\n", b, len, p);
 	return p;
 }
 
@@ -429,12 +429,12 @@ static int r_cmd_java_handle_summary_info(RCore *core, const char *cmd) {
 	}
 
 	r_cons_printf ("Summary for %s:\n", obj->file);
-	r_cons_printf ("  Size 0x%" PFMT64x ":\n", obj->size);
-	r_cons_printf ("  Constants  size: 0x%" PFMT64x " count: %d:\n", obj->cp_size, obj->cp_count);
-	r_cons_printf ("  Methods    size: 0x%" PFMT64x " count: %d:\n", obj->methods_size, obj->methods_count);
-	r_cons_printf ("  Fields     size: 0x%" PFMT64x " count: %d:\n", obj->fields_size, obj->fields_count);
-	r_cons_printf ("  Attributes size: 0x%" PFMT64x " count: %d:\n", obj->attrs_size, obj->attrs_count);
-	r_cons_printf ("  Interfaces size: 0x%" PFMT64x " count: %d:\n", obj->interfaces_size, obj->interfaces_count);
+	r_cons_printf ("  Size 0x%x:\n", obj->size);
+	r_cons_printf ("  Constants  size: 0x%x count: %d:\n", obj->cp_size, obj->cp_count);
+	r_cons_printf ("  Methods    size: 0x%x count: %d:\n", obj->methods_size, obj->methods_count);
+	r_cons_printf ("  Fields     size: 0x%x count: %d:\n", obj->fields_size, obj->fields_count);
+	r_cons_printf ("  Attributes size: 0x%x count: %d:\n", obj->attrs_size, obj->attrs_count);
+	r_cons_printf ("  Interfaces size: 0x%x count: %d:\n", obj->interfaces_size, obj->interfaces_count);
 
 	return true;
 }
@@ -480,7 +480,7 @@ static RList *cpfind_str(RBinJavaObj *obj, const char *cmd) {
 	if (!cmd) {
 		return r_list_new ();
 	}
-	IFDBG r_cons_printf ("Looking for str: %s (%d)\n", cmd, strlen (cmd));
+	IFDBG r_cons_printf ("Looking for str: %s (%zu)\n", cmd, strlen (cmd));
 	return r_bin_java_find_cp_const_by_val (obj, (const ut8 *)cmd, strlen (cmd), R_BIN_JAVA_CP_UTF8);
 }
 
@@ -742,7 +742,7 @@ static char *r_cmd_replace_name(const char *s_new, ut32 replace_len, const char 
 		p_result = result;
 		while (next && consumed < buf_len) {
 			// replace up to next
-			IFDBG r_cons_printf ("next: \"%s\", len to: %d\n", next, next - buffer);
+			IFDBG r_cons_printf ("next: \"%s\", len to: %" PFMTDPTR "\n", next, (ptrdiff_t)(next - buffer));
 			for (; buffer + consumed < next && consumed < buf_len; consumed++, p_result++) {
 				*p_result = *(buffer + consumed);
 				(*res_len)++;
@@ -1440,7 +1440,7 @@ static int r_cmd_java_handle_set_flags(RCore *core, const char *input) {
 		default: flag_value = -1;
 		}
 	}
-	IFDBG r_cons_printf ("Current args: (flag_value: 0x%04x addr: 0x%" PFMT64x ")\n.", flag_value, addr, res);
+	IFDBG r_cons_printf ("Current args: (flag_value: 0x%04x addr: 0x%" PFMT64x ")\n.", flag_value, addr);
 	if (flag_value != -1) {
 		res = r_cmd_java_set_acc_flags (core, addr, ((ut16)flag_value) & 0xffff);
 		IFDBG r_cons_printf ("Writing 0x%04x to 0x%" PFMT64x ": %d.", flag_value, addr, res);
@@ -1947,11 +1947,11 @@ static int r_cmd_java_handle_print_exceptions(RCore *core, const char *input) {
 			char *class_info = r_bin_java_resolve_without_space (bin, exc_entry->catch_type);
 			r_cons_printf ("  Catch Type: %d, %s @ 0x%"PFMT64x"\n", exc_entry->catch_type,
 				class_info, exc_entry->file_offset+6);
-			r_cons_printf ("  Start PC: (0x%"PFMT64x") 0x%"PFMT64x" @ 0x%"PFMT64x"\n",
+			r_cons_printf ("  Start PC: (0x%x) 0x%"PFMT64x" @ 0x%"PFMT64x"\n",
 				exc_entry->start_pc, exc_entry->start_pc+start, exc_entry->file_offset);
-			r_cons_printf ("  End PC: (0x%"PFMT64x") 0x%"PFMT64x" 0x%"PFMT64x"\n",
+			r_cons_printf ("  End PC: (0x%x) 0x%"PFMT64x" 0x%"PFMT64x"\n",
 				exc_entry->end_pc, exc_entry->end_pc+start, exc_entry->file_offset + 2);
-			r_cons_printf ("  Handler PC: (0x%"PFMT64x") 0x%"PFMT64x" 0x%"PFMT64x"\n",
+			r_cons_printf ("  Handler PC: (0x%x) 0x%"PFMT64x" 0x%"PFMT64x"\n",
 				exc_entry->handler_pc, exc_entry->handler_pc+start, exc_entry->file_offset+4);
 			free (class_info);
 		}

libr/core/vasm.c
@@ -19,7 +19,7 @@ static int readline_callback(void *_a, const char *str) {
 	RCoreVisualAsm *a = _a;
 	RCore *core = a->core;
 	r_cons_clear00 ();
-	r_cons_printf ("Write some %s-%d assembly...\n\n",
+	r_cons_printf ("Write some %s-%" PFMT64d " assembly...\n\n",
 		r_config_get (a->core->config, "asm.arch"),
 		r_config_get_i (a->core->config, "asm.bits"));
 	if (*str == '?') {

libr/core/visual.c
@@ -1446,7 +1446,7 @@ repeat:
 				} else {
 					RFlagItem *f = r_flag_get_at (core->flags, refi->addr, true);
 					if (f) {
-						name = r_str_newf ("%s + %d", f->name, refi->addr - f->offset);
+						name = r_str_newf ("%s + %" PFMT64d, f->name, refi->addr - f->offset);
 					} else {
 						name = strdup ("unk");
 					}

libr/core/vmarks.c
@@ -20,9 +20,9 @@ R_API bool r_core_visual_mark_dump(RCore *core) {
 	for (i = 0; i < UT8_MAX; i++) {
 		if (core->marks[i] != UT64_MAX) {
 			if (i > ASCII_MAX) {
-				r_cons_printf ("fV %d 0x%"PFMT64x"\n", i - ASCII_MAX - 1, core->marks[i]);
+				r_cons_printf ("fV %zu 0x%"PFMT64x"\n", i - ASCII_MAX - 1, core->marks[i]);
 			} else {
-				r_cons_printf ("fV %c 0x%"PFMT64x"\n", i, core->marks[i]);
+				r_cons_printf ("fV %c 0x%"PFMT64x"\n", (char)i, core->marks[i]);
 			}
 			res = true;
 		}

libr/core/yank.c
@@ -267,7 +267,7 @@ R_API bool r_core_yank_dump(RCore *core, ut64 pos, int format) {
 				r_cons_newline ();
 				break;
 			default:
-				r_cons_printf ("0x%08" PFMT64x " %d ",
+				r_cons_printf ("0x%08" PFMT64x " %" PFMT64d " ",
 						core->yank_addr + pos,
 						r_buf_size (core->yank_buf) - pos);
 				for (i = pos; i < r_buf_size (core->yank_buf); i++) {

rizinorg/rizin

librz/core/cmd_anal.c
@@ -1599,7 +1599,7 @@ static void print_trampolines(RzCore *core, ut64 a, ut64 b, size_t element_size)
 			if (element_size == 4) {
 				rz_cons_printf ("f trampoline.%x @ 0x%" PFMT64x "\n", n, core->offset + i);
 			} else {
-				rz_cons_printf ("f trampoline.%" PFMT64x " @ 0x%" PFMT64x "\n", n, core->offset + i);
+				rz_cons_printf ("f trampoline.%" PFMT32x " @ 0x%" PFMT64x "\n", n, core->offset + i);
 			}
 			rz_cons_printf ("Cd %zu @ 0x%" PFMT64x ":%zu\n", element_size, core->offset + i, element_size);
 			// TODO: add data xrefs
@@ -7835,7 +7835,7 @@ static bool cmd_anal_refs(RzCore *core, const char *input) {
 					// TODO: implement multi-line comments
 					rz_list_foreach (list, iter, ref) {
 						rz_cons_printf ("CCa 0x%" PFMT64x " \"XREF from 0x%u \n",
-								ref->at, ref->type, rz_asm_op_get_asm (&asmop), iter->n? ",": "");
+								ref->at, ref->type);
 					}
 				} else { // "axf"
 					char str[512];

librz/core/cmd_print.c
@@ -1771,7 +1771,7 @@ static void annotated_hexdump(RzCore *core, const char *str, int len) {
 			// collect comments
 			const char *comment = rz_meta_get_string (core->anal, RZ_META_TYPE_COMMENT, addr + j);
 			if (comment) {
-				note[j] = rz_str_newf (";", comment);
+				note[j] = rz_str_newf (";%s", comment);
 				marks = true;
 			}
 

librz/core/p/core_java.c
@@ -357,17 +357,17 @@ static const char *rz_cmd_java_strtok(const char *str1, const char b, size_t len
 	if (len == (size_t)-1) {
 		len = strlen (str1);
 	}
-	IFDBG rz_cons_printf ("Looking for char (%c) in (%s) up to %d\n", b, p, len);
+	IFDBG rz_cons_printf ("Looking for char (%c) in (%s) up to %zu\n", b, p, len);
 	for (; i < len; i++, p++) {
 		if (*p == b) {
-			IFDBG rz_cons_printf ("Found? for char (%c) @ %d: (%s)\n", b, i, p);
+			IFDBG rz_cons_printf ("Found? for char (%c) @ %zu: (%s)\n", b, i, p);
 			break;
 		}
 	}
 	if (i == len) {
 		p = NULL;
 	}
-	IFDBG rz_cons_printf ("Found? for char (%c) @ %d: (%s)\n", b, len, p);
+	IFDBG rz_cons_printf ("Found? for char (%c) @ %zu: (%s)\n", b, len, p);
 	return p;
 }
 
@@ -430,11 +430,11 @@ static int rz_cmd_java_handle_summary_info(RzCore *core, const char *cmd) {
 
 	rz_cons_printf ("Summary for %s:\n", obj->file);
 	rz_cons_printf ("  Size 0x%" PFMT64x ":\n", obj->size);
-	rz_cons_printf ("  Constants  size: 0x%" PFMT64x " count: %d:\n", obj->cp_size, obj->cp_count);
-	rz_cons_printf ("  Methods    size: 0x%" PFMT64x " count: %d:\n", obj->methods_size, obj->methods_count);
-	rz_cons_printf ("  Fields     size: 0x%" PFMT64x " count: %d:\n", obj->fields_size, obj->fields_count);
-	rz_cons_printf ("  Attributes size: 0x%" PFMT64x " count: %d:\n", obj->attrs_size, obj->attrs_count);
-	rz_cons_printf ("  Interfaces size: 0x%" PFMT64x " count: %d:\n", obj->interfaces_size, obj->interfaces_count);
+	rz_cons_printf ("  Constants  size: 0x%x count: %d:\n", obj->cp_size, obj->cp_count);
+	rz_cons_printf ("  Methods    size: 0x%x count: %d:\n", obj->methods_size, obj->methods_count);
+	rz_cons_printf ("  Fields     size: 0x%x count: %d:\n", obj->fields_size, obj->fields_count);
+	rz_cons_printf ("  Attributes size: 0x%x count: %d:\n", obj->attrs_size, obj->attrs_count);
+	rz_cons_printf ("  Interfaces size: 0x%x count: %d:\n", obj->interfaces_size, obj->interfaces_count);
 
 	return true;
 }
@@ -480,7 +480,7 @@ static RzList *cpfind_str(RzBinJavaObj *obj, const char *cmd) {
 	if (!cmd) {
 		return rz_list_new ();
 	}
-	IFDBG rz_cons_printf ("Looking for str: %s (%d)\n", cmd, strlen (cmd));
+	IFDBG rz_cons_printf ("Looking for str: %s (%zu)\n", cmd, strlen (cmd));
 	return rz_bin_java_find_cp_const_by_val (obj, (const ut8 *)cmd, strlen (cmd), RZ_BIN_JAVA_CP_UTF8);
 }
 
@@ -742,7 +742,7 @@ static char *rz_cmd_replace_name(const char *s_new, ut32 replace_len, const char
 		p_result = result;
 		while (next && consumed < buf_len) {
 			// replace up to next
-			IFDBG rz_cons_printf ("next: \"%s\", len to: %d\n", next, next - buffer);
+			IFDBG rz_cons_printf ("next: \"%s\", len to: %" PFMTDPTR "\n", next, (ptrdiff_t)(next - buffer));
 			for (; buffer + consumed < next && consumed < buf_len; consumed++, p_result++) {
 				*p_result = *(buffer + consumed);
 				(*res_len)++;
@@ -1440,7 +1440,7 @@ static int rz_cmd_java_handle_set_flags(RzCore *core, const char *input) {
 		default: flag_value = -1;
 		}
 	}
-	IFDBG rz_cons_printf ("Current args: (flag_value: 0x%04x addr: 0x%" PFMT64x ")\n.", flag_value, addr, res);
+	IFDBG rz_cons_printf ("Current args: (flag_value: 0x%04x addr: 0x%" PFMT64x ")\n.", flag_value, addr);
 	if (flag_value != -1) {
 		res = rz_cmd_java_set_acc_flags (core, addr, ((ut16)flag_value) & 0xffff);
 		IFDBG rz_cons_printf ("Writing 0x%04x to 0x%" PFMT64x ": %d.", flag_value, addr, res);
@@ -1947,11 +1947,11 @@ static int rz_cmd_java_handle_print_exceptions(RzCore *core, const char *input) 
 			char *class_info = rz_bin_java_resolve_without_space (bin, exc_entry->catch_type);
 			rz_cons_printf ("  Catch Type: %d, %s @ 0x%"PFMT64x"\n", exc_entry->catch_type,
 				class_info, exc_entry->file_offset+6);
-			rz_cons_printf ("  Start PC: (0x%"PFMT64x") 0x%"PFMT64x" @ 0x%"PFMT64x"\n",
+			rz_cons_printf ("  Start PC: (0x%x) 0x%"PFMT64x" @ 0x%"PFMT64x"\n",
 				exc_entry->start_pc, exc_entry->start_pc+start, exc_entry->file_offset);
-			rz_cons_printf ("  End PC: (0x%"PFMT64x") 0x%"PFMT64x" 0x%"PFMT64x"\n",
+			rz_cons_printf ("  End PC: (0x%x) 0x%"PFMT64x" 0x%"PFMT64x"\n",
 				exc_entry->end_pc, exc_entry->end_pc+start, exc_entry->file_offset + 2);
-			rz_cons_printf ("  Handler PC: (0x%"PFMT64x") 0x%"PFMT64x" 0x%"PFMT64x"\n",
+			rz_cons_printf ("  Handler PC: (0x%x) 0x%"PFMT64x" 0x%"PFMT64x"\n",
 				exc_entry->handler_pc, exc_entry->handler_pc+start, exc_entry->file_offset+4);
 			free (class_info);
 		}

librz/core/vasm.c
@@ -19,7 +19,7 @@ static int readline_callback(void *_a, const char *str) {
 	RzCoreVisualAsm *a = _a;
 	RzCore *core = a->core;
 	rz_cons_clear00 ();
-	rz_cons_printf ("Write some %s-%d assembly...\n\n",
+	rz_cons_printf ("Write some %s-%" PFMT64d " assembly...\n\n",
 		rz_config_get (a->core->config, "asm.arch"),
 		rz_config_get_i (a->core->config, "asm.bits"));
 	if (*str == '?') {

librz/core/visual.c
@@ -1446,7 +1446,7 @@ repeat:
 				} else {
 					RzFlagItem *f = rz_flag_get_at (core->flags, refi->addr, true);
 					if (f) {
-						name = rz_str_newf ("%s + %d", f->name, refi->addr - f->offset);
+						name = rz_str_newf ("%s + %" PFMT64d, f->name, refi->addr - f->offset);
 					} else {
 						name = strdup ("unk");
 					}

librz/core/vmarks.c
@@ -20,9 +20,9 @@ RZ_API bool rz_core_visual_mark_dump(RzCore *core) {
 	for (i = 0; i < UT8_MAX; i++) {
 		if (core->marks[i] != UT64_MAX) {
 			if (i > ASCII_MAX) {
-				rz_cons_printf ("fV %d 0x%"PFMT64x"\n", i - ASCII_MAX - 1, core->marks[i]);
+				rz_cons_printf ("fV %zu 0x%"PFMT64x"\n", i - ASCII_MAX - 1, core->marks[i]);
 			} else {
-				rz_cons_printf ("fV %c 0x%"PFMT64x"\n", i, core->marks[i]);
+				rz_cons_printf ("fV %c 0x%"PFMT64x"\n", (char)i, core->marks[i]);
 			}
 			res = true;
 		}

librz/core/yank.c
@@ -267,7 +267,7 @@ RZ_API bool rz_core_yank_dump(RzCore *core, ut64 pos, int format) {
 				rz_cons_newline ();
 				break;
 			default:
-				rz_cons_printf ("0x%08" PFMT64x " %d ",
+				rz_cons_printf ("0x%08" PFMT64x " %" PFMT64d " ",
 						core->yank_addr + pos,
 						rz_buf_size (core->yank_buf) - pos);
 				for (i = pos; i < rz_buf_size (core->yank_buf); i++) {