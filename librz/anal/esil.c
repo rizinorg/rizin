7eb70dbc7e861b1c18d3931872b3206e7479fc99
pancake <pancake@youterm.com>
2020-10-27 11:58:20 +0100

    Fix unexpected FPU exception in ESIL emulation ##esil (#17815)
    * Fix unexpected FPU exception in ESIL emulation ##esil
    * More fixes and add test
    * Add comment
    
    Co-authored-by: pancake <pancake@nopcode.org>

--- librz/anal/esil.c
+++ librz/anal/esil.c
@@ -1540,6 +1540,18 @@ static bool esil_mod(RzAnalEsil *esil) {
 	return ret;
 }
 
+static bool detect_fpu_div_exception(ut64 a, ut64 b) {
+	// division by zero
+	if (b == UT64_MIN) {
+		return true;
+	}
+	// undefined result (0x80000 / -1) cant be represented
+	if (a == UT64_GT0 && b == UT64_MAX) {
+		return true;
+	}
+	return false;
+}
+
 static bool esil_signed_mod(RzAnalEsil *esil) {
 	bool ret = false;
 	st64 s, d;
@@ -1547,7 +1559,7 @@ static bool esil_signed_mod(RzAnalEsil *esil) {
 	char *src = rz_anal_esil_pop (esil);
 	if (src && rz_anal_esil_get_parm (esil, src, (ut64 *)&s)) {
 		if (dst && rz_anal_esil_get_parm (esil, dst, (ut64 *)&d)) {
-			if (s == 0) {
+			if (detect_fpu_div_exception (d, s)) {
 				if (esil->verbose > 0) {
 					eprintf ("0x%08"PFMT64x" esil_mod: Division by zero!\n", esil->address);
 				}
@@ -1626,7 +1638,7 @@ static bool esil_signed_div(RzAnalEsil *esil) {
 	char *src = rz_anal_esil_pop (esil);
 	if (src && rz_anal_esil_get_parm (esil, src, (ut64 *)&s)) {
 		if (dst && rz_anal_esil_get_parm (esil, dst, (ut64 *)&d)) {
-			if (s == 0) {
+			if (detect_fpu_div_exception (d, s)) {
 				ERR ("esil_div: Division by zero!");
 				esil->trap = RZ_ANAL_TRAP_DIVBYZERO;
 				esil->trap_code = 0;
