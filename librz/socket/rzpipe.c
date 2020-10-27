393d6365227053e423d70b1a59b396c171649401
pancake <pancake@youterm.com>
2020-10-27 07:25:37 +0100

    Do not show return code message in r2pipe api if it succeds (#17854)
    Co-authored-by: pancake <pancake@nopcode.org>

--- librz/socket/rzpipe.c
+++ librz/socket/rzpipe.c
@@ -300,8 +300,9 @@ RZ_API R2Pipe *rzpipe_open(const char *cmd) {
 			r2p->input[1] = -1;
 			r2p->output[0] = -1;
 			rc = rz_sandbox_system (cmd, 1);
-			fprintf (stderr, "return code %d for %s\n", rc, cmd);
-			fflush (stderr);
+			if (rc != 0) {
+				eprintf ("return code %d for %s\n", rc, cmd);
+			}
 			// trigger the blocking read
 			write (1, "\xff", 1);
 			close (r2p->output[1]);
