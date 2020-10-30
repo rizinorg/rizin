radareorg/radare2

Add cfg.json.* encoding options (#17681) ##json

libr/core/core.c
@@ -327,6 +327,7 @@ R_API int r_core_bind(RCore *core, RCoreBind *bnd) {
 	bnd->numGet = (RCoreNumGet)numget;
 	bnd->isMapped = (RCoreIsMapped)__isMapped;
 	bnd->syncDebugMaps = (RCoreDebugMapsSync)__syncDebugMaps;
+	bnd->pjWithEncoding = (RCorePJWithEncoding)r_core_pj_new;
 	return true;
 }
 
@@ -3673,4 +3674,30 @@ R_API RTable *r_core_table(RCore *core) {
 		table->cons = core->cons;
 	}
 	return table;
+}
+
+/* Config helper function for PJ json encodings */
+R_API PJ *r_core_pj_new(RCore *core) {
+	const char *config_string_encoding = r_config_get (core->config, "cfg.json.str");
+	const char *config_num_encoding = r_config_get (core->config, "cfg.json.num");
+	PJEncodingNum number_encoding = PJ_ENCODING_NUM_DEFAULT;
+	PJEncodingStr string_encoding = PJ_ENCODING_STR_DEFAULT;
+
+	if (!strcmp ("string", config_num_encoding)) {
+		number_encoding = PJ_ENCODING_NUM_STR;
+	} else if (!strcmp ("hex", config_num_encoding)) {
+		number_encoding = PJ_ENCODING_NUM_HEX;
+	}
+
+	if (!strcmp ("base64", config_string_encoding)) {
+		string_encoding = PJ_ENCODING_STR_BASE64;
+	} else if (!strcmp ("hex", config_string_encoding)) {
+		string_encoding = PJ_ENCODING_STR_HEX;
+	} else if (!strcmp ("array", config_string_encoding)) {
+		string_encoding = PJ_ENCODING_STR_ARRAY;
+	} else if (!strcmp ("strip", config_string_encoding)) {
+		string_encoding = PJ_ENCODING_STR_STRIP;
+	}
+
+	return pj_new_with_encoding (string_encoding, number_encoding);
 }

libr/include/r_util/pj.h
@@ -8,16 +8,34 @@
 extern "C" {
 #endif
 
+/* new encoding options of j commands */
+typedef enum PJEncodingStr {
+	PJ_ENCODING_STR_DEFAULT = 0,
+	PJ_ENCODING_STR_BASE64,
+	PJ_ENCODING_STR_HEX,
+	PJ_ENCODING_STR_ARRAY,
+	PJ_ENCODING_STR_STRIP
+} PJEncodingStr;
+
+typedef enum PJEncodingNum {
+	PJ_ENCODING_NUM_DEFAULT = 0,
+	PJ_ENCODING_NUM_STR,
+	PJ_ENCODING_NUM_HEX
+} PJEncodingNum;
+
 typedef struct pj_t {
 	RStrBuf sb;
 	bool is_first;
 	bool is_key;
 	char braces[R_PRINT_JSON_DEPTH_LIMIT];
 	int level;
+	PJEncodingStr str_encoding;
+	PJEncodingNum num_encoding;
 } PJ;
 
 /* lifecycle */
 R_API PJ *pj_new(void);
+R_API PJ *pj_new_with_encoding(PJEncodingStr str_encoding, PJEncodingNum num_encoding);
 R_API void pj_free(PJ *j);
 R_API void pj_reset(PJ *j); // clear the pj contents, but keep the buffer allocated to re-use it
 R_API char *pj_drain(PJ *j);
@@ -48,7 +66,9 @@ R_API PJ *pj_r(PJ *j, const unsigned char *v, size_t v_len);
 R_API PJ *pj_kr(PJ *j, const char *k, const unsigned char *v, size_t v_len);
 R_API PJ *pj_b(PJ *j, bool v);
 R_API PJ *pj_s(PJ *j, const char *k);
+R_API PJ *pj_se(PJ *j, const char *k);
 R_API PJ *pj_n(PJ *j, ut64 n);
+R_API PJ *pj_ne(PJ *j, ut64 n);
 R_API PJ *pj_N(PJ *j, st64 n);
 R_API PJ *pj_d(PJ *j, double d);
 R_API PJ *pj_f(PJ *j, float d);

libr/util/pj.c
@@ -26,6 +26,17 @@ R_API PJ *pj_new(void) {
 	if (j) {
 		r_strbuf_init (&j->sb);
 		j->is_first = true;
+		j->str_encoding = PJ_ENCODING_STR_DEFAULT;
+		j->num_encoding = PJ_ENCODING_NUM_DEFAULT;
+	}
+	return j;
+}
+
+R_API PJ *pj_new_with_encoding(PJEncodingStr str_encoding, PJEncodingNum num_encoding) {
+	PJ *j = pj_new ();
+	if (j) {
+		j->str_encoding = str_encoding;
+		j->num_encoding = num_encoding;
 	}
 	return j;
 }
@@ -119,7 +130,11 @@ R_API PJ *pj_knull(PJ *j, const char *k) {
 R_API PJ *pj_kn(PJ *j, const char *k, ut64 n) {
 	r_return_val_if_fail (j && k, j);
 	pj_k (j, k);
-	pj_n (j, n);
+	if (j->num_encoding != PJ_ENCODING_NUM_DEFAULT) {
+		pj_ne (j, n);
+	} else {
+		pj_n (j, n);
+	}
 	return j;
 }
 
@@ -168,7 +183,11 @@ R_API PJ *pj_ka(PJ *j, const char *k) {
 R_API PJ *pj_ks(PJ *j, const char *k, const char *v) {
 	r_return_val_if_fail (j && k && v, j);
 	pj_k (j, k);
-	pj_s (j, v);
+	if (j->str_encoding != PJ_ENCODING_STR_DEFAULT) {
+		pj_se (j, v);
+	} else {
+		pj_s (j, v);
+	}
 	return j;
 }
 
@@ -207,6 +226,27 @@ R_API PJ *pj_s(PJ *j, const char *k) {
 	return j;
 }
 
+R_API PJ *pj_se(PJ *j, const char *k) {
+	r_return_val_if_fail (j && k, j);
+	pj_comma (j);
+	if (j->str_encoding == PJ_ENCODING_STR_ARRAY) {
+		pj_raw (j, "[");
+	} else {
+		pj_raw (j, "\"");
+	}
+	char *en = r_str_encoded_json (k, -1, j->str_encoding);
+	if (en) {
+		pj_raw (j, en);
+		free (en);
+	}
+	if (j->str_encoding == PJ_ENCODING_STR_ARRAY) {
+		pj_raw (j, "]");
+	} else {
+		pj_raw (j, "\"");
+	}
+	return j;
+}
+
 R_API PJ *pj_r(PJ *j, const unsigned char *v, size_t v_len) {
 	r_return_val_if_fail (j && v, j);
 	size_t i;
@@ -238,6 +278,19 @@ R_API PJ *pj_n(PJ *j, ut64 n) {
 	r_return_val_if_fail (j, j);
 	pj_comma (j);
 	pj_raw (j, sdb_fmt ("%" PFMT64u, n));
+	return j;
+}
+
+R_API PJ *pj_ne(PJ *j, ut64 n) {
+	r_return_val_if_fail (j, j);
+	pj_comma (j);
+	if (j->num_encoding == PJ_ENCODING_NUM_STR) {
+		pj_raw (j, sdb_fmt ("\"%" PFMT64u "\"", n));
+	} else if (j->num_encoding == PJ_ENCODING_NUM_HEX) {
+		pj_raw (j, sdb_fmt ("\"0x%" PFMT64x "\"", n));
+	} else {
+		pj_n(j, n);
+	}
 	return j;
 }
 

test/unit/test_str.c
@@ -470,6 +470,23 @@ bool test_r_str_str_xy(void) {
 	mu_end;
 }
 
+bool test_r_str_encoded_json(void) {
+	char *invalidJsonString = "This is my \xe2 sample © string\n";
+	size_t len = strlen (invalidJsonString);
+
+	const char *array = r_str_encoded_json (invalidJsonString, len, PJ_ENCODING_STR_ARRAY);
+	mu_assert_streq (array, "084,104,105,115,032,105,115,032,109,121,032,226,032,115,097,109,112,108,101,032,194,169,032,115,116,114,105,110,103,010", "string as array of uchar");
+	const char *hex = r_str_encoded_json (invalidJsonString, len, PJ_ENCODING_STR_HEX);
+	mu_assert_streq (hex, "54686973206973206D7920E22073616D706C6520C2A920737472696E670A", "string as hexpairs");
+	const char *b64 = r_str_encoded_json (invalidJsonString, len, PJ_ENCODING_STR_BASE64);
+	mu_assert_streq (b64, "VGhpcyBpcyBteSDiIHNhbXBsZSDCqSBzdHJpbmcK", "string as base64 encoded");
+	const char *stripped = r_str_encoded_json (invalidJsonString, len, PJ_ENCODING_STR_STRIP);
+	mu_assert_streq (stripped, "This is my  sample © string\\n", "string with bad chars stripped");
+	const char *none = r_str_encoded_json (invalidJsonString, len, PJ_ENCODING_STR_DEFAULT);
+	mu_assert_streq (none, "This is my \\xe2 sample © string\\n", "default encoding");
+	mu_end;
+}
+
 bool all_tests () {
 	mu_run_test (test_r_str_newf);
 	mu_run_test (test_r_str_replace_char_once);
@@ -500,6 +517,7 @@ bool all_tests () {
 	mu_run_test (test_r_str_constpool);
 	mu_run_test (test_r_str_format_msvc_argv);
 	mu_run_test (test_r_str_str_xy);
+	mu_run_test (test_r_str_encoded_json);
 	return tests_passed != tests_run;
 }
 

rizinorg/rizin

librz/core/core.c
@@ -327,6 +327,7 @@ RZ_API int rz_core_bind(RzCore *core, RzCoreBind *bnd) {
 	bnd->numGet = (RzCoreNumGet)numget;
 	bnd->isMapped = (RzCoreIsMapped)__isMapped;
 	bnd->syncDebugMaps = (RzCoreDebugMapsSync)__syncDebugMaps;
+	bnd->pjWithEncoding = (RzCorePJWithEncoding)rz_core_pj_new;
 	return true;
 }
 
@@ -3459,4 +3460,30 @@ RZ_API RTable *rz_core_table(RzCore *core) {
 		table->cons = core->cons;
 	}
 	return table;
+}
+
+/* Config helper function for PJ json encodings */
+RZ_API PJ *rz_core_pj_new(RzCore *core) {
+	const char *config_string_encoding = rz_config_get (core->config, "cfg.json.str");
+	const char *config_num_encoding = rz_config_get (core->config, "cfg.json.num");
+	PJEncodingNum number_encoding = PJ_ENCODING_NUM_DEFAULT;
+	PJEncodingStr string_encoding = PJ_ENCODING_STR_DEFAULT;
+
+	if (!strcmp ("string", config_num_encoding)) {
+		number_encoding = PJ_ENCODING_NUM_STR;
+	} else if (!strcmp ("hex", config_num_encoding)) {
+		number_encoding = PJ_ENCODING_NUM_HEX;
+	}
+
+	if (!strcmp ("base64", config_string_encoding)) {
+		string_encoding = PJ_ENCODING_STR_BASE64;
+	} else if (!strcmp ("hex", config_string_encoding)) {
+		string_encoding = PJ_ENCODING_STR_HEX;
+	} else if (!strcmp ("array", config_string_encoding)) {
+		string_encoding = PJ_ENCODING_STR_ARRAY;
+	} else if (!strcmp ("strip", config_string_encoding)) {
+		string_encoding = PJ_ENCODING_STR_STRIP;
+	}
+
+	return pj_new_with_encoding (string_encoding, number_encoding);
 }

librz/include/rz_util/pj.h
@@ -8,16 +8,34 @@
 extern "C" {
 #endif
 
+/* new encoding options of j commands */
+typedef enum PJEncodingStr {
+	PJ_ENCODING_STR_DEFAULT = 0,
+	PJ_ENCODING_STR_BASE64,
+	PJ_ENCODING_STR_HEX,
+	PJ_ENCODING_STR_ARRAY,
+	PJ_ENCODING_STR_STRIP
+} PJEncodingStr;
+
+typedef enum PJEncodingNum {
+	PJ_ENCODING_NUM_DEFAULT = 0,
+	PJ_ENCODING_NUM_STR,
+	PJ_ENCODING_NUM_HEX
+} PJEncodingNum;
+
 typedef struct pj_t {
 	RzStrBuf sb;
 	bool is_first;
 	bool is_key;
 	char braces[RZ_PRINT_JSON_DEPTH_LIMIT];
 	int level;
+	PJEncodingStr str_encoding;
+	PJEncodingNum num_encoding;
 } PJ;
 
 /* lifecycle */
 RZ_API PJ *pj_new(void);
+RZ_API PJ *pj_new_with_encoding(PJEncodingStr str_encoding, PJEncodingNum num_encoding);
 RZ_API void pj_free(PJ *j);
 RZ_API void pj_reset(PJ *j); // clear the pj contents, but keep the buffer allocated to re-use it
 RZ_API char *pj_drain(PJ *j);
@@ -48,7 +66,9 @@ RZ_API PJ *pj_r(PJ *j, const unsigned char *v, size_t v_len);
 RZ_API PJ *pj_kr(PJ *j, const char *k, const unsigned char *v, size_t v_len);
 RZ_API PJ *pj_b(PJ *j, bool v);
 RZ_API PJ *pj_s(PJ *j, const char *k);
+RZ_API PJ *pj_se(PJ *j, const char *k);
 RZ_API PJ *pj_n(PJ *j, ut64 n);
+RZ_API PJ *pj_ne(PJ *j, ut64 n);
 RZ_API PJ *pj_N(PJ *j, st64 n);
 RZ_API PJ *pj_d(PJ *j, double d);
 RZ_API PJ *pj_f(PJ *j, float d);

librz/util/pj.c
@@ -26,6 +26,17 @@ RZ_API PJ *pj_new(void) {
 	if (j) {
 		rz_strbuf_init (&j->sb);
 		j->is_first = true;
+		j->str_encoding = PJ_ENCODING_STR_DEFAULT;
+		j->num_encoding = PJ_ENCODING_NUM_DEFAULT;
+	}
+	return j;
+}
+
+RZ_API PJ *pj_new_with_encoding(PJEncodingStr str_encoding, PJEncodingNum num_encoding) {
+	PJ *j = pj_new ();
+	if (j) {
+		j->str_encoding = str_encoding;
+		j->num_encoding = num_encoding;
 	}
 	return j;
 }
@@ -119,7 +130,11 @@ RZ_API PJ *pj_knull(PJ *j, const char *k) {
 RZ_API PJ *pj_kn(PJ *j, const char *k, ut64 n) {
 	rz_return_val_if_fail (j && k, j);
 	pj_k (j, k);
-	pj_n (j, n);
+	if (j->num_encoding != PJ_ENCODING_NUM_DEFAULT) {
+		pj_ne (j, n);
+	} else {
+		pj_n (j, n);
+	}
 	return j;
 }
 
@@ -168,7 +183,11 @@ RZ_API PJ *pj_ka(PJ *j, const char *k) {
 RZ_API PJ *pj_ks(PJ *j, const char *k, const char *v) {
 	rz_return_val_if_fail (j && k && v, j);
 	pj_k (j, k);
-	pj_s (j, v);
+	if (j->str_encoding != PJ_ENCODING_STR_DEFAULT) {
+		pj_se (j, v);
+	} else {
+		pj_s (j, v);
+	}
 	return j;
 }
 
@@ -207,6 +226,27 @@ RZ_API PJ *pj_s(PJ *j, const char *k) {
 	return j;
 }
 
+RZ_API PJ *pj_se(PJ *j, const char *k) {
+	rz_return_val_if_fail (j && k, j);
+	pj_comma (j);
+	if (j->str_encoding == PJ_ENCODING_STR_ARRAY) {
+		pj_raw (j, "[");
+	} else {
+		pj_raw (j, "\"");
+	}
+	char *en = rz_str_encoded_json (k, -1, j->str_encoding);
+	if (en) {
+		pj_raw (j, en);
+		free (en);
+	}
+	if (j->str_encoding == PJ_ENCODING_STR_ARRAY) {
+		pj_raw (j, "]");
+	} else {
+		pj_raw (j, "\"");
+	}
+	return j;
+}
+
 RZ_API PJ *pj_r(PJ *j, const unsigned char *v, size_t v_len) {
 	rz_return_val_if_fail (j && v, j);
 	size_t i;
@@ -238,6 +278,19 @@ RZ_API PJ *pj_n(PJ *j, ut64 n) {
 	rz_return_val_if_fail (j, j);
 	pj_comma (j);
 	pj_raw (j, sdb_fmt ("%" PFMT64u, n));
+	return j;
+}
+
+RZ_API PJ *pj_ne(PJ *j, ut64 n) {
+	rz_return_val_if_fail (j, j);
+	pj_comma (j);
+	if (j->num_encoding == PJ_ENCODING_NUM_STR) {
+		pj_raw (j, sdb_fmt ("\"%" PFMT64u "\"", n));
+	} else if (j->num_encoding == PJ_ENCODING_NUM_HEX) {
+		pj_raw (j, sdb_fmt ("\"0x%" PFMT64x "\"", n));
+	} else {
+		pj_n(j, n);
+	}
 	return j;
 }
 

test/unit/test_str.c
@@ -551,6 +551,23 @@ static bool test_rz_str_wrap(void) {
 	mu_end;
 }
 
+bool test_rz_str_encoded_json(void) {
+	char *invalidJsonString = "This is my \xe2 sample © string\n";
+	size_t len = strlen (invalidJsonString);
+
+	const char *array = rz_str_encoded_json (invalidJsonString, len, PJ_ENCODING_STR_ARRAY);
+	mu_assert_streq (array, "084,104,105,115,032,105,115,032,109,121,032,226,032,115,097,109,112,108,101,032,194,169,032,115,116,114,105,110,103,010", "string as array of uchar");
+	const char *hex = rz_str_encoded_json (invalidJsonString, len, PJ_ENCODING_STR_HEX);
+	mu_assert_streq (hex, "54686973206973206D7920E22073616D706C6520C2A920737472696E670A", "string as hexpairs");
+	const char *b64 = rz_str_encoded_json (invalidJsonString, len, PJ_ENCODING_STR_BASE64);
+	mu_assert_streq (b64, "VGhpcyBpcyBteSDiIHNhbXBsZSDCqSBzdHJpbmcK", "string as base64 encoded");
+	const char *stripped = rz_str_encoded_json (invalidJsonString, len, PJ_ENCODING_STR_STRIP);
+	mu_assert_streq (stripped, "This is my  sample © string\\n", "string with bad chars stripped");
+	const char *none = rz_str_encoded_json (invalidJsonString, len, PJ_ENCODING_STR_DEFAULT);
+	mu_assert_streq (none, "This is my \\xe2 sample © string\\n", "default encoding");
+	mu_end;
+}
+
 bool all_tests () {
 	mu_run_test (test_rz_str_newf);
 	mu_run_test (test_rz_str_replace_char_once);
@@ -582,6 +599,7 @@ bool all_tests () {
 	mu_run_test (test_rz_str_format_msvc_argv);
 	mu_run_test (test_rz_str_str_xy);
 	mu_run_test (test_rz_str_wrap);
+	mu_run_test (test_rz_str_encoded_json);
 	return tests_passed != tests_run;
 }
 