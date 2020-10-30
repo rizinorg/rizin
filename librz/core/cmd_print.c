radareorg/radare2

Fix bugs and improve pxr (add pxr[1248][j] to override asm.bits) ##print

libr/core/cmd_print.c
@@ -525,7 +525,7 @@ static const char *help_msg_px[] = {
 	"pxo", "", "show octal dump",
 	"pxq", "", "show hexadecimal quad-words dump (64bit)",
 	"pxQ", "[q]", "same as above, but one per line",
-	"pxr", "[j]", "show words with references to flags and code (q=quiet)",
+	"pxr", "[1248][qj]", "show hexword references (q=quiet, j=json)",
 	"pxs", "", "show hexadecimal in sparse mode",
 	"pxt", "[*.] [origin]", "show delta pointer table in r2 commands",
 	"pxw", "", "show hexadecimal words dump (32bit)",
@@ -4486,6 +4486,119 @@ static void r_core_disasm_table(RCore * core, int l, const char *input) {
 	r_table_free (t);
 }
 
+static void cmd_pxr(RCore *core, int len, int mode, int wordsize, const char *arg) {
+	PJ *pj = NULL;
+	RTable *t = NULL;
+	if (mode == ',') {
+		t = r_table_new ();
+		RTableColumnType *n = r_table_type ("number");
+		RTableColumnType *s = r_table_type ("string");
+		r_table_add_column (t, n, "addr", 0);
+		r_table_add_column (t, n, "value", 0);
+		r_table_add_column (t, s, "refs", 0);
+	}
+	if (mode == 'j') {
+		pj = pj_new ();
+		if (!pj) {
+			return;
+		}
+	}
+	if (mode == 'j' || mode == ',' || mode == '*' || mode == 'q') {
+		size_t i;
+		const int base = core->anal->bits;
+		const int be = core->anal->big_endian;
+		if (pj) {
+			pj_a (pj);
+		}
+		const ut8 *buf = core->block;
+
+		bool withref = false;
+		int end = R_MIN (core->blocksize, len);
+		for (i = 0; i + wordsize < end; i += wordsize) {
+			ut64 addr = core->offset + i;
+			ut64 val;
+			if (base == 64) {
+				val = r_read_ble64 (buf + i, be);
+			} else if (base == 32) {
+				val = r_read_ble32 (buf + i, be);
+				val &= UT32_MAX;
+			} else if (base == 32) {
+				val = r_read_ble32 (buf + i, be);
+				val &= UT32_MAX;
+			} else if (base == 16) {
+				val = r_read_ble16 (buf + i, be);
+				val &= UT16_MAX;
+			} else {
+				val = buf[i];
+			}
+			if (pj) {
+				pj_o (pj);
+				pj_kn (pj, "addr", addr);
+				pj_kn (pj, "value", val);
+			}
+
+			// XXX: this only works in little endian
+			withref = false;
+			char *refs = NULL;
+			if (core->print->hasrefs) {
+				char *rstr = core->print->hasrefs (core->print->user, val, true);
+				if (R_STR_ISNOTEMPTY (rstr)) {
+					r_str_trim (rstr);
+					if (pj) {
+						char *ns = r_str_escape (rstr);
+						pj_ks (pj, "ref", r_str_trim_head_ro (ns));
+						pj_end (pj);
+						free (ns);
+					}
+					withref = true;
+				}
+				refs = rstr;
+			}
+			if (mode == '*' && R_STR_ISNOTEMPTY (refs)) {
+				// Show only the mapped ones?
+				r_cons_printf ("f pxr.%"PFMT64x"=0x%"PFMT64x"\n", val, addr);
+			} else if (mode == 'q' && R_STR_ISNOTEMPTY (refs)) {
+				r_cons_printf ("%s\n", refs);
+			}
+			if (t) {
+				r_table_add_rowf (t, "xxs", addr, val, refs);
+			}
+			R_FREE (refs);
+			if (!withref && pj) {
+				pj_end (pj);
+			}
+		}
+		if (t) {
+			r_table_query (t, arg? arg + 1: NULL);
+			char *s = r_table_tostring (t);
+			r_cons_println (s);
+			free (s);
+			r_table_free (t);
+		}
+		if (pj) {
+			pj_end (pj);
+			r_cons_println (pj_string (pj));
+			pj_free (pj);
+		}
+	} else {
+		const int ocols = core->print->cols;
+		int bitsize = core->rasm->bits;
+		/* Thumb is 16bit arm but handles 32bit data */
+		if (bitsize == 16) {
+			bitsize = 32;
+		}
+		core->print->cols = 1;
+		core->print->flags |= R_PRINT_FLAGS_REFS;
+		r_cons_break_push (NULL, NULL);
+		r_print_hexdump (core->print, core->offset,
+				core->block, R_MIN (len, core->blocksize),
+				wordsize * 8, bitsize / 8, 1);
+		r_cons_break_pop ();
+		core->print->flags &= ~R_PRINT_FLAGS_REFS;
+		core->print->cols = ocols;
+	}
+}
+
 static int cmd_print(void *data, const char *input) {
 	RCore *core = (RCore *) data;
 	st64 l;
@@ -6190,63 +6303,27 @@ l = use_blocksize;
 			break;
 		case 'r': // "pxr"
 			if (l) {
-				if (input[2] == 'j') {
-					PJ *pj = pj_new ();
-					if (!pj) {
-						return 0;
-					}
-					int base = core->anal->bits;
-					pj_a (pj);
-					const ut8 *buf = core->block;
-					int withref = 0;
-					const int wordsize = base / 8;
-					for (i = 0; i < core->blocksize; i += wordsize) {
-						ut64 addr = core->offset + i;
-						ut64 *foo = (ut64 *) (buf + i);
-						ut64 val = *foo;
-						if (base == 32) {
-							val &= UT32_MAX;
-						}
-						pj_o (pj);
-						pj_kn (pj, "addr", addr);
-						pj_kn (pj, "value", val);
-
-						// XXX: this only works in little endian
-						withref = 0;
-						if (core->print->hasrefs) {
-							char *rstr = core->print->hasrefs (core->print->user, val, true);
-							if (rstr && *rstr) {
-								char *ns = r_str_escape (rstr);
-								pj_ks (pj, "ref", r_str_trim_head_ro (ns));
-								pj_end (pj);
-								free (ns);
-								withref = 1;
-							}
-							free (rstr);
-						}
-						if (!withref) {
-							pj_end (pj);
-						}
-					}
-					pj_end (pj);
-					r_cons_println (pj_string (pj));
-					pj_free (pj);
-				} else {
-					const int ocols = core->print->cols;
-					int bitsize = core->rasm->bits;
-					/* Thumb is 16bit arm but handles 32bit data */
-					if (bitsize == 16) {
-						bitsize = 32;
-					}
-					core->print->cols = 1;
-					core->print->flags |= R_PRINT_FLAGS_REFS;
-					r_cons_break_push (NULL, NULL);
-					r_print_hexdump (core->print, core->offset,
-						core->block, R_MIN (len, core->blocksize),
-						bitsize, bitsize / 8, 1);
-					r_cons_break_pop ();
-					core->print->flags &= ~R_PRINT_FLAGS_REFS;
-					core->print->cols = ocols;
+				int mode = input[2];
+				int wordsize = core->anal->bits / 8;
+				if (mode == '?') {
+					eprintf ("Usage: pxr[1248][*,jq] [length]\n");
+					break;
+				}
+				if (mode && isdigit (mode)) {
+					char tmp[2] = {input[2], 0};
+					wordsize = atoi (tmp);
+					mode = input[3];
+				}
+				switch (wordsize) {
+				case 1:
+				case 2:
+				case 4:
+				case 8:
+					cmd_pxr (core, len, mode, wordsize, mode? strchr (input, mode): NULL);
+					break;
+				default:
+					eprintf ("Invalid word size. Use 1, 2, 4 or 8.\n");
+					break;
 				}
 			}
 			break;

libr/util/print.c
@@ -835,8 +835,9 @@ R_API void r_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int ba
 	int K = 0;
 	bool hex_style = false;
 	int rowbytes = p->cols;
-
-	len = len - (len % step);
+	if (step < len) {
+		len = len - (len % step);
+	}
 	if (p) {
 		pairs = p->pairs;
 		use_sparse = p->flags & R_PRINT_FLAGS_SPARSE;
@@ -888,6 +889,12 @@ R_API void r_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int ba
 	case 10:
 		bytefmt = "%3d";
 		pre = " ";
+		break;
+	case 16:
+		if (inc < 2) {
+			inc = 2;
+			use_header = false;
+		}
 		break;
 	case 32:
 		bytefmt = "0x%08x ";
@@ -1051,7 +1058,7 @@ R_API void r_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int ba
 			}
 		}
 		ut64 at = addr + (j * zoomsz);
-		if (use_offset && !isPxr) {
+		if (use_offset && (!isPxr || inc < 4)) {
 			r_print_section (p, at);
 			r_print_addr (p, at);
 		}
@@ -1083,7 +1090,7 @@ R_API void r_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int ba
 						}
 					} else {
 						if (base == 32) {
-							printfmt ((j%4)? "   ": "  ");
+							printfmt ((j % 4)? "   ": "  ");
 						} else if (base == 10) {
 							printfmt (j % 2? "     ": "  ");
 						} else {
@@ -1239,8 +1246,9 @@ R_API void r_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int ba
 			}
 			if (!p || !(p->flags & R_PRINT_FLAGS_NONASCII)) {
 				bytes = 0;
-				for (j = i; j < i + inc; j++) {
-					if (j!=i && use_align  && bytes >= rowbytes) {
+				size_t end = i + inc;
+				for (j = i; j < end; j++) {
+					if (j != i && use_align  && bytes >= rowbytes) {
 						int sz = (p && p->offsize)? p->offsize (p->user, addr + j): -1;
 						if (sz >= 0) {
 							printfmt (" ");
@@ -1262,15 +1270,24 @@ R_API void r_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int ba
 			}
 			bool eol = false;
 			if (!eol && p && p->flags & R_PRINT_FLAGS_REFS) {
-				ut64 off = 0;
-				if (i + 8 < len) {
-					ut64 *foo = (ut64 *) (buf + i);
-					off = *foo;
+				ut64 off = UT64_MAX;
+				if (inc == 8) {
+					if (i + sizeof (ut64) - 1 < len) {
+						off = r_read_le64 (buf + i);
+					}
+				} else if (inc == 4) {
+					if (i + sizeof (ut32) - 1 < len) {
+						off = r_read_le32 (buf + i);
+					}
+				} else if (inc == 2 && base == 16) {
+					if (i + sizeof (ut16) - 1 < len) {
+						off = r_read_le16 (buf + i);
+						if (off == 0) {
+							off = UT64_MAX;
+						}
+					}
 				}
-				if (base == 32) {
-					off &= UT32_MAX;
-				}
-				if (p->hasrefs) {
+				if (p->hasrefs && off != UT64_MAX) {
 					char *rstr = p->hasrefs (p->user, addr + i, false);
 					if (rstr && *rstr) {
 						printfmt (" @%s", rstr);

rizinorg/rizin

librz/core/cmd_print.c
@@ -524,7 +524,7 @@ static const char *help_msg_px[] = {
 	"pxo", "", "show octal dump",
 	"pxq", "", "show hexadecimal quad-words dump (64bit)",
 	"pxQ", "[q]", "same as above, but one per line",
-	"pxr", "[j]", "show words with references to flags and code (q=quiet)",
+	"pxr", "[1248][qj]", "show hexword references (q=quiet, j=json)",
 	"pxs", "", "show hexadecimal in sparse mode",
 	"pxt", "[*.] [origin]", "show delta pointer table in rizin commands",
 	"pxw", "", "show hexadecimal words dump (32bit)",
@@ -4485,6 +4485,119 @@ static void rz_core_disasm_table(RzCore * core, int l, const char *input) {
 	rz_table_free (t);
 }
 
+static void cmd_pxr(RzCore *core, int len, int mode, int wordsize, const char *arg) {
+	PJ *pj = NULL;
+	RzTable *t = NULL;
+	if (mode == ',') {
+		t = rz_table_new ();
+		RzTableColumnType *n = rz_table_type ("number");
+		RzTableColumnType *s = rz_table_type ("string");
+		rz_table_add_column (t, n, "addr", 0);
+		rz_table_add_column (t, n, "value", 0);
+		rz_table_add_column (t, s, "refs", 0);
+	}
+	if (mode == 'j') {
+		pj = pj_new ();
+		if (!pj) {
+			return;
+		}
+	}
+	if (mode == 'j' || mode == ',' || mode == '*' || mode == 'q') {
+		size_t i;
+		const int base = core->anal->bits;
+		const int be = core->anal->big_endian;
+		if (pj) {
+			pj_a (pj);
+		}
+		const ut8 *buf = core->block;
+
+		bool withref = false;
+		int end = RZ_MIN (core->blocksize, len);
+		for (i = 0; i + wordsize < end; i += wordsize) {
+			ut64 addr = core->offset + i;
+			ut64 val;
+			if (base == 64) {
+				val = rz_read_ble64 (buf + i, be);
+			} else if (base == 32) {
+				val = rz_read_ble32 (buf + i, be);
+				val &= UT32_MAX;
+			} else if (base == 32) {
+				val = rz_read_ble32 (buf + i, be);
+				val &= UT32_MAX;
+			} else if (base == 16) {
+				val = rz_read_ble16 (buf + i, be);
+				val &= UT16_MAX;
+			} else {
+				val = buf[i];
+			}
+			if (pj) {
+				pj_o (pj);
+				pj_kn (pj, "addr", addr);
+				pj_kn (pj, "value", val);
+			}
+
+			// XXX: this only works in little endian
+			withref = false;
+			char *refs = NULL;
+			if (core->print->hasrefs) {
+				char *rstr = core->print->hasrefs (core->print->user, val, true);
+				if (RZ_STR_ISNOTEMPTY (rstr)) {
+					rz_str_trim (rstr);
+					if (pj) {
+						char *ns = rz_str_escape (rstr);
+						pj_ks (pj, "ref", rz_str_trim_head_ro (ns));
+						pj_end (pj);
+						free (ns);
+					}
+					withref = true;
+				}
+				refs = rstr;
+			}
+			if (mode == '*' && RZ_STR_ISNOTEMPTY (refs)) {
+				// Show only the mapped ones?
+				rz_cons_printf ("f pxr.%"PFMT64x"=0x%"PFMT64x"\n", val, addr);
+			} else if (mode == 'q' && RZ_STR_ISNOTEMPTY (refs)) {
+				rz_cons_printf ("%s\n", refs);
+			}
+			if (t) {
+				rz_table_add_rowf (t, "xxs", addr, val, refs);
+			}
+			RZ_FREE (refs);
+			if (!withref && pj) {
+				pj_end (pj);
+			}
+		}
+		if (t) {
+			rz_table_query (t, arg? arg + 1: NULL);
+			char *s = rz_table_tostring (t);
+			rz_cons_println (s);
+			free (s);
+			rz_table_free (t);
+		}
+		if (pj) {
+			pj_end (pj);
+			rz_cons_println (pj_string (pj));
+			pj_free (pj);
+		}
+	} else {
+		const int ocols = core->print->cols;
+		int bitsize = core->rasm->bits;
+		/* Thumb is 16bit arm but handles 32bit data */
+		if (bitsize == 16) {
+			bitsize = 32;
+		}
+		core->print->cols = 1;
+		core->print->flags |= RZ_PRINT_FLAGS_REFS;
+		rz_cons_break_push (NULL, NULL);
+		rz_print_hexdump (core->print, core->offset,
+				core->block, RZ_MIN (len, core->blocksize),
+				wordsize * 8, bitsize / 8, 1);
+		rz_cons_break_pop ();
+		core->print->flags &= ~RZ_PRINT_FLAGS_REFS;
+		core->print->cols = ocols;
+	}
+}
+
 static int cmd_print(void *data, const char *input) {
 	RzCore *core = (RzCore *) data;
 	st64 l;
@@ -6178,63 +6291,27 @@ l = use_blocksize;
 			break;
 		case 'r': // "pxr"
 			if (l) {
-				if (input[2] == 'j') {
-					PJ *pj = pj_new ();
-					if (!pj) {
-						return 0;
-					}
-					int base = core->anal->bits;
-					pj_a (pj);
-					const ut8 *buf = core->block;
-					int withref = 0;
-					const int wordsize = base / 8;
-					for (i = 0; i < core->blocksize; i += wordsize) {
-						ut64 addr = core->offset + i;
-						ut64 *foo = (ut64 *) (buf + i);
-						ut64 val = *foo;
-						if (base == 32) {
-							val &= UT32_MAX;
-						}
-						pj_o (pj);
-						pj_kn (pj, "addr", addr);
-						pj_kn (pj, "value", val);
-
-						// XXX: this only works in little endian
-						withref = 0;
-						if (core->print->hasrefs) {
-							char *rstr = core->print->hasrefs (core->print->user, val, true);
-							if (rstr && *rstr) {
-								char *ns = rz_str_escape (rstr);
-								pj_ks (pj, "ref", rz_str_trim_head_ro (ns));
-								pj_end (pj);
-								free (ns);
-								withref = 1;
-							}
-							free (rstr);
-						}
-						if (!withref) {
-							pj_end (pj);
-						}
-					}
-					pj_end (pj);
-					rz_cons_println (pj_string (pj));
-					pj_free (pj);
-				} else {
-					const int ocols = core->print->cols;
-					int bitsize = core->rasm->bits;
-					/* Thumb is 16bit arm but handles 32bit data */
-					if (bitsize == 16) {
-						bitsize = 32;
-					}
-					core->print->cols = 1;
-					core->print->flags |= RZ_PRINT_FLAGS_REFS;
-					rz_cons_break_push (NULL, NULL);
-					rz_print_hexdump (core->print, core->offset,
-						core->block, RZ_MIN (len, core->blocksize),
-						bitsize, bitsize / 8, 1);
-					rz_cons_break_pop ();
-					core->print->flags &= ~RZ_PRINT_FLAGS_REFS;
-					core->print->cols = ocols;
+				int mode = input[2];
+				int wordsize = core->anal->bits / 8;
+				if (mode == '?') {
+					eprintf ("Usage: pxr[1248][*,jq] [length]\n");
+					break;
+				}
+				if (mode && isdigit (mode)) {
+					char tmp[2] = {input[2], 0};
+					wordsize = atoi (tmp);
+					mode = input[3];
+				}
+				switch (wordsize) {
+				case 1:
+				case 2:
+				case 4:
+				case 8:
+					cmd_pxr (core, len, mode, wordsize, mode? strchr (input, mode): NULL);
+					break;
+				default:
+					eprintf ("Invalid word size. Use 1, 2, 4 or 8.\n");
+					break;
 				}
 			}
 			break;

librz/util/print.c
@@ -835,8 +835,9 @@ RZ_API void rz_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int 
 	int K = 0;
 	bool hex_style = false;
 	int rowbytes = p->cols;
-
-	len = len - (len % step);
+	if (step < len) {
+		len = len - (len % step);
+	}
 	if (p) {
 		pairs = p->pairs;
 		use_sparse = p->flags & RZ_PRINT_FLAGS_SPARSE;
@@ -888,6 +889,12 @@ RZ_API void rz_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int 
 	case 10:
 		bytefmt = "%3d";
 		pre = " ";
+		break;
+	case 16:
+		if (inc < 2) {
+			inc = 2;
+			use_header = false;
+		}
 		break;
 	case 32:
 		bytefmt = "0x%08x ";
@@ -1051,7 +1058,7 @@ RZ_API void rz_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int 
 			}
 		}
 		ut64 at = addr + (j * zoomsz);
-		if (use_offset && !isPxr) {
+		if (use_offset && (!isPxr || inc < 4)) {
 			rz_print_section (p, at);
 			rz_print_addr (p, at);
 		}
@@ -1083,7 +1090,7 @@ RZ_API void rz_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int 
 						}
 					} else {
 						if (base == 32) {
-							printfmt ((j%4)? "   ": "  ");
+							printfmt ((j % 4)? "   ": "  ");
 						} else if (base == 10) {
 							printfmt (j % 2? "     ": "  ");
 						} else {
@@ -1239,8 +1246,9 @@ RZ_API void rz_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int 
 			}
 			if (!p || !(p->flags & RZ_PRINT_FLAGS_NONASCII)) {
 				bytes = 0;
-				for (j = i; j < i + inc; j++) {
-					if (j!=i && use_align  && bytes >= rowbytes) {
+				size_t end = i + inc;
+				for (j = i; j < end; j++) {
+					if (j != i && use_align  && bytes >= rowbytes) {
 						int sz = (p && p->offsize)? p->offsize (p->user, addr + j): -1;
 						if (sz >= 0) {
 							printfmt (" ");
@@ -1262,15 +1270,24 @@ RZ_API void rz_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int 
 			}
 			bool eol = false;
 			if (!eol && p && p->flags & RZ_PRINT_FLAGS_REFS) {
-				ut64 off = 0;
-				if (i + 8 < len) {
-					ut64 *foo = (ut64 *) (buf + i);
-					off = *foo;
+				ut64 off = UT64_MAX;
+				if (inc == 8) {
+					if (i + sizeof (ut64) - 1 < len) {
+						off = rz_read_le64 (buf + i);
+					}
+				} else if (inc == 4) {
+					if (i + sizeof (ut32) - 1 < len) {
+						off = rz_read_le32 (buf + i);
+					}
+				} else if (inc == 2 && base == 16) {
+					if (i + sizeof (ut16) - 1 < len) {
+						off = rz_read_le16 (buf + i);
+						if (off == 0) {
+							off = UT64_MAX;
+						}
+					}
 				}
-				if (base == 32) {
-					off &= UT32_MAX;
-				}
-				if (p->hasrefs) {
+				if (p->hasrefs && off != UT64_MAX) {
 					char *rstr = p->hasrefs (p->user, addr + i, false);
 					if (rstr && *rstr) {
 						printfmt (" @%s", rstr);