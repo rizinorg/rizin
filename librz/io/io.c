radareorg/radare2

Add RIO on write event ##io

libr/io/io.c
@@ -111,6 +111,7 @@ R_API RIO* r_io_init(RIO* io) {
 	r_io_cache_init (io);
 	r_io_plugin_init (io);
 	r_io_undo_init (io);
+	io->event = r_event_new (io);
 	return io;
 }
 
@@ -670,6 +671,7 @@ R_API int r_io_fini(RIO* io) {
 	if (io->runprofile) {
 		R_FREE (io->runprofile);
 	}
+	r_event_free (io->event);
 #if R_IO_USE_PTRACE_WRAP
 	if (io->ptrace_wrap) {
 		ptrace_wrap_instance_stop (io->ptrace_wrap);

libr/io/io_cache.c
@@ -169,6 +169,8 @@ R_API bool r_io_cache_write(RIO *io, ut64 addr, const ut8 *buf, int len) {
 	}
 	memcpy (ch->data, buf, len);
 	r_list_append (io->cache, ch);
+	REventIOWrite iow = { addr, buf, len };
+	r_event_send (io->event, R_EVENT_IO_WRITE, &iow);
 	return true;
 }
 

libr/io/io_plugin.c
@@ -172,7 +172,11 @@ R_API int r_io_plugin_write(RIODesc *desc, const ut8 *buf, int len) {
 	if (!desc->plugin->write) {
 		return -1;
 	}
-	return desc->plugin->write (desc->io, desc, buf, len);
+	const ut64 cur_addr = r_io_desc_seek (desc, 0LL, R_IO_SEEK_CUR);
+	int ret = desc->plugin->write (desc->io, desc, buf, len);
+	REventIOWrite iow = { cur_addr, buf, len };
+	r_event_send (desc->io->event, R_EVENT_IO_WRITE, &iow);
+	return ret;
 }
 
 R_API int r_io_plugin_read_at(RIODesc *desc, ut64 addr, ut8 *buf, int len) {

rizinorg/rizin

librz/io/io.c
@@ -111,6 +111,7 @@ RZ_API RzIO* rz_io_init(RzIO* io) {
 	rz_io_cache_init (io);
 	rz_io_plugin_init (io);
 	rz_io_undo_init (io);
+	io->event = rz_event_new (io);
 	return io;
 }
 
@@ -670,6 +671,7 @@ RZ_API int rz_io_fini(RzIO* io) {
 	if (io->runprofile) {
 		RZ_FREE (io->runprofile);
 	}
+	rz_event_free (io->event);
 #if RZ_IO_USE_PTRACE_WRAP
 	if (io->ptrace_wrap) {
 		ptrace_wrap_instance_stop (io->ptrace_wrap);

librz/io/io_cache.c
@@ -169,6 +169,8 @@ RZ_API bool rz_io_cache_write(RzIO *io, ut64 addr, const ut8 *buf, int len) {
 	}
 	memcpy (ch->data, buf, len);
 	rz_list_append (io->cache, ch);
+	REventIOWrite iow = { addr, buf, len };
+	rz_event_send (io->event, RZ_EVENT_IO_WRITE, &iow);
 	return true;
 }
 

librz/io/io_plugin.c
@@ -172,7 +172,11 @@ RZ_API int rz_io_plugin_write(RzIODesc *desc, const ut8 *buf, int len) {
 	if (!desc->plugin->write) {
 		return -1;
 	}
-	return desc->plugin->write (desc->io, desc, buf, len);
+	const ut64 cur_addr = rz_io_desc_seek (desc, 0LL, RZ_IO_SEEK_CUR);
+	int ret = desc->plugin->write (desc->io, desc, buf, len);
+	REventIOWrite iow = { cur_addr, buf, len };
+	rz_event_send (desc->io->event, RZ_EVENT_IO_WRITE, &iow);
+	return ret;
 }
 
 RZ_API int rz_io_plugin_read_at(RzIODesc *desc, ut64 addr, ut8 *buf, int len) {