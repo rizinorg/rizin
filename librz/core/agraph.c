radareorg/radare2

Fix formatting warnings

libr/core/agraph.c
@@ -3932,8 +3932,8 @@ static void goto_asmqjmps(RAGraph *g, RCore *core) {
 	r_cons_get_size (&rows);
 	r_cons_gotoxy (0, rows);
 	r_cons_clear_line (0);
-	r_cons_printf (Color_RESET);
-	r_cons_printf (h);
+	r_cons_print (Color_RESET);
+	r_cons_print (h);
 	r_cons_flush ();
 
 	do {

libr/core/cmd_anal.c
@@ -2077,7 +2077,7 @@ static void core_anal_bytes(RCore *core, const ut8 *buf, int len, int nops, int 
 				printline ("opex", "%s\n", opexstr);
 			}
 #endif
-			printline ("bytes", NULL, 0);
+			printline ("bytes", "%s", "");
 			int minsz = R_MIN (len, size);
 			minsz = R_MAX (minsz, 0);
 			for (j = 0; j < minsz; j++) {
@@ -8284,7 +8284,7 @@ static void agraph_print_node(RANode *n, void *user) {
 	}
 	encbody = r_base64_encode_dyn (n->body, len);
 	cmd = r_str_newf ("agn \"%s\" base64:%s\n", n->title, encbody);
-	r_cons_printf (cmd);
+	r_cons_print (cmd);
 	free (cmd);
 	free (encbody);
 }

libr/core/cmd_cmp.c
@@ -406,14 +406,14 @@ static int cmd_cmp_disasm(RCore *core, const char *input, int mode) {
 				colpad[pos] = 0;
 			}
 			if (hascolor) {
-				r_cons_printf (iseq? pal->graph_true: pal->graph_false);
+				r_cons_print (iseq? pal->graph_true: pal->graph_false);
 			}
 			r_cons_printf (" 0x%08"PFMT64x "  %s %s",
 				core->offset + i, r_strbuf_get (&op.buf_asm), colpad);
 			r_cons_printf ("%c 0x%08"PFMT64x "  %s\n",
 				iseq? '=': '!', off + j, r_strbuf_get (&op2.buf_asm));
 			if (hascolor) {
-				r_cons_printf (Color_RESET);
+				r_cons_print (Color_RESET);
 			}
 			if (op.size < 1) {
 				op.size = 1;
@@ -444,17 +444,17 @@ static int cmd_cmp_disasm(RCore *core, const char *input, int mode) {
 					core->offset + i, r_strbuf_get (&op.buf_asm));
 			} else {
 				if (hascolor) {
-					r_cons_printf (pal->graph_false);
+					r_cons_print (pal->graph_false);
 				}
 				r_cons_printf ("-0x%08"PFMT64x "  %s\n",
 					core->offset + i, r_strbuf_get (&op.buf_asm));
 				if (hascolor) {
-					r_cons_printf (pal->graph_true);
+					r_cons_print (pal->graph_true);
 				}
 				r_cons_printf ("+0x%08"PFMT64x "  %s\n",
 					off + j, r_strbuf_get (&op2.buf_asm));
 				if (hascolor) {
-					r_cons_printf (Color_RESET);
+					r_cons_print (Color_RESET);
 				}
 			}
 			if (op.size < 1) {

libr/core/cmd_egg.c
@@ -183,7 +183,7 @@ static int cmd_egg(void *data, const char *input) {
 				r_egg_option_set (egg, "egg.padding", input + 2);
 			}
 		} else {
-			eprintf ("Usage: gp [padding]\n");	
+			eprintf ("Usage: gp [padding]\n");
 		}
 		break;
 	case 'e': // "ge"
@@ -201,14 +201,14 @@ static int cmd_egg(void *data, const char *input) {
 				r_egg_option_set (egg, "key", p + 1);
 				r_egg_option_set (egg, "egg.encoder", oa);
 			} else {
-				eprintf ("Usage: ge [encoder] [key]\n");	
+				eprintf ("Usage: ge [encoder] [key]\n");
 			}
 			free (oa);
 		} else {
 			eprintf ("Usage: ge [encoder] [key]\n");
 		}
 		break;
-	case 'i': // "gi" 
+	case 'i': // "gi"
 		if (input[1] == ' ') {
 			if (input[0] && input[2]) {
 				r_egg_option_set (egg, "egg.shellcode", input + 2);
@@ -274,7 +274,7 @@ static int cmd_egg(void *data, const char *input) {
 			} else {
 				char *o = r_egg_option_get (egg, oa);
 				if (o) {
-					r_cons_printf (o);
+					r_cons_print (o);
 					free (o);
 				}
 			}

libr/core/disasm.c
@@ -465,7 +465,7 @@ R_API const char *r_core_get_section_name(RCore *core, ut64 addr) {
 static void _ds_comment_align_(RDisasmState *ds, bool up, bool nl) {
 	if (ds->show_comment_right) {
 		if (ds->show_color) {
-			r_cons_printf (ds->pal_comment);
+			r_cons_print (ds->pal_comment);
 		}
 		return;
 	}
@@ -3636,7 +3636,7 @@ static void ds_begin_nl_comment(RDisasmState *ds) {
 		ds_pre_xrefs (ds, false);
 	}
 	if (ds->show_color && (ds->cmtcount > 0 || ds->show_comment_right)) {
-		r_cons_printf (ds->pal_comment);
+		r_cons_print (ds->pal_comment);
 	}
 }
 

libr/core/visual.c
@@ -223,7 +223,7 @@ static const char *__core_visual_print_command(RCore *core) {
 	}
 	if (r_config_get_i (core->config, "scr.dumpcols")) {
 		free (core->stkcmd);
-		core->stkcmd = r_str_newf (stackPrintCommand (core));
+		core->stkcmd = r_str_new (stackPrintCommand (core));
 		return printfmtColumns[PIDX];
 	}
 	return printfmtSingle[PIDX];

rizinorg/rizin

librz/core/agraph.c
@@ -3932,8 +3932,8 @@ static void goto_asmqjmps(RzAGraph *g, RzCore *core) {
 	rz_cons_get_size (&rows);
 	rz_cons_gotoxy (0, rows);
 	rz_cons_clear_line (0);
-	rz_cons_printf (Color_RESET);
-	rz_cons_printf (h);
+	rz_cons_print (Color_RESET);
+	rz_cons_print (h);
 	rz_cons_flush ();
 
 	do {

librz/core/cmd_anal.c
@@ -2077,7 +2077,7 @@ static void core_anal_bytes(RzCore *core, const ut8 *buf, int len, int nops, int
 				printline ("opex", "%s\n", opexstr);
 			}
 #endif
-			printline ("bytes", NULL, 0);
+			printline ("bytes", "%s", "");
 			int minsz = RZ_MIN (len, size);
 			minsz = RZ_MAX (minsz, 0);
 			for (j = 0; j < minsz; j++) {
@@ -8284,7 +8284,7 @@ static void agraph_print_node(RzANode *n, void *user) {
 	}
 	encbody = rz_base64_encode_dyn (n->body, len);
 	cmd = rz_str_newf ("agn \"%s\" base64:%s\n", n->title, encbody);
-	rz_cons_printf (cmd);
+	rz_cons_print (cmd);
 	free (cmd);
 	free (encbody);
 }

librz/core/cmd_cmp.c
@@ -406,14 +406,14 @@ static int cmd_cmp_disasm(RzCore *core, const char *input, int mode) {
 				colpad[pos] = 0;
 			}
 			if (hascolor) {
-				rz_cons_printf (iseq? pal->graph_true: pal->graph_false);
+				rz_cons_print (iseq? pal->graph_true: pal->graph_false);
 			}
 			rz_cons_printf (" 0x%08"PFMT64x "  %s %s",
 				core->offset + i, rz_strbuf_get (&op.buf_asm), colpad);
 			rz_cons_printf ("%c 0x%08"PFMT64x "  %s\n",
 				iseq? '=': '!', off + j, rz_strbuf_get (&op2.buf_asm));
 			if (hascolor) {
-				rz_cons_printf (Color_RESET);
+				rz_cons_print (Color_RESET);
 			}
 			if (op.size < 1) {
 				op.size = 1;
@@ -444,17 +444,17 @@ static int cmd_cmp_disasm(RzCore *core, const char *input, int mode) {
 					core->offset + i, rz_strbuf_get (&op.buf_asm));
 			} else {
 				if (hascolor) {
-					rz_cons_printf (pal->graph_false);
+					rz_cons_print (pal->graph_false);
 				}
 				rz_cons_printf ("-0x%08"PFMT64x "  %s\n",
 					core->offset + i, rz_strbuf_get (&op.buf_asm));
 				if (hascolor) {
-					rz_cons_printf (pal->graph_true);
+					rz_cons_print (pal->graph_true);
 				}
 				rz_cons_printf ("+0x%08"PFMT64x "  %s\n",
 					off + j, rz_strbuf_get (&op2.buf_asm));
 				if (hascolor) {
-					rz_cons_printf (Color_RESET);
+					rz_cons_print (Color_RESET);
 				}
 			}
 			if (op.size < 1) {

librz/core/cmd_egg.c
@@ -183,7 +183,7 @@ static int cmd_egg(void *data, const char *input) {
 				rz_egg_option_set (egg, "egg.padding", input + 2);
 			}
 		} else {
-			eprintf ("Usage: gp [padding]\n");	
+			eprintf ("Usage: gp [padding]\n");
 		}
 		break;
 	case 'e': // "ge"
@@ -201,14 +201,14 @@ static int cmd_egg(void *data, const char *input) {
 				rz_egg_option_set (egg, "key", p + 1);
 				rz_egg_option_set (egg, "egg.encoder", oa);
 			} else {
-				eprintf ("Usage: ge [encoder] [key]\n");	
+				eprintf ("Usage: ge [encoder] [key]\n");
 			}
 			free (oa);
 		} else {
 			eprintf ("Usage: ge [encoder] [key]\n");
 		}
 		break;
-	case 'i': // "gi" 
+	case 'i': // "gi"
 		if (input[1] == ' ') {
 			if (input[0] && input[2]) {
 				rz_egg_option_set (egg, "egg.shellcode", input + 2);
@@ -274,7 +274,7 @@ static int cmd_egg(void *data, const char *input) {
 			} else {
 				char *o = rz_egg_option_get (egg, oa);
 				if (o) {
-					rz_cons_printf (o);
+					rz_cons_print (o);
 					free (o);
 				}
 			}

librz/core/disasm.c
@@ -465,7 +465,7 @@ RZ_API const char *rz_core_get_section_name(RzCore *core, ut64 addr) {
 static void _ds_comment_align_(RDisasmState *ds, bool up, bool nl) {
 	if (ds->show_comment_right) {
 		if (ds->show_color) {
-			rz_cons_printf (ds->pal_comment);
+			rz_cons_print (ds->pal_comment);
 		}
 		return;
 	}
@@ -3636,7 +3636,7 @@ static void ds_begin_nl_comment(RDisasmState *ds) {
 		ds_pre_xrefs (ds, false);
 	}
 	if (ds->show_color && (ds->cmtcount > 0 || ds->show_comment_right)) {
-		rz_cons_printf (ds->pal_comment);
+		rz_cons_print (ds->pal_comment);
 	}
 }
 

librz/core/visual.c
@@ -223,7 +223,7 @@ static const char *__core_visual_print_command(RzCore *core) {
 	}
 	if (rz_config_get_i (core->config, "scr.dumpcols")) {
 		free (core->stkcmd);
-		core->stkcmd = rz_str_newf (stackPrintCommand (core));
+		core->stkcmd = rz_str_new (stackPrintCommand (core));
 		return printfmtColumns[PIDX];
 	}
 	return printfmtSingle[PIDX];