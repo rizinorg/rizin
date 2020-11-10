radareorg/radare2

Fix r_core_editor() on Windows (#17887) ##windows

libr/core/core.c
@@ -3492,7 +3492,9 @@ R_API char *r_core_editor(const RCore *core, const char *file, const char *str) 
 		cons->cb_editor = tmp;
 	} else {
 		if (editor && name) {
-			r_sys_cmdf ("%s '%s'", editor, name);
+			char *escaped_name = r_str_escape_sh (name);
+			r_sys_cmdf ("%s \"%s\"", editor, escaped_name);
+			free (escaped_name);
 		}
 	}
 	size_t len = 0;

libr/util/str.c
@@ -885,7 +885,7 @@ R_API char *r_str_append(char *ptr, const char *string) {
 	if (string && !ptr) {
 		return strdup (string);
 	}
-	if (!string) {
+	if (R_STR_ISEMPTY (string)) {
 		return ptr;
 	}
 	int plen = strlen (ptr);

rizinorg/rizin

librz/core/core.c
@@ -3281,7 +3281,9 @@ RZ_API char *rz_core_editor(const RzCore *core, const char *file, const char *st
 		cons->cb_editor = tmp;
 	} else {
 		if (editor && name) {
-			rz_sys_cmdf ("%s '%s'", editor, name);
+			char *escaped_name = rz_str_escape_sh (name);
+			rz_sys_cmdf ("%s \"%s\"", editor, escaped_name);
+			free (escaped_name);
 		}
 	}
 	size_t len = 0;

librz/util/str.c
@@ -885,7 +885,7 @@ RZ_API char *rz_str_append(char *ptr, const char *string) {
 	if (string && !ptr) {
 		return strdup (string);
 	}
-	if (!string) {
+	if (RZ_STR_ISEMPTY (string)) {
 		return ptr;
 	}
 	int plen = strlen (ptr);