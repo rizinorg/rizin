radareorg/radare2

Fix formatting warnings for sdb_fmt() ##refactor

libr/bin/p/bin_pe64.c
@@ -34,7 +34,8 @@ static RList *fields(RBinFile *bf) {
 	}
 
 	#define ROWL(nam,siz,val,fmt) \
-	r_list_append (ret, r_bin_field_new (addr, addr, siz, nam, sdb_fmt ("0x%08x", val), fmt, false));
+		r_list_append (ret, r_bin_field_new (addr, addr, siz, nam, \
+				sdb_fmt ("0x%08"PFMT64x, (ut64)val), fmt, false));
 
 	struct PE_(r_bin_pe_obj_t) * bin = bf->o->bin_obj;
 	ut64 addr = bin->rich_header_offset ? bin->rich_header_offset : 128;

libr/core/cmd_flag.c
@@ -431,7 +431,7 @@ static bool __tableItemCallback(RFlagItem *flag, void *user) {
 		RTable *t = ftd->t;
 		const char *spaceName = (flag->space && flag->space->name)? flag->space->name: "";
 		const char *addr = sdb_fmt ("0x%08"PFMT64x, flag->offset);
-		r_table_add_row (t, addr, sdb_fmt ("%d", flag->size), spaceName, flag->name, NULL);
+		r_table_add_row (t, addr, sdb_fmt ("%"PFMT64d, flag->size), spaceName, flag->name, NULL);
 	}
 	return true;
 }

libr/core/pseudo.c
@@ -247,7 +247,7 @@ R_API int r_core_pseudo_code(RCore *core, const char *input) {
 		r_cons_push ();
 		bool html = r_config_get_i (core->config, "scr.html");
 		r_config_set_i (core->config, "scr.html", 0);
-		char *code = r_core_cmd_str (core, sdb_fmt ("pD %d @ 0x%08"PFMT64x"\n", bb->size, bb->addr));
+		char *code = r_core_cmd_str (core, sdb_fmt ("pD %"PFMT64d" @ 0x%08"PFMT64x"\n", bb->size, bb->addr));
 		r_cons_pop ();
 		r_config_set_i (core->config, "scr.html", html);
 		if (indent * I_TAB + 2 >= sizeof (indentstr)) {

rizinorg/rizin

librz/bin/p/bin_pe64.c
@@ -34,7 +34,8 @@ static RzList *fields(RzBinFile *bf) {
 	}
 
 	#define ROWL(nam,siz,val,fmt) \
-	rz_list_append (ret, rz_bin_field_new (addr, addr, siz, nam, sdb_fmt ("0x%08x", val), fmt, false));
+		rz_list_append (ret, rz_bin_field_new (addr, addr, siz, nam, \
+				sdb_fmt ("0x%08"PFMT64x, (ut64)val), fmt, false));
 
 	struct PE_(rz_bin_pe_obj_t) * bin = bf->o->bin_obj;
 	ut64 addr = bin->rich_header_offset ? bin->rich_header_offset : 128;

librz/core/cmd_flag.c
@@ -423,7 +423,7 @@ static bool __tableItemCallback(RzFlagItem *flag, void *user) {
 		RTable *t = ftd->t;
 		const char *spaceName = (flag->space && flag->space->name)? flag->space->name: "";
 		const char *addr = sdb_fmt ("0x%08"PFMT64x, flag->offset);
-		rz_table_add_row (t, addr, sdb_fmt ("%d", flag->size), spaceName, flag->name, NULL);
+		rz_table_add_row (t, addr, sdb_fmt ("%"PFMT64d, flag->size), spaceName, flag->name, NULL);
 	}
 	return true;
 }

librz/core/pseudo.c
@@ -247,7 +247,7 @@ RZ_API int rz_core_pseudo_code(RzCore *core, const char *input) {
 		rz_cons_push ();
 		bool html = rz_config_get_i (core->config, "scr.html");
 		rz_config_set_i (core->config, "scr.html", 0);
-		char *code = rz_core_cmd_str (core, sdb_fmt ("pD %d @ 0x%08"PFMT64x"\n", bb->size, bb->addr));
+		char *code = rz_core_cmd_str (core, sdb_fmt ("pD %"PFMT64d" @ 0x%08"PFMT64x"\n", bb->size, bb->addr));
 		rz_cons_pop ();
 		rz_config_set_i (core->config, "scr.html", html);
 		if (indent * I_TAB + 2 >= sizeof (indentstr)) {