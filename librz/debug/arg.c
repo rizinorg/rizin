7afc443e56e91b4696b649c0435519634a5003af
pancake <pancake@nopcode.org>
2020-11-22 23:44:52 +0100

    Remove all uses of deprecated RZ_ANALYSIS_CC_*

--- librz/debug/arg.c
+++ librz/debug/arg.c
@@ -2,17 +2,18 @@
 
 #include <rz_debug.h>
 
-RZ_API ut64 rz_debug_arg_get (RzDebug *dbg, int cctype, int num) {
-	r_return_val_if_fail (dbg, UT64_MAX);
-	ut32 n32;
-	ut64 n64, sp;
-	char reg[32];
+RZ_API ut64 rz_debug_arg_get (RzDebug *dbg, const char *cc, int num) {
+	rz_return_val_if_fail (dbg, UT64_MAX);
+	if (!cc) {
+		cc = rz_analysis_syscc_default (dbg->analysis);
+	}
 	if (dbg->analysis) {
 		const char *cc = rz_analysis_syscc_default (dbg->analysis);
 		if (!RZ_STR_ISEMPTY (cc)) {
 			if (!strcmp (cc, "stdcall") || !strcmp (cc, "pascal")) {
-				sp = rz_debug_reg_get (dbg, "SP");
+				ut64 sp = rz_debug_reg_get (dbg, "SP");
 				if (dbg->bits == 64) {
+					ut64 n64;
 					sp += 8; // skip return address, assume we are inside the call
 					sp += 8 * num;
 					// FIXME: honor endianness of platform
@@ -21,26 +22,27 @@ RZ_API ut64 rz_debug_arg_get (RzDebug *dbg, int cctype, int num) {
 				} else {
 					sp += 4; // skip return address, assume we are inside the call
 					sp += 4 * num;
+					ut32 n32;
 					// FIXME: honor endianness of platform
 					dbg->iob.read_at (dbg->iob.io, sp, (ut8*)&n32, sizeof(ut32));
 					return (ut64)n32;
 				}
 			}
-			const char *rn = rz_analysis_cc_arg (dbg->anal, cc, num);
+			const char *rn = rz_analysis_cc_arg (dbg->analysis, cc, num);
 			if (rn) {
 				return rz_debug_reg_get (dbg, rn);
 			}
 		}
 	}
+	char reg[32];
 	snprintf (reg, sizeof (reg) - 1, "A%d", num);
 	return rz_debug_reg_get (dbg, reg);
 }
 
-RZ_API bool rz_debug_arg_set (RzDebug *dbg, int cctype, int num, ut64 val) {
+RZ_API bool rz_debug_arg_set (RzDebug *dbg, const char* cc, int num, ut64 val) {
 	rz_return_val_if_fail (dbg, false);
-	const char *cc = rz_analysis_syscc_default (dbg->analysis);
 	if (!RZ_STR_ISEMPTY (cc)) {
-		cc = "reg";
+		cc = rz_analysis_syscc_default (dbg->analysis);
 	}
 	const char *rn = rz_analysis_cc_arg (dbg->analysis, cc, num);
 	if (rn) {
