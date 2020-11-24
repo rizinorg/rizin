radareorg/radare2

Optimize io.cache ##io (#17952)
* Add RSkyline ##util
* Add `r_vector_len()`
* Optimize io.cache ##io
* Use RSkyline instead of RList
* Add cache skyline for faster reads
* Use PJ API in `wcj`
* Add test and fix

libr/core/cmd_search.c
@@ -707,15 +707,15 @@ R_API RList *r_core_get_boundaries_prot(RCore *core, int perm, const char *mode,
 		int len = strlen ("io.sky.");
 		int mask = (mode[len - 1] == '.')? r_str_rwx (mode + len): 0;
 		bool only = (bool)(size_t)strstr (mode, ".only");
-		const RPVector *skyline = &core->io->map_skyline;
+		RVector *skyline = &core->io->map_skyline.v;
 		ut64 begin = UT64_MAX;
 		ut64 end = UT64_MAX;
 		size_t i;
-		for (i = 0; i < r_pvector_len (skyline); i++) {
-			const RIOMapSkyline *part = r_pvector_at (skyline, i);
+		for (i = 0; i < r_vector_len (skyline); i++) {
+			const RSkylineItem *part = r_vector_index_ptr (skyline, i);
 			ut64 from = part->itv.addr;
 			ut64 to = part->itv.addr + part->itv.size;
-			int perm = part->map->perm;
+			int perm = ((RIOMap *)part->user)->perm;
 			if (maskMatches (perm, mask, only)) {
 				continue;
 			}

libr/core/disasm.c
@@ -6618,16 +6618,19 @@ R_API int r_core_disasm_pde(RCore *core, int nb_opcodes, int mode) {
 		}
 	}
 	RAnalEsil *esil = core->anal->esil;
-	RList *ocache = core->io->cache;
+	RPVector ocache = core->io->cache;
 	RCache *ocacheb = core->io->buffer;
 	const int ocached = core->io->cached;
-	if (ocache) {
+	if (ocache.v.a) {
 		if (ocacheb && ocacheb->len) {
 			RCache *c = r_cache_new ();
 			r_cache_set (c, ocacheb->base, ocacheb->buf, ocacheb->len);
 			core->io->buffer = c;
 		}
-		core->io->cache = r_list_clone (ocache);
+		RPVector *vec = (RPVector *)r_vector_clone ((RVector *)&ocache);
+		vec->v.free = NULL;
+		core->io->cache = *vec;
+		free (vec);
 	} else {
 		r_io_cache_init (core->io);
 	}
@@ -6729,17 +6732,23 @@ R_API int r_core_disasm_pde(RCore *core, int nb_opcodes, int mode) {
 	}
 	free (buf);
 	r_reg_arena_pop (reg);
-	int len = r_list_length (ocache);
-	if (r_list_length (core->io->cache) > len) {
+	int len = r_pvector_len (&ocache);
+	if (r_pvector_len (&core->io->cache) > len) {
 		// TODO: Implement push/pop for IO.cache
 		while (len > 0) {
-			(void)r_list_pop_head (core->io->cache);
+			(void)r_pvector_pop_front (&core->io->cache);
 			len--;
 		}
-		core->io->cache->free = ocache->free;
+		core->io->cache.v.free = ocache.v.free;
 	}
 	r_io_cache_fini (core->io);
 	core->io->cache = ocache;
+	r_skyline_clear (&core->io->cache_skyline);
+	void **it;
+	r_pvector_foreach (&ocache, it) {
+		RIOCache *c = (RIOCache *)*it;
+		r_skyline_add (&core->io->cache_skyline, c->itv, c);
+	}
 	core->io->buffer = ocacheb;
 	core->io->cached = ocached;
 	r_config_hold_restore (chold);

libr/io/io.c
@@ -20,30 +20,30 @@ typedef int (*cbOnIterMap)(RIO *io, int fd, ut64 addr, ut8 *buf, int len, RIOMap
 // If prefix_mode is true, returns the number of bytes of operated prefix; returns < 0 on error.
 // If prefix_mode is false, operates in non-stop mode and returns true iff all IO operations on overlapped maps are complete.
 static st64 on_map_skyline(RIO *io, ut64 vaddr, ut8 *buf, int len, int match_flg, cbOnIterMap op, bool prefix_mode) {
-	const RPVector *skyline = &io->map_skyline;
+	RVector *skyline = &io->map_skyline.v;
 	ut64 addr = vaddr;
 	size_t i;
 	bool ret = true, wrap = !prefix_mode && vaddr + len < vaddr;
-#define CMP(addr, part) ((addr) < r_itv_end (((RIOMapSkyline *)(part))->itv) - 1 ? -1 : \
-			(addr) > r_itv_end (((RIOMapSkyline *)(part))->itv) - 1 ? 1 : 0)
+#define CMP(addr, part) ((addr) < r_itv_end (((RSkylineItem *)(part))->itv) - 1 ? -1 : \
+			(addr) > r_itv_end (((RSkylineItem *)(part))->itv) - 1 ? 1 : 0)
 	// Let i be the first skyline part whose right endpoint > addr
 	if (!len) {
-		i = r_pvector_len (skyline);
+		i = r_vector_len (skyline);
 	} else {
-		r_pvector_lower_bound (skyline, addr, i, CMP);
-		if (i == r_pvector_len (skyline) && wrap) {
+		r_vector_lower_bound (skyline, addr, i, CMP);
+		if (i == r_vector_len (skyline) && wrap) {
 			wrap = false;
 			i = 0;
 			addr = 0;
 		}
 	}
 #undef CMP
-	while (i < r_pvector_len (skyline)) {
-		const RIOMapSkyline *part = r_pvector_at (skyline, i);
+	while (i < r_vector_len (skyline)) {
+		const RSkylineItem *part = r_vector_index_ptr (skyline, i);
 		// Right endpoint <= addr
 		if (r_itv_end (part->itv) - 1 < addr) {
 			i++;
-			if (wrap && i == r_pvector_len (skyline)) {
+			if (wrap && i == r_vector_len (skyline)) {
 				wrap = false;
 				i = 0;
 				addr = 0;
@@ -62,10 +62,11 @@ static st64 on_map_skyline(RIO *io, ut64 vaddr, ut8 *buf, int len, int match_flg
 		if (len1 < 1) {
 			break;
 		}
+		RIOMap *map = part->user;
 		// The map satisfies the permission requirement or p_cache is enabled
-		if (((part->map->perm & match_flg) == match_flg || io->p_cache)) {
-			st64 result = op (io, part->map->fd, part->map->delta + addr - part->map->itv.addr,
-					buf + (addr - vaddr), len1, part->map, NULL);
+		if (((map->perm & match_flg) == match_flg || io->p_cache)) {
+			st64 result = op (io, map->fd, map->delta + addr - map->itv.addr,
+					buf + (addr - vaddr), len1, map, NULL);
 			if (prefix_mode) {
 				if (result < 0) {
 					return result;
@@ -106,7 +107,7 @@ R_API RIO* r_io_init(RIO* io) {
 	r_return_val_if_fail (io, NULL);
 	io->addrbytes = 1;
 	r_io_desc_init (io);
-	r_pvector_init (&io->map_skyline, free);
+	r_skyline_init (&io->map_skyline);
 	r_io_map_init (io);
 	r_io_cache_init (io);
 	r_io_plugin_init (io);
@@ -666,7 +667,7 @@ R_API int r_io_fini(RIO* io) {
 	r_io_desc_fini (io);
 	r_io_map_fini (io);
 	ls_free (io->plugins);
-	r_list_free (io->cache);
+	r_io_cache_fini (io);
 	r_list_free (io->undo.w_list);
 	if (io->runprofile) {
 		R_FREE (io->runprofile);

libr/io/io_map.c
@@ -9,73 +9,14 @@
 
 #define END_OF_MAP_IDS UT32_MAX
 
-#define CMP_END_GTE(addr, itv) \
-	(((addr) < r_itv_end (*(RInterval *)(itv))) ? -1 : 1)
-
-#define CMP_END_GTE_PART(addr, part) \
-	(((addr) < (r_itv_end (((RIOMapSkyline *)(part))->itv)) || !r_itv_end (((RIOMapSkyline *)(part))->itv)) ? -1 : 1)
-
-#define CMP_BEGIN_GTE_PART(addr, part) \
-	(((addr) > (r_itv_begin (((RIOMapSkyline *)(part))->itv))) - ((addr) < (r_itv_begin (((RIOMapSkyline *)(part))->itv))))
-
-static bool add_map_to_skyline(RIO *io, RIOMap *map) {
-	size_t slot;
-	RPVector *skyline = &io->map_skyline;
-
-	RIOMapSkyline *new_part = R_NEW (RIOMapSkyline);
-	new_part->map = map;
-	new_part->itv = map->itv;
-	const ut64 new_part_end = r_itv_end (new_part->itv);
-
-	// `part` is the first RIOMapSkyline with part->itv.addr >= new_part->itv.addr
-	r_pvector_lower_bound (skyline, new_part->itv.addr, slot, CMP_BEGIN_GTE_PART);
-	RIOMapSkyline *part = slot < r_pvector_len (skyline) ? r_pvector_at (skyline, slot) : NULL;
-	if (slot) {
-		RIOMapSkyline *prev_part = r_pvector_at (skyline, slot - 1);
-		const ut64 prev_part_end = r_itv_end (prev_part->itv);
-		if (prev_part_end > r_itv_begin (new_part->itv)) {
-			if (prev_part_end > new_part_end) {
-				RIOMapSkyline *tail = R_NEW (RIOMapSkyline);
-				tail->map = prev_part->map;
-				tail->itv.addr = new_part_end;
-				tail->itv.size = prev_part_end - r_itv_begin (tail->itv);
-				if (slot < r_pvector_len (skyline)) {
-					r_pvector_insert (skyline, slot, tail);
-				} else {
-					r_pvector_push (skyline, tail);
-				}
-			}
-			prev_part->itv.size = r_itv_begin (new_part->itv) - r_itv_begin (prev_part->itv);
-		}
-	}
-	if (part) {
-		while (part && r_itv_include (new_part->itv, part->itv)) {
-			// Remove `part` that fits in `new_part`
-			r_pvector_remove_at (skyline, slot);
-			part = slot < r_pvector_len (skyline) ? r_pvector_at (skyline, slot) : NULL;
-		}
-		if (part && r_itv_overlap (new_part->itv, part->itv)) {
-			// Chop start of last `part` that intersects `new_part`
-			const ut64 oaddr = r_itv_begin (part->itv);
-			part->itv.addr = new_part_end;
-			part->itv.size -= r_itv_begin (part->itv) - oaddr;
-		}
-	}
-	if (slot < r_pvector_len (skyline)) {
-		r_pvector_insert (skyline, slot, new_part);
-	} else {
-		r_pvector_push (skyline, new_part);
-	}
-	return true;
-}
-
 // Store map parts that are not covered by others into io->map_skyline
 void io_map_calculate_skyline(RIO *io) {
-	r_pvector_clear (&io->map_skyline);
+	r_skyline_clear (&io->map_skyline);
 	// Last map has highest priority (it shadows previous maps)
 	void **it;
 	r_pvector_foreach (&io->maps, it) {
-		add_map_to_skyline (io, (RIOMap *)*it);
+		RIOMap *map = (RIOMap *)*it;
+		r_skyline_add (&io->map_skyline, map->itv, map);
 	}
 }
 
@@ -101,7 +42,7 @@ RIOMap* io_map_new(RIO* io, int fd, int perm, ut64 delta, ut64 addr, ut64 size) 
 	map->delta = delta;
 	// new map lives on the top, being top the list's tail
 	r_pvector_push (&io->maps, map);
-	add_map_to_skyline (io, map);
+	r_skyline_add (&io->map_skyline, map->itv, map);
 	return map;
 }
 
@@ -223,14 +164,7 @@ R_API RIOMap* r_io_map_get_paddr(RIO* io, ut64 paddr) {
 // gets first map where addr fits in
 R_API RIOMap *r_io_map_get(RIO* io, ut64 addr) {
 	r_return_val_if_fail (io, NULL);
-	const RPVector *skyline = &io->map_skyline;
-	size_t i, len = r_pvector_len (skyline);
-	r_pvector_lower_bound (skyline, addr, i, CMP_END_GTE_PART);
-	if (i == len) {
-		return NULL;
-	}
-	const RIOMapSkyline *sky = r_pvector_at (skyline, i);
-	return sky->itv.addr <= addr ? sky->map : NULL;
+	return r_skyline_get (&io->map_skyline, addr);
 }
 
 R_API bool r_io_map_is_mapped(RIO* io, ut64 addr) {
@@ -379,7 +313,7 @@ R_API void r_io_map_fini(RIO* io) {
 	r_pvector_clear (&io->maps);
 	r_id_pool_free (io->map_ids);
 	io->map_ids = NULL;
-	r_pvector_clear (&io->map_skyline);
+	r_skyline_clear (&io->map_skyline);
 }
 
 R_API void r_io_map_set_name(RIOMap* map, const char* name) {

libr/util/skyline.c
@@ -1,0 +1,70 @@
+/* radare2 - LGPL - Copyright 2020 - GustavoLCR */
+
+#include <r_skyline.h>
+
+#define CMP_BEGIN_GTE_PART(addr, part) \
+	(((addr) > (r_itv_begin (((RSkylineItem *)(part))->itv))) - ((addr) < (r_itv_begin (((RSkylineItem *)(part))->itv))))
+
+#define CMP_END_GTE_PART(addr, part) \
+	(((addr) < (r_itv_end (((RSkylineItem *)(part))->itv)) || !r_itv_end (((RSkylineItem *)(part))->itv)) ? -1 : 1)
+
+R_API bool r_skyline_add(RSkyline *skyline, RInterval itv, void *user) {
+	r_return_val_if_fail (skyline && r_itv_size (itv), false);
+	RVector *skyline_vec = &skyline->v;
+	RSkylineItem new_part = { itv, user };
+	const ut64 new_part_end = r_itv_end (new_part.itv);
+
+	// `part` is the first RSkylineItem with part->itv.addr >= new_part.itv.addr
+	size_t slot;
+	r_vector_lower_bound (skyline_vec, new_part.itv.addr, slot, CMP_BEGIN_GTE_PART);
+	RSkylineItem *part = slot < r_vector_len (skyline_vec) ? r_vector_index_ptr (skyline_vec, slot) : NULL;
+	if (slot) {
+		RSkylineItem *prev_part = r_vector_index_ptr (skyline_vec, slot - 1);
+		const ut64 prev_part_end = r_itv_end (prev_part->itv);
+		if (prev_part_end > r_itv_begin (new_part.itv)) {
+			if (prev_part_end > new_part_end) {
+				RSkylineItem tail;
+				tail.user = prev_part->user;
+				tail.itv.addr = new_part_end;
+				tail.itv.size = prev_part_end - r_itv_begin (tail.itv);
+				if (slot < r_vector_len (skyline_vec)) {
+					r_vector_insert (skyline_vec, slot, &tail);
+				} else {
+					r_vector_push (skyline_vec, &tail);
+				}
+			}
+			prev_part->itv.size = r_itv_begin (new_part.itv) - r_itv_begin (prev_part->itv);
+		}
+	}
+	if (part) {
+		while (part && r_itv_include (new_part.itv, part->itv)) {
+			// Remove `part` that fits in `new_part`
+			r_vector_remove_at (skyline_vec, slot, NULL);
+			part = slot < r_vector_len (skyline_vec) ? r_vector_index_ptr (skyline_vec, slot) : NULL;
+		}
+		if (part && r_itv_overlap (new_part.itv, part->itv)) {
+			// Chop start of last `part` that intersects `new_part`
+			const ut64 oaddr = r_itv_begin (part->itv);
+			part->itv.addr = new_part_end;
+			part->itv.size -= r_itv_begin (part->itv) - oaddr;
+		}
+	}
+	if (slot < r_vector_len (skyline_vec)) {
+		r_vector_insert (skyline_vec, slot, &new_part);
+	} else {
+		r_vector_push (skyline_vec, &new_part);
+	}
+	return true;
+}
+
+R_API const RSkylineItem *r_skyline_get_item_intersect(RSkyline *skyline, ut64 addr, ut64 len) {
+	r_return_val_if_fail (skyline, NULL);
+	RVector *skyline_vec = &skyline->v;
+	size_t i, l = r_vector_len (skyline_vec);
+	r_vector_lower_bound (skyline_vec, addr, i, CMP_END_GTE_PART);
+	if (i == l) {
+		return false;
+	}
+	const RSkylineItem *item = r_vector_index_ptr (skyline_vec, i);
+	return item->itv.addr <= addr + len ? item : NULL;
+}

rizinorg/rizin

librz/core/cmd_search.c
@@ -695,15 +695,15 @@ RZ_API RzList *rz_core_get_boundaries_prot(RzCore *core, int perm, const char *m
 		int len = strlen ("io.sky.");
 		int mask = (mode[len - 1] == '.')? rz_str_rwx (mode + len): 0;
 		bool only = (bool)(size_t)strstr (mode, ".only");
-		const RzPVector *skyline = &core->io->map_skyline;
+		RzVector *skyline = &core->io->map_skyline.v;
 		ut64 begin = UT64_MAX;
 		ut64 end = UT64_MAX;
 		size_t i;
-		for (i = 0; i < rz_pvector_len (skyline); i++) {
-			const RzIOMapSkyline *part = rz_pvector_at (skyline, i);
+		for (i = 0; i < rz_vector_len (skyline); i++) {
+			const RzSkylineItem *part = rz_vector_index_ptr (skyline, i);
 			ut64 from = part->itv.addr;
 			ut64 to = part->itv.addr + part->itv.size;
-			int perm = part->map->perm;
+			int perm = ((RzIOMap *)part->user)->perm;
 			if (maskMatches (perm, mask, only)) {
 				continue;
 			}

librz/core/disasm.c
@@ -6618,16 +6618,19 @@ RZ_API int rz_core_disasm_pde(RzCore *core, int nb_opcodes, int mode) {
 		}
 	}
 	RzAnalEsil *esil = core->anal->esil;
-	RzList *ocache = core->io->cache;
+	RzPVector ocache = core->io->cache;
 	RzCache *ocacheb = core->io->buffer;
 	const int ocached = core->io->cached;
-	if (ocache) {
+	if (ocache.v.a) {
 		if (ocacheb && ocacheb->len) {
 			RzCache *c = rz_cache_new ();
 			rz_cache_set (c, ocacheb->base, ocacheb->buf, ocacheb->len);
 			core->io->buffer = c;
 		}
-		core->io->cache = rz_list_clone (ocache);
+		RzPVector *vec = (RzPVector *)rz_vector_clone ((RzVector *)&ocache);
+		vec->v.free = NULL;
+		core->io->cache = *vec;
+		free (vec);
 	} else {
 		rz_io_cache_init (core->io);
 	}
@@ -6729,17 +6732,23 @@ RZ_API int rz_core_disasm_pde(RzCore *core, int nb_opcodes, int mode) {
 	}
 	free (buf);
 	rz_reg_arena_pop (reg);
-	int len = rz_list_length (ocache);
-	if (rz_list_length (core->io->cache) > len) {
+	int len = rz_pvector_len (&ocache);
+	if (rz_pvector_len (&core->io->cache) > len) {
 		// TODO: Implement push/pop for IO.cache
 		while (len > 0) {
-			(void)rz_list_pop_head (core->io->cache);
+			(void)rz_pvector_pop_front (&core->io->cache);
 			len--;
 		}
-		core->io->cache->free = ocache->free;
+		core->io->cache.v.free = ocache.v.free;
 	}
 	rz_io_cache_fini (core->io);
 	core->io->cache = ocache;
+	rz_skyline_clear (&core->io->cache_skyline);
+	void **it;
+	rz_pvector_foreach (&ocache, it) {
+		RzIOCache *c = (RzIOCache *)*it;
+		rz_skyline_add (&core->io->cache_skyline, c->itv, c);
+	}
 	core->io->buffer = ocacheb;
 	core->io->cached = ocached;
 	rz_config_hold_restore (chold);

librz/io/io.c
@@ -20,30 +20,30 @@ typedef int (*cbOnIterMap)(RzIO *io, int fd, ut64 addr, ut8 *buf, int len, RzIOM
 // If prefix_mode is true, returns the number of bytes of operated prefix; returns < 0 on error.
 // If prefix_mode is false, operates in non-stop mode and returns true iff all IO operations on overlapped maps are complete.
 static st64 on_map_skyline(RzIO *io, ut64 vaddr, ut8 *buf, int len, int match_flg, cbOnIterMap op, bool prefix_mode) {
-	const RzPVector *skyline = &io->map_skyline;
+	RzVector *skyline = &io->map_skyline.v;
 	ut64 addr = vaddr;
 	size_t i;
 	bool ret = true, wrap = !prefix_mode && vaddr + len < vaddr;
-#define CMP(addr, part) ((addr) < rz_itv_end (((RzIOMapSkyline *)(part))->itv) - 1 ? -1 : \
-			(addr) > rz_itv_end (((RzIOMapSkyline *)(part))->itv) - 1 ? 1 : 0)
+#define CMP(addr, part) ((addr) < rz_itv_end (((RzSkylineItem *)(part))->itv) - 1 ? -1 : \
+			(addr) > rz_itv_end (((RzSkylineItem *)(part))->itv) - 1 ? 1 : 0)
 	// Let i be the first skyline part whose right endpoint > addr
 	if (!len) {
-		i = rz_pvector_len (skyline);
+		i = rz_vector_len (skyline);
 	} else {
-		rz_pvector_lower_bound (skyline, addr, i, CMP);
-		if (i == rz_pvector_len (skyline) && wrap) {
+		rz_vector_lower_bound (skyline, addr, i, CMP);
+		if (i == rz_vector_len (skyline) && wrap) {
 			wrap = false;
 			i = 0;
 			addr = 0;
 		}
 	}
 #undef CMP
-	while (i < rz_pvector_len (skyline)) {
-		const RzIOMapSkyline *part = rz_pvector_at (skyline, i);
+	while (i < rz_vector_len (skyline)) {
+		const RzSkylineItem *part = rz_vector_index_ptr (skyline, i);
 		// Right endpoint <= addr
 		if (rz_itv_end (part->itv) - 1 < addr) {
 			i++;
-			if (wrap && i == rz_pvector_len (skyline)) {
+			if (wrap && i == rz_vector_len (skyline)) {
 				wrap = false;
 				i = 0;
 				addr = 0;
@@ -62,10 +62,11 @@ static st64 on_map_skyline(RzIO *io, ut64 vaddr, ut8 *buf, int len, int match_fl
 		if (len1 < 1) {
 			break;
 		}
+		RzIOMap *map = part->user;
 		// The map satisfies the permission requirement or p_cache is enabled
-		if (((part->map->perm & match_flg) == match_flg || io->p_cache)) {
-			st64 result = op (io, part->map->fd, part->map->delta + addr - part->map->itv.addr,
-					buf + (addr - vaddr), len1, part->map, NULL);
+		if (((map->perm & match_flg) == match_flg || io->p_cache)) {
+			st64 result = op (io, map->fd, map->delta + addr - map->itv.addr,
+					buf + (addr - vaddr), len1, map, NULL);
 			if (prefix_mode) {
 				if (result < 0) {
 					return result;
@@ -106,7 +107,7 @@ RZ_API RzIO* rz_io_init(RzIO* io) {
 	rz_return_val_if_fail (io, NULL);
 	io->addrbytes = 1;
 	rz_io_desc_init (io);
-	rz_pvector_init (&io->map_skyline, free);
+	rz_skyline_init (&io->map_skyline);
 	rz_io_map_init (io);
 	rz_io_cache_init (io);
 	rz_io_plugin_init (io);
@@ -666,7 +667,7 @@ RZ_API int rz_io_fini(RzIO* io) {
 	rz_io_desc_fini (io);
 	rz_io_map_fini (io);
 	ls_free (io->plugins);
-	rz_list_free (io->cache);
+	rz_io_cache_fini (io);
 	rz_list_free (io->undo.w_list);
 	if (io->runprofile) {
 		RZ_FREE (io->runprofile);

librz/io/io_map.c
@@ -9,73 +9,14 @@
 
 #define END_OF_MAP_IDS UT32_MAX
 
-#define CMP_END_GTE(addr, itv) \
-	(((addr) < rz_itv_end (*(RzInterval *)(itv))) ? -1 : 1)
-
-#define CMP_END_GTE_PART(addr, part) \
-	(((addr) < (rz_itv_end (((RzIOMapSkyline *)(part))->itv)) || !rz_itv_end (((RzIOMapSkyline *)(part))->itv)) ? -1 : 1)
-
-#define CMP_BEGIN_GTE_PART(addr, part) \
-	(((addr) > (rz_itv_begin (((RzIOMapSkyline *)(part))->itv))) - ((addr) < (rz_itv_begin (((RzIOMapSkyline *)(part))->itv))))
-
-static bool add_map_to_skyline(RzIO *io, RzIOMap *map) {
-	size_t slot;
-	RzPVector *skyline = &io->map_skyline;
-
-	RzIOMapSkyline *new_part = RZ_NEW (RzIOMapSkyline);
-	new_part->map = map;
-	new_part->itv = map->itv;
-	const ut64 new_part_end = rz_itv_end (new_part->itv);
-
-	// `part` is the first RzIOMapSkyline with part->itv.addr >= new_part->itv.addr
-	rz_pvector_lower_bound (skyline, new_part->itv.addr, slot, CMP_BEGIN_GTE_PART);
-	RzIOMapSkyline *part = slot < rz_pvector_len (skyline) ? rz_pvector_at (skyline, slot) : NULL;
-	if (slot) {
-		RzIOMapSkyline *prev_part = rz_pvector_at (skyline, slot - 1);
-		const ut64 prev_part_end = rz_itv_end (prev_part->itv);
-		if (prev_part_end > rz_itv_begin (new_part->itv)) {
-			if (prev_part_end > new_part_end) {
-				RzIOMapSkyline *tail = RZ_NEW (RzIOMapSkyline);
-				tail->map = prev_part->map;
-				tail->itv.addr = new_part_end;
-				tail->itv.size = prev_part_end - rz_itv_begin (tail->itv);
-				if (slot < rz_pvector_len (skyline)) {
-					rz_pvector_insert (skyline, slot, tail);
-				} else {
-					rz_pvector_push (skyline, tail);
-				}
-			}
-			prev_part->itv.size = rz_itv_begin (new_part->itv) - rz_itv_begin (prev_part->itv);
-		}
-	}
-	if (part) {
-		while (part && rz_itv_include (new_part->itv, part->itv)) {
-			// Remove `part` that fits in `new_part`
-			rz_pvector_remove_at (skyline, slot);
-			part = slot < rz_pvector_len (skyline) ? rz_pvector_at (skyline, slot) : NULL;
-		}
-		if (part && rz_itv_overlap (new_part->itv, part->itv)) {
-			// Chop start of last `part` that intersects `new_part`
-			const ut64 oaddr = rz_itv_begin (part->itv);
-			part->itv.addr = new_part_end;
-			part->itv.size -= rz_itv_begin (part->itv) - oaddr;
-		}
-	}
-	if (slot < rz_pvector_len (skyline)) {
-		rz_pvector_insert (skyline, slot, new_part);
-	} else {
-		rz_pvector_push (skyline, new_part);
-	}
-	return true;
-}
-
 // Store map parts that are not covered by others into io->map_skyline
 void io_map_calculate_skyline(RzIO *io) {
-	rz_pvector_clear (&io->map_skyline);
+	rz_skyline_clear (&io->map_skyline);
 	// Last map has highest priority (it shadows previous maps)
 	void **it;
 	rz_pvector_foreach (&io->maps, it) {
-		add_map_to_skyline (io, (RzIOMap *)*it);
+		RzIOMap *map = (RzIOMap *)*it;
+		rz_skyline_add (&io->map_skyline, map->itv, map);
 	}
 }
 
@@ -101,7 +42,7 @@ RzIOMap* io_map_new(RzIO* io, int fd, int perm, ut64 delta, ut64 addr, ut64 size
 	map->delta = delta;
 	// new map lives on the top, being top the list's tail
 	rz_pvector_push (&io->maps, map);
-	add_map_to_skyline (io, map);
+	rz_skyline_add (&io->map_skyline, map->itv, map);
 	return map;
 }
 
@@ -223,14 +164,7 @@ RZ_API RzIOMap* rz_io_map_get_paddr(RzIO* io, ut64 paddr) {
 // gets first map where addr fits in
 RZ_API RzIOMap *rz_io_map_get(RzIO* io, ut64 addr) {
 	rz_return_val_if_fail (io, NULL);
-	const RzPVector *skyline = &io->map_skyline;
-	size_t i, len = rz_pvector_len (skyline);
-	rz_pvector_lower_bound (skyline, addr, i, CMP_END_GTE_PART);
-	if (i == len) {
-		return NULL;
-	}
-	const RzIOMapSkyline *sky = rz_pvector_at (skyline, i);
-	return sky->itv.addr <= addr ? sky->map : NULL;
+	return rz_skyline_get (&io->map_skyline, addr);
 }
 
 RZ_API bool rz_io_map_is_mapped(RzIO* io, ut64 addr) {
@@ -379,7 +313,7 @@ RZ_API void rz_io_map_fini(RzIO* io) {
 	rz_pvector_clear (&io->maps);
 	rz_id_pool_free (io->map_ids);
 	io->map_ids = NULL;
-	rz_pvector_clear (&io->map_skyline);
+	rz_skyline_clear (&io->map_skyline);
 }
 
 RZ_API void rz_io_map_set_name(RzIOMap* map, const char* name) {

librz/util/skyline.c
@@ -1,0 +1,70 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include <rz_skyline.h>
+
+#define CMP_BEGIN_GTE_PART(addr, part) \
+	(((addr) > (rz_itv_begin (((RzSkylineItem *)(part))->itv))) - ((addr) < (rz_itv_begin (((RzSkylineItem *)(part))->itv))))
+
+#define CMP_END_GTE_PART(addr, part) \
+	(((addr) < (rz_itv_end (((RzSkylineItem *)(part))->itv)) || !rz_itv_end (((RzSkylineItem *)(part))->itv)) ? -1 : 1)
+
+RZ_API bool rz_skyline_add(RzSkyline *skyline, RzInterval itv, void *user) {
+	rz_return_val_if_fail (skyline && rz_itv_size (itv), false);
+	RzVector *skyline_vec = &skyline->v;
+	RzSkylineItem new_part = { itv, user };
+	const ut64 new_part_end = rz_itv_end (new_part.itv);
+
+	// `part` is the first RzSkylineItem with part->itv.addr >= new_part.itv.addr
+	size_t slot;
+	rz_vector_lower_bound (skyline_vec, new_part.itv.addr, slot, CMP_BEGIN_GTE_PART);
+	RzSkylineItem *part = slot < rz_vector_len (skyline_vec) ? rz_vector_index_ptr (skyline_vec, slot) : NULL;
+	if (slot) {
+		RzSkylineItem *prev_part = rz_vector_index_ptr (skyline_vec, slot - 1);
+		const ut64 prev_part_end = rz_itv_end (prev_part->itv);
+		if (prev_part_end > rz_itv_begin (new_part.itv)) {
+			if (prev_part_end > new_part_end) {
+				RzSkylineItem tail;
+				tail.user = prev_part->user;
+				tail.itv.addr = new_part_end;
+				tail.itv.size = prev_part_end - rz_itv_begin (tail.itv);
+				if (slot < rz_vector_len (skyline_vec)) {
+					rz_vector_insert (skyline_vec, slot, &tail);
+				} else {
+					rz_vector_push (skyline_vec, &tail);
+				}
+			}
+			prev_part->itv.size = rz_itv_begin (new_part.itv) - rz_itv_begin (prev_part->itv);
+		}
+	}
+	if (part) {
+		while (part && rz_itv_include (new_part.itv, part->itv)) {
+			// Remove `part` that fits in `new_part`
+			rz_vector_remove_at (skyline_vec, slot, NULL);
+			part = slot < rz_vector_len (skyline_vec) ? rz_vector_index_ptr (skyline_vec, slot) : NULL;
+		}
+		if (part && rz_itv_overlap (new_part.itv, part->itv)) {
+			// Chop start of last `part` that intersects `new_part`
+			const ut64 oaddr = rz_itv_begin (part->itv);
+			part->itv.addr = new_part_end;
+			part->itv.size -= rz_itv_begin (part->itv) - oaddr;
+		}
+	}
+	if (slot < rz_vector_len (skyline_vec)) {
+		rz_vector_insert (skyline_vec, slot, &new_part);
+	} else {
+		rz_vector_push (skyline_vec, &new_part);
+	}
+	return true;
+}
+
+RZ_API const RzSkylineItem *rz_skyline_get_item_intersect(RzSkyline *skyline, ut64 addr, ut64 len) {
+	rz_return_val_if_fail (skyline, NULL);
+	RzVector *skyline_vec = &skyline->v;
+	size_t i, l = rz_vector_len (skyline_vec);
+	rz_vector_lower_bound (skyline_vec, addr, i, CMP_END_GTE_PART);
+	if (i == l) {
+		return false;
+	}
+	const RzSkylineItem *item = rz_vector_index_ptr (skyline_vec, i);
+	return item->itv.addr <= addr + len ? item : NULL;
+}