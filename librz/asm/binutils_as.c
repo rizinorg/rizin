radareorg/radare2

Implement ppc assembler plugin ##port ##refactor (#17923)
* implement ppc assembler plugin
* reuse code
* update .travis.yml & Dockerfile
* support arm.as big endian

libr/asm/binutils_as.c
@@ -1,0 +1,88 @@
+/* radare - LGPL - Copyright 2009-2020 - pancake, eagleoflqj */
+
+#include "binutils_as.h"
+
+int binutils_assemble(RAsm *a, RAsmOp *op, const char *buf, const char *as, const char *env, const char *header, const char *cmd_opt) {
+	char *user_as = r_sys_getenv (env);
+	if (user_as) {
+		as = user_as;
+	}
+	if (R_STR_ISEMPTY (as)) {
+		eprintf ("Please set %s env to define a %s assembler program\n", env, a->cur->arch);
+		return 1;
+	}
+
+	char *ipath, *opath;
+	int ifd = r_file_mkstemp ("r_as", &ipath);
+	if (ifd == -1) {
+		free (user_as);
+		return -1;
+	}
+	int ofd = r_file_mkstemp ("r_as", &opath);
+	if (ofd == -1) {
+		free (user_as);
+		free (ipath);
+		close (ifd);
+		return -1;
+	}
+
+	int res = -1;
+	char *asm_buf = r_str_newf ("%s"
+		".ascii \"   BEGINMARK\"\n" // 4 bit align
+		"%s\n"
+		".ascii \"ENDMARK\"\n",
+		header, buf);
+	if (!asm_buf) {
+		goto beach;
+	}
+	const size_t asm_buf_len = strlen (asm_buf);
+	const bool success = write (ifd, asm_buf, asm_buf_len) == asm_buf_len;
+	free (asm_buf);
+	if (!success) {
+		goto beach;
+	}
+
+	char cmd[4096];
+	snprintf (cmd, sizeof(cmd), "%s %s %s -o %s", as, cmd_opt, ipath, opath);
+	if (!r_sys_cmd (cmd)) {
+		int len = 0;
+		const ut8 *begin, *end;
+		close (ofd);
+		ofd = r_sandbox_open (opath, O_BINARY | O_RDONLY, 0644);
+		if (ofd < 0) {
+			goto skip_ofd;
+		}
+		ut8 obuf[4096];
+		len = read (ofd, obuf, sizeof (obuf));
+		begin = r_mem_mem (obuf, len, (const ut8*)"BEGINMARK", 9);
+		end = r_mem_mem (obuf, len, (const ut8*)"ENDMARK", 7);
+		if (!begin || !end) {
+			eprintf ("Cannot find water marks\n");
+			len = 0;
+		} else {
+			len = (int)(size_t)(end - begin - 9);
+			if (len > 0) {
+				r_strbuf_setbin (&op->buf, begin + 9, len);
+			} else {
+				len = 0;
+			}
+		}
+		res = op->size = len;
+	} else {
+		eprintf ("Error running: %s", cmd);
+	}
+
+beach:
+	close (ofd);
+skip_ofd:
+	close (ifd);
+
+	unlink (ipath);
+	unlink (opath);
+
+	free (ipath);
+	free (opath);
+	free (user_as);
+
+	return res;
+}

libr/asm/binutils_as.h
@@ -1,0 +1,9 @@
+#ifndef BINUTILS_AS_H
+#define BINUTILS_AS_H
+
+#include <r_types.h>
+#include <r_asm.h>
+
+int binutils_assemble(RAsm *a, RAsmOp *op, const char *buf, const char *as, const char *env, const char *header, const char *cmd_opt);
+
+#endif

libr/asm/p/asm_arm_as.c
@@ -1,116 +1,39 @@
 /* radare - LGPL - Copyright 2015-2020 pancake */
 
-#include <r_types.h>
-#include <r_util.h>
 #include <r_lib.h>
-#include <r_asm.h>
+#include "../binutils_as.h"
 
-#ifndef O_BINARY
-#define O_BINARY 0
-#endif
-
-// USE ARM_AS environment variable
-#define ARM32_AS "arm-linux-androideabi-as"
-#define ARM64_AS "aarch64-linux-android-as"
-// toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-arm_64/bin/
-// toolchains/aarch64-linux-android-4.9/prebuilt/darwin-arm_64/bin/
+#define ASSEMBLER32 "R2_ARM32_AS"
+#define ASSEMBLER64 "R2_ARM64_AS"
 
 static int assemble(RAsm *a, RAsmOp *op, const char *buf) {
-	const char *bitconfig = "";
-	char *ipath, *opath;
-	char *as = NULL;
-
-	int ifd = r_file_mkstemp ("r_as", &ipath);
-	if (ifd == -1) {
-		return -1;
+	int bits = a->bits;
+	char *as = "";
+#if __arm__
+	if (bits <= 32) {
+		as = "as";
 	}
-
-	int ofd = r_file_mkstemp ("r_as", &opath);
-	if (ofd == -1) {
-		free (ipath);
-		return -1;
+#elif __aarch64__
+	if (bits == 64) {
+		as = "as";
 	}
-
-	as = r_sys_getenv ("ARM_AS");
-	if (!as || !*as) {
-		free (as);
-		if (a->bits == 64) {
-			as = strdup (ARM64_AS);
-		} else {
-			as = strdup (ARM32_AS);
-		}
-	}
-	if (a->bits == 16) {
-		bitconfig = ".thumb";
-	}
-
-	char *asm_buf = r_str_newf ("%s\n" //.org 0x%"PFMT64x"\n"
-			".ascii \"BEGINMARK\"\n"
-			"%s\n"
-			".ascii \"ENDMARK\"\n",
-			bitconfig, buf); // a->pc ??
-	if (asm_buf) {
-		const size_t asm_buf_len = strlen (asm_buf);
-		const bool success = write (ifd, asm_buf, asm_buf_len) != asm_buf_len;
-		(void)close (ifd);
-		free (asm_buf);
-		if (!success) {
-			free (as);
-			free (ipath);
-			free (opath);
-			return -1;
-		}
-	}
-
-	int len = 0;
-	if (!r_sys_cmdf ("%s %s -o %s", as, ipath, opath)) {
-		const ut8 *begin, *end;
-		close (ofd);
-		ofd = r_sandbox_open (opath, O_BINARY|O_RDONLY, 0644);
-		if (ofd < 0) {
-			free (as);
-			free (ipath);
-			free (opath);
-			return -1;
-		}
-		ut8 buf[4096];
-		len = read (ofd, buf, sizeof (buf));
-		begin = r_mem_mem (buf, len, (const ut8*)"BEGINMARK", 9);
-		end = r_mem_mem (buf, len, (const ut8*)"ENDMARK", 7);
-		if (!begin || !end) {
-			eprintf ("Cannot find water marks\n");
-			len = 0;
-		} else {
-			len = (int)(size_t)(end - begin - 9);
-			if (len > 0) {
-				r_strbuf_setbin (&op->buf, begin + 9, len);
-			} else {
-				len = 0;
-			}
-		}
-	} else {
-		eprintf ("Error running: %s %s -o %s", as, ipath, opath);
-		eprintf ("export PATH=~/NDK/toolchains/arm-linux*/prebuilt/darwin-arm_64/bin\n");
-	}
-
-	close (ofd);
-
-	unlink (ipath);
-	unlink (opath);
-	free (ipath);
-	free (opath);
-	free (as);
-
-	return op->size = len;
+#endif
+	char cmd_opt[4096];
+	snprintf (cmd_opt, sizeof (cmd_opt), "%s %s",
+		bits == 16 ? "-mthumb" : "",
+		a->big_endian ? "-EB" : "-EL");
+	return binutils_assemble (a, op, buf, as,
+		bits == 64 ? ASSEMBLER64 : ASSEMBLER32,
+		bits <= 32 ? ".syntax unified\n" : "", cmd_opt);
 }
 
 RAsmPlugin r_asm_plugin_arm_as = {
 	.name = "arm.as",
-	.desc = "as ARM Assembler (use ARM_AS environment)",
+	.desc = "as ARM Assembler (use "ASSEMBLER32" and "ASSEMBLER64" environment)",
 	.arch = "arm",
 	.author = "pancake",
 	.license = "LGPL3",
-	.bits = 16|32|64,
+	.bits = 16 | 32 | 64,
 	.endian = R_SYS_ENDIAN_LITTLE | R_SYS_ENDIAN_BIG,
 	.assemble = &assemble,
 };

libr/asm/p/asm_x86_as.c
@@ -1,25 +1,17 @@
 /* radare - LGPL - Copyright 2011-2020 pancake */
 
 #include <r_lib.h>
-#include <r_asm.h>
+#include "../binutils_as.h"
+
+#define ASSEMBLER "R2_X86_AS"
 
 static int assemble(RAsm *a, RAsmOp *op, const char *buf) {
-	char *ipath, *opath;
+#if __i386__ || __x86_64__
+	const char *as = "as";
+#else
+	const char *as = "";
+#endif
 	const char *syntaxstr = "";
-	int len = 0;
-
-	int ifd = r_file_mkstemp ("r_as", &ipath);
-	if (ifd == -1) {
-		return -1;
-	}
-
-	int ofd = r_file_mkstemp ("r_as", &opath);
-	if (ofd == -1) {
-		free (ipath);
-		close (ifd);
-		return -1;
-	}
-
 	switch (a->syntax) {
 	case R_ASM_SYNTAX_INTEL:
 		syntaxstr = ".intel_syntax noprefix\n";
@@ -29,78 +21,15 @@ static int assemble(RAsm *a, RAsmOp *op, const char *buf) {
 		break;
 	}
 
-	char *asm_buf = r_str_newf (
-			"%s.code%i\n" //.org 0x%"PFMT64x"\n"
-			".ascii \"BEGINMARK\"\n"
-			"%s\n"
-			".ascii \"ENDMARK\"\n",
-			syntaxstr, a->bits, buf); // a->pc ??
-	const size_t asm_buf_len = strlen (asm_buf);
-	const bool success = write (ifd, asm_buf, asm_buf_len) == asm_buf_len;
-	close (ifd);
-	free (asm_buf);
-	if (!success) {
-		return -1;
-	}
-#if __i386__ || __x86_64__
-	const char *x86as = "as";
-#else
-	const char *x86as = "";
-#endif
-	char *user_ass = r_sys_getenv ("R2_X86AS");
-	if (user_ass) {
-		x86as = user_ass;
-	}
-	if (R_STR_ISEMPTY (x86as)) {
-		eprintf ("Please set R2_X86AS env to define an x86 assembler program\n");
-		return 1;
-	}
-	bool res = r_sys_cmdf ("%s %s -o %s", x86as, ipath, opath);
-	free (user_ass);
-	if (!res) {
-		const ut8 *begin, *end;
-		close (ofd);
-// r_sys_cmdf ("cat %s", opath);
-		ofd = r_sandbox_open (opath, O_BINARY | O_RDONLY, 0644);
-		if (ofd < 0) {
-			free (ipath);
-			free (opath);
-			return -1;
-		}
-		ut8 opbuf[512] = {0};
-		len = read (ofd, opbuf, sizeof (opbuf));
-		begin = r_mem_mem (opbuf, len, (const ut8*)"BEGINMARK", 9);
-		end = r_mem_mem (opbuf, len, (const ut8*)"ENDMARK", 7);
-		if (!begin || !end) {
-			eprintf ("Cannot find water marks\n");
-			len = 0;
-		} else {
-			len = (int)(size_t)(end - begin - 9);
-			if (len > 0) {
-				r_asm_op_set_buf (op, begin + 9, len);
-			} else {
-				len = 0;
-			}
-		}
-	} else {
-		eprintf ("Error running: as %s -o %s", ipath, opath);
-		len = 0;
-	}
-
-	close (ofd);
-
-	unlink (ipath);
-	unlink (opath);
-	free (ipath);
-	free (opath);
-
-	op->size = len;
-	return len;
+	char header[4096];
+	snprintf (header, sizeof (header), "%s.code%i\n", // .org 0x%"PFMT64x"\n"
+		syntaxstr, a->bits);
+	return binutils_assemble (a, op, buf, as, ASSEMBLER, header, "");
 }
 
 RAsmPlugin r_asm_plugin_x86_as = {
 	.name = "x86.as",
-	.desc = "Intel X86 GNU Assembler (Use R2_X86AS env)",
+	.desc = "Intel X86 GNU Assembler (Use "ASSEMBLER" env)",
 	.arch = "x86",
 	.license = "LGPL3",
 	// NOTE: 64bits is not supported on OSX's nasm :(

rizinorg/rizin

librz/asm/binutils_as.c
@@ -1,0 +1,88 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include "binutils_as.h"
+
+int binutils_assemble(RzAsm *a, RzAsmOp *op, const char *buf, const char *as, const char *env, const char *header, const char *cmd_opt) {
+	char *user_as = rz_sys_getenv (env);
+	if (user_as) {
+		as = user_as;
+	}
+	if (RZ_STR_ISEMPTY (as)) {
+		eprintf ("Please set %s env to define a %s assembler program\n", env, a->cur->arch);
+		return 1;
+	}
+
+	char *ipath, *opath;
+	int ifd = rz_file_mkstemp ("rz_as", &ipath);
+	if (ifd == -1) {
+		free (user_as);
+		return -1;
+	}
+	int ofd = rz_file_mkstemp ("rz_as", &opath);
+	if (ofd == -1) {
+		free (user_as);
+		free (ipath);
+		close (ifd);
+		return -1;
+	}
+
+	int res = -1;
+	char *asm_buf = rz_str_newf ("%s"
+		".ascii \"   BEGINMARK\"\n" // 4 bit align
+		"%s\n"
+		".ascii \"ENDMARK\"\n",
+		header, buf);
+	if (!asm_buf) {
+		goto beach;
+	}
+	const size_t asm_buf_len = strlen (asm_buf);
+	const bool success = write (ifd, asm_buf, asm_buf_len) == asm_buf_len;
+	free (asm_buf);
+	if (!success) {
+		goto beach;
+	}
+
+	char cmd[4096];
+	snprintf (cmd, sizeof(cmd), "%s %s %s -o %s", as, cmd_opt, ipath, opath);
+	if (!rz_sys_cmd (cmd)) {
+		int len = 0;
+		const ut8 *begin, *end;
+		close (ofd);
+		ofd = rz_sandbox_open (opath, O_BINARY | O_RDONLY, 0644);
+		if (ofd < 0) {
+			goto skip_ofd;
+		}
+		ut8 obuf[4096];
+		len = read (ofd, obuf, sizeof (obuf));
+		begin = rz_mem_mem (obuf, len, (const ut8*)"BEGINMARK", 9);
+		end = rz_mem_mem (obuf, len, (const ut8*)"ENDMARK", 7);
+		if (!begin || !end) {
+			eprintf ("Cannot find water marks\n");
+			len = 0;
+		} else {
+			len = (int)(size_t)(end - begin - 9);
+			if (len > 0) {
+				rz_strbuf_setbin (&op->buf, begin + 9, len);
+			} else {
+				len = 0;
+			}
+		}
+		res = op->size = len;
+	} else {
+		eprintf ("Error running: %s", cmd);
+	}
+
+beach:
+	close (ofd);
+skip_ofd:
+	close (ifd);
+
+	unlink (ipath);
+	unlink (opath);
+
+	free (ipath);
+	free (opath);
+	free (user_as);
+
+	return res;
+}

librz/asm/binutils_as.h
@@ -1,0 +1,9 @@
+#ifndef BINUTILS_AS_H
+#define BINUTILS_AS_H
+
+#include <rz_types.h>
+#include <rz_asm.h>
+
+int binutils_assemble(RzAsm *a, RzAsmOp *op, const char *buf, const char *as, const char *env, const char *header, const char *cmd_opt);
+
+#endif

librz/asm/p/asm_arm_as.c
@@ -4,113 +4,39 @@
 #include <rz_util.h>
 #include <rz_lib.h>
 #include <rz_asm.h>
+#include "../binutils_as.h"
 
-#ifndef O_BINARY
-#define O_BINARY 0
-#endif
-
-// USE ARM_AS environment variable
-#define ARM32_AS "arm-linux-androideabi-as"
-#define ARM64_AS "aarch64-linux-android-as"
-// toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-arm_64/bin/
-// toolchains/aarch64-linux-android-4.9/prebuilt/darwin-arm_64/bin/
+#define ASSEMBLER32 "RZ_ARM32_AS"
+#define ASSEMBLER64 "RZ_ARM64_AS"
 
 static int assemble(RzAsm *a, RzAsmOp *op, const char *buf) {
-	const char *bitconfig = "";
-	char *ipath, *opath;
-	char *as = NULL;
-
-	int ifd = rz_file_mkstemp ("rz_as", &ipath);
-	if (ifd == -1) {
-		return -1;
+	int bits = a->bits;
+	char *as = "";
+#if __arm__
+	if (bits <= 32) {
+		as = "as";
 	}
-
-	int ofd = rz_file_mkstemp ("rz_as", &opath);
-	if (ofd == -1) {
-		free (ipath);
-		return -1;
+#elif __aarch64__
+	if (bits == 64) {
+		as = "as";
 	}
-
-	as = rz_sys_getenv ("ARM_AS");
-	if (!as || !*as) {
-		free (as);
-		if (a->bits == 64) {
-			as = strdup (ARM64_AS);
-		} else {
-			as = strdup (ARM32_AS);
-		}
-	}
-	if (a->bits == 16) {
-		bitconfig = ".thumb";
-	}
-
-	char *asm_buf = rz_str_newf ("%s\n" //.org 0x%"PFMT64x"\n"
-			".ascii \"BEGINMARK\"\n"
-			"%s\n"
-			".ascii \"ENDMARK\"\n",
-			bitconfig, buf); // a->pc ??
-	if (asm_buf) {
-		const size_t asm_buf_len = strlen (asm_buf);
-		const bool success = write (ifd, asm_buf, asm_buf_len) != asm_buf_len;
-		(void)close (ifd);
-		free (asm_buf);
-		if (!success) {
-			free (as);
-			free (ipath);
-			free (opath);
-			return -1;
-		}
-	}
-
-	int len = 0;
-	if (!rz_sys_cmdf ("%s %s -o %s", as, ipath, opath)) {
-		const ut8 *begin, *end;
-		close (ofd);
-		ofd = rz_sandbox_open (opath, O_BINARY|O_RDONLY, 0644);
-		if (ofd < 0) {
-			free (as);
-			free (ipath);
-			free (opath);
-			return -1;
-		}
-		ut8 buf[4096];
-		len = read (ofd, buf, sizeof (buf));
-		begin = rz_mem_mem (buf, len, (const ut8*)"BEGINMARK", 9);
-		end = rz_mem_mem (buf, len, (const ut8*)"ENDMARK", 7);
-		if (!begin || !end) {
-			eprintf ("Cannot find water marks\n");
-			len = 0;
-		} else {
-			len = (int)(size_t)(end - begin - 9);
-			if (len > 0) {
-				rz_strbuf_setbin (&op->buf, begin + 9, len);
-			} else {
-				len = 0;
-			}
-		}
-	} else {
-		eprintf ("Error running: %s %s -o %s", as, ipath, opath);
-		eprintf ("export PATH=~/NDK/toolchains/arm-linux*/prebuilt/darwin-arm_64/bin\n");
-	}
-
-	close (ofd);
-
-	unlink (ipath);
-	unlink (opath);
-	free (ipath);
-	free (opath);
-	free (as);
-
-	return op->size = len;
+#endif
+	char cmd_opt[4096];
+	snprintf (cmd_opt, sizeof (cmd_opt), "%s %s",
+		bits == 16 ? "-mthumb" : "",
+		a->big_endian ? "-EB" : "-EL");
+	return binutils_assemble (a, op, buf, as,
+		bits == 64 ? ASSEMBLER64 : ASSEMBLER32,
+		bits <= 32 ? ".syntax unified\n" : "", cmd_opt);
 }
 
 RzAsmPlugin rz_asm_plugin_arm_as = {
 	.name = "arm.as",
-	.desc = "as ARM Assembler (use ARM_AS environment)",
+	.desc = "as ARM Assembler (use "ASSEMBLER32" and "ASSEMBLER64" environment)",
 	.arch = "arm",
 	.author = "pancake",
 	.license = "LGPL3",
-	.bits = 16|32|64,
+	.bits = 16 | 32 | 64,
 	.endian = RZ_SYS_ENDIAN_LITTLE | RZ_SYS_ENDIAN_BIG,
 	.assemble = &assemble,
 };

librz/asm/p/asm_x86_as.c
@@ -2,23 +2,17 @@
 
 #include <rz_lib.h>
 #include <rz_asm.h>
+#include "../binutils_as.h"
+
+#define ASSEMBLER "RZ_X86_AS"
 
 static int assemble(RzAsm *a, RzAsmOp *op, const char *buf) {
-	char *ipath, *opath;
+#if __i386__ || __x86_64__
+	const char *as = "as";
+#else
+	const char *as = "";
+#endif
 	const char *syntaxstr = "";
-	int len = 0;
-
-	int ifd = rz_file_mkstemp ("rz_as", &ipath);
-	if (ifd == -1) {
-		return -1;
-	}
-
-	int ofd = rz_file_mkstemp ("rz_as", &opath);
-	if (ofd == -1) {
-		free (ipath);
-		close (ifd);
-		return -1;
-	}
 
 	switch (a->syntax) {
 	case RZ_ASM_SYNTAX_INTEL:
@@ -29,78 +23,15 @@ static int assemble(RzAsm *a, RzAsmOp *op, const char *buf) {
 		break;
 	}
 
-	char *asm_buf = rz_str_newf (
-			"%s.code%i\n" //.org 0x%"PFMT64x"\n"
-			".ascii \"BEGINMARK\"\n"
-			"%s\n"
-			".ascii \"ENDMARK\"\n",
-			syntaxstr, a->bits, buf); // a->pc ??
-	const size_t asm_buf_len = strlen (asm_buf);
-	const bool success = write (ifd, asm_buf, asm_buf_len) == asm_buf_len;
-	close (ifd);
-	free (asm_buf);
-	if (!success) {
-		return -1;
-	}
-#if __i386__ || __x86_64__
-	const char *x86as = "as";
-#else
-	const char *x86as = "";
-#endif
-	char *user_ass = rz_sys_getenv ("RZ_X86AS");
-	if (user_ass) {
-		x86as = user_ass;
-	}
-	if (RZ_STR_ISEMPTY (x86as)) {
-		eprintf ("Please set RZ_X86AS env to define an x86 assembler program\n");
-		return 1;
-	}
-	bool res = rz_sys_cmdf ("%s %s -o %s", x86as, ipath, opath);
-	free (user_ass);
-	if (!res) {
-		const ut8 *begin, *end;
-		close (ofd);
-// rz_sys_cmdf ("cat %s", opath);
-		ofd = rz_sandbox_open (opath, O_BINARY | O_RDONLY, 0644);
-		if (ofd < 0) {
-			free (ipath);
-			free (opath);
-			return -1;
-		}
-		ut8 opbuf[512] = {0};
-		len = read (ofd, opbuf, sizeof (opbuf));
-		begin = rz_mem_mem (opbuf, len, (const ut8*)"BEGINMARK", 9);
-		end = rz_mem_mem (opbuf, len, (const ut8*)"ENDMARK", 7);
-		if (!begin || !end) {
-			eprintf ("Cannot find water marks\n");
-			len = 0;
-		} else {
-			len = (int)(size_t)(end - begin - 9);
-			if (len > 0) {
-				rz_asm_op_set_buf (op, begin + 9, len);
-			} else {
-				len = 0;
-			}
-		}
-	} else {
-		eprintf ("Error running: as %s -o %s", ipath, opath);
-		len = 0;
-	}
-
-	close (ofd);
-
-	unlink (ipath);
-	unlink (opath);
-	free (ipath);
-	free (opath);
-
-	op->size = len;
-	return len;
+	char header[4096];
+	snprintf (header, sizeof (header), "%s.code%i\n", // .org 0x%"PFMT64x"\n"
+		syntaxstr, a->bits);
+	return binutils_assemble (a, op, buf, as, ASSEMBLER, header, "");
 }
 
 RzAsmPlugin rz_asm_plugin_x86_as = {
 	.name = "x86.as",
-	.desc = "Intel X86 GNU Assembler (Use RZ_X86AS env)",
+	.desc = "Intel X86 GNU Assembler (Use "ASSEMBLER" env)",
 	.arch = "x86",
 	.license = "LGPL3",
 	// NOTE: 64bits is not supported on OSX's nasm :(