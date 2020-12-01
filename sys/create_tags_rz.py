7912502149225405b83a76aa1c95cd7e60fcd0cc
Riccardo Schirone <ret2libc@users.noreply.github.com>
2020-12-01 09:17:24 +0100

    Fix compilation when python is run through `meson runpython` (#111)
    Multiline scripts had issues on my windows system while building
    from `cmd`. I removed some "pre_sdb" scripts used to read symlinks
    because there are only two and it's probably easier to just avoid
    symlinks for better portability.
    
    Also moved a bunch of inlined script to separate files in sys/.

---
+++ sys/create_tags_rz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+""" Portable python script to create tags.rz file from a list of files """
+
+import os
+from sys import argv
+
+for fname in argv[1:]:
+    with open(fname) as f:
+        text = ' '.join(f.read().splitlines())
+    print('ft %s %s' % (os.path.basename(fname), text))
---
+++ sys/syscall_preprocessing.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+""" Portable python script to preprocess syscall/d files """
+
+import re
+import sys
+
+inf = open(sys.argv[1])
+outf = open(sys.argv[2], 'w')
+for line in inf:
+    if not line.startswith('_') and '=' in line:
+        arr = re.split('=|,', line)
+        print('%s.%s=%s' % (arr[1], arr[2], arr[0]), file=outf)
+    print(line, file=outf, end='')
+inf.close()
+outf.close()
