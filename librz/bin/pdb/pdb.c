radareorg/radare2

Set realname for PDB flags (#17967)

libr/bin/pdb/pdb.c
@@ -1374,13 +1374,9 @@ static void print_gvars(RPdb *pdb, ut64 img_base, PJ *pj, int format) {
 		gdata = (SGlobal *)r_list_iter_get (it);
 		sctn_header = r_list_get_n (pe_stream->sections_hdrs, (gdata->segment - 1));
 		if (sctn_header) {
+			char *filtered_name;
 			name = r_bin_demangle_msvc (gdata->name.name);
 			name = (name) ? name : strdup (gdata->name.name);
-			if (name && format != 'd') {
-				char *_name = name;
-				name = r_name_filter2 (_name);
-				free (_name);
-			}
 			switch (format) {
 			case 2:
 			case 'j': // JSON
@@ -1394,10 +1390,13 @@ static void print_gvars(RPdb *pdb, ut64 img_base, PJ *pj, int format) {
 			case 1:
 			case '*':
 			case 'r':
+				filtered_name = r_name_filter2 (name);
 				pdb->cb_printf ("f pdb.%s = 0x%" PFMT64x " # %d %.*s\n",
-					name,
+					filtered_name,
 					(ut64) (img_base + omap_remap ((omap) ? (omap->stream) : 0, gdata->offset + sctn_header->virtual_address)),
 					gdata->symtype, PDB_SIZEOF_SECTION_NAME, sctn_header->name);
+				pdb->cb_printf ("\"fN pdb.%s %s\"\n", filtered_name, name);
+				free (filtered_name);
 				break;
 			case 'd':
 			default:

libr/util/name.c
@@ -87,7 +87,7 @@ R_API bool r_name_filter(char *name, int maxlen) {
 
 R_API char *r_name_filter2(const char *name) {
 	size_t i;
-	while (!IS_PRINTABLE (*name)) {
+	while (!r_name_validate_char (*name)) {
 		name++;
 	}
 	char *res = strdup (name);
@@ -95,6 +95,9 @@ R_API char *r_name_filter2(const char *name) {
 		if (!r_name_validate_char (res[i])) {
 			res[i] = '_';
 		}
+	}
+	for (i--; i != 0 && res[i] == '_'; i--) {
+		res[i] = '\0';
 	}
 	return res;
 }

rizinorg/rizin

librz/bin/pdb/pdb.c
@@ -1374,13 +1374,9 @@ static void print_gvars(RzPdb *pdb, ut64 img_base, PJ *pj, int format) {
 		gdata = (SGlobal *)rz_list_iter_get (it);
 		sctn_header = rz_list_get_n (pe_stream->sections_hdrs, (gdata->segment - 1));
 		if (sctn_header) {
+			char *filtered_name;
 			name = rz_bin_demangle_msvc (gdata->name.name);
 			name = (name) ? name : strdup (gdata->name.name);
-			if (name && format != 'd') {
-				char *_name = name;
-				name = rz_name_filter2 (_name);
-				free (_name);
-			}
 			switch (format) {
 			case 2:
 			case 'j': // JSON
@@ -1394,10 +1390,13 @@ static void print_gvars(RzPdb *pdb, ut64 img_base, PJ *pj, int format) {
 			case 1:
 			case '*':
 			case 'r':
+				filtered_name = rz_name_filter2 (name);
 				pdb->cb_printf ("f pdb.%s = 0x%" PFMT64x " # %d %.*s\n",
-					name,
+					filtered_name,
 					(ut64) (img_base + omap_remap ((omap) ? (omap->stream) : 0, gdata->offset + sctn_header->virtual_address)),
 					gdata->symtype, PDB_SIZEOF_SECTION_NAME, sctn_header->name);
+				pdb->cb_printf ("\"fN pdb.%s %s\"\n", filtered_name, name);
+				free (filtered_name);
 				break;
 			case 'd':
 			default:

librz/util/name.c
@@ -87,7 +87,7 @@ RZ_API bool rz_name_filter(char *name, int maxlen) {
 
 RZ_API char *rz_name_filter2(const char *name) {
 	size_t i;
-	while (!IS_PRINTABLE (*name)) {
+	while (!rz_name_validate_char (*name)) {
 		name++;
 	}
 	char *res = strdup (name);
@@ -95,6 +95,9 @@ RZ_API char *rz_name_filter2(const char *name) {
 		if (!rz_name_validate_char (res[i])) {
 			res[i] = '_';
 		}
+	}
+	for (i--; i != 0 && res[i] == '_'; i--) {
+		res[i] = '\0';
 	}
 	return res;
 }