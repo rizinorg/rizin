ed034c3a51de6df1d3d1a0badcb0fa2641727a1c
Aswin C <realc@protonmail.com>
2020-12-07 12:10:08 +0530

    Add help for `aij` and `arj` ##anal (#17973)
    * Add internal help for `aij` and `arj`
    
    Add internal help for `aij` and `arj` under `ai?` and `ar?` respectively. Also updated test/db/json.
    
    * Add help for `pda?` and `pds?`
    
    Added help for `pdaj` under `pda` and `psdf`, `pdsb` under `pds`. Also added `pi` and `pI` under `p?`
    
    * Add help for `@v`
    
    Add help for `@v` under `@v?`

--- librz/core/cmd_print.c
+++ librz/core/cmd_print.c
@@ -157,6 +157,7 @@ static const char *help_msg_at[] = {
 	"@o:", "fd", "temporary switch to another fd",
 	"@r:", "reg", "tmp seek to reg value (f.ex pd@r:PC)",
 	"@s:", "string", "same as above but from a string",
+	"@v:", "value", "modify the current offset to a custom value",
 	"@x:", "909192", "from hex pairs string",
 	"@@=", "1 2 3", "run the previous command at offsets 1, 2 and 3",
 	"@@", " hit*", "run the command on every flag matching 'hit*'",
@@ -237,6 +238,8 @@ static const char *help_msg_p[] = {
 	"pF", "[?][apx]", "print asn1, pkcs7 or x509",
 	"pg", "[?][x y w h] [cmd]", "create new visual gadget or print it (see pg? for details)",
 	"ph", "[?][=|hash] ([len])", "calculate hash for a block",
+	"pi", "[?][bdefrj] [num]", "print instructions",
+	"pI", "[?][iI][df] [len]", "print N instructions/bytes (f=func)",
 	"pj", "[?] [len]", "print as indented JSON",
 	"pm", "[?] [magic]", "print libmagic data (see pm? and /m?)",
 	"po", "[?] hex", "print operation applied to block (see po?)",
@@ -310,7 +313,7 @@ static const char *help_msg_pd[] = {
 	"pd", " -N", "disassemble N instructions backward",
 	"pd", " N", "disassemble N instructions",
 	"pd--", "[n]", "context disassembly of N instructions",
-	"pda", "", "disassemble all possible opcodes (byte per byte)",
+	"pda", "[?]", "disassemble all possible opcodes (byte per byte)",
 	"pdb", "", "disassemble basic block",
 	"pdC", "", "show comments found in N instructions",
 	"pde", "[q|qq|j] [N]", "disassemble N instructions following execution flow from current PC",
@@ -327,6 +330,19 @@ static const char *help_msg_pd[] = {
 	"pds", "[?]", "disassemble summary (strings, calls, jumps, refs) (see pdsf and pdfs)",
 	"pdt", " [n] [query]", "disassemble N instructions in a table (see dtd for debug traces)",
 	"pdx", " [hex]", "alias for pad or pix",
+	NULL
+};
+
+static const char *help_msg_pda[] = {
+	"Usage:", "pda[j]", "Print disassembly of all possbile opcodes",
+	"pdaj", "", "Display the disassembly of all possbile opcodes (byte per byte) in JSON",
+	NULL
+};
+
+static const char *help_msg_pds[] = {
+	"Usage:", "pds[bf]", "Summarize N bytes or function",
+	"pdsf", "", "Summarize the current function",
+	"pdsb", "", "Summarize N bytes",
 	NULL
 };
 
@@ -5234,6 +5250,10 @@ RZ_IPI int rz_cmd_print(void *data, const char *input) {
 			break;
 		case 'a': // "pda"
 			processed_cmd = true;
+			if (input[2] == '?') {
+				rz_core_cmd_help (core, help_msg_pda);
+				break;
+			}
 			rz_core_print_disasm_all (core, core->offset, l, len, input[2]);
 			pd_result = true;
 			break;
@@ -5315,7 +5335,7 @@ RZ_IPI int rz_cmd_print(void *data, const char *input) {
 		case 's': // "pds" and "pdsf"
 			processed_cmd = true;
 			if (input[2] == '?') {
-				rz_cons_printf ("Usage: pds[bf]  - sumarize N bytes or function (pdfs)\n");
+				rz_core_cmd_help (core, help_msg_pds);
 			} else {
 				disasm_strings (core, input, NULL);
 			}
@@ -5323,7 +5343,7 @@ RZ_IPI int rz_cmd_print(void *data, const char *input) {
 		case 'f': // "pdf"
 			processed_cmd = true;
 			if (input[2] == '?') {
-				rz_core_cmd_help(core, help_msg_pdf);
+				rz_core_cmd_help (core, help_msg_pdf);
 			} else if (input[2] == 's') { // "pdfs"
 				ut64 oseek = core->offset;
 				int oblock = core->blocksize;
