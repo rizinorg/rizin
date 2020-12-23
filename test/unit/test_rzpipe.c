a3bfa7e3f9a315b4ff3456672b345763950b6887
Riccardo Schirone <sirmy15@gmail.com>
2020-12-23 11:57:32 +0100

    Disable the entire test_rzpipe on Windows
    Mark also the other rzpipe test as broken

--- test/unit/test_rzpipe.c
+++ test/unit/test_rzpipe.c
@@ -3,21 +3,26 @@
 #include "minunit.h"
 
 static bool test_rzpipe(void) {
-#if __WINDOWS__
-	mu_test_status = MU_TEST_BROKEN;
-#endif
+#ifndef __WINDOWS__
 	RzPipe *r = rzpipe_open ("rizin -q0 -");
 	mu_assert ("rzpipe can spawn", r);
 	char *hello = rzpipe_cmd (r, "?e hello world");
 	mu_assert_streq (hello, "hello world\n", "rzpipe hello world");
 	free (hello);
 	rzpipe_close (r);
+#else
+	mu_test_status = MU_TEST_BROKEN;
+#endif
 	mu_end;
 }
 
 static bool test_rzpipe_404(void) {
+#ifndef __WINDOWS__
 	RzPipe *r = rzpipe_open ("ricin -q0 -");
 	mu_assert ("rzpipe can spawn", !r);
+#else
+	mu_test_status = MU_TEST_BROKEN;
+#endif
 	mu_end;
 }
 
