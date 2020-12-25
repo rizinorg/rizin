radareorg/radare2

Use pj in pdb_downloader ##refactor (#18084)

libr/bin/pdb/pdb_downloader.c
@@ -200,12 +200,19 @@ int r_bin_pdb_download(RCore *core, int isradjson, int *actions_done, SPDBOption
 
 	init_pdb_downloader (&opt, &pdb_downloader);
 	ret = pdb_downloader.download ? pdb_downloader.download (&pdb_downloader) : 0;
-	if (isradjson && actions_done) {
-		//TODO PJ
-		printf ("%s\"pdb\":{\"file\":\"%s\",\"download\":%s}",
-		        *actions_done ? "," : "", opt.dbg_file, ret ? "true" : "false");
+	if (isradjson) {
+		PJ *pj = pj_new ();
+		if (!pj) {
+			return 1;
+		}
+		pj_ko (pj, "pdb");
+		pj_ks (pj, "file", opt.dbg_file);
+		pj_kb (pj, "download", (bool) ret);
+		pj_end (pj);
+		r_cons_printf ("%s%s", actions_done && *actions_done ? "," : "", pj_string (pj));
+		pj_free (pj);
 	} else {
-		printf ("PDB \"%s\" download %s\n",
+		r_cons_printf ("PDB \"%s\" download %s\n",
 		        opt.dbg_file, ret ? "success" : "failed");
 	}
 	if (actions_done) {

libr/core/cmd_info.c
@@ -834,7 +834,7 @@ static int cmd_info(void *data, const char *input) {
 					int r = 1;
 					r_list_foreach (server_l, it, server) {
 						pdbopts.symbol_server = server;
-						r = r_bin_pdb_download (core, 0, NULL, &pdbopts);
+						r = r_bin_pdb_download (core, input[3] == 'j', NULL, &pdbopts);
 						if (!r) {
 							break;
 						}
@@ -1243,8 +1243,7 @@ static int cmd_info(void *data, const char *input) {
 done:
 	if (is_array && !is_izzzj) {
 		r_cons_printf ("}\n");
-	}
-	if (newline) {
+	} else if (newline) {
 		r_cons_newline ();
 	}
 redone:

rizinorg/rizin

librz/bin/pdb/pdb_downloader.c
@@ -200,11 +200,19 @@ int rz_bin_pdb_download(RzCore *core, int isradjson, int *actions_done, SPDBOpti
 
 	init_pdb_downloader (&opt, &pdb_downloader);
 	ret = pdb_downloader.download ? pdb_downloader.download (&pdb_downloader) : 0;
-	if (isradjson && actions_done) {
-		printf ("%s\"pdb\":{\"file\":\"%s\",\"download\":%s}",
-		        *actions_done ? "," : "", opt.dbg_file, ret ? "true" : "false");
+	if (isradjson) {
+		PJ *pj = pj_new ();
+		if (!pj) {
+			return 1;
+		}
+		pj_ko (pj, "pdb");
+		pj_ks (pj, "file", opt.dbg_file);
+		pj_kb (pj, "download", (bool) ret);
+		pj_end (pj);
+		rz_cons_printf ("%s%s", actions_done && *actions_done ? "," : "", pj_string (pj));
+		pj_free (pj);
 	} else {
-		printf ("PDB \"%s\" download %s\n",
+		rz_cons_printf ("PDB \"%s\" download %s\n",
 		        opt.dbg_file, ret ? "success" : "failed");
 	}
 	if (actions_done) {

librz/core/cmd_info.c
@@ -766,7 +766,7 @@ RZ_IPI int rz_cmd_info(void *data, const char *input) {
 					int r = 1;
 					rz_list_foreach (server_l, it, server) {
 						pdbopts.symbol_server = server;
-						r = rz_bin_pdb_download (core, 0, NULL, &pdbopts);
+						r = rz_bin_pdb_download (core, input[3] == 'j', NULL, &pdbopts);
 						if (!r) {
 							break;
 						}
@@ -1171,8 +1171,7 @@ RZ_IPI int rz_cmd_info(void *data, const char *input) {
 done:
 	if (is_array && !is_izzzj) {
 		rz_cons_printf ("}\n");
-	}
-	if (newline) {
+	} else if (newline) {
 		rz_cons_newline ();
 	}
 redone: