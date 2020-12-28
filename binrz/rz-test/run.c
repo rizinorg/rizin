cf5334773037b0403d5a9603305499d0ec42780a
yossizap <yossizap@gmail.com>
2020-12-28 10:04:35 +0000

    Added support for regex in test output ##test (#269)
    * Added REGEX/_ERR to enable regex in EXPECT/_ERR ##test
    * Modify output based on REGEXP_OUT/ERR and compare with EXPECT/ERR ##test

--- binrz/rz-test/run.c
+++ binrz/rz-test/run.c
@@ -165,16 +165,30 @@ RZ_API RzSubprocessOutput *rz_test_run_cmd_test(RzTestRunConfig *config, RzCmdTe
 	return out;
 }
 
+RZ_API bool rz_test_cmp_cmd_output(const char *output, const char *expect, const char *regexp) {
+	if (regexp) {
+		RzList *matches = rz_regex_get_match_list (regexp, "en", output);
+		const char *match = rz_list_to_str (matches, '\0');
+		bool equal = (0 == strcmp (expect, match));
+		rz_list_free (matches);
+		RZ_FREE (match);
+		return equal;
+	}
+	return (0 == strcmp (expect, output));
+}
+
 RZ_API bool rz_test_check_cmd_test(RzSubprocessOutput *out, RzCmdTest *test) {
 	if (!out || out->ret != 0 || !out->out || !out->err || out->timeout) {
 		return false;
 	}
 	const char *expect_out = test->expect.value;
-	if (expect_out && strcmp (out->out, expect_out) != 0) {
+	const char *regexp_out = test->regexp_out.value;
+	if (expect_out && !rz_test_cmp_cmd_output (out->out, expect_out, regexp_out)) {
 		return false;
 	}
 	const char *expect_err = test->expect_err.value;
-	if (expect_err && strcmp (out->err, expect_err) != 0) {
+	const char *regexp_err = test->regexp_err.value;
+	if (expect_err && !rz_test_cmp_cmd_output (out->err, expect_err, regexp_err)) {
 		return false;
 	}
 	return true;
