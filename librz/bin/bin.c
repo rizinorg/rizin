radareorg/radare2

Use pj in cmd_info.c ##refactor

libr/bin/bin.c
@@ -480,21 +480,15 @@ R_API void r_bin_free(RBin *bin) {
 	}
 }
 
-static bool r_bin_print_plugin_details(RBin *bin, RBinPlugin *bp, int json) {
+static bool r_bin_print_plugin_details(RBin *bin, RBinPlugin *bp, PJ *pj, int json) {
 	if (json == 'q') {
 		bin->cb_printf ("%s\n", bp->name);
 	} else if (json) {
-		PJ *pj = pj_new ();
-		if (!pj) {
-			return false;
-		}
 		pj_o (pj);
 		pj_ks (pj, "name", bp->name);
 		pj_ks (pj, "description", bp->desc);
 		pj_ks (pj, "license", r_str_get_fail (bp->license, "???"));
 		pj_end (pj);
-		bin->cb_printf ("%s\n", pj_string (pj));
-		pj_free (pj);
 	} else {
 		bin->cb_printf ("Name: %s\n", bp->name);
 		bin->cb_printf ("Description: %s\n", bp->desc);
@@ -535,7 +529,7 @@ static void __printXtrPluginDetails(RBin *bin, RBinXtrPlugin *bx, int json) {
 	}
 }
 
-R_API bool r_bin_list_plugin(RBin *bin, const char* name, int json) {
+R_API bool r_bin_list_plugin(RBin *bin, const char* name, PJ *pj, int json) {
 	RListIter *it;
 	RBinPlugin *bp;
 	RBinXtrPlugin *bx;
@@ -546,7 +540,7 @@ R_API bool r_bin_list_plugin(RBin *bin, const char* name, int json) {
 		if (!r_str_cmp (name, bp->name, strlen (name))) {
 			continue;
 		}
-		return r_bin_print_plugin_details (bin, bp, json);
+		return r_bin_print_plugin_details (bin, bp, pj, json);
 	}
 	r_list_foreach (bin->binxtrs, it, bx) {
 		if (!r_str_cmp (name, bx->name, strlen (name))) {
@@ -560,7 +554,7 @@ R_API bool r_bin_list_plugin(RBin *bin, const char* name, int json) {
 	return false;
 }
 
-R_API void r_bin_list(RBin *bin, int format) {
+R_API void r_bin_list(RBin *bin, PJ *pj, int format) {
 	RListIter *it;
 	RBinPlugin *bp;
 	RBinXtrPlugin *bx;
@@ -574,10 +568,6 @@ R_API void r_bin_list(RBin *bin, int format) {
 			bin->cb_printf ("%s\n", bx->name);
 		}
 	} else if (format) {
-		PJ *pj = pj_new ();
-		if (!pj) {
-			return;
-		}
 		pj_o (pj);
 		pj_ka (pj, "bin");
 		r_list_foreach (bin->plugins, it, bp) {
@@ -607,8 +597,6 @@ R_API void r_bin_list(RBin *bin, int format) {
 		}
 		pj_end (pj);
 		pj_end (pj);
-		bin->cb_printf ("%s\n", pj_string (pj));
-		pj_free (pj);
 	} else {
 		r_list_foreach (bin->plugins, it, bp) {
 			bin->cb_printf ("bin  %-11s %s (%s) %s %s\n",
@@ -959,20 +947,15 @@ R_API bool r_bin_select_bfid (RBin *bin, ut32 bf_id) {
 	return bf? r_bin_file_set_obj (bin, bf, NULL): false;
 }
 
-static void list_xtr_archs(RBin *bin, int mode) {
+static void list_xtr_archs(RBin *bin, PJ *pj, int mode) {
 	RBinFile *binfile = r_bin_cur (bin);
 	if (binfile->xtr_data) {
 		RListIter *iter_xtr;
 		RBinXtrData *xtr_data;
 		int bits, i = 0;
 		char *arch, *machine;
-		PJ *pj;
 
 		if (mode == 'j') {
-			pj = pj_new ();
-			if (!pj) {
-				return;
-			}
 			pj_ka (pj, "bins");
 		}
 
@@ -1010,13 +993,11 @@ static void list_xtr_archs(RBin *bin, int mode) {
 
 		if (mode == 'j') {
 			pj_end (pj);
-			bin->cb_printf ("%s", pj_string (pj));
-			pj_free (pj);
 		}
 	}
 }
 
-R_API void r_bin_list_archs(RBin *bin, int mode) {
+R_API void r_bin_list_archs(RBin *bin, PJ *pj, int mode) {
 	r_return_if_fail (bin);
 
 	char unk[128];
@@ -1027,7 +1008,7 @@ R_API void r_bin_list_archs(RBin *bin, int mode) {
 
 	//are we with xtr format?
 	if (binfile && binfile->curxtr) {
-		list_xtr_archs (bin, mode);
+		list_xtr_archs (bin, pj, mode);
 		return;
 	}
 	Sdb *binfile_sdb = binfile? binfile->sdb: NULL;
@@ -1046,12 +1027,7 @@ R_API void r_bin_list_archs(RBin *bin, int mode) {
 	}
 	RTable *table = r_table_new ();
 	const char *fmt = "dXnss";
-	PJ *pj;
 	if (mode == 'j') {
-		pj = pj_new ();
-		if (!pj) {
-			return;
-		}
 		pj_ka (pj, "bins");
 	}
 	RBinObject *obj = nbinfile->o;
@@ -1161,11 +1137,6 @@ R_API void r_bin_list_archs(RBin *bin, int mode) {
 	}
 	if (mode == 'j') {
 		pj_end (pj);
-		const char *s = pj_string (pj);
-		if (s) {
-			bin->cb_printf ("%s", s);
-		}
-		pj_free (pj);
 	}
 	r_table_free (table);
 }

libr/bin/pdb/pdb_downloader.c
@@ -170,7 +170,7 @@ static bool is_valid_guid(const char *guid) {
 	return i >= 33; // len of GUID and age
 }
 
-int r_bin_pdb_download(RCore *core, int isradjson, int *actions_done, SPDBOptions *options) {
+int r_bin_pdb_download(RCore *core, PJ *pj, int isradjson, SPDBOptions *options) {
 	int ret;
 	SPDBDownloaderOpt opt;
 	SPDBDownloader pdb_downloader;
@@ -201,22 +201,13 @@ int r_bin_pdb_download(RCore *core, int isradjson, int *actions_done, SPDBOption
 	init_pdb_downloader (&opt, &pdb_downloader);
 	ret = pdb_downloader.download ? pdb_downloader.download (&pdb_downloader) : 0;
 	if (isradjson) {
-		PJ *pj = pj_new ();
-		if (!pj) {
-			return 1;
-		}
 		pj_ko (pj, "pdb");
 		pj_ks (pj, "file", opt.dbg_file);
 		pj_kb (pj, "download", (bool) ret);
 		pj_end (pj);
-		r_cons_printf ("%s%s", actions_done && *actions_done ? "," : "", pj_string (pj));
-		pj_free (pj);
 	} else {
 		r_cons_printf ("PDB \"%s\" download %s\n",
 		        opt.dbg_file, ret ? "success" : "failed");
-	}
-	if (actions_done) {
-		(*actions_done)++;
 	}
 	deinit_pdb_downloader (&pdb_downloader);
 

libr/bin/pdb/pdb_downloader.h
@@ -45,7 +45,7 @@ void deinit_pdb_downloader(SPDBDownloader *pdb_downloader);
 
 ///
 /// \brief download PDB file
-R_API int r_bin_pdb_download(RCore *core, int isradjson, int *actions_done, SPDBOptions *options);
+R_API int r_bin_pdb_download(RCore *core, PJ *pj, int isradjson, SPDBOptions *options);
 
 #ifdef __cplusplus
 }

libr/core/cmd_debug.c
@@ -1436,7 +1436,7 @@ static int r_debug_heap(RCore *core, const char *input) {
 	return true;
 }
 
-static bool get_bin_info(RCore *core, const char *file, ut64 baseaddr, int mode, bool symbols_only, RCoreBinFilter *filter) {
+static bool get_bin_info(RCore *core, const char *file, ut64 baseaddr, PJ *pj, int mode, bool symbols_only, RCoreBinFilter *filter) {
 	int fd;
 	if ((fd = r_io_fd_open (core->io, file, R_PERM_R, 0)) == -1) {
 		return false;
@@ -1456,7 +1456,7 @@ static bool get_bin_info(RCore *core, const char *file, ut64 baseaddr, int mode,
 	} else if (mode == R_MODE_SET || mode == R_MODE_RADARE) {
 		action &= ~R_CORE_BIN_ACC_ENTRIES & ~R_CORE_BIN_ACC_MAIN;
 	}
-	r_core_bin_info (core, action, mode, 1, filter, NULL);
+	r_core_bin_info (core, action, pj, mode, 1, filter, NULL);
 	RBinFile *bf = r_bin_cur (core->bin);
 	r_bin_file_delete (core->bin, bf->id);
 	r_bin_file_set_cur_binfile (core->bin, obf);
@@ -1583,6 +1583,7 @@ static int cmd_debug_map(RCore *core, const char *input) {
 					symbols_only = false;
 					input++;
 				}
+				PJ *pj = NULL;
 				switch (input[1]) {
 				case 's':
 					mode = R_MODE_SET;
@@ -1592,6 +1593,10 @@ static int cmd_debug_map(RCore *core, const char *input) {
 					break;
 				case 'j':
 					mode = R_MODE_JSON;
+					pj = r_core_pj_new (core);
+					if (!pj) {
+						return false;
+					}
 					break;
 				case 'q':
 					mode = input[2] == 'q' ? input++, R_MODE_SIMPLEST : R_MODE_SIMPLE;
@@ -1645,7 +1650,7 @@ static int cmd_debug_map(RCore *core, const char *input) {
 								             file, map->size, baddr, R_SYS_DEVNULL);
 							}
 						}
-						get_bin_info (core, file, baddr, mode, symbols_only, &filter);
+						get_bin_info (core, file, baddr, pj, mode, symbols_only, &filter);
 						if (newfile) {
 							if (!r_file_rm (newfile)) {
 								eprintf ("Error when removing %s\n", newfile);
@@ -1654,9 +1659,13 @@ static int cmd_debug_map(RCore *core, const char *input) {
 						}
 					} else {
 						r_bin_set_baddr (core->bin, map->addr);
-						r_core_bin_info (core, R_CORE_BIN_ACC_SYMBOLS, (input[1]=='*'), true, &filter, NULL);
+						r_core_bin_info (core, R_CORE_BIN_ACC_SYMBOLS, pj, input[1] == '*', true, &filter, NULL);
 						r_bin_set_baddr (core->bin, baddr);
 					}
+				}
+				if (mode == R_MODE_JSON) {
+					r_cons_println (pj_string (pj));
+					pj_free (pj);
 				}
 				free (ptr);
 			}
@@ -1688,7 +1697,7 @@ static int cmd_debug_map(RCore *core, const char *input) {
 						filter.name = (char *) closest_symbol->name;
 
 						r_bin_set_baddr (core->bin, map->addr);
-						r_core_bin_info (core, R_CORE_BIN_ACC_SYMBOLS, false, true, &filter, NULL);
+						r_core_bin_info (core, R_CORE_BIN_ACC_SYMBOLS, NULL, false, true, &filter, NULL);
 					}
 				}
 			}
@@ -5071,10 +5080,8 @@ static int cmd_debug(void *data, const char *input) {
 	case 'L': // "dL"
 		switch (input[1]) {
 		case 'q':
-			r_debug_plugin_list (core->dbg, 'q');
-			break;
 		case 'j':
-			r_debug_plugin_list (core->dbg, 'j');
+			r_debug_plugin_list (core->dbg, input[1]);
 			break;
 		case '?':
 			r_core_cmd_help (core, help_msg_dL);

rizinorg/rizin

librz/bin/bin.c
@@ -480,21 +480,15 @@ RZ_API void rz_bin_free(RzBin *bin) {
 	}
 }
 
-static bool rz_bin_print_plugin_details(RzBin *bin, RzBinPlugin *bp, int json) {
+static bool rz_bin_print_plugin_details(RzBin *bin, RzBinPlugin *bp, PJ *pj, int json) {
 	if (json == 'q') {
 		bin->cb_printf ("%s\n", bp->name);
 	} else if (json) {
-		PJ *pj = pj_new ();
-		if (!pj) {
-			return false;
-		}
 		pj_o (pj);
 		pj_ks (pj, "name", bp->name);
 		pj_ks (pj, "description", bp->desc);
 		pj_ks (pj, "license", bp->license ? bp->license : "???");
 		pj_end (pj);
-		bin->cb_printf ("%s\n", pj_string (pj));
-		pj_free (pj);
 	} else {
 		bin->cb_printf ("Name: %s\n", bp->name);
 		bin->cb_printf ("Description: %s\n", bp->desc);
@@ -535,7 +529,7 @@ static void __printXtrPluginDetails(RzBin *bin, RzBinXtrPlugin *bx, int json) {
 	}
 }
 
-RZ_API bool rz_bin_list_plugin(RzBin *bin, const char* name, int json) {
+RZ_API bool rz_bin_list_plugin(RzBin *bin, const char* name, PJ *pj, int json) {
 	RzListIter *it;
 	RzBinPlugin *bp;
 	RzBinXtrPlugin *bx;
@@ -546,7 +540,7 @@ RZ_API bool rz_bin_list_plugin(RzBin *bin, const char* name, int json) {
 		if (!rz_str_cmp (name, bp->name, strlen (name))) {
 			continue;
 		}
-		return rz_bin_print_plugin_details (bin, bp, json);
+		return rz_bin_print_plugin_details (bin, bp, pj, json);
 	}
 	rz_list_foreach (bin->binxtrs, it, bx) {
 		if (!rz_str_cmp (name, bx->name, strlen (name))) {
@@ -560,7 +554,7 @@ RZ_API bool rz_bin_list_plugin(RzBin *bin, const char* name, int json) {
 	return false;
 }
 
-RZ_API void rz_bin_list(RzBin *bin, int format) {
+RZ_API void rz_bin_list(RzBin *bin, PJ *pj, int format) {
 	RzListIter *it;
 	RzBinPlugin *bp;
 	RzBinXtrPlugin *bx;
@@ -574,10 +568,6 @@ RZ_API void rz_bin_list(RzBin *bin, int format) {
 			bin->cb_printf ("%s\n", bx->name);
 		}
 	} else if (format) {
-		PJ *pj = pj_new ();
-		if (!pj) {
-			return;
-		}
 		pj_o (pj);
 		pj_ka (pj, "bin");
 		rz_list_foreach (bin->plugins, it, bp) {
@@ -607,8 +597,6 @@ RZ_API void rz_bin_list(RzBin *bin, int format) {
 		}
 		pj_end (pj);
 		pj_end (pj);
-		bin->cb_printf ("%s\n", pj_string (pj));
-		pj_free (pj);
 	} else {
 		rz_list_foreach (bin->plugins, it, bp) {
 			bin->cb_printf ("bin  %-11s %s (%s) %s %s\n",
@@ -959,20 +947,15 @@ RZ_API bool rz_bin_select_bfid (RzBin *bin, ut32 bf_id) {
 	return bf? rz_bin_file_set_obj (bin, bf, NULL): false;
 }
 
-static void list_xtr_archs(RzBin *bin, int mode) {
+static void list_xtr_archs(RzBin *bin, PJ *pj, int mode) {
 	RzBinFile *binfile = rz_bin_cur (bin);
 	if (binfile->xtr_data) {
 		RzListIter *iter_xtr;
 		RzBinXtrData *xtr_data;
 		int bits, i = 0;
 		char *arch, *machine;
-		PJ *pj;
 
 		if (mode == 'j') {
-			pj = pj_new ();
-			if (!pj) {
-				return;
-			}
 			pj_ka (pj, "bins");
 		}
 
@@ -1010,13 +993,11 @@ static void list_xtr_archs(RzBin *bin, int mode) {
 
 		if (mode == 'j') {
 			pj_end (pj);
-			bin->cb_printf ("%s", pj_string (pj));
-			pj_free (pj);
 		}
 	}
 }
 
-RZ_API void rz_bin_list_archs(RzBin *bin, int mode) {
+RZ_API void rz_bin_list_archs(RzBin *bin, PJ *pj, int mode) {
 	rz_return_if_fail (bin);
 
 	char unk[128];
@@ -1027,7 +1008,7 @@ RZ_API void rz_bin_list_archs(RzBin *bin, int mode) {
 
 	//are we with xtr format?
 	if (binfile && binfile->curxtr) {
-		list_xtr_archs (bin, mode);
+		list_xtr_archs (bin, pj, mode);
 		return;
 	}
 	Sdb *binfile_sdb = binfile? binfile->sdb: NULL;
@@ -1046,12 +1027,7 @@ RZ_API void rz_bin_list_archs(RzBin *bin, int mode) {
 	}
 	RzTable *table = rz_table_new ();
 	const char *fmt = "dXnss";
-	PJ *pj;
 	if (mode == 'j') {
-		pj = pj_new ();
-		if (!pj) {
-			return;
-		}
 		pj_ka (pj, "bins");
 	}
 	RzBinObject *obj = nbinfile->o;
@@ -1161,10 +1137,6 @@ RZ_API void rz_bin_list_archs(RzBin *bin, int mode) {
 	}
 	if (mode == 'j') {
 		pj_end (pj);
-		const char *s = pj_string (pj);
-		if (s) {
-			bin->cb_printf ("%s", s);
-		}
 	}
 	rz_table_free (table);
 }

librz/bin/pdb/pdb_downloader.c
@@ -170,7 +170,7 @@ static bool is_valid_guid(const char *guid) {
 	return i >= 33; // len of GUID and age
 }
 
-int rz_bin_pdb_download(RzCore *core, int isradjson, int *actions_done, SPDBOptions *options) {
+int rz_bin_pdb_download(RzCore *core, PJ *pj, int isradjson, SPDBOptions *options) {
 	int ret;
 	SPDBDownloaderOpt opt;
 	SPDBDownloader pdb_downloader;
@@ -201,22 +201,13 @@ int rz_bin_pdb_download(RzCore *core, int isradjson, int *actions_done, SPDBOpti
 	init_pdb_downloader (&opt, &pdb_downloader);
 	ret = pdb_downloader.download ? pdb_downloader.download (&pdb_downloader) : 0;
 	if (isradjson) {
-		PJ *pj = pj_new ();
-		if (!pj) {
-			return 1;
-		}
 		pj_ko (pj, "pdb");
 		pj_ks (pj, "file", opt.dbg_file);
 		pj_kb (pj, "download", (bool) ret);
 		pj_end (pj);
-		rz_cons_printf ("%s%s", actions_done && *actions_done ? "," : "", pj_string (pj));
-		pj_free (pj);
 	} else {
 		rz_cons_printf ("PDB \"%s\" download %s\n",
 		        opt.dbg_file, ret ? "success" : "failed");
-	}
-	if (actions_done) {
-		(*actions_done)++;
 	}
 	deinit_pdb_downloader (&pdb_downloader);
 

librz/bin/pdb/pdb_downloader.h
@@ -45,7 +45,7 @@ void deinit_pdb_downloader(SPDBDownloader *pdb_downloader);
 
 ///
 /// \brief download PDB file
-RZ_API int rz_bin_pdb_download(RzCore *core, int isradjson, int *actions_done, SPDBOptions *options);
+RZ_API int rz_bin_pdb_download(RzCore *core, PJ *pj, int isradjson, SPDBOptions *options);
 
 #ifdef __cplusplus
 }

librz/core/cmd_debug.c
@@ -1405,7 +1405,7 @@ static int rz_debug_heap(RzCore *core, const char *input) {
 	return true;
 }
 
-static bool get_bin_info(RzCore *core, const char *file, ut64 baseaddr, int mode, bool symbols_only, RzCoreBinFilter *filter) {
+static bool get_bin_info(RzCore *core, const char *file, ut64 baseaddr, PJ *pj, int mode, bool symbols_only, RzCoreBinFilter *filter) {
 	int fd;
 	if ((fd = rz_io_fd_open (core->io, file, RZ_PERM_R, 0)) == -1) {
 		return false;
@@ -1425,7 +1425,7 @@ static bool get_bin_info(RzCore *core, const char *file, ut64 baseaddr, int mode
 	} else if (mode == RZ_MODE_SET || mode == RZ_MODE_RIZINCMD) {
 		action &= ~RZ_CORE_BIN_ACC_ENTRIES & ~RZ_CORE_BIN_ACC_MAIN;
 	}
-	rz_core_bin_info (core, action, mode, 1, filter, NULL);
+	rz_core_bin_info (core, action, pj, mode, 1, filter, NULL);
 	RzBinFile *bf = rz_bin_cur (core->bin);
 	rz_bin_file_delete (core->bin, bf->id);
 	rz_bin_file_set_cur_binfile (core->bin, obf);
@@ -1552,6 +1552,7 @@ static int cmd_debug_map(RzCore *core, const char *input) {
 					symbols_only = false;
 					input++;
 				}
+				PJ *pj = NULL;
 				switch (input[1]) {
 				case 's':
 					mode = RZ_MODE_SET;
@@ -1561,6 +1562,10 @@ static int cmd_debug_map(RzCore *core, const char *input) {
 					break;
 				case 'j':
 					mode = RZ_MODE_JSON;
+					pj = rz_core_pj_new (core);
+					if (!pj) {
+						return false;
+					}
 					break;
 				case 'q':
 					mode = input[2] == 'q' ? input++, RZ_MODE_SIMPLEST : RZ_MODE_SIMPLE;
@@ -1614,7 +1619,7 @@ static int cmd_debug_map(RzCore *core, const char *input) {
 								             file, map->size, baddr, RZ_SYS_DEVNULL);
 							}
 						}
-						get_bin_info (core, file, baddr, mode, symbols_only, &filter);
+						get_bin_info (core, file, baddr, pj, mode, symbols_only, &filter);
 						if (newfile) {
 							if (!rz_file_rm (newfile)) {
 								eprintf ("Error when removing %s\n", newfile);
@@ -1623,9 +1628,13 @@ static int cmd_debug_map(RzCore *core, const char *input) {
 						}
 					} else {
 						rz_bin_set_baddr (core->bin, map->addr);
-						rz_core_bin_info (core, RZ_CORE_BIN_ACC_SYMBOLS, (input[1]=='*'), true, &filter, NULL);
+						rz_core_bin_info (core, RZ_CORE_BIN_ACC_SYMBOLS, pj, (input[1]=='*'), true, &filter, NULL);
 						rz_bin_set_baddr (core->bin, baddr);
 					}
+				}
+				if (mode == RZ_MODE_JSON) {
+					rz_cons_println (pj_string (pj));
+					pj_free (pj);
 				}
 				free (ptr);
 			}
@@ -1657,7 +1666,7 @@ static int cmd_debug_map(RzCore *core, const char *input) {
 						filter.name = (char *) closest_symbol->name;
 
 						rz_bin_set_baddr (core->bin, map->addr);
-						rz_core_bin_info (core, RZ_CORE_BIN_ACC_SYMBOLS, false, true, &filter, NULL);
+						rz_core_bin_info (core, RZ_CORE_BIN_ACC_SYMBOLS, NULL, false, true, &filter, NULL);
 					}
 				}
 			}
@@ -5035,10 +5044,8 @@ RZ_IPI int rz_cmd_debug(void *data, const char *input) {
 	case 'L': // "dL"
 		switch (input[1]) {
 		case 'q':
-			rz_debug_plugin_list (core->dbg, 'q');
-			break;
 		case 'j':
-			rz_debug_plugin_list (core->dbg, 'j');
+			rz_debug_plugin_list (core->dbg, input[1]);
 			break;
 		case '?':
 			rz_core_cmd_help (core, help_msg_dL);