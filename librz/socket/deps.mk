fb7d7df1ec06bc79b47e0f123307714755f35bd2
Anton Kochkov <anton.kochkov@gmail.com>
2021-01-08 17:25:19 +0800

    Remove Mingw and Cygwin from ACR build

--- librz/socket/deps.mk
+++ librz/socket/deps.mk
@@ -21,12 +21,8 @@ LDFLAGS+=-lwsock32
 #LDFLAGS+=-lws2_32
 endif
 
-ifneq (,$(findstring mingw32,$(OSTYPE))$(findstring mingw64,$(OSTYPE)))
-LDFLAGS+=-lws2_32
-else
 # no libutil for android
 #ifneq (,$(findstring linux,$(OSTYPE))$(findstring android,$(OSTYPE))$(findstring bsd,$(OSTYPE)))
 ifneq (,$(findstring linux,$(OSTYPE))$(findstring bsd,$(OSTYPE)))
 LDFLAGS+=-lutil
-endif
 endif
