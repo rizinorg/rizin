radareorg/radare2

Fix and refactor the ar:// plugin ##io (#18180)
Co-authored-by: Dennis Goodlett <dennis@hurricanelabs.com>

libr/io/p/io_ar.c
@@ -20,9 +20,9 @@ static RIODesc *r_io_ar_open(RIO *io, const char *file, int rw, int mode) {
 		filename += 2;
 	}
 
-	RBuffer *b = ar_open_file (arname, filename);
-	if (b) {
-		res = r_io_desc_new (io, &r_io_plugin_ar, filename, rw, mode, b);
+	RArFp *arf = ar_open_file (arname, filename);
+	if (arf) {
+		res = r_io_desc_new (io, &r_io_plugin_ar, filename, rw, mode, arf);
 	}
 	free (url);
 	return res;
@@ -34,60 +34,46 @@ static RList *r_io_ar_open_many(RIO *io, const char *file, int rw, int mode) {
 }
 
 static ut64 r_io_ar_lseek(RIO *io, RIODesc *fd, ut64 offset, int whence) {
-	RBuffer *b;
-	ut64 seek_val = 0;
+	r_return_val_if_fail (io && fd && fd->data, -1);
 
-	if (!fd || !fd->data) {
+	RArFp *arf = (RArFp *) fd->data;
+	ut64 size = arf->end - arf->start;
+	switch (whence) {
+	case SEEK_SET:
+		io->off = R_MIN (size, offset);
+		break;
+	case SEEK_CUR:
+		io->off = R_MIN (size, io->off + offset);
+		break;
+	case SEEK_END:
+		io->off = size;
+		break;
+	default:
 		return -1;
 	}
 
-	b = fd->data;
-	seek_val = r_buf_tell (b);
-
-	switch (whence) {
-	case SEEK_SET:
-		seek_val = (r_buf_size (b) < offset)? r_buf_size (b) : offset;
-		io->off = seek_val;
-		r_buf_seek (b, seek_val, R_BUF_SET);
-		return seek_val;
-	case SEEK_CUR:
-		seek_val = (r_buf_size (b) < offset)? r_buf_size (b) : offset;
-		io->off = seek_val;
-		r_buf_seek (b, seek_val, R_BUF_SET);
-		return seek_val;
-	case SEEK_END:
-		seek_val = r_buf_size (b);
-		io->off = seek_val;
-		r_buf_seek (b, seek_val, R_BUF_SET);
-		return seek_val;
-	}
-	return seek_val;
+	return io->off;
 }
 
 static int r_io_ar_read(RIO *io, RIODesc *fd, ut8 *buf, int count) {
-	RBuffer *b;
 	if (!fd || !fd->data || !buf) {
 		return -1;
 	}
-	b = fd->data;
-	return ar_read_at (b, io->off, buf, count);
+	return ar_read_at ((RArFp *) fd->data, io->off, buf, count);
 }
 
 static int r_io_ar_write(RIO *io, RIODesc *fd, const ut8 *buf, int count) {
-	RBuffer *b = NULL;
 	if (!fd || !fd->data || !buf) {
 		return -1;
 	}
-	return ar_write_at (b, io->off, (void *) buf, count);
+	return ar_write_at ((RArFp *) fd->data, io->off, (void *) buf, count);
 }
 
 static int r_io_ar_close(RIODesc *fd) {
-	RBuffer *b = NULL;
 	if (!fd || !fd->data) {
 		return -1;
 	}
-	b = fd->data;
-	return ar_close (b);
+	return ar_close ((RArFp *) fd->data);
 }
 
 RIOPlugin r_io_plugin_ar = {

shlr/ar/ar.h
@@ -2,16 +2,18 @@
 #ifndef _AR_H
 #define _AR_H
 
+typedef struct RARFP {
+	char *name;
+	ut64 start;
+	ut64 end;
+	RBuffer *buf;
+	ut32 *refcount;
+} RArFp;
+
 /* Offset passed is always the real io->off of the inspected file,
  * the functions automatically translate it to relative offset within the archive */
-R_API RBuffer *ar_open_file(const char *arname, const char *filename);
-R_API int ar_close(RBuffer *b);
-R_API int ar_read_at(RBuffer *b, ut64 off, void *buf, int count);
-R_API int ar_write_at(RBuffer *b, ut64 off, void *buf, int count);
-
-int ar_read(RBuffer *b, void *dest, int len);
-int ar_read_until_slash(RBuffer *b, char *buffer, int limit);
-int ar_read_header(RBuffer *b, char *buffer);
-int ar_read_file(RBuffer *b, char *buffer, bool lookup, RList *files, const char *filename);
-int ar_read_filename_table(RBuffer *b, char *buffer, RList *files, const char *filename);
-#endif	// _AR_H
+R_API RArFp *ar_open_file(const char *arname, const char *filename);
+R_API int ar_close(RArFp *f);
+R_API int ar_read_at(RArFp *f, ut64 off, void *buf, int count);
+R_API int ar_write_at(RArFp *f, ut64 off, void *buf, int count);
+#endif // _AR_H

rizinorg/rizin

librz/io/p/io_ar.c
@@ -20,9 +20,9 @@ static RzIODesc *rz_io_ar_open(RzIO *io, const char *file, int rw, int mode) {
 		filename += 2;
 	}
 
-	RzBuffer *b = ar_open_file (arname, filename);
-	if (b) {
-		res = rz_io_desc_new (io, &rz_io_plugin_ar, filename, rw, mode, b);
+	RzArFp *arf = ar_open_file (arname, filename);
+	if (arf) {
+		res = rz_io_desc_new (io, &rz_io_plugin_ar, filename, rw, mode, arf);
 	}
 	free (url);
 	return res;
@@ -34,60 +34,46 @@ static RzList *rz_io_ar_open_many(RzIO *io, const char *file, int rw, int mode) 
 }
 
 static ut64 rz_io_ar_lseek(RzIO *io, RzIODesc *fd, ut64 offset, int whence) {
-	RzBuffer *b;
-	ut64 seek_val = 0;
+	rz_return_val_if_fail (io && fd && fd->data, -1);
 
-	if (!fd || !fd->data) {
+	RzArFp *arf = (RzArFp *) fd->data;
+	ut64 size = arf->end - arf->start;
+	switch (whence) {
+	case SEEK_SET:
+		io->off = RZ_MIN (size, offset);
+		break;
+	case SEEK_CUR:
+		io->off = RZ_MIN (size, io->off + offset);
+		break;
+	case SEEK_END:
+		io->off = size;
+		break;
+	default:
 		return -1;
 	}
 
-	b = fd->data;
-	seek_val = rz_buf_tell (b);
-
-	switch (whence) {
-	case SEEK_SET:
-		seek_val = (rz_buf_size (b) < offset)? rz_buf_size (b) : offset;
-		io->off = seek_val;
-		rz_buf_seek (b, seek_val, RZ_BUF_SET);
-		return seek_val;
-	case SEEK_CUR:
-		seek_val = (rz_buf_size (b) < offset)? rz_buf_size (b) : offset;
-		io->off = seek_val;
-		rz_buf_seek (b, seek_val, RZ_BUF_SET);
-		return seek_val;
-	case SEEK_END:
-		seek_val = rz_buf_size (b);
-		io->off = seek_val;
-		rz_buf_seek (b, seek_val, RZ_BUF_SET);
-		return seek_val;
-	}
-	return seek_val;
+	return io->off;
 }
 
 static int rz_io_ar_read(RzIO *io, RzIODesc *fd, ut8 *buf, int count) {
-	RzBuffer *b;
 	if (!fd || !fd->data || !buf) {
 		return -1;
 	}
-	b = fd->data;
-	return ar_read_at (b, io->off, buf, count);
+	return ar_read_at ((RzArFp *) fd->data, io->off, buf, count);
 }
 
 static int rz_io_ar_write(RzIO *io, RzIODesc *fd, const ut8 *buf, int count) {
-	RzBuffer *b = NULL;
 	if (!fd || !fd->data || !buf) {
 		return -1;
 	}
-	return ar_write_at (b, io->off, (void *) buf, count);
+	return ar_write_at ((RzArFp *) fd->data, io->off, (void *) buf, count);
 }
 
 static int rz_io_ar_close(RzIODesc *fd) {
-	RzBuffer *b = NULL;
 	if (!fd || !fd->data) {
 		return -1;
 	}
-	b = fd->data;
-	return ar_close (b);
+	return ar_close ((RzArFp *) fd->data);
 }
 
 RzIOPlugin rz_io_plugin_ar = {

shlr/ar/ar.h
@@ -2,16 +2,18 @@
 #ifndef _AR_H
 #define _AR_H
 
+typedef struct RZARFP {
+	char *name;
+	ut64 start;
+	ut64 end;
+	RzBuffer *buf;
+	ut32 *refcount;
+} RzArFp;
+
 /* Offset passed is always the real io->off of the inspected file,
  * the functions automatically translate it to relative offset within the archive */
-RZ_API RzBuffer *ar_open_file(const char *arname, const char *filename);
-RZ_API int ar_close(RzBuffer *b);
-RZ_API int ar_read_at(RzBuffer *b, ut64 off, void *buf, int count);
-RZ_API int ar_write_at(RzBuffer *b, ut64 off, void *buf, int count);
-
-int ar_read(RzBuffer *b, void *dest, int len);
-int ar_read_until_slash(RzBuffer *b, char *buffer, int limit);
-int ar_read_header(RzBuffer *b, char *buffer);
-int ar_read_file(RzBuffer *b, char *buffer, bool lookup, RzList *files, const char *filename);
-int ar_read_filename_table(RzBuffer *b, char *buffer, RzList *files, const char *filename);
-#endif	// _AR_H
+RZ_API RzArFp *ar_open_file(const char *arname, const char *filename);
+RZ_API int ar_close(RzArFp *f);
+RZ_API int ar_read_at(RzArFp *f, ut64 off, void *buf, int count);
+RZ_API int ar_write_at(RzArFp *f, ut64 off, void *buf, int count);
+#endif // _AR_H