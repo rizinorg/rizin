07c5c7a5105aef3a5623dce2f2b07bab1b9961f3
Riccardo Schirone <sirmy15@gmail.com>
2021-01-08 15:23:02 +0100

    Use rz_buf_size and pass permissions to ar_open_file
    * Extract rz_sys_open_perms to convert from rizin permissions to posix

--- shlr/ar/ar.h
+++ shlr/ar/ar.h
@@ -12,7 +12,7 @@ typedef struct RZARFP {
 
 /* Offset passed is always the real io->off of the inspected file,
  * the functions automatically translate it to relative offset within the archive */
-RZ_API RzArFp *ar_open_file(const char *arname, const char *filename);
+RZ_API RzArFp *ar_open_file(const char *arname, int perm, const char *filename);
 RZ_API int ar_close(RzArFp *f);
 RZ_API int ar_read_at(RzArFp *f, ut64 off, void *buf, int count);
 RZ_API int ar_write_at(RzArFp *f, ut64 off, void *buf, int count);
