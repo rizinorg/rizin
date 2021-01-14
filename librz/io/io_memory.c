radareorg/radare2

Reuse code in memory-based io plugins ##refactor

libr/io/io_memory.c
@@ -1,0 +1,144 @@
+/* radare - LGPL - Copyright 2008-2020 - pancake */
+
+#include "io_memory.h"
+
+static inline ut32 _io_malloc_sz(RIODesc *desc) {
+	if (!desc) {
+		return 0;
+	}
+	RIOMalloc *mal = (RIOMalloc*)desc->data;
+	return mal? mal->size: 0;
+}
+
+static inline void _io_malloc_set_sz(RIODesc *desc, ut32 sz) {
+	if (!desc) {
+		return;
+	}
+	RIOMalloc *mal = (RIOMalloc*)desc->data;
+	if (mal) {
+		mal->size = sz;
+	}
+}
+
+static inline ut8* _io_malloc_buf(RIODesc *desc) {
+	if (!desc) {
+		return NULL;
+	}
+	RIOMalloc *mal = (RIOMalloc*)desc->data;
+	return mal->buf;
+}
+
+
+static inline ut8* _io_malloc_set_buf(RIODesc *desc, ut8* buf) {
+	if (!desc) {
+		return NULL;
+	}
+	RIOMalloc *mal = (RIOMalloc*)desc->data;
+	return mal->buf = buf;
+}
+
+static inline ut64 _io_malloc_off(RIODesc *desc) {
+	if (!desc) {
+		return 0;
+	}
+	RIOMalloc *mal = (RIOMalloc*)desc->data;
+	return mal->offset;
+}
+
+static inline void _io_malloc_set_off(RIODesc *desc, ut64 off) {
+	if (!desc) {
+		return;
+	}
+	RIOMalloc *mal = (RIOMalloc*)desc->data;
+	mal->offset = off;
+}
+
+int io_memory_write(RIO *io, RIODesc *fd, const ut8 *buf, int count) {
+	if (!fd || !buf || count < 0 || !fd->data) {
+		return -1;
+	}
+	if (_io_malloc_off (fd) > _io_malloc_sz (fd)) {
+		return -1;
+	}
+	if (_io_malloc_off (fd) + count > _io_malloc_sz (fd)) {
+		count -= (_io_malloc_off (fd) + count -_io_malloc_sz (fd));
+	}
+	if (count > 0) {
+		memcpy (_io_malloc_buf (fd) + _io_malloc_off (fd), buf, count);
+		_io_malloc_set_off (fd, _io_malloc_off (fd) + count);
+		return count;
+	}
+	return -1;
+}
+
+bool io_memory_resize(RIO *io, RIODesc *fd, ut64 count) {
+	ut8 * new_buf = NULL;
+	if (!fd || !fd->data || count == 0) {
+		return false;
+	}
+	ut32 mallocsz = _io_malloc_sz (fd);
+	if (_io_malloc_off (fd) > mallocsz) {
+		return false;
+	}
+	new_buf = malloc (count);
+	if (!new_buf) {
+		return false;
+	}
+	memcpy (new_buf, _io_malloc_buf (fd), R_MIN (count, mallocsz));
+	if (count > mallocsz) {
+		memset (new_buf + mallocsz, 0, count - mallocsz);
+	}
+	free (_io_malloc_buf (fd));
+	_io_malloc_set_buf (fd, new_buf);
+	_io_malloc_set_sz (fd, count);
+	return true;
+}
+
+int io_memory_read(RIO *io, RIODesc *fd, ut8 *buf, int count) {
+	memset (buf, 0xff, count);
+	if (!fd || !fd->data) {
+		return -1;
+	}
+	ut32 mallocsz = _io_malloc_sz (fd);
+	if (_io_malloc_off (fd) > mallocsz) {
+		return -1;
+	}
+	if (_io_malloc_off (fd) + count >= mallocsz) {
+		count = mallocsz - _io_malloc_off (fd);
+	}
+	memcpy (buf, _io_malloc_buf (fd) + _io_malloc_off (fd), count);
+	_io_malloc_set_off (fd, _io_malloc_off (fd) + count);
+	return count;
+}
+
+int io_memory_close(RIODesc *fd) {
+	RIOMalloc *riom;
+	if (!fd || !fd->data) {
+		return -1;
+	}
+	riom = fd->data;
+	R_FREE (riom->buf);
+	R_FREE (fd->data);
+	return 0;
+}
+
+ut64 io_memory_lseek(RIO* io, RIODesc *fd, ut64 offset, int whence) {
+	ut64 r_offset = offset;
+	if (!fd || !fd->data) {
+		return offset;
+	}
+	ut32 mallocsz = _io_malloc_sz (fd);
+	switch (whence) {
+	case SEEK_SET:
+		r_offset = (offset <= mallocsz) ? offset : mallocsz;
+		break;
+	case SEEK_CUR:
+		r_offset = (_io_malloc_off (fd) + offset <= mallocsz ) ? _io_malloc_off (fd) + offset : mallocsz;
+		break;
+	case SEEK_END:
+		r_offset = _io_malloc_sz (fd);
+		break;
+	}
+	_io_malloc_set_off (fd, r_offset);
+	return r_offset;
+}

libr/io/p/io_http.c
@@ -4,100 +4,27 @@
 #include "r_lib.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include "../io_memory.h"
 
-typedef struct {
-	int fd;
-	ut8 *buf;
-	ut32 size;
-} RIOMalloc;
-
-#define RIOHTTP_FD(x) (((RIOMalloc*)(x)->data)->fd)
-#define RIOHTTP_SZ(x) (((RIOMalloc*)(x)->data)->size)
-#define RIOHTTP_BUF(x) (((RIOMalloc*)(x)->data)->buf)
-
-static int __write(RIO *io, RIODesc *fd, const ut8 *buf, int count) {
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	if (io->off + count > RIOHTTP_SZ (fd)) {
-		return -1;
-	}
-	memcpy (RIOHTTP_BUF (fd)+io->off, buf, count);
-	return count;
-}
-
-static int __read(RIO *io, RIODesc *fd, ut8 *buf, int count) {
-	unsigned int sz;
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	sz = RIOHTTP_SZ (fd);
-	if (io->off >= sz) {
-		return -1;
-	}
-	if (io->off + count >= sz) {
-		count = sz - io->off;
-	}
-	memcpy (buf, RIOHTTP_BUF (fd) + io->off, count);
-	return count;
-}
-
-static int __close(RIODesc *fd) {
-	RIOMalloc *riom;
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	riom = fd->data;
-	R_FREE (riom->buf);
-	R_FREE (fd->data);
-	return 0;
-}
-
-static ut64 __lseek(RIO *io, RIODesc *fd, ut64 offset, int whence) {
-	switch (whence) {
-	case SEEK_SET: return offset;
-	case SEEK_CUR: return io->off + offset;
-	case SEEK_END: return RIOHTTP_SZ (fd);
-	}
-	return offset;
-}
-
-static bool __plugin_open(RIO *io, const char *pathname, bool many) {
+static bool __check(RIO *io, const char *pathname, bool many) {
 	return (!strncmp (pathname, "http://", 7));
 }
 
-static inline int getmalfd (RIOMalloc *mal) {
-	return (UT32_MAX >> 1) & (int)(size_t)mal->buf;
-}
-
 static RIODesc *__open(RIO *io, const char *pathname, int rw, int mode) {
-	char *out;
-	int rlen, code;
-	if (__plugin_open (io, pathname, 0)) {
-		out = r_socket_http_get (pathname, &code, &rlen);
-		if (out) {
-			RIOMalloc *mal = R_NEW0 (RIOMalloc);
-			if (!mal) {
-				return NULL;
-			}
-			mal->size = rlen;
-			mal->buf = malloc (mal->size+1);
-			if (!mal->buf) {
-				free (mal);
-				return NULL;
-			}
-			if (mal->buf != NULL) {
-				mal->fd = getmalfd (mal);
-				memcpy (mal->buf, out, mal->size);
-				free (out);
-				return r_io_desc_new (io, &r_io_plugin_http,
-					pathname, rw, mode, mal);
-			}
-			eprintf ("Cannot allocate (%s) %d byte(s)\n", pathname+9, mal->size);
-			free (mal);
+	if (__check (io, pathname, 0)) {
+		int rlen, code;
+		RIOMalloc *mal = R_NEW0 (RIOMalloc);
+		if (!mal) {
+			return NULL;
 		}
-		free (out);
+		mal->offset = 0;
+		mal->buf = r_socket_http_get (pathname, &code, &rlen);
+		if (mal->buf && rlen > 0) {
+			mal->size = rlen;
+			return r_io_desc_new (io, &r_io_plugin_malloc, pathname, R_PERM_RW | rw, mode, mal);
+		}
+		eprintf ("No HTTP response\n");
+		free (mal);
 	}
 	return NULL;
 }
@@ -108,11 +35,11 @@ RIOPlugin r_io_plugin_http = {
 	.uris = "http://",
 	.license = "LGPL3",
 	.open = __open,
-	.close = __close,
-	.read = __read,
-	.check = __plugin_open,
-	.lseek = __lseek,
-	.write = __write,
+	.close = io_memory_close,
+	.read = io_memory_read,
+	.check = __check,
+	.lseek = io_memory_lseek,
+	.write = io_memory_write,
 };
 
 #ifndef R2_PLUGIN_INCORE

libr/io/p/io_tcp.c
@@ -4,71 +4,10 @@
 #include "r_lib.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include "../io_memory.h"
 
-typedef struct {
-	int fd;
-	ut8 *buf;
-	ut32 size;
-} RIOMalloc;
-
-#define RIOTCP_FD(x) (((RIOMalloc*)(x)->data)->fd)
-#define RIOTCP_SZ(x) (((RIOMalloc*)(x)->data)->size)
-#define RIOTCP_BUF(x) (((RIOMalloc*)(x)->data)->buf)
-
-static int __write(RIO *io, RIODesc *fd, const ut8 *buf, int count) {
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	if (io->off + count > RIOTCP_SZ (fd)) {
-		return -1;
-	}
-	memcpy (RIOTCP_BUF (fd)+io->off, buf, count);
-	return count;
-}
-
-static int __read(RIO *io, RIODesc *fd, ut8 *buf, int count) {
-	unsigned int sz;
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	sz = RIOTCP_SZ (fd);
-	if (io->off >= sz) {
-		return -1;
-	}
-	if (io->off + count >= sz) {
-		count = sz - io->off;
-	}
-	memcpy (buf, RIOTCP_BUF (fd) + io->off, count);
-	return count;
-}
-
-static int __close(RIODesc *fd) {
-	RIOMalloc *riom;
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	riom = fd->data;
-	R_FREE (riom->buf);
-	R_FREE (fd->data);
-	return 0;
-}
-
-static ut64 __lseek(RIO *io, RIODesc *fd, ut64 offset, int whence) {
-	switch (whence) {
-	case SEEK_SET: return offset;
-	case SEEK_CUR: return io->off + offset;
-	case SEEK_END: return RIOTCP_SZ (fd);
-	}
-	return offset;
-}
-
-static bool __plugin_open(RIO *io, const char *pathname, bool many) {
+static bool __check(RIO *io, const char *pathname, bool many) {
 	return (!strncmp (pathname, "tcp://", 6));
-}
-
-static inline int getmalfd (RIOMalloc *mal) {
-	return (UT32_MAX >> 1) & (int)(size_t)mal->buf;
 }
 
 static ut8 *tcpme (const char *pathname, int *code, int *len) {
@@ -124,35 +63,20 @@ static ut8 *tcpme (const char *pathname, int *code, int *len) {
 }
 
 static RIODesc *__open(RIO *io, const char *pathname, int rw, int mode) {
-	ut8 *out;
-	int rlen, code;
-	if (__plugin_open (io, pathname, 0)) {
-		out = tcpme (pathname, &code, &rlen);
-		if (out && rlen > 0) {
-			RIOMalloc *mal = R_NEW0 (RIOMalloc);
-			if (!mal) {
-				free (out);
-				return NULL;
-			}
-			mal->size = rlen;
-			mal->buf = malloc (mal->size + 1);
-			if (!mal->buf) {
-				free (mal);
-				free (out);
-				return NULL;
-			}
-			if (mal->buf != NULL) {
-				mal->fd = getmalfd (mal);
-				memcpy (mal->buf, out, mal->size);
-				free (out);
-				rw = 7;
-				return r_io_desc_new (io, &r_io_plugin_tcp,
-					pathname, rw, mode, mal);
-			}
-			eprintf ("Cannot allocate (%s) %d byte(s)\n", pathname + 9, mal->size);
-			free (mal);
+	if (__check (io, pathname, 0)) {
+		int rlen, code;
+		RIOMalloc *mal = R_NEW0 (RIOMalloc);
+		if (!mal) {
+			return NULL;
 		}
-		free (out);
+		mal->offset = 0;
+		mal->buf = tcpme (pathname, &code, &rlen);
+		if (mal->buf && rlen > 0) {
+			mal->size = rlen;
+			return r_io_desc_new (io, &r_io_plugin_tcp, pathname, rw, mode, mal);
+		}
+		eprintf ("No TCP segment\n");
+		free (mal);
 	}
 	return NULL;
 }
@@ -163,11 +87,11 @@ RIOPlugin r_io_plugin_tcp = {
 	.uris = "tcp://",
 	.license = "LGPL3",
 	.open = __open,
-	.close = __close,
-	.read = __read,
-	.check = __plugin_open,
-	.lseek = __lseek,
-	.write = __write,
+	.close = io_memory_close,
+	.read = io_memory_read,
+	.check = __check,
+	.lseek = io_memory_lseek,
+	.write = io_memory_write,
 };
 
 #ifndef R2_PLUGIN_INCORE

rizinorg/rizin

librz/io/io_memory.c
@@ -1,0 +1,144 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include "io_memory.h"
+
+static inline ut32 _io_malloc_sz(RzIODesc *desc) {
+	if (!desc) {
+		return 0;
+	}
+	RzIOMalloc *mal = (RzIOMalloc*)desc->data;
+	return mal? mal->size: 0;
+}
+
+static inline void _io_malloc_set_sz(RzIODesc *desc, ut32 sz) {
+	if (!desc) {
+		return;
+	}
+	RzIOMalloc *mal = (RzIOMalloc*)desc->data;
+	if (mal) {
+		mal->size = sz;
+	}
+}
+
+static inline ut8* _io_malloc_buf(RzIODesc *desc) {
+	if (!desc) {
+		return NULL;
+	}
+	RzIOMalloc *mal = (RzIOMalloc*)desc->data;
+	return mal->buf;
+}
+
+
+static inline ut8* _io_malloc_set_buf(RzIODesc *desc, ut8* buf) {
+	if (!desc) {
+		return NULL;
+	}
+	RzIOMalloc *mal = (RzIOMalloc*)desc->data;
+	return mal->buf = buf;
+}
+
+static inline ut64 _io_malloc_off(RzIODesc *desc) {
+	if (!desc) {
+		return 0;
+	}
+	RzIOMalloc *mal = (RzIOMalloc*)desc->data;
+	return mal->offset;
+}
+
+static inline void _io_malloc_set_off(RzIODesc *desc, ut64 off) {
+	if (!desc) {
+		return;
+	}
+	RzIOMalloc *mal = (RzIOMalloc*)desc->data;
+	mal->offset = off;
+}
+
+int io_memory_write(RzIO *io, RzIODesc *fd, const ut8 *buf, int count) {
+	if (!fd || !buf || count < 0 || !fd->data) {
+		return -1;
+	}
+	if (_io_malloc_off (fd) > _io_malloc_sz (fd)) {
+		return -1;
+	}
+	if (_io_malloc_off (fd) + count > _io_malloc_sz (fd)) {
+		count -= (_io_malloc_off (fd) + count -_io_malloc_sz (fd));
+	}
+	if (count > 0) {
+		memcpy (_io_malloc_buf (fd) + _io_malloc_off (fd), buf, count);
+		_io_malloc_set_off (fd, _io_malloc_off (fd) + count);
+		return count;
+	}
+	return -1;
+}
+
+bool io_memory_resize(RzIO *io, RzIODesc *fd, ut64 count) {
+	ut8 * new_buf = NULL;
+	if (!fd || !fd->data || count == 0) {
+		return false;
+	}
+	ut32 mallocsz = _io_malloc_sz (fd);
+	if (_io_malloc_off (fd) > mallocsz) {
+		return false;
+	}
+	new_buf = malloc (count);
+	if (!new_buf) {
+		return false;
+	}
+	memcpy (new_buf, _io_malloc_buf (fd), RZ_MIN (count, mallocsz));
+	if (count > mallocsz) {
+		memset (new_buf + mallocsz, 0, count - mallocsz);
+	}
+	free (_io_malloc_buf (fd));
+	_io_malloc_set_buf (fd, new_buf);
+	_io_malloc_set_sz (fd, count);
+	return true;
+}
+
+int io_memory_read(RzIO *io, RzIODesc *fd, ut8 *buf, int count) {
+	memset (buf, 0xff, count);
+	if (!fd || !fd->data) {
+		return -1;
+	}
+	ut32 mallocsz = _io_malloc_sz (fd);
+	if (_io_malloc_off (fd) > mallocsz) {
+		return -1;
+	}
+	if (_io_malloc_off (fd) + count >= mallocsz) {
+		count = mallocsz - _io_malloc_off (fd);
+	}
+	memcpy (buf, _io_malloc_buf (fd) + _io_malloc_off (fd), count);
+	_io_malloc_set_off (fd, _io_malloc_off (fd) + count);
+	return count;
+}
+
+int io_memory_close(RzIODesc *fd) {
+	RzIOMalloc *riom;
+	if (!fd || !fd->data) {
+		return -1;
+	}
+	riom = fd->data;
+	RZ_FREE (riom->buf);
+	RZ_FREE (fd->data);
+	return 0;
+}
+
+ut64 io_memory_lseek(RzIO* io, RzIODesc *fd, ut64 offset, int whence) {
+	ut64 rz_offset = offset;
+	if (!fd || !fd->data) {
+		return offset;
+	}
+	ut32 mallocsz = _io_malloc_sz (fd);
+	switch (whence) {
+	case SEEK_SET:
+		rz_offset = (offset <= mallocsz) ? offset : mallocsz;
+		break;
+	case SEEK_CUR:
+		rz_offset = (_io_malloc_off (fd) + offset <= mallocsz ) ? _io_malloc_off (fd) + offset : mallocsz;
+		break;
+	case SEEK_END:
+		rz_offset = _io_malloc_sz (fd);
+		break;
+	}
+	_io_malloc_set_off (fd, rz_offset);
+	return rz_offset;
+}

librz/io/p/io_http.c
@@ -4,100 +4,27 @@
 #include "rz_lib.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include "../io_memory.h"
 
-typedef struct {
-	int fd;
-	ut8 *buf;
-	ut32 size;
-} RzIOMalloc;
-
-#define RzIOHTTP_FD(x) (((RzIOMalloc*)(x)->data)->fd)
-#define RzIOHTTP_SZ(x) (((RzIOMalloc*)(x)->data)->size)
-#define RzIOHTTP_BUF(x) (((RzIOMalloc*)(x)->data)->buf)
-
-static int __write(RzIO *io, RzIODesc *fd, const ut8 *buf, int count) {
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	if (io->off + count > RzIOHTTP_SZ (fd)) {
-		return -1;
-	}
-	memcpy (RzIOHTTP_BUF (fd)+io->off, buf, count);
-	return count;
-}
-
-static int __read(RzIO *io, RzIODesc *fd, ut8 *buf, int count) {
-	unsigned int sz;
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	sz = RzIOHTTP_SZ (fd);
-	if (io->off >= sz) {
-		return -1;
-	}
-	if (io->off + count >= sz) {
-		count = sz - io->off;
-	}
-	memcpy (buf, RzIOHTTP_BUF (fd) + io->off, count);
-	return count;
-}
-
-static int __close(RzIODesc *fd) {
-	RzIOMalloc *riom;
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	riom = fd->data;
-	RZ_FREE (riom->buf);
-	RZ_FREE (fd->data);
-	return 0;
-}
-
-static ut64 __lseek(RzIO *io, RzIODesc *fd, ut64 offset, int whence) {
-	switch (whence) {
-	case SEEK_SET: return offset;
-	case SEEK_CUR: return io->off + offset;
-	case SEEK_END: return RzIOHTTP_SZ (fd);
-	}
-	return offset;
-}
-
-static bool __plugin_open(RzIO *io, const char *pathname, bool many) {
+static bool __check(RzIO *io, const char *pathname, bool many) {
 	return (!strncmp (pathname, "http://", 7));
 }
 
-static inline int getmalfd (RzIOMalloc *mal) {
-	return (UT32_MAX >> 1) & (int)(size_t)mal->buf;
-}
-
 static RzIODesc *__open(RzIO *io, const char *pathname, int rw, int mode) {
-	char *out;
-	int rlen, code;
-	if (__plugin_open (io, pathname, 0)) {
-		out = rz_socket_http_get (pathname, &code, &rlen);
-		if (out) {
-			RzIOMalloc *mal = RZ_NEW0 (RzIOMalloc);
-			if (!mal) {
-				return NULL;
-			}
-			mal->size = rlen;
-			mal->buf = malloc (mal->size+1);
-			if (!mal->buf) {
-				free (mal);
-				return NULL;
-			}
-			if (mal->buf != NULL) {
-				mal->fd = getmalfd (mal);
-				memcpy (mal->buf, out, mal->size);
-				free (out);
-				return rz_io_desc_new (io, &rz_io_plugin_http,
-					pathname, rw, mode, mal);
-			}
-			eprintf ("Cannot allocate (%s) %d byte(s)\n", pathname+9, mal->size);
-			free (mal);
+	if (__check (io, pathname, 0)) {
+		int rlen, code;
+		RzIOMalloc *mal = RZ_NEW0 (RzIOMalloc);
+		if (!mal) {
+			return NULL;
 		}
-		free (out);
+		mal->offset = 0;
+		mal->buf = rz_socket_http_get (pathname, &code, &rlen);
+		if (mal->buf && rlen > 0) {
+			mal->size = rlen;
+			return rz_io_desc_new (io, &rz_io_plugin_malloc, pathname, RZ_PERM_RW | rw, mode, mal);
+		}
+		eprintf ("No HTTP response\n");
+		free (mal);
 	}
 	return NULL;
 }
@@ -108,11 +35,11 @@ RzIOPlugin rz_io_plugin_http = {
 	.uris = "http://",
 	.license = "LGPL3",
 	.open = __open,
-	.close = __close,
-	.read = __read,
-	.check = __plugin_open,
-	.lseek = __lseek,
-	.write = __write,
+	.close = io_memory_close,
+	.read = io_memory_read,
+	.check = __check,
+	.lseek = io_memory_lseek,
+	.write = io_memory_write,
 };
 
 #ifndef RZ_PLUGIN_INCORE

librz/io/p/io_tcp.c
@@ -4,71 +4,10 @@
 #include "rz_lib.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include "../io_memory.h"
 
-typedef struct {
-	int fd;
-	ut8 *buf;
-	ut32 size;
-} RzIOMalloc;
-
-#define RzIOTCP_FD(x) (((RzIOMalloc*)(x)->data)->fd)
-#define RzIOTCP_SZ(x) (((RzIOMalloc*)(x)->data)->size)
-#define RzIOTCP_BUF(x) (((RzIOMalloc*)(x)->data)->buf)
-
-static int __write(RzIO *io, RzIODesc *fd, const ut8 *buf, int count) {
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	if (io->off + count > RzIOTCP_SZ (fd)) {
-		return -1;
-	}
-	memcpy (RzIOTCP_BUF (fd)+io->off, buf, count);
-	return count;
-}
-
-static int __read(RzIO *io, RzIODesc *fd, ut8 *buf, int count) {
-	unsigned int sz;
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	sz = RzIOTCP_SZ (fd);
-	if (io->off >= sz) {
-		return -1;
-	}
-	if (io->off + count >= sz) {
-		count = sz - io->off;
-	}
-	memcpy (buf, RzIOTCP_BUF (fd) + io->off, count);
-	return count;
-}
-
-static int __close(RzIODesc *fd) {
-	RzIOMalloc *riom;
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	riom = fd->data;
-	RZ_FREE (riom->buf);
-	RZ_FREE (fd->data);
-	return 0;
-}
-
-static ut64 __lseek(RzIO *io, RzIODesc *fd, ut64 offset, int whence) {
-	switch (whence) {
-	case SEEK_SET: return offset;
-	case SEEK_CUR: return io->off + offset;
-	case SEEK_END: return RzIOTCP_SZ (fd);
-	}
-	return offset;
-}
-
-static bool __plugin_open(RzIO *io, const char *pathname, bool many) {
+static bool __check(RzIO *io, const char *pathname, bool many) {
 	return (!strncmp (pathname, "tcp://", 6));
-}
-
-static inline int getmalfd (RzIOMalloc *mal) {
-	return (UT32_MAX >> 1) & (int)(size_t)mal->buf;
 }
 
 static ut8 *tcpme (const char *pathname, int *code, int *len) {
@@ -124,35 +63,20 @@ static ut8 *tcpme (const char *pathname, int *code, int *len) {
 }
 
 static RzIODesc *__open(RzIO *io, const char *pathname, int rw, int mode) {
-	ut8 *out;
-	int rlen, code;
-	if (__plugin_open (io, pathname, 0)) {
-		out = tcpme (pathname, &code, &rlen);
-		if (out && rlen > 0) {
-			RzIOMalloc *mal = RZ_NEW0 (RzIOMalloc);
-			if (!mal) {
-				free (out);
-				return NULL;
-			}
-			mal->size = rlen;
-			mal->buf = malloc (mal->size + 1);
-			if (!mal->buf) {
-				free (mal);
-				free (out);
-				return NULL;
-			}
-			if (mal->buf != NULL) {
-				mal->fd = getmalfd (mal);
-				memcpy (mal->buf, out, mal->size);
-				free (out);
-				rw = 7;
-				return rz_io_desc_new (io, &rz_io_plugin_tcp,
-					pathname, rw, mode, mal);
-			}
-			eprintf ("Cannot allocate (%s) %d byte(s)\n", pathname + 9, mal->size);
-			free (mal);
+	if (__check (io, pathname, 0)) {
+		int rlen, code;
+		RzIOMalloc *mal = RZ_NEW0 (RzIOMalloc);
+		if (!mal) {
+			return NULL;
 		}
-		free (out);
+		mal->offset = 0;
+		mal->buf = tcpme (pathname, &code, &rlen);
+		if (mal->buf && rlen > 0) {
+			mal->size = rlen;
+			return rz_io_desc_new (io, &rz_io_plugin_tcp, pathname, rw, mode, mal);
+		}
+		eprintf ("No TCP segment\n");
+		free (mal);
 	}
 	return NULL;
 }
@@ -163,11 +87,11 @@ RzIOPlugin rz_io_plugin_tcp = {
 	.uris = "tcp://",
 	.license = "LGPL3",
 	.open = __open,
-	.close = __close,
-	.read = __read,
-	.check = __plugin_open,
-	.lseek = __lseek,
-	.write = __write,
+	.close = io_memory_close,
+	.read = io_memory_read,
+	.check = __check,
+	.lseek = io_memory_lseek,
+	.write = io_memory_write,
 };
 
 #ifndef RZ_PLUGIN_INCORE