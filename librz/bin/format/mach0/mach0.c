radareorg/radare2

Fix issues spotted with gcc -Werror

libr/bin/format/mach0/mach0.c
@@ -2290,13 +2290,11 @@ RList *MACH0_(get_segments)(RBinFile *bf) {
 
 // XXX this function is called so many times
 struct section_t *MACH0_(get_sections)(struct MACH0_(obj_t) *bin) {
+	r_return_val_if_fail (bin, NULL);
 	struct section_t *sections;
-	char segname[32], sectname[32], raw_segname[17];
+	char segname[64], sectname[64], raw_segname[17];
 	size_t i, j, to;
 
-	if (!bin) {
-		return NULL;
-	}
 	/* for core files */
 	if (bin->nsects < 1 && bin->nsegs > 0) {
 		struct MACH0_(segment_command) *seg;
@@ -2341,11 +2339,7 @@ struct section_t *MACH0_(get_sections)(struct MACH0_(obj_t) *bin) {
 		sections[i].flags = bin->sects[i].flags;
 		r_str_ncpy (sectname, bin->sects[i].sectname, 17);
 		r_str_filter (sectname, -1);
-		// hack to support multiple sections with same name
-		// snprintf (segname, sizeof (segname), "%d", i); // wtf
-		memcpy (raw_segname, bin->sects[i].segname, 16);
-		raw_segname[16] = 0;
-		snprintf (segname, sizeof (segname), "%zd.%s", i, raw_segname);
+		r_str_ncpy (raw_segname, bin->sects[i].segname, 16);
 		for (j = 0; j < bin->nsegs; j++) {
 			if (sections[i].addr >= bin->segs[j].vmaddr &&
 				sections[i].addr < (bin->segs[j].vmaddr + bin->segs[j].vmsize)) {
@@ -2353,11 +2347,8 @@ struct section_t *MACH0_(get_sections)(struct MACH0_(obj_t) *bin) {
 				break;
 			}
 		}
-		// XXX: if two sections have the same name are merged :O
-		// XXX: append section index in flag name maybe?
-		// XXX: do not load out of bound sections?
-		// XXX: load segments instead of sections? what about PAGEZERO and ...
-		snprintf (sections[i].name, sizeof (sections[i].name), "%s.%s", segname, sectname);
+		snprintf (sections[i].name, sizeof (sections[i].name),
+			"%d.%s.%s", (int)i, raw_segname, sectname);
 		sections[i].last = 0;
 	}
 	sections[i].last = 1;
@@ -4092,7 +4083,7 @@ void MACH0_(mach_headerfields)(RBinFile *bf) {
 		}
 		case LC_MAIN:
 			{
-				ut8 data[64];
+				ut8 data[64] = {0};
 				r_buf_read_at (buf, addr, data, sizeof (data));
 #if R_BIN_MACH064
 				ut64 ep = r_read_ble64 (&data, false); //  bin->big_endian);

libr/bin/format/pyc/marshal.c
@@ -1004,7 +1004,7 @@ static pyc_object *get_object(RBuffer *buffer) {
 	pyc_object *ret = NULL;
 	ut8 code = get_ut8 (buffer, &error);
 	ut8 flag = code & FLAG_REF;
-	RListIter *ref_idx;
+	RListIter *ref_idx = NULL;
 	ut8 type = code & ~FLAG_REF;
 
 	if (error) {
@@ -1121,17 +1121,10 @@ static pyc_object *get_object(RBuffer *buffer) {
 		return NULL;
 	}
 
-	/* for debugging purpose
-    if (ret == NULL) {
-        eprintf("***%d***\n", type);
-    }
-    */
-
-	if (flag) {
+	if (flag && ref_idx) {
 		free_object (ref_idx->data);
 		ref_idx->data = copy_object (ret);
 	}
-
 	return ret;
 }
 

libr/bp/bp.c
@@ -284,7 +284,7 @@ R_API int r_bp_list(RBreakpoint *bp, int rad) {
 	int n = 0;
 	RBreakpointItem *b;
 	RListIter *iter;
-	PJ *pj;
+	PJ *pj = NULL;
 	if (rad == 'j') {
 		pj = pj_new ();
 		if (!pj) {
@@ -294,8 +294,25 @@ R_API int r_bp_list(RBreakpoint *bp, int rad) {
 	}
 	//eprintf ("Breakpoint list:\n");
 	r_list_foreach (bp->bps, iter, b) {
-		switch (rad) {
-		case 0:
+		if (pj) {
+			pj_o (pj);
+			pj_kN (pj, "addr", b->addr);
+			pj_ki (pj, "size", b->size);
+			pj_ks (pj, "perm", r_str_rwx_i (b->perm & 7)); /* filter out R_BP_PROT_ACCESS */
+			pj_kb (pj, "hw", b->hw);
+			pj_kb (pj, "trace", b->trace);
+			pj_kb (pj, "enabled", b->enabled);
+			pj_kb (pj, "valid", r_bp_is_valid (bp, b));
+			pj_ks (pj, "data", r_str_get (b->data));
+			pj_ks (pj, "cond", r_str_get (b->cond));
+			pj_end (pj);
+		} else if (rad) {
+			if (b->module_name) {
+				bp->cb_printf ("dbm %s %"PFMT64d"\n", b->module_name, b->module_delta);
+			} else {
+				bp->cb_printf ("db 0x%08"PFMT64x"\n", b->addr);
+			}
+		} else {
 			bp->cb_printf ("0x%08"PFMT64x" - 0x%08"PFMT64x \
 				" %d %c%c%c %s %s %s %s cmd=\"%s\" cond=\"%s\" " \
 				"name=\"%s\" module=\"%s\"\n",
@@ -311,38 +328,10 @@ R_API int r_bp_list(RBreakpoint *bp, int rad) {
 				r_str_get (b->cond),
 				r_str_get (b->name),
 				r_str_get (b->module_name));
-			break;
-		case 1:
-		case 'r':
-		case '*':
-			// TODO: add command, tracing, enable, ..
-			if (b->module_name) {
-			    	bp->cb_printf ("dbm %s %"PFMT64d"\n", b->module_name, b->module_delta);
-			} else {
-				bp->cb_printf ("db 0x%08"PFMT64x"\n", b->addr);
-			}
-			//b->trace? "trace": "break",
-			//b->enabled? "enabled": "disabled",
-			//r_str_get (b->data));
-			break;
-		case 'j':
-			pj_o (pj);
-			pj_kN (pj, "addr", b->addr);
-			pj_ki (pj, "size", b->size);
-			pj_ks (pj, "perm", r_str_rwx_i (b->perm & 7)); /* filter out R_BP_PROT_ACCESS */
-			pj_kb (pj, "hw", b->hw);
-			pj_kb (pj, "trace", b->trace);
-			pj_kb (pj, "enabled", b->enabled);
-			pj_kb (pj, "valid", r_bp_is_valid (bp, b));
-			pj_ks (pj, "data", r_str_get (b->data));
-			pj_ks (pj, "cond", r_str_get (b->cond));
-			pj_end (pj);
-			break;
 		}
-		/* TODO: Show list of pids and trace points, conditionals */
 		n++;
 	}
-	if (rad == 'j') {
+	if (pj) {
 		pj_end (pj);
 		bp->cb_printf ("%s\n", pj_string (pj));
 		pj_free (pj);

rizinorg/rizin

librz/bin/format/mach0/mach0.c
@@ -2290,13 +2290,11 @@ RzList *MACH0_(get_segments)(RzBinFile *bf) {
 
 // XXX this function is called so many times
 struct section_t *MACH0_(get_sections)(struct MACH0_(obj_t) *bin) {
+	rz_return_val_if_fail (bin, NULL);
 	struct section_t *sections;
-	char segname[32], sectname[32], raw_segname[17];
+	char sectname[64], raw_segname[17];
 	size_t i, j, to;
 
-	if (!bin) {
-		return NULL;
-	}
 	/* for core files */
 	if (bin->nsects < 1 && bin->nsegs > 0) {
 		struct MACH0_(segment_command) *seg;
@@ -2341,11 +2339,7 @@ struct section_t *MACH0_(get_sections)(struct MACH0_(obj_t) *bin) {
 		sections[i].flags = bin->sects[i].flags;
 		rz_str_ncpy (sectname, bin->sects[i].sectname, 17);
 		rz_str_filter (sectname, -1);
-		// hack to support multiple sections with same name
-		// snprintf (segname, sizeof (segname), "%d", i);
-		memcpy (raw_segname, bin->sects[i].segname, 16);
-		raw_segname[16] = 0;
-		snprintf (segname, sizeof (segname), "%zd.%s", i, raw_segname);
+		rz_str_ncpy (raw_segname, bin->sects[i].segname, 16);
 		for (j = 0; j < bin->nsegs; j++) {
 			if (sections[i].addr >= bin->segs[j].vmaddr &&
 				sections[i].addr < (bin->segs[j].vmaddr + bin->segs[j].vmsize)) {
@@ -2353,11 +2347,8 @@ struct section_t *MACH0_(get_sections)(struct MACH0_(obj_t) *bin) {
 				break;
 			}
 		}
-		// XXX: if two sections have the same name are merged :O
-		// XXX: append section index in flag name maybe?
-		// XXX: do not load out of bound sections?
-		// XXX: load segments instead of sections? what about PAGEZERO and ...
-		snprintf (sections[i].name, sizeof (sections[i].name), "%s.%s", segname, sectname);
+		snprintf (sections[i].name, sizeof (sections[i].name),
+			"%d.%s.%s", (int)i, raw_segname, sectname);
 		sections[i].last = 0;
 	}
 	sections[i].last = 1;
@@ -4092,7 +4083,7 @@ void MACH0_(mach_headerfields)(RzBinFile *bf) {
 		}
 		case LC_MAIN:
 			{
-				ut8 data[64];
+				ut8 data[64] = {0};
 				rz_buf_read_at (buf, addr, data, sizeof (data));
 #if RZ_BIN_MACH064
 				ut64 ep = rz_read_ble64 (&data, false); //  bin->big_endian);

librz/bin/format/pyc/marshal.c
@@ -1002,7 +1002,7 @@ static pyc_object *get_object(RzBuffer *buffer) {
 	pyc_object *ret = NULL;
 	ut8 code = get_ut8 (buffer, &error);
 	ut8 flag = code & FLAG_REF;
-	RzListIter *ref_idx;
+	RzListIter *ref_idx = NULL;
 	ut8 type = code & ~FLAG_REF;
 
 	if (error) {
@@ -1119,17 +1119,10 @@ static pyc_object *get_object(RzBuffer *buffer) {
 		return NULL;
 	}
 
-	/* for debugging purpose
-    if (ret == NULL) {
-        eprintf("***%d***\n", type);
-    }
-    */
-
-	if (flag) {
+	if (flag && ref_idx) {
 		free_object (ref_idx->data);
 		ref_idx->data = copy_object (ret);
 	}
-
 	return ret;
 }
 

librz/bp/bp.c
@@ -284,7 +284,7 @@ RZ_API int rz_bp_list(RzBreakpoint *bp, int rad) {
 	int n = 0;
 	RzBreakpointItem *b;
 	RzListIter *iter;
-	PJ *pj;
+	PJ *pj = NULL;
 	if (rad == 'j') {
 		pj = pj_new ();
 		if (!pj) {
@@ -294,8 +294,25 @@ RZ_API int rz_bp_list(RzBreakpoint *bp, int rad) {
 	}
 	//eprintf ("Breakpoint list:\n");
 	rz_list_foreach (bp->bps, iter, b) {
-		switch (rad) {
-		case 0:
+		if (pj) {
+			pj_o (pj);
+			pj_kN (pj, "addr", b->addr);
+			pj_ki (pj, "size", b->size);
+			pj_ks (pj, "perm", rz_str_rwx_i (b->perm & 7)); /* filter out R_BP_PROT_ACCESS */
+			pj_kb (pj, "hw", b->hw);
+			pj_kb (pj, "trace", b->trace);
+			pj_kb (pj, "enabled", b->enabled);
+			pj_kb (pj, "valid", rz_bp_is_valid (bp, b));
+			pj_ks (pj, "data", rz_str_get2 (b->data));
+			pj_ks (pj, "cond", rz_str_get2 (b->cond));
+			pj_end (pj);
+		} else if (rad) {
+			if (b->module_name) {
+				bp->cb_printf ("dbm %s %"PFMT64d"\n", b->module_name, b->module_delta);
+			} else {
+				bp->cb_printf ("db 0x%08"PFMT64x"\n", b->addr);
+			}
+		} else {
 			bp->cb_printf ("0x%08"PFMT64x" - 0x%08"PFMT64x \
 				" %d %c%c%c %s %s %s %s cmd=\"%s\" cond=\"%s\" " \
 				"name=\"%s\" module=\"%s\"\n",
@@ -311,38 +328,10 @@ RZ_API int rz_bp_list(RzBreakpoint *bp, int rad) {
 				rz_str_get2 (b->cond),
 				rz_str_get2 (b->name),
 				rz_str_get2 (b->module_name));
-			break;
-		case 1:
-		case 'r':
-		case '*':
-			// TODO: add command, tracing, enable, ..
-			if (b->module_name) {
-			    	bp->cb_printf ("dbm %s %"PFMT64d"\n", b->module_name, b->module_delta);
-			} else {
-				bp->cb_printf ("db 0x%08"PFMT64x"\n", b->addr);
-			}
-			//b->trace? "trace": "break",
-			//b->enabled? "enabled": "disabled",
-			// b->data? b->data: "");
-			break;
-		case 'j':
-			pj_o (pj);
-			pj_kN (pj, "addr", b->addr);
-			pj_ki (pj, "size", b->size);
-			pj_ks (pj, "perm", rz_str_rwx_i (b->perm & 7)); /* filter out RZ_BP_PROT_ACCESS */
-			pj_kb (pj, "hw", b->hw);
-			pj_kb (pj, "trace", b->trace);
-			pj_kb (pj, "enabled", b->enabled);
-			pj_kb (pj, "valid", rz_bp_is_valid (bp, b));
-			pj_ks (pj, "data", b->data ? b->data : "");
-			pj_ks (pj, "cond", b->cond ? b->cond : "");
-			pj_end (pj);
-			break;
 		}
-		/* TODO: Show list of pids and trace points, conditionals */
 		n++;
 	}
-	if (rad == 'j') {
+	if (pj) {
 		pj_end (pj);
 		bp->cb_printf ("%s\n", pj_string (pj));
 		pj_free (pj);