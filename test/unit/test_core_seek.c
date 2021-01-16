04fc5bf63d8a7cfb7f493693ec0fe673c793069c
Riccardo Schirone <sirmy15@gmail.com>
2021-01-16 13:35:06 +0100

    Convert `s` commands to newshell
    * Rename sj/s* and move them under sH. Remove s=
    * Rename s-* to sH-
    * Use sd for "seek delta", which moves relative to the current offset
    * Enforce <cmd> <arg> syntax. So no more "s+16" but "s +16". Command
      "sHr" is for redo history, "sHu" for undo history. If one wants to move
      relative to the current address, he needs to do "sd +10"/"sd -10".
    * Remove `ss` commands in favour of cmd.seek.silent. This was anyway
      used mainly for scripts, so no need to replicate all `s` commands
      under `ss.
    
    Change a bit rz_core_seek API to avoid dupped entries in seek history

--- test/unit/test_core_seek.c
+++ test/unit/test_core_seek.c
@@ -232,7 +232,61 @@ bool test_core_seek_dupped(void) {
 	csu = (RzCoreSeekItem *)rz_list_get_n (l, 3);
 	mu_assert_eq (csu->offset, 0x400, "4th is 0x400 seek (0x300 was not saved because we moved away from it withhout saving)");
 	csu = (RzCoreSeekItem *)rz_list_get_n (l, 4);
-	mu_assert_eq (csu->offset, 0x500, "5th is 0x300 seek");
+	mu_assert_eq (csu->offset, 0x500, "5th is 0x500 seek");
+	mu_assert_true (csu->is_current, "5th is current");
+	rz_list_free (l);
+
+	rz_core_seek (core, 0x400, false);
+	rz_core_seek_mark (core);
+	rz_core_seek_and_save (core, 0x600, false);
+	l = rz_core_seek_list (core);
+	mu_assert_eq (rz_list_length (l), 5, "no other element in history because 0x400 is already last element in history");
+	csu = (RzCoreSeekItem *)rz_list_get_n (l, 3);
+	mu_assert_eq (csu->offset, 0x400, "4th is still 0x400 seek");
+	csu = (RzCoreSeekItem *)rz_list_get_n (l, 4);
+	mu_assert_eq (csu->offset, 0x600, "5th is 0x600 seek");
+	mu_assert_true (csu->is_current, "5th is current");
+	rz_list_free (l);
+
+	rz_core_free (core);
+	mu_end;
+}
+
+bool test_core_seek_marksave(void) {
+	RzCoreSeekItem *csu = NULL;
+	RzList *l = NULL;
+	RzCore *core = rz_core_new ();
+	rz_core_seek_and_save (core, 0x100, false);
+
+	rz_core_seek_save (core);
+	l = rz_core_seek_list (core);
+	mu_assert_eq (rz_list_length (l), 2, "only 0 and 0x100 (current) are in list, save did not work because no mark");
+	rz_list_free (l);
+
+	rz_core_seek_mark (core);
+	rz_core_seek_save (core);
+	l = rz_core_seek_list (core);
+	mu_assert_eq (rz_list_length (l), 2, "only 0 and 0x100 (current) are in list, we did not move from mark");
+	rz_list_free (l);
+
+	rz_core_seek_mark (core);
+	rz_core_seek (core, 0x200, false);
+	rz_core_seek (core, 0x300, false);
+	rz_core_seek_and_save (core, 0x400, false);
+	l = rz_core_seek_list (core);
+	mu_assert_eq (rz_list_length (l), 3, "0, 0x100, 0x400 (current) are in list");
+	csu = (RzCoreSeekItem *)rz_list_get_n (l, 0);
+	mu_assert_eq (csu->offset, 0x0, "1st is 0 seek");
+	csu = (RzCoreSeekItem *)rz_list_get_n (l, 1);
+	mu_assert_eq (csu->offset, 0x100, "2nd is 0x100 seek");
+	csu = (RzCoreSeekItem *)rz_list_get_n (l, 2);
+	mu_assert_eq (csu->offset, 0x400, "3rd is 0x400 seek");
+	mu_assert_true (csu->is_current, "3rd is current");
+	rz_list_free (l);
+
+	rz_core_seek_save (core);
+	l = rz_core_seek_list (core);
+	mu_assert_eq (rz_list_length (l), 3, "save did not work, because no mark after saving");
 	rz_list_free (l);
 
 	rz_core_free (core);
@@ -246,6 +300,7 @@ int all_tests () {
 	mu_run_test (test_core_seek_list_undo);
 	mu_run_test (test_core_seek_undo_redo);
 	mu_run_test (test_core_seek_dupped);
+	mu_run_test (test_core_seek_marksave);
 	return tests_passed != tests_run;
 }
 
