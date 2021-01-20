a95437ad94e9723f0690cfdb0bd9e0b14b5e2397
Riccardo Schirone <sirmy15@gmail.com>
2021-01-20 22:38:26 +0100

    Fix rpath on MacOS, broken by moving binaries for signing

--- binrz/rizin/macos_sign.sh
+++ binrz/rizin/macos_sign.sh
@@ -4,13 +4,5 @@
 
 ENTITLEMENT="$1"
 SRC="$2"
-if [ "$#" -ne 2 ]; then
-        DST="$3"
 
-        rm -f "${DST}"
-        cp -a "${SRC}" "${DST}"
-else
-        DST="${MESON_INSTALL_DESTDIR_PREFIX}/${SRC}"
-fi
-
-codesign --entitlements "${ENTITLEMENT}" --force -s - "${DST}"
+codesign --entitlements "${ENTITLEMENT}" --force -s - "${SRC}"
