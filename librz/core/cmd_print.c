radareorg/radare2

Use pj in core ##refactor (#18267)
* Use pj in core ##refactor

* fix tests

libr/core/cmd_print.c
@@ -1143,7 +1143,7 @@ static void cmd_pDj(RCore *core, const char *arg) {
 		eprintf ("cannot allocate %d byte(s)\n", bsize);
 	}
 	pj_end (pj);
-	r_cons_printf ("%s", pj_string (pj));
+	r_cons_println (pj_string (pj));
 	pj_free (pj);
 }
 
@@ -1156,7 +1156,7 @@ static void cmd_pdj(RCore *core, const char *arg, ut8* block) {
 	pj_a (pj);
 	r_core_print_disasm_json (core, core->offset, block, core->blocksize, nblines, pj);
 	pj_end (pj);
-	r_cons_printf ("%s\n", pj_string (pj));
+	r_cons_println (pj_string (pj));
 	pj_free (pj);
 }
 
@@ -5498,7 +5498,6 @@ static int cmd_print(void *data, const char *input) {
 			} else {
 				cmd_pdj (core, input + 2, block);
 			}
-			r_cons_newline ();
 			pd_result = 0;
 			break;
 		case 'J': // pdJ

libr/core/disasm.c
@@ -6176,7 +6176,6 @@ R_API int r_core_print_disasm_json(RCore *core, ut64 addr, ut8 *buf, int nb_byte
 			break;
 		}
 	}
-	// r_cons_printf ("]");
 	core->offset = old_offset;
 	r_anal_op_fini (&ds->analop);
 	ds_free (ds);
@@ -6202,8 +6201,13 @@ R_API int r_core_print_disasm_all(RCore *core, ut64 addr, int l, int len, int mo
 		buf = malloc (l + 1);
 		r_io_read_at (core->io, addr, buf, l);
 	}
+	PJ *pj = NULL;
 	if (mode == 'j') {
-		r_cons_print ("[");
+		pj = r_core_pj_new (core);
+		if (!pj) {
+			return 0;
+		}
+		pj_a (pj);
 	}
 	r_cons_break_push (NULL, NULL);
 	for (i = 0; i < l; i++) {
@@ -6269,10 +6273,12 @@ R_API int r_core_print_disasm_all(RCore *core, ut64 addr, int l, int len, int mo
 				}
 				break;
 			case 'j': {
-				//TODO PJ
 				char *op_hex = r_asm_op_get_hex (&asmop);
-				r_cons_printf ("{\"addr\":%08"PFMT64d",\"bytes\":\"%s\",\"inst\":\"%s\"}%s",
-					addr + i, op_hex, r_asm_op_get_asm (&asmop), ",");
+				pj_o (pj);
+				pj_kn (pj, "addr", addr + i);
+				pj_ks (pj, "bytes", op_hex);
+				pj_ks (pj, "inst", r_asm_op_get_asm (&asmop));
+				pj_end (pj);
 				free (op_hex);
 				break;
 			}
@@ -6291,7 +6297,9 @@ R_API int r_core_print_disasm_all(RCore *core, ut64 addr, int l, int len, int mo
 		free (buf);
 	}
 	if (mode == 'j') {
-		r_cons_printf ("{}]\n");
+		pj_end (pj);
+		r_cons_println (pj_string (pj));
+		pj_free (pj);
 	}
 	ds_free (ds);
 	return count;

libr/core/yank.c
@@ -248,16 +248,24 @@ R_API bool r_core_yank_dump(RCore *core, ut64 pos, int format) {
 				}
 				r_cons_newline ();
 				break;
-			case 'j':
-				//TODO PJ
-				{
-					r_cons_printf ("{\"addr\":%"PFMT64u",\"bytes\":\"", core->yank_addr);
-					for (i = pos; i < r_buf_size (core->yank_buf); i++) {
-						r_cons_printf ("%02x", r_buf_read8_at (core->yank_buf, i));
-					}
-					r_cons_printf ("\"}\n");
+			case 'j': {
+				PJ *pj = r_core_pj_new (core);
+				if (!pj) {
+					break;
 				}
+				pj_o (pj);
+				pj_kn (pj, "addr", core->yank_addr);
+				RStrBuf *buf = r_strbuf_new ("");
+				for (i = pos; i < r_buf_size (core->yank_buf); i++) {
+					r_strbuf_appendf (buf, "%02x", r_buf_read8_at (core->yank_buf, i));
+				}
+				pj_ks (pj, "bytes", r_strbuf_get (buf));
+				r_strbuf_free (buf);
+				pj_end (pj);
+				r_cons_println (pj_string (pj));
+				pj_free (pj);
 				break;
+			}
 			case '*':
 				//r_cons_printf ("yfx ");
 				r_cons_printf ("wx ");

rizinorg/rizin

librz/core/cmd_print.c
@@ -1363,7 +1363,7 @@ static void cmd_pDj(RzCore *core, const char *arg) {
 		eprintf("cannot allocate %d byte(s)\n", bsize);
 	}
 	pj_end(pj);
-	rz_cons_printf("%s", pj_string(pj));
+	rz_cons_println(pj_string(pj));
 	pj_free(pj);
 }
 
@@ -1376,7 +1376,7 @@ static void cmd_pdj(RzCore *core, const char *arg, ut8 *block) {
 	pj_a(pj);
 	rz_core_print_disasm_json(core, core->offset, block, core->blocksize, nblines, pj);
 	pj_end(pj);
-	rz_cons_printf("%s\n", pj_string(pj));
+	rz_cons_println(pj_string(pj));
 	pj_free(pj);
 }
 
@@ -5658,7 +5658,6 @@ RZ_IPI int rz_cmd_print(void *data, const char *input) {
 			} else {
 				cmd_pdj(core, input + 2, block);
 			}
-			rz_cons_newline();
 			pd_result = 0;
 			break;
 		case 'J': // pdJ

librz/core/disasm.c
@@ -6112,7 +6112,6 @@ RZ_API int rz_core_print_disasm_json(RzCore *core, ut64 addr, ut8 *buf, int nb_b
 			break;
 		}
 	}
-	// rz_cons_printf ("]");
 	core->offset = old_offset;
 	rz_analysis_op_fini(&ds->analop);
 	ds_free(ds);
@@ -6138,8 +6137,13 @@ RZ_API int rz_core_print_disasm_all(RzCore *core, ut64 addr, int l, int len, int
 		buf = malloc(l + 1);
 		rz_io_read_at(core->io, addr, buf, l);
 	}
+	PJ *pj = NULL;
 	if (mode == 'j') {
-		rz_cons_print("[");
+		pj = rz_core_pj_new(core);
+		if (!pj) {
+			return 0;
+		}
+		pj_a(pj);
 	}
 	rz_cons_break_push(NULL, NULL);
 	for (i = 0; i < l; i++) {
@@ -6206,8 +6210,11 @@ RZ_API int rz_core_print_disasm_all(RzCore *core, ut64 addr, int l, int len, int
 				break;
 			case 'j': {
 				char *op_hex = rz_asm_op_get_hex(&asmop);
-				rz_cons_printf("{\"addr\":%08" PFMT64d ",\"bytes\":\"%s\",\"inst\":\"%s\"}%s",
-					addr + i, op_hex, rz_asm_op_get_asm(&asmop), ",");
+				pj_o(pj);
+				pj_kn(pj, "addr", addr + i);
+				pj_ks(pj, "bytes", op_hex);
+				pj_ks(pj, "inst", rz_asm_op_get_asm(&asmop));
+				pj_end(pj);
 				free(op_hex);
 				break;
 			}
@@ -6226,7 +6233,9 @@ RZ_API int rz_core_print_disasm_all(RzCore *core, ut64 addr, int l, int len, int
 		free(buf);
 	}
 	if (mode == 'j') {
-		rz_cons_printf("{}]\n");
+		pj_end(pj);
+		rz_cons_println(pj_string(pj));
+		pj_free(pj);
 	}
 	ds_free(ds);
 	return count;

librz/core/yank.c
@@ -246,12 +246,23 @@ RZ_API bool rz_core_yank_dump(RzCore *core, ut64 pos, int format) {
 				rz_cons_newline();
 				break;
 			case 'j': {
-				rz_cons_printf("{\"addr\":%" PFMT64u ",\"bytes\":\"", core->yank_addr);
-				for (i = pos; i < rz_buf_size(core->yank_buf); i++) {
-					rz_cons_printf("%02x", rz_buf_read8_at(core->yank_buf, i));
+				PJ *pj = rz_core_pj_new(core);
+				if (!pj) {
+					break;
 				}
-				rz_cons_printf("\"}\n");
-			} break;
+				pj_o(pj);
+				pj_kn(pj, "addr", core->yank_addr);
+				RzStrBuf *buf = rz_strbuf_new("");
+				for (i = pos; i < rz_buf_size(core->yank_buf); i++) {
+					rz_strbuf_appendf(buf, "%02x", rz_buf_read8_at(core->yank_buf, i));
+				}
+				pj_ks(pj, "bytes", rz_strbuf_get(buf));
+				rz_strbuf_free(buf);
+				pj_end(pj);
+				rz_cons_println(pj_string(pj));
+				pj_free(pj);
+				break;
+			}
 			case '*':
 				//rz_cons_printf ("yfx ");
 				rz_cons_printf("wx ");