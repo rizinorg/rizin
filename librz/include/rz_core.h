d04e2a805c982bbdf817fd42c4905cf6bf17397c
GustavoLCR <gugulcr@gmail.com>
2021-01-26 23:36:07 -0300

    Add rz_core_debug_(set/clear)_register_flags apis

--- librz/include/rz_core.h
+++ librz/include/rz_core.h
@@ -542,6 +542,8 @@ RZ_API ut32 rz_core_file_cur_fd(RzCore *core);
 RZ_API bool rz_core_debug_step_one(RzCore *core, int times);
 
 RZ_API void rz_core_debug_rr(RzCore *core, RzReg *reg, int mode);
+RZ_API void rz_core_debug_set_register_flags(RzCore *core);
+RZ_API void rz_core_debug_clear_register_flags(RzCore *core);
 
 /* fortune */
 RZ_API void rz_core_fortune_list_types(void);
