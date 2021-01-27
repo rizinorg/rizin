d0a836cfbae1780b66fd110de3ebfc2216890967
Riccardo Schirone <sirmy15@gmail.com>
2021-01-27 17:13:23 +0100

    Remove configure.acr and use meson project version

--- sys/version.py
+++ sys/version.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python
 
-""" Portable python script to read version from configure.acr """
+""" Portable python script to read version from meson.build until meson provides a proper way """
 
+import os
 import sys
 
-full_version = False
+meson_file = 'meson.build'
+if len(sys.argv) > 1:
+    meson_file = os.path.join(sys.argv[1], meson_file)
 
-for arg in sys.argv[1:]:
-    if arg == '--full-version':
-        full_version = True
-    else:
-        print('Option %s not supported' % (arg,))
-        sys.exit(1)
+with open(meson_file, 'r') as f:
+    # Read only first 10 lines of the meson file, looking for 'version: ' string
+    for i in range(10):
+        fields = [x.strip() for x in f.readline().strip().split(':')]
+        if fields[0] == 'version':
+            fields = fields[1].split('\'')
+            sys.stdout.write(fields[1] + '\n')
+            sys.exit(0)
 
-with open('configure.acr', 'r') as f:
-    f.readline()
-    version = f.readline().split()[1]
-    sys.stdout.write(version + '\n')
-    if full_version:
-        versions = version.split('.')
-        version_major = versions[0] if len(versions) > 0 else 0
-        version_minor = versions[1] if len(versions) > 1 else 0
-        version_patch = versions[2].replace('-git', '') if len(versions) > 2 else 0
-        version_number = "%d%02d%02d"%(int(version_major), int(version_minor), int(version_patch))
-        sys.stdout.write(version_major + '\n')
-        sys.stdout.write(version_minor + '\n')
-        sys.stdout.write(version_patch + '\n')
-        sys.stdout.write(version_number + '\n')
+sys.exit(1)
