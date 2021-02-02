8534d014f5d54af15a45384965066f1b658abb40
Riccardo Schirone <sirmy15@gmail.com>
2021-02-02 11:56:24 +0100

    Fix check_meson_subproject script

--- sys/check_meson_subproject.py
+++ sys/check_meson_subproject.py
@@ -13,26 +13,30 @@ meson_root = os.environ['MESON_SOURCE_ROOT']
 
 subproject_filename = os.path.join(meson_root, 'subprojects', subproject + '.wrap')
 
-f = open(subproject_filename, 'r')
+try:
+    f = open(subproject_filename, 'r')
 
-is_wrap_git = False
-revision = None
-directory = subproject
-for l in f:
-    if 'wrap-git' in l:
-        is_wrap_git = True
-    elif l.startswith('revision'):
-        revision = l.split('=')[1].strip()
-    elif l.startswith('directory'):
-        directory = l.split('=')[1].strip()
+    is_wrap_git = False
+    revision = None
+    directory = subproject
+    for l in f:
+        if 'wrap-git' in l:
+            is_wrap_git = True
+        elif l.startswith('revision'):
+            revision = l.split('=')[1].strip()
+        elif l.startswith('directory'):
+            directory = l.split('=')[1].strip()
 
-if not is_wrap_git or not revision:
+    if not is_wrap_git or not revision:
+        sys.exit(0)
+
+    subproject_dir = os.path.join(meson_root, 'subprojects', directory)
+    subproject_git_dir = os.path.join(subproject_dir, '.git')
+    if os.path.isdir(subproject_dir) and os.path.isdir(subproject_git_dir):
+        head = open(os.path.join(subproject_git_dir, 'HEAD'), 'r').read().strip()
+        if head != revision:
+            sys.exit(1)
+
     sys.exit(0)
-
-subproject_dir = os.path.join(meson_root, 'subprojects', directory)
-if os.path.isdir(subproject_dir):
-    head = open(os.path.join(subproject_dir, '.git', 'HEAD'), 'r').read().strip()
-    if head != revision:
-        sys.exit(1)
-
-sys.exit(0)
+except FileNotFoundError:
+    sys.exit(0)
