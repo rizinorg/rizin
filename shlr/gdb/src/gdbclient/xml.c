radareorg/radare2

Fix some of the last coverities related to UAF and OOBREAD

shlr/gdb/src/gdbclient/xml.c
@@ -334,7 +334,7 @@ static int gdbr_parse_target_xml(libgdbr_t *g, char *xml_data, ut64 len) {
 	}
 	r_list_free (flags);
 	r_list_free (regs);
-	free (g->target.regprofile);
+	R_FREE (g->target.regprofile);
 	if (profile) {
 		g->target.regprofile = strdup (profile);
 		free (profile);

rizinorg/rizin

shlr/gdb/src/gdbclient/xml.c
@@ -331,7 +331,7 @@ static int gdbr_parse_target_xml(libgdbr_t *g, char *xml_data, ut64 len) {
 	}
 	rz_list_free(flags);
 	rz_list_free(regs);
-	free(g->target.regprofile);
+	RZ_FREE(g->target.regprofile);
 	if (profile) {
 		g->target.regprofile = strdup(profile);
 		free(profile);