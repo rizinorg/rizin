radareorg/radare2

Fix memleak in db/formats/mangling/mangling

libr/util/str.c
@@ -847,8 +847,9 @@ R_API char *r_str_ndup(const char *ptr, int len) {
 
 // TODO: deprecate?
 R_API char *r_str_dup(char *ptr, const char *string) {
-	free (ptr);
-	return r_str_new (string);
+	char *str = r_str_new (string);
+	free (ptr); // in case ptr == string
+	return str;
 }
 
 R_API char *r_str_prepend(char *ptr, const char *string) {

rizinorg/rizin

librz/util/str.c
@@ -841,8 +841,9 @@ RZ_API char *rz_str_ndup(const char *ptr, int len) {
 
 // TODO: deprecate?
 RZ_API char *rz_str_dup(char *ptr, const char *string) {
-	free(ptr);
-	return rz_str_new(string);
+	char *str = rz_str_new(string);
+	free(ptr); // in case ptr == string
+	return str;
 }
 
 RZ_API char *rz_str_prepend(char *ptr, const char *string) {