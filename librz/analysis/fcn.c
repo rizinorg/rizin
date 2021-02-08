e12919cab634a5ffe667efa82f190f742c01c341
Paul I <pelijah@users.noreply.github.com>
2021-02-08 21:06:22 +0300

    Fix memleak in fcn_recurse() (#527)

--- librz/analysis/fcn.c
+++ librz/analysis/fcn.c
@@ -522,6 +522,7 @@ static int fcn_recurse(RzAnalysis *analysis, RzAnalysisFunction *fcn, ut64 addr,
 	const int continue_after_jump = analysis->opt.afterjmp;
 	const int addrbytes = analysis->iob.io ? analysis->iob.io->addrbytes : 1;
 	char *last_reg_mov_lea_name = NULL;
+	char *movbasereg = NULL;
 	RzAnalysisBlock *bb = NULL;
 	RzAnalysisBlock *bbg = NULL;
 	int ret = RZ_ANALYSIS_RET_END, skip_ret = 0;
@@ -635,7 +636,6 @@ static int fcn_recurse(RzAnalysis *analysis, RzAnalysisFunction *fcn, ut64 addr,
 	}
 	ut64 movdisp = UT64_MAX; // used by jmptbl when coded as "mov reg, [reg * scale + disp]"
 	ut64 movscale = 0;
-	char *movbasereg = NULL;
 	ut8 buf[32]; // 32 bytes is enough to hold any instruction.
 	int maxlen = len * addrbytes;
 	if (is_dalvik) {
@@ -1366,6 +1366,7 @@ beach:
 	}
 	rz_analysis_block_update_hash(bb);
 	rz_analysis_block_unref(bb);
+	free(movbasereg);
 	return ret;
 }
 
