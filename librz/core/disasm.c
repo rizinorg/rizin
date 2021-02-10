35437b54faa8c127831670b073f2c43dfaee9c05
Riccardo Schirone <sirmy15@gmail.com>
2021-02-10 20:18:50 +0100

    core: use esil API instead of aeim/aeim- commands

--- librz/core/disasm.c
+++ librz/core/disasm.c
@@ -6517,7 +6517,7 @@ RZ_API int rz_core_disasm_pde(RzCore *core, int nb_opcodes, int mode) {
 	if (!core->analysis->esil) {
 		rz_core_analysis_esil_init(core);
 		if (!rz_config_get_b(core->config, "cfg.debug")) {
-			rz_core_cmd0(core, "aeim");
+			rz_core_analysis_esil_init_mem(core, NULL, UT64_MAX, UT32_MAX);
 		}
 	}
 	RzAnalysisEsil *esil = core->analysis->esil;
