fa84d7bb27961432243e60ae61162e55b453057e
Alexis Ehret <08a@riseup.net>
2021-02-12 14:23:53 +0100

    Fix coding style python files (black, isort)

--- sys/check_meson_subproject.py
+++ sys/check_meson_subproject.py
@@ -5,50 +5,52 @@
 
 """ Portable python script to check if subproject is up-to-date and warn if not """
 
-import sys
-import os
 import filecmp
+import os
+import sys
 
 subproject = sys.argv[1]
-meson_root = os.environ['MESON_SOURCE_ROOT']
+meson_root = os.environ["MESON_SOURCE_ROOT"]
 
-subproject_filename = os.path.join(meson_root, 'subprojects', subproject + '.wrap')
+subproject_filename = os.path.join(meson_root, "subprojects", subproject + ".wrap")
 
 try:
-    f = open(subproject_filename, 'r')
+    f = open(subproject_filename, "r")
 
     is_wrap_git = False
     revision = None
     directory = subproject
     patch_directory = subproject
     for l in f:
-        if 'wrap-git' in l:
+        if "wrap-git" in l:
             is_wrap_git = True
-        elif 'wrap-file' in l:
+        elif "wrap-file" in l:
             is_wrap_file = True
-        elif l.startswith('revision'):
-            revision = l.split('=')[1].strip()
-        elif l.startswith('directory'):
-            directory = l.split('=')[1].strip()
-        elif l.startswith('patch_directory'):
-            patch_directory = l.split('=')[1].strip()
+        elif l.startswith("revision"):
+            revision = l.split("=")[1].strip()
+        elif l.startswith("directory"):
+            directory = l.split("=")[1].strip()
+        elif l.startswith("patch_directory"):
+            patch_directory = l.split("=")[1].strip()
 
     if is_wrap_git:
         if not revision:
             sys.exit(0)
 
-        subproject_dir = os.path.join(meson_root, 'subprojects', directory)
-        subproject_git_dir = os.path.join(subproject_dir, '.git')
+        subproject_dir = os.path.join(meson_root, "subprojects", directory)
+        subproject_git_dir = os.path.join(subproject_dir, ".git")
         if os.path.isdir(subproject_dir) and os.path.isdir(subproject_git_dir):
-            head = open(os.path.join(subproject_git_dir, 'HEAD'), 'r').read().strip()
+            head = open(os.path.join(subproject_git_dir, "HEAD"), "r").read().strip()
             if head != revision:
                 sys.exit(1)
 
     if not patch_directory:
         sys.exit(0)
 
-    subproject_dir = os.path.join(meson_root, 'subprojects', directory)
-    patch_subproject_dir = os.path.join(meson_root, 'subprojects', 'packagefiles', patch_directory)
+    subproject_dir = os.path.join(meson_root, "subprojects", directory)
+    patch_subproject_dir = os.path.join(
+        meson_root, "subprojects", "packagefiles", patch_directory
+    )
     if os.path.isdir(patch_subproject_dir) and os.path.isdir(subproject_dir):
         for f in os.listdir(patch_subproject_dir):
             subproject_f = os.path.join(subproject_dir, f)
--- sys/meson_git_wrapper.py
+++ sys/meson_git_wrapper.py
@@ -3,23 +3,29 @@
 """ Portable python script to execute git -C (even on system where -C is not available) """
 
 import os
-import sys
 import subprocess
+import sys
 
 if len(sys.argv) <= 3:
-    print('Usage: %s <git_executable_path> <repo_path> [git_args...]')
+    print("Usage: %s <git_executable_path> <repo_path> [git_args...]")
     sys.exit(1)
 
 git_exe = sys.argv[1]
 repo_path = sys.argv[2]
 args = sys.argv[3:]
 
+
 def isCArgSupported(git_exe, repo_path):
-    r = subprocess.run([git_exe, '-C', repo_path, 'status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    r = subprocess.run(
+        [git_exe, "-C", repo_path, "status"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
     return r.returncode == 0
 
+
 if isCArgSupported(git_exe, repo_path):
-    called = subprocess.run([git_exe, '-C', repo_path] + args)
+    called = subprocess.run([git_exe, "-C", repo_path] + args)
     sys.exit(called.returncode)
 else:
     os.chdir(repo_path)
