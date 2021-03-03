14258a8a051101ebab534d7e028b1e3ea22d08d0
Anton Kochkov <anton.kochkov@gmail.com>
2021-03-03 16:08:20 +0800

    Use newshell for all `t` commands

--- librz/core/cmd_descs/cmd_descs.h
+++ librz/core/cmd_descs/cmd_descs.h
@@ -150,6 +150,9 @@ RZ_IPI RzCmdStatus rz_seek_prev_handler(RzCore *core, int argc, const char **arg
 RZ_IPI RzCmdStatus rz_seek_opcode_handler(RzCore *core, int argc, const char **argv);
 RZ_IPI RzCmdStatus rz_seek_register_handler(RzCore *core, int argc, const char **argv);
 RZ_IPI RzCmdStatus rz_sleep_handler(RzCore *core, int argc, const char **argv);
+RZ_IPI RzCmdStatus rz_type_handler(RzCore *core, int argc, const char **argv, RzOutputMode mode);
+RZ_IPI RzCmdStatus rz_type_del_handler(RzCore *core, int argc, const char **argv);
+RZ_IPI RzCmdStatus rz_type_del_all_handler(RzCore *core, int argc, const char **argv);
 RZ_IPI RzCmdStatus rz_type_list_c_handler(RzCore *core, int argc, const char **argv);
 RZ_IPI RzCmdStatus rz_type_list_c_nl_handler(RzCore *core, int argc, const char **argv);
 RZ_IPI RzCmdStatus rz_type_cc_list_handler(RzCore *core, int argc, const char **argv, RzOutputMode mode);
@@ -188,7 +191,6 @@ RZ_IPI RzCmdStatus rz_type_xrefs_list_handler(RzCore *core, int argc, const char
 RZ_IPI RzCmdStatus rz_type_xrefs_function_handler(RzCore *core, int argc, const char **argv);
 RZ_IPI RzCmdStatus rz_type_xrefs_graph_handler(RzCore *core, int argc, const char **argv);
 RZ_IPI RzCmdStatus rz_type_xrefs_list_all_handler(RzCore *core, int argc, const char **argv);
-RZ_IPI int rz_cmd_type(void *data, const char *input);
 RZ_IPI RzCmdStatus rz_uniq_handler(RzCore *core, int argc, const char **argv);
 RZ_IPI RzCmdStatus rz_uname_handler(RzCore *core, int argc, const char **argv);
 RZ_IPI int rz_cmd_visual(void *data, const char *input);
