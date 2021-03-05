851b388262d1b2247a60edcf9b21963e93044138
Heersin <teablearcher@gmail.com>
2021-03-05 11:02:32 +0800

    Apply header style to rizin format by using clang-format

--- librz/bin/format/luac/luac_54.h
+++ librz/bin/format/luac/luac_54.h
@@ -9,28 +9,27 @@
 
 #include "luac_specs.h"
 
-
 /* Macros for bin_luac.c */
 #define LUAC_HDR_SIZE_54 sizeof(luacHdr54)
 
 /* luac 5.4 spec */
 RZ_PACKED(
-        typedef struct {
-            ut8 signature[4]; /* == '.Lua' */
-            ut8 version;      /* version of luac */
-            ut8 format;       /* 1 if it's modified luac */
+	typedef struct {
+		ut8 signature[4]; /* == '.Lua' */
+		ut8 version; /* version of luac */
+		ut8 format; /* 1 if it's modified luac */
 
-            ut8 luac_data[6];               /* luac data for error detection */
-            ut8 instruction_size;
-            ut8 integer_size;
-            ut8 number_size;
+		ut8 luac_data[6]; /* luac data for error detection */
+		ut8 instruction_size;
+		ut8 integer_size;
+		ut8 number_size;
 
-            ut8 integer_valid_data[8];        /* test integer data loading */
-            ut8 number_valid_data[8];         /* test number data loading */
+		ut8 integer_valid_data[8]; /* test integer data loading */
+		ut8 number_valid_data[8]; /* test number data loading */
 
-            ut8 upvalues_number;            /* number of upvalue arrays */
-        }) luacHdr54;
-
+		ut8 upvalues_number; /* number of upvalue arrays */
+	})
+luacHdr54;
 
 /* Exported Api to bin_luac.c */
 RzBinInfo *info_54(RzBinFile *bf, int major, int minor);
--- librz/bin/format/luac/luac_specs.h
+++ librz/bin/format/luac/luac_specs.h
@@ -8,34 +8,33 @@
 #include <rz_bin.h>
 #include <rz_lib.h>
 
-/* Macros/Typedefs used in luac */
-typedef	double LUA_NUMBER;
+	/* Macros/Typedefs used in luac */
+	typedef double LUA_NUMBER;
 typedef uint32_t LUA_INSTRUCTION;
 typedef uint64_t LUA_INTEGER;
 
 /* Macro Functions */
 /* type casts (a macro highlights casts in the code) */
-#define luac_cast(t, exp)	((t)(exp))
-#define luac_cast_num(i)	luac_cast(LUA_NUMBER, (i))
-#define luac_cast_int(i)	luac_cast(int, (i))
+#define luac_cast(t, exp) ((t)(exp))
+#define luac_cast_num(i)  luac_cast(LUA_NUMBER, (i))
+#define luac_cast_int(i)  luac_cast(int, (i))
 
 /* Macros About Luac Format */
-#define LUAC_MAGIC "\x1b\x4c\x75\x61"
-#define LUAC_FORMAT 0           /* this is the official format */
-#define LUAC_DATA "\x19\x93\r\n\x1a\n"
-#define LUAC_INT_VALIDATION 0x5678
+#define LUAC_MAGIC             "\x1b\x4c\x75\x61"
+#define LUAC_FORMAT            0 /* this is the official format */
+#define LUAC_DATA              "\x19\x93\r\n\x1a\n"
+#define LUAC_INT_VALIDATION    0x5678
 #define LUAC_NUMBER_VALIDATION luac_cast_num(370.5)
 
 /* Lua Functions */
 void luaLoadBlock(void *src, void *dest, size_t size);
-#define luaLoadVector(src, buf, n)     luaLoadBlock(src, buf, (n)*sizeof((buf)[0]))
-#define luaLoadVar(raw_data, var)      luaLoadVector(raw_data, &(var), 1)
+#define luaLoadVector(src, buf, n) luaLoadBlock(src, buf, (n) * sizeof((buf)[0]))
+#define luaLoadVar(raw_data, var)  luaLoadVector(raw_data, &(var), 1)
 
 LUA_INTEGER luaLoadInteger(void *src);
 LUA_NUMBER luaLoadNumber(void *src);
 size_t luaLoadUnsigned(void *src, size_t limit);
 size_t luaLoadSize(void *src);
 char *luaLoadString(void *src);
-
 
 #endif //BUILD_LUAC_SPECS_H
