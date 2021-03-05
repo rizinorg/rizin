7d22cfd7aef316281c7ec9660e3f589b3e52165b
Heersin <teablearcher@gmail.com>
2021-03-05 11:00:51 +0800

    Apply code style by using clang-format

--- librz/bin/format/luac/luac_54.c
+++ librz/bin/format/luac/luac_54.c
@@ -6,76 +6,74 @@
 
 #include "luac_54.h"
 
-RzBinInfo *info_54(RzBinFile *bf, int major, int minor)
-{
-    int INNER_BUFFER_SIZE = 256;
-    unsigned char work_buffer[INNER_BUFFER_SIZE];
+RzBinInfo *info_54(RzBinFile *bf, int major, int minor) {
+	int INNER_BUFFER_SIZE = 256;
+	unsigned char work_buffer[INNER_BUFFER_SIZE];
 
-    RzBinInfo *ret = NULL;
-    luacHdr54 hdr;
-    memset(&hdr, 0, LUAC_HDR_SIZE_54);
+	RzBinInfo *ret = NULL;
+	luacHdr54 hdr;
+	memset(&hdr, 0, LUAC_HDR_SIZE_54);
 
-    int reat = rz_buf_read_at(bf->buf, 0, (ut8 *)&hdr, LUAC_HDR_SIZE_54);
-    if (reat != LUAC_HDR_SIZE_54){
-        eprintf("Truncated Header\n");
-        return NULL;
-    }
+	int reat = rz_buf_read_at(bf->buf, 0, (ut8 *)&hdr, LUAC_HDR_SIZE_54);
+	if (reat != LUAC_HDR_SIZE_54) {
+		eprintf("Truncated Header\n");
+		return NULL;
+	}
 
-    /* Common Ret */
-    if (!(ret = RZ_NEW0(RzBinInfo))){
-        return NULL;
-    }
-    ret->file = strdup(bf->file);
-    ret->type = rz_str_newf("Lua %c.%c compiled file", major + '0', minor + '0');
-    ret->bclass = strdup("Lua compiled file");
-    ret->rclass = strdup("luac");
-    ret->arch = strdup("luac");
-    ret->machine = rz_str_newf("Lua %c.%c VM ", major + '0', minor + '0');
-    ret->os = strdup("any");
-    ret->bits = 8;
+	/* Common Ret */
+	if (!(ret = RZ_NEW0(RzBinInfo))) {
+		return NULL;
+	}
+	ret->file = strdup(bf->file);
+	ret->type = rz_str_newf("Lua %c.%c compiled file", major + '0', minor + '0');
+	ret->bclass = strdup("Lua compiled file");
+	ret->rclass = strdup("luac");
+	ret->arch = strdup("luac");
+	ret->machine = rz_str_newf("Lua %c.%c VM ", major + '0', minor + '0');
+	ret->os = strdup("any");
+	ret->bits = 8;
 
+	/* official format ? */
+	if (hdr.format != 0x00) {
+		ret->compiler = strdup("Unofficial Lua Compiler");
+		return ret;
+	}
+	ret->compiler = strdup("Official Lua Compiler");
 
-    /* official format ? */
-    if (hdr.format != 0x00){
-        ret->compiler = strdup("Unofficial Lua Compiler");
-        return ret;
-    }
-    ret->compiler = strdup("Official Lua Compiler");
+	/* if LUAC_DATA checksum corrupted */
+	if (memcmp(hdr.luac_data, LUAC_DATA, sizeof(hdr.luac_data)) != 0) {
+		eprintf("Corrupted Luac\n");
+		return ret;
+	}
 
-    /* if LUAC_DATA checksum corrupted */
-    if (memcmp(hdr.luac_data, LUAC_DATA, sizeof(hdr.luac_data)) != 0){
-        eprintf("Corrupted Luac\n");
-        return ret;
-    }
+	/* Check Size */
+	if ((hdr.instruction_size != sizeof(LUA_INSTRUCTION)) ||
+		(hdr.integer_size != sizeof(LUA_INTEGER)) ||
+		(hdr.number_size != sizeof(LUA_NUMBER))) {
+		eprintf("Size Definition not matched\n");
+		return ret;
+	}
 
-    /* Check Size */
-    if ((hdr.instruction_size != sizeof(LUA_INSTRUCTION)) ||
-            (hdr.integer_size != sizeof(LUA_INTEGER)) ||
-        (hdr.number_size != sizeof(LUA_NUMBER))){
-        eprintf("Size Definition not matched\n");
-        return ret;
-    }
+	/* Check Loader -- endian */
+	if (luaLoadInteger(hdr.integer_valid_data) != LUAC_INT_VALIDATION) {
+		eprintf("Integer Format Not Matched\n");
+		return ret;
+	}
+	if (luaLoadNumber(hdr.number_valid_data) != LUAC_NUMBER_VALIDATION) {
+		eprintf("Number Format Not Matched\n");
+		return ret;
+	}
 
-    /* Check Loader -- endian */
-    if (luaLoadInteger(hdr.integer_valid_data) != LUAC_INT_VALIDATION){
-        eprintf("Integer Format Not Matched\n");
-        return ret;
-    }
-    if (luaLoadNumber(hdr.number_valid_data) != LUAC_NUMBER_VALIDATION){
-        eprintf("Number Format Not Matched\n");
-        return ret;
-    }
+	rz_buf_read_at(bf->buf, LUAC_HDR_SIZE_54, work_buffer, INNER_BUFFER_SIZE);
+	char *src_file = luaLoadString(work_buffer);
 
-    rz_buf_read_at(bf->buf, LUAC_HDR_SIZE_54, work_buffer, INNER_BUFFER_SIZE);
-    char *src_file = luaLoadString(work_buffer);
+	/* put source file info into GUID */
+	if (src_file == NULL) {
+		ret->guid = strdup("stripped");
+		return ret;
+	}
+	ret->guid = strdup(src_file);
+	free(src_file);
 
-    /* put source file info into GUID */
-    if (src_file == NULL){
-        ret->guid = strdup("stripped");
-        return ret;
-    }
-    ret->guid = strdup(src_file);
-    free(src_file);
-    
-    return ret;
+	return ret;
 }
--- librz/bin/p/bin_luac.c
+++ librz/bin/p/bin_luac.c
@@ -9,54 +9,54 @@ static int MINOR_VERSION;
 static ut8 MAJOR_MINOR_VERSION;
 
 static bool check_buffer(RzBuffer *buff) {
-    if (rz_buf_size(buff) > 4 ){
-        ut8 buf[4];
-        rz_buf_read_at(buff, 0, buf, sizeof(buf));
-        return (!memcmp(buf, LUAC_MAGIC, sizeof(buf)));
-    }
-    return false;
+	if (rz_buf_size(buff) > 4) {
+		ut8 buf[4];
+		rz_buf_read_at(buff, 0, buf, sizeof(buf));
+		return (!memcmp(buf, LUAC_MAGIC, sizeof(buf)));
+	}
+	return false;
 }
 
 static bool load_buffer(RzBinFile *bf, void **bin_obj, RzBuffer *buf, ut64 loadaddr, Sdb *sdb) {
-    rz_buf_read_at(buf, 4, &MAJOR_MINOR_VERSION, sizeof(MAJOR_MINOR_VERSION));        /* 1-byte in fact */
-    MAJOR_VERSION = (MAJOR_MINOR_VERSION & 0xF0) >> 4;
-    MINOR_VERSION = MAJOR_MINOR_VERSION & 0x0F;
-    return check_buffer(buf);
+	rz_buf_read_at(buf, 4, &MAJOR_MINOR_VERSION, sizeof(MAJOR_MINOR_VERSION)); /* 1-byte in fact */
+	MAJOR_VERSION = (MAJOR_MINOR_VERSION & 0xF0) >> 4;
+	MINOR_VERSION = MAJOR_MINOR_VERSION & 0x0F;
+	return check_buffer(buf);
 }
 
 static RzBinInfo *info(RzBinFile *bf) {
-    if (MAJOR_VERSION != 5){
-        eprintf("currently support lua 5.x only\n");
-        return NULL;
-    }
+	if (MAJOR_VERSION != 5) {
+		eprintf("currently support lua 5.x only\n");
+		return NULL;
+	}
 
-    switch (MINOR_VERSION) {
-        case 4:
-            return info_54(bf, MAJOR_VERSION, MINOR_VERSION);
-            break;
-        default:
-            eprintf("lua 5.%c not support now\n", MINOR_VERSION + '0');
-            return NULL;
-    }
+	switch (MINOR_VERSION) {
+	case 4:
+		return info_54(bf, MAJOR_VERSION, MINOR_VERSION);
+		break;
+	default:
+		eprintf("lua 5.%c not support now\n", MINOR_VERSION + '0');
+		return NULL;
+	}
 }
 
 RzBinPlugin rz_bin_plugin_luac = {
-        .name = "luac",
-        .desc = "LUA Compiled File",
-        .license = "LGPL3",
-        .get_sdb = NULL,
-        .load_buffer = &load_buffer,
-        .check_buffer = &check_buffer,
-        .baddr = NULL,
-        .entries = NULL,
-        .sections = NULL,
-        .info = &info
+	.name = "luac",
+	.desc = "LUA Compiled File",
+	.license = "LGPL3",
+	.get_sdb = NULL,
+	.load_buffer = &load_buffer,
+	.check_buffer = &check_buffer,
+	.baddr = NULL,
+	.entries = NULL,
+	.sections = NULL,
+	.info = &info
 };
 
 #ifndef RZ_PLUGIN_INCORE
 RZ_API RzLibStruct rizin_plugin = {
-        .type = RZ_LIB_TYPE_BIN,
-        .data = &rz_bin_plugin_luac,
-        .version = RZ_VERSION
+	.type = RZ_LIB_TYPE_BIN,
+	.data = &rz_bin_plugin_luac,
+	.version = RZ_VERSION
 };
 #endif
