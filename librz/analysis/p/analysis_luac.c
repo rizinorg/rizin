040267e6ddf02511796b5adf34d6bf4ff867d9a2
Heersin <teablearcher@gmail.com>
2021-03-10 19:34:32 +0800

    Init source file and update meson script for luac analysis plugin

---
+++ librz/analysis/p/analysis_luac.c
@@ -0,0 +1,11 @@
+
+#include <string.h>
+#include <rz_types.h>
+#include <rz_lib.h>
+#include <rz_analysis.h>
+
+#include "../asm/arch/luac/luac_dis.h"
+
+int lua_anal_op(RzAnalysis *analysis, RzAnalysisOp *op, ut64 addr, const ut8 *data, int len){
+	
+}
