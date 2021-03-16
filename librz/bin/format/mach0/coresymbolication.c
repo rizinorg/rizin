radareorg/radare2

Add Support for new CoreSymbolication element format ##bin (#18392)

libr/bin/format/mach0/coresymbolication.c
@@ -157,7 +157,7 @@ static char *str_dup_safe_fixed(const ut8 *b, const ut8 *str, ut64 len, const ut
 	return NULL;
 }
 
-RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut64 off, int bits) {
+RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut64 off, int bits, char * file_name) {
 	RCoreSymCacheElement *result = NULL;
 	ut8 *b = NULL;
 	RCoreSymCacheElementHdr *hdr = r_coresym_cache_element_header_new (buf, off, bits);
@@ -185,13 +185,23 @@ RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut
 		goto beach;
 	}
 	ut8 *end = b + hdr->size;
-	if (hdr->file_name_off) {
+	if (file_name) {
+		result->file_name = file_name;
+	} else if (hdr->file_name_off) {
 		result->file_name = str_dup_safe (b, b + (size_t)hdr->file_name_off, end);
 	}
 	if (hdr->version_off) {
 		result->binary_version = str_dup_safe (b, b + (size_t)hdr->version_off, end);
 	}
 	const size_t word_size = bits / 8;
+	const ut64 start_of_sections = (ut64)hdr->n_segments * R_CS_EL_SIZE_SEG + R_CS_EL_OFF_SEGS;
+	const ut64 sect_size = (bits == 32) ? R_CS_EL_SIZE_SECT_32 : R_CS_EL_SIZE_SECT_64;
+	const ut64 start_of_symbols = start_of_sections + (ut64)hdr->n_sections * sect_size;
+	const ut64 start_of_lined_symbols = start_of_symbols + (ut64)hdr->n_symbols * R_CS_EL_SIZE_SYM;
+	const ut64 start_of_line_info = start_of_lined_symbols + (ut64)hdr->n_lined_symbols * R_CS_EL_SIZE_LSYM;
+	const ut64 start_of_unknown_pairs = start_of_line_info + (ut64)hdr->n_line_info * R_CS_EL_SIZE_LINFO;
+	const ut64 start_of_strings = start_of_unknown_pairs + (ut64)hdr->n_symbols * 8;
+
 	ut64 page_zero_size = 0;
 	size_t page_zero_idx = 0;
 	if (hdr->n_segments > 0) {
@@ -236,7 +246,8 @@ RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut
 			}
 		}
 	}
-	const ut64 start_of_sections = (ut64)hdr->n_segments * R_CS_EL_SIZE_SEG + R_CS_EL_OFF_SEGS;
+	bool relative_to_strings = false;
+	ut8* string_origin;
 	if (hdr->n_sections > 0) {
 		result->sections = R_NEWS0 (RCoreSymCacheElementSection, hdr->n_sections);
 		if (!result->sections) {
@@ -261,15 +272,17 @@ RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut
 				break;
 			}
 			ut64 sect_name_off = r_read_ble (cursor, false, bits);
+			if (!i && !sect_name_off) {
+				relative_to_strings = true;
+			}
 			cursor += word_size;
 			if (bits == 32) {
 				cursor += word_size;
 			}
-			sect->name = str_dup_safe (b, sect_start + (size_t)sect_name_off, end);
+			string_origin = relative_to_strings? b + start_of_strings : sect_start;
+			sect->name = str_dup_safe (b, string_origin + (size_t)sect_name_off, end);
 		}
 	}
-	const ut64 sect_size = (bits == 32) ? R_CS_EL_SIZE_SECT_32 : R_CS_EL_SIZE_SECT_64;
-	const ut64 start_of_symbols = start_of_sections + (ut64)hdr->n_sections * sect_size;
 	if (hdr->n_symbols) {
 		result->symbols = R_NEWS0 (RCoreSymCacheElementSymbol, hdr->n_symbols);
 		if (!result->symbols) {
@@ -285,12 +298,14 @@ RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut
 			size_t name_off = r_read_le32 (cursor + 0xc);
 			size_t mangled_name_off = r_read_le32 (cursor + 0x10);
 			sym->unk2 = (st32)r_read_le32 (cursor + 0x14);
-			sym->name = str_dup_safe (b, cursor + name_off, end);
+			string_origin = relative_to_strings? b + start_of_strings : cursor;
+			sym->name = str_dup_safe (b, string_origin + name_off, end);
 			if (!sym->name) {
 				cursor += R_CS_EL_SIZE_SYM;
 				continue;
 			}
-			sym->mangled_name = str_dup_safe (b, cursor + mangled_name_off, end);
+			string_origin = relative_to_strings? b + start_of_strings : cursor;
+			sym->mangled_name = str_dup_safe (b, string_origin + mangled_name_off, end);
 			if (!sym->mangled_name) {
 				cursor += R_CS_EL_SIZE_SYM;
 				continue;
@@ -298,7 +313,6 @@ RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut
 			cursor += R_CS_EL_SIZE_SYM;
 		}
 	}
-	const ut64 start_of_lined_symbols = start_of_symbols + (ut64)hdr->n_symbols * R_CS_EL_SIZE_SYM;
 	if (hdr->n_lined_symbols) {
 		result->lined_symbols = R_NEWS0 (RCoreSymCacheElementLinedSymbol, hdr->n_lined_symbols);
 		if (!result->lined_symbols) {
@@ -317,17 +331,20 @@ RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut
 			size_t file_name_off = r_read_le32 (cursor + 0x18);
 			lsym->flc.line = r_read_le32 (cursor + 0x1c);
 			lsym->flc.col = r_read_le32 (cursor + 0x20);
-			lsym->sym.name = str_dup_safe (b, cursor + name_off, end);
+			string_origin = relative_to_strings? b + start_of_strings : cursor;
+			lsym->sym.name = str_dup_safe (b, string_origin + name_off, end);
 			if (!lsym->sym.name) {
 				cursor += R_CS_EL_SIZE_LSYM;
 				continue;
 			}
-			lsym->sym.mangled_name = str_dup_safe (b, cursor + mangled_name_off, end);
+			string_origin = relative_to_strings? b + start_of_strings : cursor;
+			lsym->sym.mangled_name = str_dup_safe (b, string_origin + mangled_name_off, end);
 			if (!lsym->sym.mangled_name) {
 				cursor += R_CS_EL_SIZE_LSYM;
 				continue;
 			}
-			lsym->flc.file = str_dup_safe (b, cursor + file_name_off, end);
+			string_origin = relative_to_strings? b + start_of_strings : cursor;
+			lsym->flc.file = str_dup_safe (b, string_origin + file_name_off, end);
 			if (!lsym->flc.file) {
 				cursor += R_CS_EL_SIZE_LSYM;
 				continue;
@@ -336,7 +353,6 @@ RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut
 			meta_add_fileline (bf, r_coresym_cache_element_pa2va (result, lsym->sym.paddr), lsym->sym.size, &lsym->flc);
 		}
 	}
-	const ut64 start_of_line_info = start_of_lined_symbols + (ut64)hdr->n_lined_symbols * R_CS_EL_SIZE_LSYM;
 	if (hdr->n_line_info) {
 		result->line_info = R_NEWS0 (RCoreSymCacheElementLineInfo, hdr->n_line_info);
 		if (!result->line_info) {
@@ -351,7 +367,8 @@ RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut
 			size_t file_name_off = r_read_le32 (cursor + 8);
 			info->flc.line = r_read_le32 (cursor + 0xc);
 			info->flc.col = r_read_le32 (cursor + 0x10);
-			info->flc.file = str_dup_safe (b, cursor + file_name_off, end);
+			string_origin = relative_to_strings? b + start_of_strings : cursor;
+			info->flc.file = str_dup_safe (b, string_origin + file_name_off, end);
 			if (!info->flc.file) {
 				break;
 			}

libr/bin/format/mach0/coresymbolication.h
@@ -78,7 +78,7 @@ typedef struct r_coresym_cache_element_t {
 	RCoreSymCacheElementLineInfo *line_info;
 } RCoreSymCacheElement;
 
-R_API RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut64 off, int bits);
+R_API RCoreSymCacheElement *r_coresym_cache_element_new(RBinFile *bf, RBuffer *buf, ut64 off, int bits, R_OWN char * file_name);
 R_API void r_coresym_cache_element_free(RCoreSymCacheElement *element);
 R_API ut64 r_coresym_cache_element_pa2va(RCoreSymCacheElement *element, ut64 pa);
 

libr/bin/p/bin_symbols.c
@@ -118,20 +118,6 @@ static SymbolsMetadata parseMetadata(RBuffer *buf, int off) {
 	return sm;
 }
 
-static void printSymbolsHeader(SymbolsHeader sh) {
-	// eprintf ("0x%08x  version  0x%x\n", 4, sh.version);
-	eprintf ("0x%08x  uuid     ", 24);
-	int i;
-	for (i = 0; i < 16; i++) {
-		eprintf ("%02x", sh.uuid[i]);
-	}
-	eprintf ("\n");
-	// parse header
-	// eprintf ("0x%08x  unknown  0x%x\n", 0x28, sh.unk0); //r_read_le32 (b+ 0x28));
-	// eprintf ("0x%08x  unknown  0x%x\n", 0x2c, sh.unk1); //r_read_le16 (b+ 0x2c));
-	// eprintf ("0x%08x  slotsize %d\n", 0x2e, sh.slotsize); // r_read_le16 (b+ 0x2e));
-}
-
 static RBinSection *bin_section_from_section(RCoreSymCacheElementSection *sect) {
 	if (!sect->name) {
 		return NULL;
@@ -193,7 +179,7 @@ static RBinSymbol *bin_symbol_from_symbol(RCoreSymCacheElement *element, RCoreSy
 	return sym;
 }
 
-static RCoreSymCacheElement *parseDragons(RBinFile *bf, RBuffer *buf, int off, int bits) {
+static RCoreSymCacheElement *parseDragons(RBinFile *bf, RBuffer *buf, int off, int bits, R_OWN char *file_name) {
 	D eprintf ("Dragons at 0x%x\n", off);
 	ut64 size = r_buf_size (buf);
 	if (off >= size) {
@@ -261,7 +247,7 @@ static RCoreSymCacheElement *parseDragons(RBinFile *bf, RBuffer *buf, int off, i
 		eprintf ("0x%08x  eoss   0x%x\n", off + 12, e0ss);
 	}
 	free (b);
-	return r_coresym_cache_element_new (bf, buf, off + 16, bits);
+	return r_coresym_cache_element_new (bf, buf, off + 16, bits, file_name);
 }
 
 static bool load_buffer(RBinFile *bf, void **bin_obj, RBuffer *buf, ut64 loadaddr, Sdb *sdb) {
@@ -289,13 +275,23 @@ static bool load_buffer(RBinFile *bf, void **bin_obj, RBuffer *buf, ut64 loadadd
 		eprintf ("Invalid headers\n");
 		return false;
 	}
-	printSymbolsHeader (sh);
 	SymbolsMetadata sm = parseMetadata (buf, 0x40);
-	RCoreSymCacheElement *element = parseDragons (bf, buf, sm.addr + sm.size, sm.bits);
+	char * file_name = NULL;
+	if (sm.namelen) {
+		file_name = calloc (sm.namelen + 1, 1);
+		if (!file_name) {
+			return false;
+		}
+		if (r_buf_read_at (buf, 0x50, (ut8*)file_name, sm.namelen) != sm.namelen) {
+			return false;
+		}
+	}
+	RCoreSymCacheElement *element = parseDragons (bf, buf, sm.addr + sm.size, sm.bits, file_name);
 	if (element) {
 		*bin_obj = element;
 		return true;
 	}
+	free (file_name);
 	return false;
 }
 

rizinorg/rizin

librz/bin/format/mach0/coresymbolication.c
@@ -158,7 +158,7 @@ static char *str_dup_safe_fixed(const ut8 *b, const ut8 *str, ut64 len, const ut
 	return NULL;
 }
 
-RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf, ut64 off, int bits) {
+RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf, ut64 off, int bits, char *file_name) {
 	RzCoreSymCacheElement *result = NULL;
 	ut8 *b = NULL;
 	RzCoreSymCacheElementHdr *hdr = rz_coresym_cache_element_header_new(buf, off, bits);
@@ -186,13 +186,23 @@ RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf
 		goto beach;
 	}
 	ut8 *end = b + hdr->size;
-	if (hdr->file_name_off) {
+	if (file_name) {
+		result->file_name = file_name;
+	} else if (hdr->file_name_off) {
 		result->file_name = str_dup_safe(b, b + (size_t)hdr->file_name_off, end);
 	}
 	if (hdr->version_off) {
 		result->binary_version = str_dup_safe(b, b + (size_t)hdr->version_off, end);
 	}
 	const size_t word_size = bits / 8;
+	const ut64 start_of_sections = (ut64)hdr->n_segments * RZ_CS_EL_SIZE_SEG + RZ_CS_EL_OFF_SEGS;
+	const ut64 sect_size = (bits == 32) ? RZ_CS_EL_SIZE_SECT_32 : RZ_CS_EL_SIZE_SECT_64;
+	const ut64 start_of_symbols = start_of_sections + (ut64)hdr->n_sections * sect_size;
+	const ut64 start_of_lined_symbols = start_of_symbols + (ut64)hdr->n_symbols * RZ_CS_EL_SIZE_SYM;
+	const ut64 start_of_line_info = start_of_lined_symbols + (ut64)hdr->n_lined_symbols * RZ_CS_EL_SIZE_LSYM;
+	const ut64 start_of_unknown_pairs = start_of_line_info + (ut64)hdr->n_line_info * RZ_CS_EL_SIZE_LINFO;
+	const ut64 start_of_strings = start_of_unknown_pairs + (ut64)hdr->n_symbols * 8;
+
 	ut64 page_zero_size = 0;
 	size_t page_zero_idx = 0;
 	if (hdr->n_segments > 0) {
@@ -237,7 +247,8 @@ RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf
 			}
 		}
 	}
-	const ut64 start_of_sections = (ut64)hdr->n_segments * RZ_CS_EL_SIZE_SEG + RZ_CS_EL_OFF_SEGS;
+	bool relative_to_strings = false;
+	ut8 *string_origin;
 	if (hdr->n_sections > 0) {
 		result->sections = RZ_NEWS0(RzCoreSymCacheElementSection, hdr->n_sections);
 		if (!result->sections) {
@@ -262,15 +273,17 @@ RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf
 				break;
 			}
 			ut64 sect_name_off = rz_read_ble(cursor, false, bits);
+			if (!i && !sect_name_off) {
+				relative_to_strings = true;
+			}
 			cursor += word_size;
 			if (bits == 32) {
 				cursor += word_size;
 			}
-			sect->name = str_dup_safe(b, sect_start + (size_t)sect_name_off, end);
+			string_origin = relative_to_strings ? b + start_of_strings : sect_start;
+			sect->name = str_dup_safe(b, string_origin + (size_t)sect_name_off, end);
 		}
 	}
-	const ut64 sect_size = (bits == 32) ? RZ_CS_EL_SIZE_SECT_32 : RZ_CS_EL_SIZE_SECT_64;
-	const ut64 start_of_symbols = start_of_sections + (ut64)hdr->n_sections * sect_size;
 	if (hdr->n_symbols) {
 		result->symbols = RZ_NEWS0(RzCoreSymCacheElementSymbol, hdr->n_symbols);
 		if (!result->symbols) {
@@ -286,12 +299,14 @@ RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf
 			size_t name_off = rz_read_le32(cursor + 0xc);
 			size_t mangled_name_off = rz_read_le32(cursor + 0x10);
 			sym->unk2 = (st32)rz_read_le32(cursor + 0x14);
-			sym->name = str_dup_safe(b, cursor + name_off, end);
+			string_origin = relative_to_strings ? b + start_of_strings : cursor;
+			sym->name = str_dup_safe(b, string_origin + name_off, end);
 			if (!sym->name) {
 				cursor += RZ_CS_EL_SIZE_SYM;
 				continue;
 			}
-			sym->mangled_name = str_dup_safe(b, cursor + mangled_name_off, end);
+			string_origin = relative_to_strings ? b + start_of_strings : cursor;
+			sym->mangled_name = str_dup_safe(b, string_origin + mangled_name_off, end);
 			if (!sym->mangled_name) {
 				cursor += RZ_CS_EL_SIZE_SYM;
 				continue;
@@ -299,7 +314,6 @@ RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf
 			cursor += RZ_CS_EL_SIZE_SYM;
 		}
 	}
-	const ut64 start_of_lined_symbols = start_of_symbols + (ut64)hdr->n_symbols * RZ_CS_EL_SIZE_SYM;
 	if (hdr->n_lined_symbols) {
 		result->lined_symbols = RZ_NEWS0(RzCoreSymCacheElementLinedSymbol, hdr->n_lined_symbols);
 		if (!result->lined_symbols) {
@@ -318,17 +332,20 @@ RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf
 			size_t file_name_off = rz_read_le32(cursor + 0x18);
 			lsym->flc.line = rz_read_le32(cursor + 0x1c);
 			lsym->flc.col = rz_read_le32(cursor + 0x20);
-			lsym->sym.name = str_dup_safe(b, cursor + name_off, end);
+			string_origin = relative_to_strings ? b + start_of_strings : cursor;
+			lsym->sym.name = str_dup_safe(b, string_origin + name_off, end);
 			if (!lsym->sym.name) {
 				cursor += RZ_CS_EL_SIZE_LSYM;
 				continue;
 			}
-			lsym->sym.mangled_name = str_dup_safe(b, cursor + mangled_name_off, end);
+			string_origin = relative_to_strings ? b + start_of_strings : cursor;
+			lsym->sym.mangled_name = str_dup_safe(b, string_origin + mangled_name_off, end);
 			if (!lsym->sym.mangled_name) {
 				cursor += RZ_CS_EL_SIZE_LSYM;
 				continue;
 			}
-			lsym->flc.file = str_dup_safe(b, cursor + file_name_off, end);
+			string_origin = relative_to_strings ? b + start_of_strings : cursor;
+			lsym->flc.file = str_dup_safe(b, string_origin + file_name_off, end);
 			if (!lsym->flc.file) {
 				cursor += RZ_CS_EL_SIZE_LSYM;
 				continue;
@@ -337,7 +354,6 @@ RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf
 			meta_add_fileline(bf, rz_coresym_cache_element_pa2va(result, lsym->sym.paddr), lsym->sym.size, &lsym->flc);
 		}
 	}
-	const ut64 start_of_line_info = start_of_lined_symbols + (ut64)hdr->n_lined_symbols * RZ_CS_EL_SIZE_LSYM;
 	if (hdr->n_line_info) {
 		result->line_info = RZ_NEWS0(RzCoreSymCacheElementLineInfo, hdr->n_line_info);
 		if (!result->line_info) {
@@ -352,7 +368,8 @@ RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf
 			size_t file_name_off = rz_read_le32(cursor + 8);
 			info->flc.line = rz_read_le32(cursor + 0xc);
 			info->flc.col = rz_read_le32(cursor + 0x10);
-			info->flc.file = str_dup_safe(b, cursor + file_name_off, end);
+			string_origin = relative_to_strings ? b + start_of_strings : cursor;
+			info->flc.file = str_dup_safe(b, string_origin + file_name_off, end);
 			if (!info->flc.file) {
 				break;
 			}

librz/bin/format/mach0/coresymbolication.h
@@ -78,7 +78,7 @@ typedef struct rz_coresym_cache_element_t {
 	RzCoreSymCacheElementLineInfo *line_info;
 } RzCoreSymCacheElement;
 
-RZ_API RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf, ut64 off, int bits);
+RZ_API RzCoreSymCacheElement *rz_coresym_cache_element_new(RzBinFile *bf, RzBuffer *buf, ut64 off, int bits, RZ_OWN char *file_name);
 RZ_API void rz_coresym_cache_element_free(RzCoreSymCacheElement *element);
 RZ_API ut64 rz_coresym_cache_element_pa2va(RzCoreSymCacheElement *element, ut64 pa);
 

librz/bin/p/bin_symbols.c
@@ -119,20 +119,6 @@ static SymbolsMetadata parseMetadata(RzBuffer *buf, int off) {
 	return sm;
 }
 
-static void printSymbolsHeader(SymbolsHeader sh) {
-	// eprintf ("0x%08x  version  0x%x\n", 4, sh.version);
-	eprintf("0x%08x  uuid     ", 24);
-	int i;
-	for (i = 0; i < 16; i++) {
-		eprintf("%02x", sh.uuid[i]);
-	}
-	eprintf("\n");
-	// parse header
-	// eprintf ("0x%08x  unknown  0x%x\n", 0x28, sh.unk0); //rz_read_le32 (b+ 0x28));
-	// eprintf ("0x%08x  unknown  0x%x\n", 0x2c, sh.unk1); //rz_read_le16 (b+ 0x2c));
-	// eprintf ("0x%08x  slotsize %d\n", 0x2e, sh.slotsize); // rz_read_le16 (b+ 0x2e));
-}
-
 static RzBinSection *bin_section_from_section(RzCoreSymCacheElementSection *sect) {
 	if (!sect->name) {
 		return NULL;
@@ -194,7 +180,7 @@ static RzBinSymbol *bin_symbol_from_symbol(RzCoreSymCacheElement *element, RzCor
 	return sym;
 }
 
-static RzCoreSymCacheElement *parseDragons(RzBinFile *bf, RzBuffer *buf, int off, int bits) {
+static RzCoreSymCacheElement *parseDragons(RzBinFile *bf, RzBuffer *buf, int off, int bits, RZ_OWN char *file_name) {
 	D eprintf("Dragons at 0x%x\n", off);
 	ut64 size = rz_buf_size(buf);
 	if (off >= size) {
@@ -262,7 +248,7 @@ static RzCoreSymCacheElement *parseDragons(RzBinFile *bf, RzBuffer *buf, int off
 		eprintf("0x%08x  eoss   0x%x\n", off + 12, e0ss);
 	}
 	free(b);
-	return rz_coresym_cache_element_new(bf, buf, off + 16, bits);
+	return rz_coresym_cache_element_new(bf, buf, off + 16, bits, file_name);
 }
 
 static bool load_buffer(RzBinFile *bf, void **bin_obj, RzBuffer *buf, ut64 loadaddr, Sdb *sdb) {
@@ -290,13 +276,23 @@ static bool load_buffer(RzBinFile *bf, void **bin_obj, RzBuffer *buf, ut64 loada
 		eprintf("Invalid headers\n");
 		return false;
 	}
-	printSymbolsHeader(sh);
 	SymbolsMetadata sm = parseMetadata(buf, 0x40);
-	RzCoreSymCacheElement *element = parseDragons(bf, buf, sm.addr + sm.size, sm.bits);
+	char *file_name = NULL;
+	if (sm.namelen) {
+		file_name = calloc(sm.namelen + 1, 1);
+		if (!file_name) {
+			return false;
+		}
+		if (rz_buf_read_at(buf, 0x50, (ut8 *)file_name, sm.namelen) != sm.namelen) {
+			return false;
+		}
+	}
+	RzCoreSymCacheElement *element = parseDragons(bf, buf, sm.addr + sm.size, sm.bits, file_name);
 	if (element) {
 		*bin_obj = element;
 		return true;
 	}
+	free(file_name);
 	return false;
 }
 