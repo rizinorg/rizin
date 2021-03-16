a866544de6bb091807f51ddcf3ebe7bfd6b4e430
Heersin <56885667+Heersin@users.noreply.github.com>
2021-03-16 22:38:30 +0800

    Pyc plugin segmentation fault fix and add tests (#822)
    * Patch segmentation fault when fetching version in pyc_opcodes_equal
    * Add tests

--- librz/asm/arch/pyc/opcode.c
+++ librz/asm/arch/pyc/opcode.c
@@ -127,6 +127,9 @@ static version_opcode version_op[] = {
 };
 
 bool pyc_opcodes_equal(pyc_opcodes *op, const char *version) {
+	if (version == NULL || op == NULL) {
+		return false;
+	}
 	version_opcode *vop = version_op;
 
 	while (vop->version) {
@@ -142,6 +145,9 @@ bool pyc_opcodes_equal(pyc_opcodes *op, const char *version) {
 }
 
 pyc_opcodes *get_opcode_by_version(char *version) {
+	if (version == NULL) {
+		return NULL;
+	}
 	version_opcode *vop = version_op;
 
 	while (vop->version) {
@@ -188,11 +194,18 @@ pyc_opcodes *new_pyc_opcodes() {
 
 void free_opcode(pyc_opcodes *opcodes) {
 	size_t i;
+	if (opcodes == NULL || opcodes->opcodes == NULL) {
+		return;
+	}
 	for (i = 0; i < 256; i++) {
-		free(opcodes->opcodes[i].op_name);
+		if (opcodes->opcodes[i].op_name) {
+			free(opcodes->opcodes[i].op_name);
+		}
 	}
 	free(opcodes->opcodes);
-	rz_list_free(opcodes->opcode_arg_fmt);
+	if (opcodes->opcode_arg_fmt) {
+		rz_list_free(opcodes->opcode_arg_fmt);
+	}
 	free(opcodes);
 }
 
