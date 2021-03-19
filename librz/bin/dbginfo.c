46fb5cdb8306aafe8c59fa2e15ab7bbc67b59356
Florian Märkl <info@florianmaerkl.de>
2021-03-19 14:18:12 +0100

    Add new Line Data Structure

--- librz/bin/dbginfo.c
+++ librz/bin/dbginfo.c
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2009-2020 nibble <nibble.ds@gmail.com>
 // SPDX-FileCopyrightText: 2009-2020 pancake <pancake@nopcode.org>
+// SPDX-FileCopyrightText: 2021 thestr4ng3r <info@florianmaerkl.de>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #include <rz_types.h>
@@ -11,6 +12,197 @@ RZ_API void rz_bin_source_row_free(RzBinSourceRow *row) {
 	}
 	free(row->file);
 	free(row);
+}
+
+RZ_API void rz_bin_source_line_info_builder_init(RzBinSourceLineInfoBuilder *builder) {
+	rz_vector_init(&builder->samples, sizeof(RzBinSourceLineSample), NULL, NULL);
+	rz_str_constpool_init(&builder->filename_pool);
+}
+
+RZ_API void rz_bin_source_line_info_builder_fini(RzBinSourceLineInfoBuilder *builder) {
+	rz_vector_fini(&builder->samples);
+	rz_str_constpool_fini(&builder->filename_pool);
+}
+
+/**
+ * \brief Push a new sample into the builder
+ *
+ * This function is used to continuously fill the builder with concrete samples of line info for a specific address,
+ * usually during parsing of debug info from a file.
+ * The samples may be pushed in any order and the builder will later take care of generating a valid RzBinSourceLineInfo from it.
+ *
+ * \param line may be 0 or a positive line number, where 0 means that this entry closes the one before it. see also RzBinSourceLine.
+ */
+RZ_API void rz_bin_source_line_info_builder_push_sample(RzBinSourceLineInfoBuilder *builder, ut64 address, ut32 line, ut32 column, const char *file) {
+	RzBinSourceLineSample *sample = rz_vector_push(&builder->samples, NULL);
+	if (!sample) {
+		return;
+	}
+	sample->address = address;
+	sample->line = line;
+	sample->column = column;
+	sample->file = file ? rz_str_constpool_get(&builder->filename_pool, file) : NULL;
+}
+
+static int line_sample_cmp(const void *a, const void *b) {
+	const RzBinSourceLineSample *sa = a;
+	const RzBinSourceLineSample *sb = b;
+	// first, sort by addr
+	if (sa->address < sb->address) {
+		return -1;
+	}
+	if (sa->address > sb->address) {
+		return 1;
+	}
+	// closing samples are always equal (rest of their fields are ignored anyway)
+	if (rz_bin_source_line_sample_is_closing(sa) && rz_bin_source_line_sample_is_closing(sb)) {
+		return 0;
+	}
+	// push closing samples to the back, which is necessary to skip them during packing
+	if (rz_bin_source_line_sample_is_closing(sa)) {
+		return 1;
+	}
+	if (rz_bin_source_line_sample_is_closing(sb)) {
+		return -1;
+	}
+	// then sort by line
+	if (sa->line < sb->line) {
+		return -1;
+	}
+	if (sa->line > sb->line) {
+		return 1;
+	}
+	// then by column
+	if (sa->column < sb->column) {
+		return -1;
+	}
+	if (sa->column > sb->column) {
+		return 1;
+	}
+	// and eventually by file because this is the most exponsive operation
+	if (!sa->file && !sb->file) {
+		return 0;
+	}
+	if (!sa->file) {
+		return -1;
+	}
+	if (!sb->file) {
+		return 1;
+	}
+	return strcmp(sa->file, sb->file);
+}
+
+RZ_API RzBinSourceLineInfo *rz_bin_source_line_info_builder_build_and_fini(RzBinSourceLineInfoBuilder *builder) {
+	RzBinSourceLineInfo *r = RZ_NEW0(RzBinSourceLineInfo);
+	if (!r) {
+		goto err;
+	}
+	size_t initial_samples_count = rz_vector_len(&builder->samples); // final count may be less after removing unnecessary closing samples
+	if (initial_samples_count) {
+		r->samples = RZ_NEWS0(RzBinSourceLineSample, initial_samples_count);
+		if (!r->samples) {
+			goto err_r;
+		}
+
+		// samples should be built in flat RzVector to avoid excessive small mallocs,
+		// for sorting we use a pvector with references into our flat vectors (after flushing them).
+
+		RzPVector sorter;
+		rz_pvector_init(&sorter, NULL);
+		RzBinSourceLineSample *initial_samples = rz_vector_flush(&builder->samples);
+		rz_pvector_reserve(&sorter, initial_samples_count);
+		for (size_t i = 0; i < initial_samples_count; i++) {
+			rz_pvector_push(&sorter, &initial_samples[i]);
+		}
+		rz_pvector_sort(&sorter, line_sample_cmp);
+
+		r->samples_count = 0;
+		for (size_t i = 0; i < initial_samples_count; i++) {
+			RzBinSourceLineSample *new_sample = rz_pvector_at(&sorter, i);
+			if (r->samples_count) {
+				RzBinSourceLineSample *prev = &r->samples[r->samples_count - 1];
+				if (prev->address == new_sample->address && rz_bin_source_line_sample_is_closing(new_sample)) {
+					// closing sample but there are others that are not closing so this is dropped
+					continue;
+				}
+			}
+			r->samples[r->samples_count++] = *new_sample;
+		}
+		free(initial_samples); // all inner strings are moved already
+		rz_pvector_fini(&sorter);
+	}
+	r->filename_pool = builder->filename_pool;
+	// don't call regular fini on the builder because we moved its string pool!
+	rz_vector_fini(&builder->samples);
+	return r;
+err_r:
+	free(r);
+err:
+	rz_bin_source_line_info_builder_fini(builder);
+	return NULL;
+}
+
+RZ_API void rz_bin_source_line_info_free(RzBinSourceLineInfo *sli) {
+	if (!sli) {
+		return;
+	}
+	free(sli->samples);
+	rz_str_constpool_fini(&sli->filename_pool);
+	free(sli);
+}
+
+/**
+ * \brief Find the first sample that affects the given address.
+ * i.e. find the first sample with the highest address less or equal to addr.
+ * There may be more which can be retrieved by repeatedly calling rz_bin_source_line_info_get_next() until it returns NULL.
+ */
+RZ_API const RzBinSourceLineSample *rz_bin_source_line_info_get_first_at(const RzBinSourceLineInfo *sli, ut64 addr) {
+	if (!sli->samples_count) {
+		return NULL;
+	}
+	// binary search
+	size_t l = 0;
+	size_t h = sli->samples_count;
+	while (l < h - 1) {
+		size_t m = l + ((h - l) >> 1);
+		if (addr < sli->samples[m].address) {
+			h = m;
+		} else {
+			l = m;
+		}
+	}
+	if (l >= sli->samples_count) {
+		return NULL;
+	}
+	RzBinSourceLineSample *r = &sli->samples[l];
+	if (r->address > addr || rz_bin_source_line_sample_is_closing(r)) {
+		return NULL;
+	}
+	// walk back to the very first entry with this addr
+	while (r > sli->samples) {
+		if ((r - 1)->address == r->address) {
+			r--;
+		} else {
+			break;
+		}
+	}
+	return r;
+}
+
+/**
+ * \param cur MUST be a pointer returned by either rz_bin_source_line_info_get_first_at() or rz_bin_source_line_info_get_next().
+ * \return The next sample at the same address as cur or NULL if there is none.
+ */
+RZ_API const RzBinSourceLineSample *rz_bin_source_line_info_get_next(const RzBinSourceLineInfo *sli, RZ_NONNULL const RzBinSourceLineSample *cur) {
+	rz_return_val_if_fail(sli && cur && cur >= sli->samples && cur < sli->samples + sli->samples_count, NULL);
+	if (cur == sli->samples + sli->samples_count - 1) {
+		return NULL;
+	}
+	const RzBinSourceLineSample *next = cur + 1;
+	if (next->address != cur->address) {
+		return NULL;
+	}
+	return next;
 }
 
 RZ_API bool rz_bin_addr2line(RzBin *bin, ut64 addr, char *file, int len, int *line) {
--- librz/include/rz_bin.h
+++ librz/include/rz_bin.h
@@ -9,6 +9,7 @@
 
 typedef struct rz_bin_t RzBin;
 typedef struct rz_bin_file_t RzBinFile;
+typedef struct rz_bin_source_line_info_t RzBinSourceLineInfo;
 
 #include <rz_bin_dwarf.h>
 #include <rz_pdb.h>
@@ -421,6 +422,84 @@ typedef struct rz_bin_trycatch_t {
 RZ_API RzBinTrycatch *rz_bin_trycatch_new(ut64 source, ut64 from, ut64 to, ut64 handler, ut64 filter);
 RZ_API void rz_bin_trycatch_free(RzBinTrycatch *tc);
 
+/**
+ * \brief A single sample of source line info for a specific address
+ *
+ * If at least one of the line, column and file members is not 0/NULL, such a sample specifies the line info
+ * for all addresses greater or equal to address until the next address that has another sample.
+ *
+ * If all the members line, column and file are 0/NULL, then this is a closing sample, indicating that the
+ * previous entry stops here. The address is the first address **not contained** by the previous record.
+ * Such a case corresponds for example to what DW_LNE_end_sequence emits in Dwarf.
+ * Use rz_bin_source_line_sample_is_closing() for checking if a sample is closing.
+ */
+typedef struct rz_bin_source_line_sample_t {
+	/**
+	 * The first address that is covered by the given line and column,
+	 * or, if all other members are 0/NULL, this is the first.
+	 */
+	ut64 address;
+
+	/**
+	 * If > 0, then indicates the line for the given address and the following.
+	 * If == 0, then indicates that no line information is known.
+	 *
+	 * 32bit for this value is an intentional decision to lower memory consumption.
+	 */
+	ut32 line;
+
+	/**
+	 * If > 0, then indicates the column.
+	 * If == 0, then no column information is known.
+	 *
+	 * 32bit for this value is an intentional decision to lower memory consumption.
+	 */
+	ut32 column;
+
+	/**
+	 * Filename, which must come out of the const pool of the owning
+	 * RzBinSourceLineInfo or RzBinSourceLineInfoBuilder.
+	 */
+	const char *file;
+} RzBinSourceLineSample;
+
+/*
+ * see documentation of RzBinSourceLineSample about what closing exactly means.
+ */
+static inline bool rz_bin_source_line_sample_is_closing(const RzBinSourceLineSample *s) {
+	return !s->line && !s->column && !s->file;
+}
+
+struct rz_bin_source_line_info_t {
+	/**
+	 * \brief All source line references for given adresses
+	 *
+	 * These elements must be sorted by address and addresses must be unique, so binary search can be applied.
+	 * Source file information is not contained within this array because source file changes
+	 * are generally much sparser than line changes.
+	 */
+	RzBinSourceLineSample *samples;
+	size_t samples_count;
+	RzStrConstPool filename_pool;
+}; // RzBinSourceLineInfo
+
+/**
+ * Temporary data structure for building an RzBinSourceLineInfo.
+ */
+typedef struct rz_bin_source_line_info_builder_t {
+	RzVector /*<RzBinSourceLineSample>*/ samples; //< may be unsorted and will be sorted in the finalization step
+	RzStrConstPool filename_pool;
+} RzBinSourceLineInfoBuilder;
+
+RZ_API void rz_bin_source_line_info_builder_init(RzBinSourceLineInfoBuilder *builder);
+RZ_API void rz_bin_source_line_info_builder_fini(RzBinSourceLineInfoBuilder *builder);
+RZ_API void rz_bin_source_line_info_builder_push_sample(RzBinSourceLineInfoBuilder *builder, ut64 address, ut32 line, ut32 column, const char *file);
+RZ_API RzBinSourceLineInfo *rz_bin_source_line_info_builder_build_and_fini(RzBinSourceLineInfoBuilder *builder);
+
+RZ_API void rz_bin_source_line_info_free(RzBinSourceLineInfo *sli);
+RZ_API const RzBinSourceLineSample *rz_bin_source_line_info_get_first_at(const RzBinSourceLineInfo *sli, ut64 addr);
+RZ_API const RzBinSourceLineSample *rz_bin_source_line_info_get_next(const RzBinSourceLineInfo *sli, RZ_NONNULL const RzBinSourceLineSample *cur);
+
 typedef struct {
 	/**
 	 * The first address that is covered by the given line and column,
@@ -443,7 +522,6 @@ typedef struct {
 	 */
 	unsigned int column;
 } RzBinSourceRow;
-
 RZ_API void rz_bin_source_row_free(RzBinSourceRow *row);
 
 typedef struct rz_bin_plugin_t {
