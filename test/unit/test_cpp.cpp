53afc63bf10fa6f7e57fd08ca6badcf1f66a0605
Florian Märkl <info@florianmaerkl.de>
2021-03-20 10:26:37 +0100

    Add a Test for C++ Compilation

---
+++ test/unit/test_cpp.cpp
@@ -0,0 +1,25 @@
+
+#include <rz_core.h>
+#include <rz_util.h>
+
+#include "minunit.h"
+
+/*
+ * This is mostly just a test for ensuring all headers can be
+ * compiled as C++.
+ */
+
+bool test_cpp(void) {
+	RzCore *core = rz_core_new();
+	char *r = rz_core_cmd_str(core, "?e Hello from C++!");
+	mu_assert_streq(r, "Hello from C++", "cmd");
+	rz_core_free(core);
+	mu_end;
+}
+
+bool all_tests() {
+	mu_run_test(test_cpp);
+	return tests_passed != tests_run;
+}
+
+mu_main(all_tests)
