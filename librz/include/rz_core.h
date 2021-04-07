acaa73355995ac6faeb0c36d589d64b3f5e0dec0
Florian Märkl <info@florianmaerkl.de>
2021-04-07 14:33:25 +0200

    Print Project Load Info on Success too

--- librz/include/rz_core.h
+++ librz/include/rz_core.h
@@ -1016,6 +1016,11 @@ RZ_API void rz_serialize_core_save(RZ_NONNULL Sdb *db, RZ_NONNULL RzCore *core, 
 RZ_API bool rz_serialize_core_load(RZ_NONNULL Sdb *db, RZ_NONNULL RzCore *core, bool load_bin_io,
 	RZ_NULLABLE const char *prj_file, RZ_NULLABLE RzSerializeResultInfo *res);
 
+/**
+ * \brief Load a project and print info and errors
+ */
+RZ_API bool rz_core_project_load_for_cli(RzCore *core, const char *file, bool load_bin_io);
+
 #endif
 
 #ifdef __cplusplus
