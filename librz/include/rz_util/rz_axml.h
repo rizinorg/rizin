bb50f10244c04e21038ba3d8cd672b5194ac9805
meme <18178821+meme@users.noreply.github.com>
2021-04-08 12:26:55 -0500

    Android Binary XML support (#18545) ##print
    Signed-off-by: Riccardo Schirone <sirmy15@gmail.com>

---
+++ librz/include/rz_util/rz_axml.h
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2021 keegan
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#ifndef RZ_AXML_H
+#define RZ_AXML_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+RZ_API char *rz_axml_decode(const ut8 *buffer, const ut64 size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //  RZ_AXML_H
