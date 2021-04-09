b3ee26e85f692db8394cc3471ce62decdc4fd63a
Anton Kochkov <anton.kochkov@gmail.com>
2021-04-09 13:01:26 +0800

    Fix Coverity CID 317139

--- librz/core/cbin.c
+++ librz/core/cbin.c
@@ -742,6 +742,7 @@ RZ_API bool rz_core_bin_apply_sections(RzCore *core, RzBinFile *binfile, bool va
 
 	RzBinObject *o = binfile->o;
 	if (!o) {
+		ht_pp_free(dup_chk_ht);
 		return false;
 	}
 	RzList *sections = o->sections;
