f472ac3995e0948b7e2a370798e8c0fe77e0cefe
Florian Märkl <info@florianmaerkl.de>
2021-04-14 21:52:12 +0200

    Fix FreeBSD Build for removed KF_TYPE_CRYPTO

--- librz/debug/p/native/bsd/bsd_debug.c
+++ librz/debug/p/native/bsd/bsd_debug.c
@@ -494,7 +494,10 @@ RzList *bsd_desc_list(int pid) {
 		case KF_TYPE_PIPE: type = 'p'; break;
 		case KF_TYPE_FIFO: type = 'f'; break;
 		case KF_TYPE_KQUEUE: type = 'k'; break;
+#if __FreeBSD_version < 1300130
+		// removed in https://reviews.freebsd.org/D27302
 		case KF_TYPE_CRYPTO: type = 'c'; break;
+#endif
 		case KF_TYPE_MQUEUE: type = 'm'; break;
 		case KF_TYPE_SHM: type = 'h'; break;
 		case KF_TYPE_PTS: type = 't'; break;
