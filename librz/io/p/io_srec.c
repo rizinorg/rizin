6dcbf2475ad168e3a9c1c5e2ef800b999d35b682
Manolache183 <manolache.alexandru8@gmail.com>
2021-04-30 20:53:02 +0300

    switched to a new branch cause the old one was messy

--- librz/io/p/io_srec.c
+++ librz/io/p/io_srec.c
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2021 Manolache183 <manolache.alexandru8@gmail.com>
+// SPDX-FileCopyrightText: 2021 swym  <0xfd000000@gmail.com>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 /*
@@ -30,10 +31,29 @@ typedef struct {
 	RzBuffer *rbuf;
 } RzSREC;
 
-static st32 fw04b(FILE *fd, ut16 eaddr);
-static st32 fwblock(FILE *fd, ut8 *b, ut32 start_addr, ut16 size);
+static st32 write_record_S3(FILE *fd, ut32 start_addr, ut8 *b, ut8 size) {
+	ut8 recordSize = 4 + size + 1;
+
+	ut8 cks = recordSize;
+	cks += start_addr & 0xff;
+	cks += (start_addr >> 8) & 0xff;
+	cks += (start_addr >> 16) & 0xff;
+	cks += start_addr >> 24;
+
+	fprintf(fd, "S3%02x%08x", recordSize, start_addr);
+
+	for (int j = 0; j < size; j++) {
+		cks += b[j];
+		fprintf(fd, "%02x", b[j]);
+	}
+
+	fprintf(fd, "%02x\n", cks);
+
+	return 1;
+}
 
 static st32 __write(RzIO *io, RzIODesc *fd, const ut8 *buf, st32 count) {
+
 	const char *pathname;
 	FILE *out;
 	RzSREC *rih;
@@ -43,130 +63,48 @@ static st32 __write(RzIO *io, RzIODesc *fd, const ut8 *buf, st32 count) {
 	if (!fd || !fd->data || (fd->perm & RZ_PERM_W) == 0 || count <= 0) {
 		return -1;
 	}
+
 	rih = fd->data;
 	pathname = fd->name + 7;
 	out = rz_sys_fopen(pathname, "w");
+
+	// random starting record
+	fprintf(out, "S00F000068656C6C6F202020202000003C\n");
+
 	if (!out) {
 		eprintf("Cannot open '%s' for writing\n", pathname);
 		return -1;
 	}
+
 	/* mem write */
 	if (rz_buf_write_at(rih->rbuf, io->off, buf, count) != count) {
 		eprintf("srec:write(): sparse write failed\n");
 		fclose(out);
 		return -1;
 	}
+
 	rz_buf_seek(rih->rbuf, count, RZ_BUF_CUR);
 
-	/* disk write : process each sparse chunk */
-	// TODO : sort addresses + check overlap?
 	RzList *nonempty = rz_buf_nonempty_list(rih->rbuf);
-	rz_list_foreach (nonempty, iter, rbs) {
-		ut16 addl0 = rbs->from & 0xffff;
-		ut16 addh0 = rbs->from >> 16;
-		ut16 addh1 = rbs->to >> 16;
-		ut16 tsiz = 0;
-		if (rbs->size == 0) {
-			continue;
-		}
 
-		if (addh0 != addh1) {
-			// we cross a 64k boundary, so write in two steps
-			if (fw04b(out, addh0) < 0) {
-				eprintf("srec:write: file error\n");
-				rz_list_free(nonempty);
-				fclose(out);
-				return -1;
+	rz_list_foreach (nonempty, iter, rbs) {
+		const ut8 RecordSize = 64; //bytes per record
+
+		for (ut32 rbsOffset = 0; rbsOffset < rbs->size; rbsOffset += RecordSize) {
+			const ut32 address = rbs->from + rbsOffset;
+			ut8 bytesInRecord = RecordSize;
+			if (rbsOffset + RecordSize > rbs->size) {
+				bytesInRecord = rbs->size - rbsOffset;
 			}
-			tsiz = -addl0;
-			addl0 = 0;
-			if (fwblock(out, rbs->data, rbs->from, tsiz)) {
-				eprintf("srec:fwblock error\n");
-				rz_list_free(nonempty);
-				fclose(out);
-				return -1;
-			}
-		}
-		if (fw04b(out, addh1) < 0) {
-			eprintf("srec:write: file error\n");
-			rz_list_free(nonempty);
-			fclose(out);
-			return -1;
-		}
-		if (fwblock(out, rbs->data + tsiz, (addh1 << 16) | addl0, rbs->size - tsiz)) {
-			eprintf("srec:fwblock error\n");
-			rz_list_free(nonempty);
-			fclose(out);
-			return -1;
+			write_record_S3(out, address, rbs->data + rbsOffset, bytesInRecord);
 		}
 	} // list_foreach
 
 	rz_list_free(nonempty);
-	fprintf(out, ":00000001FF\n");
+	fprintf(out, "S9030000FC\n");
 	fclose(out);
 	out = NULL;
 	return 0;
-}
-
-// write contiguous block of data to file; ret 0 if ok
-// max 65535 bytes;
-static st32 fwblock(FILE *fd, ut8 *b, ut32 start_addr, ut16 size) {
-	ut8 cks;
-	char linebuf[80];
-	ut16 last_addr;
-	st32 j;
-	ut32 i; // has to be bigger than size !
-
-	if (size < 1 || !fd || !b) {
-		return -1;
-	}
-
-	for (i = 0; (i + 0x10) < size; i += 0x10) {
-		cks = 0x10;
-		cks += (i + start_addr) >> 8;
-		cks += (i + start_addr);
-		for (j = 0; j < 0x10; j++) {
-			cks += b[j];
-		}
-		cks = 0 - cks;
-		if (fprintf(fd, ":10%04x00%02x%02x%02x%02x%02x%02x%02x"
-				"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n",
-			    (i + start_addr) & 0xffff, b[0], b[1], b[2], b[3], b[4], b[5], b[6],
-			    b[7], b[8], b[9], b[10], b[11], b[12], b[13],
-			    b[14], b[15], cks) < 0) {
-			return -1;
-		}
-		start_addr += 0x10;
-		b += 0x10;
-		if ((start_addr & 0xffff) < 0x10) {
-			// addr rollover: write ext address record
-			if (fw04b(fd, start_addr >> 16) < 0) {
-				return -1;
-			}
-		}
-	}
-	if (i == size) {
-		return 0;
-	}
-	// write crumbs
-	last_addr = i + start_addr;
-	cks = -last_addr;
-	cks -= last_addr >> 8;
-	for (j = 0; i < size; i++, j++) {
-		cks -= b[j];
-		sprintf(linebuf + (2 * j), "%02X", b[j]);
-	}
-	cks -= j;
-
-	if (fprintf(fd, ":%02X%04X00%.*s%02X\n", j, last_addr, 2 * j, linebuf, cks) < 0) {
-		return -1;
-	}
-	return 0;
-}
-
-static st32 fw04b(FILE *fd, ut16 eaddr) {
-	ut8 cks = 0 - (6 + (eaddr >> 8) + (eaddr & 0xff));
-	return fprintf(fd, ":02000004%04X%02X\n", eaddr, cks);
 }
 
 static st32 __read(RzIO *io, RzIODesc *fd, ut8 *buf, st32 count) {
@@ -179,6 +117,7 @@ static st32 __read(RzIO *io, RzIODesc *fd, ut8 *buf, st32 count) {
 	if (r >= 0) {
 		rz_buf_seek(rih->rbuf, r, RZ_BUF_CUR);
 	}
+
 	return r;
 }
 
@@ -217,15 +156,17 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 	char *eol;
 	ut8 cksum;
 	st32 bc = 0, byte, i, l;
+	st32 dataByteCount;
 	char type; // 1 digit
-	// ugly macro to prevent an overflow of rz_buf_write_at() len
-#define SEC_MAX (sec_size < ST32_MAX) ? sec_size : ST32_MAX
 	ut32 sec_size = 0;
+
 	const st32 sec_count = UT16_MAX;
+
 	sec_tmp = calloc(1, sec_count);
 	if (!sec_tmp) {
 		goto fail;
 	}
+
 	do {
 
 		l = sscanf(str, "S%c%02x", &type, &bc);
@@ -243,7 +184,7 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 		// then the 32 bit
 
 		switch (type) {
-		case 0: // 16 bit adress, with header instead of data field
+		case '0': // 16 bit adress, with header instead of data field
 
 			// S / 0 / (addres+data+checksum) bytes / 0000 / header / checksum
 
@@ -259,21 +200,6 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			cksum += addr_tmp & 0xff;
 			cksum += addr_tmp >> 8;
 
-			if ((next_addr != addr_tmp) || ((sec_size + bc) > SEC_MAX)) {
-				// previous block is not contiguous, or
-				// section buffer is full => write a sparse chunk
-				if (sec_size && sec_size < UT16_MAX) {
-					if (rz_buf_write_at(rbuf, sec_start, sec_tmp, (st32)sec_size) != sec_size) {
-						eprintf("sparse buffer problem, giving up\n");
-						goto fail;
-					}
-				}
-				// advance cursor, reset section
-				sec_start = segreg + addr_tmp;
-				next_addr = addr_tmp;
-				sec_size = 0;
-			}
-
 			for (i = 0; i < bc - 3; i++) {
 				if (sscanf(str + 8 + (i * 2), "%02x", &byte) != 1) {
 					eprintf("unparsable data !\n");
@@ -284,8 +210,6 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 				}
 				cksum += byte;
 			}
-			sec_size += bc;
-			next_addr += bc;
 			if (eol) {
 				// checksum
 				if (sscanf(str + 8 + (i * 2), "%02x", &byte) != 1) {
@@ -302,9 +226,11 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			str = eol;
 			break;
 
-		case 1: // 16 bit adress, with data field!, kinda same with S0
+		case '1': // 16 bit adress, with data field!, kinda same with S0
 
 			// S / 1 / (addres+data+checksum) bytes / 0000 / data / checksum
+			dataByteCount = bc - 3;
+
 			l = sscanf(str + 4, "%04x", &addr_tmp);
 			addr_tmp &= 0xffff;
 
@@ -317,8 +243,9 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			cksum += addr_tmp & 0xff;
 			cksum += addr_tmp >> 8;
 
-			if ((next_addr != addr_tmp) || ((sec_size + bc) > SEC_MAX)) {
+			if ((next_addr != addr_tmp) || ((sec_size + dataByteCount) > sec_count)) {
 				// previous block is not contiguous, or
+
 				// section buffer is full => write a sparse chunk
 				if (sec_size && sec_size < UT16_MAX) {
 					if (rz_buf_write_at(rbuf, sec_start, sec_tmp, (st32)sec_size) != sec_size) {
@@ -342,8 +269,8 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 				}
 				cksum += byte;
 			}
-			sec_size += bc;
-			next_addr += bc;
+			sec_size += dataByteCount;
+			next_addr += dataByteCount;
 			if (eol) {
 				// checksum
 				if (sscanf(str + 8 + (i * 2), "%02x", &byte) != 1) {
@@ -360,7 +287,7 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			str = eol;
 			break;
 
-		case 5: // optional, the adress field is a 16bit counter of S1/S2/S3 records
+		case '5': // optional, the adress field is a 16bit counter of S1/S2/S3 records
 
 			// S / 5 / (counter+checksum) bytes (3)/ counter / - / checksum
 
@@ -376,23 +303,6 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			cksum += addr_tmp & 0xff;
 			cksum += addr_tmp >> 8;
 
-			if ((next_addr != addr_tmp) || ((sec_size + bc) > SEC_MAX)) {
-				// previous block is not contiguous, or
-				// section buffer is full => write a sparse chunk
-				if (sec_size && sec_size < UT16_MAX) {
-					if (rz_buf_write_at(rbuf, sec_start, sec_tmp, (st32)sec_size) != sec_size) {
-						eprintf("sparse buffer problem, giving up\n");
-						goto fail;
-					}
-				}
-				// advance cursor, reset section
-				sec_start = segreg + addr_tmp;
-				next_addr = addr_tmp;
-				sec_size = 0;
-			}
-
-			sec_size += bc;
-			next_addr += bc;
 			if (eol) {
 				// checksum
 				if (sscanf(str + 8, "%02x", &byte) != 1) {
@@ -409,7 +319,7 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			str = eol;
 			break;
 
-		case 9: // a lot like S5
+		case '9': // a lot like S5
 			// S / 9 / (counter+checksum) bytes (3)/ adress / - / checksum
 
 			l = sscanf(str + 4, "%04x", &addr_tmp);
@@ -424,23 +334,6 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			cksum += addr_tmp & 0xff;
 			cksum += addr_tmp >> 8;
 
-			if ((next_addr != addr_tmp) || ((sec_size + bc) > SEC_MAX)) {
-				// previous block is not contiguous, or
-				// section buffer is full => write a sparse chunk
-				if (sec_size && sec_size < UT16_MAX) {
-					if (rz_buf_write_at(rbuf, sec_start, sec_tmp, (st32)sec_size) != sec_size) {
-						eprintf("sparse buffer problem, giving up\n");
-						goto fail;
-					}
-				}
-				// advance cursor, reset section
-				sec_start = segreg + addr_tmp;
-				next_addr = addr_tmp;
-				sec_size = 0;
-			}
-
-			sec_size += bc;
-			next_addr += bc;
 			if (eol) {
 				// checksum
 				if (sscanf(str + 8, "%02x", &byte) != 1) {
@@ -456,9 +349,11 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			}
 			str = eol;
 			break;
-		case 2: // 24 bit adress, with data field!, kinda same with S1
+		case '2': // 24 bit adress, with data field!, kinda same with S1
 
 			// S / 2 / (addres+data+checksum) bytes / 0000 / data / checksum
+			dataByteCount = bc - 4;
+
 			l = sscanf(str + 4, "%06x", &addr_tmp);
 			addr_tmp &= 0xffffff;
 
@@ -472,7 +367,7 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			cksum += (addr_tmp >> 8) & 0xff;
 			cksum += addr_tmp >> 16;
 
-			if ((next_addr != addr_tmp) || ((sec_size + bc) > SEC_MAX)) {
+			if ((next_addr != addr_tmp) || ((sec_size + dataByteCount) > sec_count)) {
 				// previous block is not contiguous, or
 				// section buffer is full => write a sparse chunk
 				if (sec_size && sec_size < UT16_MAX) {
@@ -497,8 +392,8 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 				}
 				cksum += byte;
 			}
-			sec_size += bc;
-			next_addr += bc;
+			sec_size += dataByteCount;
+			next_addr += dataByteCount;
 			if (eol) {
 				// checksum
 				if (sscanf(str + 10 + (i * 2), "%02x", &byte) != 1) {
@@ -515,7 +410,7 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			str = eol;
 			break;
 
-		case 6: // same with S5 but on 24 bits instead of 16
+		case '6': // same with S5 but on 24 bits instead of 16
 
 			// S / 6 / (addres+data+checksum) bytes / counter / - / checksum
 			l = sscanf(str + 4, "%06x", &addr_tmp);
@@ -531,22 +426,6 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			cksum += (addr_tmp >> 8) & 0xff;
 			cksum += addr_tmp >> 16;
 
-			if ((next_addr != addr_tmp) || ((sec_size + bc) > SEC_MAX)) {
-				// previous block is not contiguous, or
-				// section buffer is full => write a sparse chunk
-				if (sec_size && sec_size < UT16_MAX) {
-					if (rz_buf_write_at(rbuf, sec_start, sec_tmp, (st32)sec_size) != sec_size) {
-						eprintf("sparse buffer problem, giving up\n");
-						goto fail;
-					}
-				}
-				// advance cursor, reset section
-				sec_start = segreg + addr_tmp;
-				next_addr = addr_tmp;
-				sec_size = 0;
-			}
-			sec_size += bc;
-			next_addr += bc;
 			if (eol) {
 				// checksum
 				if (sscanf(str + 10, "%02x", &byte) != 1) {
@@ -563,7 +442,7 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			str = eol;
 			break;
 
-		case 8: // same with S9 but on 24 bits instead of 16
+		case '8': // same with S9 but on 24 bits instead of 16
 
 			// S / 8 / (addres+data+checksum) bytes / adress / - / checksum
 			l = sscanf(str + 4, "%06x", &addr_tmp);
@@ -579,22 +458,6 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			cksum += (addr_tmp >> 8) & 0xff;
 			cksum += addr_tmp >> 16;
 
-			if ((next_addr != addr_tmp) || ((sec_size + bc) > SEC_MAX)) {
-				// previous block is not contiguous, or
-				// section buffer is full => write a sparse chunk
-				if (sec_size && sec_size < UT16_MAX) {
-					if (rz_buf_write_at(rbuf, sec_start, sec_tmp, (st32)sec_size) != sec_size) {
-						eprintf("sparse buffer problem, giving up\n");
-						goto fail;
-					}
-				}
-				// advance cursor, reset section
-				sec_start = segreg + addr_tmp;
-				next_addr = addr_tmp;
-				sec_size = 0;
-			}
-			sec_size += bc;
-			next_addr += bc;
 			if (eol) {
 				// checksum
 				if (sscanf(str + 10, "%02x", &byte) != 1) {
@@ -611,9 +474,12 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			str = eol;
 			break;
 
-		case 3: // 32 bit adress, with data field!, kinda same with S1
+		case '3': // 32 bit adress, with data field!, kinda same with S1
 
 			// S / 3 / (addres+data+checksum) bytes / address / data / checksum
+
+			dataByteCount = bc - 5;
+
 			l = sscanf(str + 4, "%08x", &addr_tmp);
 			addr_tmp &= 0xffffffff;
 
@@ -628,7 +494,7 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			cksum += (addr_tmp >> 16) & 0xff;
 			cksum += addr_tmp >> 24;
 
-			if ((next_addr != addr_tmp) || ((sec_size + bc) > SEC_MAX)) {
+			if ((next_addr != addr_tmp) || ((sec_size + dataByteCount) > sec_count)) {
 				// previous block is not contiguous, or
 				// section buffer is full => write a sparse chunk
 				if (sec_size && sec_size < UT16_MAX) {
@@ -653,8 +519,8 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 				}
 				cksum += byte;
 			}
-			sec_size += bc;
-			next_addr += bc;
+			sec_size += dataByteCount;
+			next_addr += dataByteCount;
 			if (eol) {
 				// checksum
 				if (sscanf(str + 12 + (i * 2), "%02x", &byte) != 1) {
@@ -671,7 +537,7 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			str = eol;
 			break;
 
-		case 7: // same with S9 but on 32 bits
+		case '7': // same with S9 but on 32 bits
 
 			// S / 7 / (addres+data+checksum) bytes / address / - / checksum
 			l = sscanf(str + 4, "%08x", &addr_tmp);
@@ -688,22 +554,6 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			cksum += (addr_tmp >> 16) & 0xff;
 			cksum += addr_tmp >> 24;
 
-			if ((next_addr != addr_tmp) || ((sec_size + bc) > SEC_MAX)) {
-				// previous block is not contiguous, or
-				// section buffer is full => write a sparse chunk
-				if (sec_size && sec_size < UT16_MAX) {
-					if (rz_buf_write_at(rbuf, sec_start, sec_tmp, (st32)sec_size) != sec_size) {
-						eprintf("sparse buffer problem, giving up\n");
-						goto fail;
-					}
-				}
-				// advance cursor, reset section
-				sec_start = segreg + addr_tmp;
-				next_addr = addr_tmp;
-				sec_size = 0;
-			}
-			sec_size += bc;
-			next_addr += bc;
 			if (eol) {
 				// checksum
 				if (sscanf(str + 12, "%02x", &byte) != 1) {
@@ -720,9 +570,17 @@ static bool SREC_parse(RzBuffer *rbuf, char *str) {
 			str = eol;
 			break;
 
-		case 4: break; // reserved
+		case '4': break; // reserved
 		}
 	} while (str);
+
+	if (sec_size && sec_size < UT16_MAX) {
+		if (rz_buf_write_at(rbuf, sec_start, sec_tmp, (st32)sec_size) != sec_size) {
+			eprintf("sparse buffer problem, giving up\n");
+			goto fail;
+		}
+	}
+
 	free(sec_tmp);
 	return true;
 fail:
@@ -757,7 +615,7 @@ static RzIODesc *__open(RzIO *io, const char *pathname, st32 rw, st32 mode) {
 			return NULL;
 		}
 		free(str);
-		return rz_io_desc_new(io, &rz_io_plugin_ihex,
+		return rz_io_desc_new(io, &rz_io_plugin_srec,
 			pathname, rw, mode, mal);
 	}
 	return NULL;
