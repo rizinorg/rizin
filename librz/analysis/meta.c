f7065daa8cd655908062032ff29b5955632c739f
valdaarhun <icegambit91@gmail.com>
2021-05-10 13:32:41 +0530

    Resolve errors pt. 4.20

--- librz/analysis/meta.c
+++ librz/analysis/meta.c
@@ -367,7 +367,7 @@ RZ_API void rz_meta_print(RzAnalysis *a, RzAnalysisMetaItem *d, ut64 start, ut64
 				free(s);
 			} break;
 			case RZ_META_TYPE_STRING:
-				if (mode == RZ_OUTPUT_MODE_STANDARD || mode == RZ_OUTPUT_MODE_RIZIN || mode == RZ_OUTPUT_MODE_QUIET) {
+				if (mode == RZ_OUTPUT_MODE_STANDARD || mode == RZ_OUTPUT_MODE_RIZIN) {
 					char cmd[] = "Cs#";
 					switch (d->subtype) {
 					case 'a':
