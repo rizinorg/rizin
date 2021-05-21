ecd48402d12b521a1423e1aac8bad8e6b682818a
Anton Kochkov <anton.kochkov@gmail.com>
2021-05-21 20:05:26 +0800

    Complete the migration to the new RzType API

--- librz/type/helpers.c
+++ librz/type/helpers.c
@@ -5,33 +5,53 @@
 #include <rz_type.h>
 #include <string.h>
 
-RZ_API RZ_OWN RzType *rz_type_identifier_of_base_type(RzTypeDB *typedb, RZ_NONNULL RzBaseType *btype) {
+/**
+ * \brief Creates a new RzType indentifier from the given RzBaseType
+ *
+ * \param typedb Type Database instance
+ * \param btype RzBaseType pointer
+ * \param is_const Set the identifier to "const" if true
+ */
+RZ_API RZ_OWN RzType *rz_type_identifier_of_base_type(const RzTypeDB *typedb, RZ_NONNULL const RzBaseType *btype, bool is_const) {
 	rz_return_val_if_fail(typedb && btype, NULL);
 	RzType *type = RZ_NEW0(RzType);
 	if (!type) {
 		return NULL;
 	}
 	type->kind = RZ_TYPE_KIND_IDENTIFIER;
-	type->identifier.name = btype->name;
-	type->identifier.is_const = false; // not "const" by default
+	type->identifier.name = strdup(btype->name);
+	type->identifier.is_const = is_const;
 	return type;
 }
 
-RZ_API RZ_OWN RzType *rz_type_identifier_of_base_type_str(RzTypeDB *typedb, RZ_NONNULL const char *name) {
+/**
+ * \brief Creates a new RzType indentifier from the given RzBaseType name
+ *
+ * \param typedb Type Database instance
+ * \param name RzBaseType name
+ */
+RZ_API RZ_OWN RzType *rz_type_identifier_of_base_type_str(const RzTypeDB *typedb, RZ_NONNULL const char *name) {
 	rz_return_val_if_fail(typedb && name, NULL);
 	RzBaseType *btype = rz_type_db_get_base_type(typedb, name);
 	if (!btype) {
 		return NULL;
 	}
-	return rz_type_identifier_of_base_type(typedb, btype);
+	return rz_type_identifier_of_base_type(typedb, btype, false);
 }
 
-RZ_API RZ_OWN RzType *rz_type_pointer_of_base_type(RzTypeDB *typedb, RZ_NONNULL RzBaseType *btype, bool is_const) {
+/**
+ * \brief Creates a new pointer RzType from the given RzBaseType
+ *
+ * \param typedb Type Database instance
+ * \param btype RzBaseType pointer
+ * \param is_const Set the pointer to "const" if true
+ */
+RZ_API RZ_OWN RzType *rz_type_pointer_of_base_type(const RzTypeDB *typedb, RZ_NONNULL const RzBaseType *btype, bool is_const) {
 	RzType *type = RZ_NEW0(RzType);
 	if (!type) {
 		return NULL;
 	}
-	RzType *t = rz_type_identifier_of_base_type(typedb, btype);
+	RzType *t = rz_type_identifier_of_base_type(typedb, btype, false);
 	if (!t) {
 		rz_type_free(type);
 		return NULL;
@@ -42,7 +62,14 @@ RZ_API RZ_OWN RzType *rz_type_pointer_of_base_type(RzTypeDB *typedb, RZ_NONNULL 
 	return type;
 }
 
-RZ_API RZ_OWN RzType *rz_type_pointer_of_base_type_str(RzTypeDB *typedb, RZ_NONNULL const char *name, bool is_const) {
+/**
+ * \brief Creates a new pointer RzType from the given RzBaseType name
+ *
+ * \param typedb Type Database instance
+ * \param name RzBaseType name
+ * \param is_const Set the pointer to "const" if true
+ */
+RZ_API RZ_OWN RzType *rz_type_pointer_of_base_type_str(const RzTypeDB *typedb, RZ_NONNULL const char *name, bool is_const) {
 	rz_return_val_if_fail(typedb && name, NULL);
 	RzBaseType *btype = rz_type_db_get_base_type(typedb, name);
 	if (!btype) {
@@ -51,7 +78,14 @@ RZ_API RZ_OWN RzType *rz_type_pointer_of_base_type_str(RzTypeDB *typedb, RZ_NONN
 	return rz_type_pointer_of_base_type(typedb, btype, is_const);
 }
 
-RZ_API RZ_OWN RzType *rz_type_pointer_of_type(RzTypeDB *typedb, RZ_NONNULL RzType *type, bool is_const) {
+/**
+ * \brief Creates a new pointer RzType from the given RzType
+ *
+ * \param typedb Type Database instance
+ * \param type RzType pointer
+ * \param is_const Set the pointer to "const" if true
+ */
+RZ_API RZ_OWN RzType *rz_type_pointer_of_type(const RzTypeDB *typedb, RZ_NONNULL RzType *type, bool is_const) {
 	rz_return_val_if_fail(typedb && type, NULL);
 	if (type->kind == RZ_TYPE_KIND_IDENTIFIER) {
 		return rz_type_pointer_of_base_type_str(typedb, type->identifier.name, is_const);
@@ -66,12 +100,19 @@ RZ_API RZ_OWN RzType *rz_type_pointer_of_type(RzTypeDB *typedb, RZ_NONNULL RzTyp
 	return NULL;
 }
 
-RZ_API RZ_OWN RzType *rz_type_array_of_base_type(RzTypeDB *typedb, RZ_NONNULL RzBaseType *btype, size_t count) {
+/**
+ * \brief Creates a new array RzType from the given RzBaseType
+ *
+ * \param typedb Type Database instance
+ * \param btype RzBaseType pointer
+ * \param count The number of the array elements
+ */
+RZ_API RZ_OWN RzType *rz_type_array_of_base_type(const RzTypeDB *typedb, RZ_NONNULL const RzBaseType *btype, size_t count) {
 	RzType *type = RZ_NEW0(RzType);
 	if (!type) {
 		return NULL;
 	}
-	RzType *t = rz_type_identifier_of_base_type(typedb, btype);
+	RzType *t = rz_type_identifier_of_base_type(typedb, btype, false);
 	if (!t) {
 		rz_type_free(type);
 		return NULL;
@@ -82,7 +123,14 @@ RZ_API RZ_OWN RzType *rz_type_array_of_base_type(RzTypeDB *typedb, RZ_NONNULL Rz
 	return type;
 }
 
-RZ_API RZ_OWN RzType *rz_type_array_of_base_type_str(RzTypeDB *typedb, RZ_NONNULL const char *name, size_t count) {
+/**
+ * \brief Creates a new array RzType from the given RzBaseType name
+ *
+ * \param typedb Type Database instance
+ * \param name RzBaseType name
+ * \param count The number of the array elements
+ */
+RZ_API RZ_OWN RzType *rz_type_array_of_base_type_str(const RzTypeDB *typedb, RZ_NONNULL const char *name, size_t count) {
 	rz_return_val_if_fail(typedb && name && count, NULL);
 	RzBaseType *btype = rz_type_db_get_base_type(typedb, name);
 	if (!btype) {
@@ -91,7 +139,14 @@ RZ_API RZ_OWN RzType *rz_type_array_of_base_type_str(RzTypeDB *typedb, RZ_NONNUL
 	return rz_type_array_of_base_type(typedb, btype, count);
 }
 
-RZ_API RZ_OWN RzType *rz_type_array_of_type(RzTypeDB *typedb, RZ_NONNULL RzType *type, size_t count) {
+/**
+ * \brief Creates a new array RzType from the given RzType
+ *
+ * \param typedb Type Database instance
+ * \param type RzType pointer
+ * \param count The number of the array elements
+ */
+RZ_API RZ_OWN RzType *rz_type_array_of_type(const RzTypeDB *typedb, RZ_NONNULL RzType *type, size_t count) {
 	RzType *newtype = RZ_NEW0(RzType);
 	if (!newtype) {
 		return NULL;
@@ -102,7 +157,16 @@ RZ_API RZ_OWN RzType *rz_type_array_of_type(RzTypeDB *typedb, RZ_NONNULL RzType 
 	return type;
 }
 
-RZ_API bool rz_type_atomic_eq(RzTypeDB *typedb, RzType *typ1, RzType *typ2) {
+// Equivalence checking
+
+/**
+ * \brief Checks if two atomic RzTypes are equivalent
+ *
+ * \param typedb Type Database instance
+ * \param typ1 First RzType type
+ * \param typ2 Second RzType type
+ */
+RZ_API bool rz_type_atomic_eq(const RzTypeDB *typedb, RZ_NONNULL const RzType *typ1, RZ_NONNULL const RzType *typ2) {
 	// We aim to compare only atomic types, we can't compare more complex ones for now
 	rz_return_val_if_fail(typ1 && typ2, false);
 	rz_return_val_if_fail(typ1->kind == RZ_TYPE_KIND_IDENTIFIER && typ2 == RZ_TYPE_KIND_IDENTIFIER, false);
@@ -118,7 +182,14 @@ RZ_API bool rz_type_atomic_eq(RzTypeDB *typedb, RzType *typ1, RzType *typ2) {
 	// TODO: Should we also compare the btyp->type?
 }
 
-RZ_API bool rz_type_atomic_str_eq(RzTypeDB *typedb, RzType *typ1, RZ_NONNULL const char *name) {
+/**
+ * \brief Checks if two atomic types (RzType and RzBaseType) are equivalent
+ *
+ * \param typedb Type Database instance
+ * \param typ1 First RzType type
+ * \param typ2 Second RzBaseType type name
+ */
+RZ_API bool rz_type_atomic_str_eq(const RzTypeDB *typedb, RZ_NONNULL const RzType *typ1, RZ_NONNULL const char *name) {
 	// We aim to compare only atomic types, we can't compare more complex ones for now
 	rz_return_val_if_fail(typ1 && name, false);
 	rz_return_val_if_fail(typ1->kind == RZ_TYPE_KIND_IDENTIFIER, false);
@@ -135,7 +206,13 @@ RZ_API bool rz_type_atomic_str_eq(RzTypeDB *typedb, RzType *typ1, RZ_NONNULL con
 
 // Here we provide helpers for some commonly used RzTypes for use within the analysis
 
-RZ_API bool rz_type_atomic_is_void(RzTypeDB *typedb, RzType *type) {
+/**
+ * \brief Checks if the RzType is "void"
+ *
+ * \param typedb Type Database instance
+ * \param type RzType type pointer
+ */
+RZ_API bool rz_type_atomic_is_void(const RzTypeDB *typedb, RZ_NONNULL const RzType *type) {
 	rz_return_val_if_fail(type, false);
 	if (type->kind != RZ_TYPE_KIND_IDENTIFIER) {
 		return false;
@@ -143,7 +220,13 @@ RZ_API bool rz_type_atomic_is_void(RzTypeDB *typedb, RzType *type) {
 	return !strcmp(type->identifier.name, "void");
 }
 
-RZ_API bool rz_type_atomic_is_signed(RzTypeDB *typedb, RzType *type) {
+/**
+ * \brief Checks if the RzType is signed
+ *
+ * \param typedb Type Database instance
+ * \param type RzType type pointer
+ */
+RZ_API bool rz_type_atomic_is_signed(const RzTypeDB *typedb, RZ_NONNULL const RzType *type) {
 	rz_return_val_if_fail(type, false);
 	if (type->kind != RZ_TYPE_KIND_IDENTIFIER) {
 		return false;
@@ -155,7 +238,13 @@ RZ_API bool rz_type_atomic_is_signed(RzTypeDB *typedb, RzType *type) {
 	return false;
 }
 
-RZ_API bool rz_type_atomic_is_const(RzTypeDB *typedb, RzType *type) {
+/**
+ * \brief Checks if the atomic RzType is "const"
+ *
+ * \param typedb Type Database instance
+ * \param type RzType type pointer
+ */
+RZ_API bool rz_type_atomic_is_const(const RzTypeDB *typedb, RZ_NONNULL const RzType *type) {
 	rz_return_val_if_fail(type, false);
 	if (type->kind != RZ_TYPE_KIND_IDENTIFIER) {
 		return false;
@@ -163,7 +252,13 @@ RZ_API bool rz_type_atomic_is_const(RzTypeDB *typedb, RzType *type) {
 	return type->identifier.is_const;
 }
 
-RZ_API bool rz_type_atomic_is_num(RzTypeDB *typedb, RzType *type) {
+/**
+ * \brief Checks if the atomic RzType is number
+ *
+ * \param typedb Type Database instance
+ * \param type RzType type pointer
+ */
+RZ_API bool rz_type_atomic_is_num(const RzTypeDB *typedb, RZ_NONNULL const RzType *type) {
 	rz_return_val_if_fail(type, false);
 	if (type->kind != RZ_TYPE_KIND_IDENTIFIER) {
 		return false;
@@ -175,7 +270,13 @@ RZ_API bool rz_type_atomic_is_num(RzTypeDB *typedb, RzType *type) {
 	return false;
 }
 
-RZ_API bool rz_type_pointer_is_const(RzTypeDB *typedb, RzType *type) {
+/**
+ * \brief Checks if the pointer RzType is "const"
+ *
+ * \param typedb Type Database instance
+ * \param type RzType type pointer
+ */
+RZ_API bool rz_type_pointer_is_const(const RzTypeDB *typedb, RZ_NONNULL const RzType *type) {
 	rz_return_val_if_fail(type, false);
 	if (type->kind != RZ_TYPE_KIND_POINTER) {
 		return false;
@@ -183,7 +284,12 @@ RZ_API bool rz_type_pointer_is_const(RzTypeDB *typedb, RzType *type) {
 	return type->pointer.is_const;
 }
 
-RZ_API bool rz_type_is_void_ptr(RzType *type) {
+/**
+ * \brief Checks if the pointer RzType is abstract pointer ("void *")
+ *
+ * \param type RzType type pointer
+ */
+RZ_API bool rz_type_is_void_ptr(RZ_NONNULL const RzType *type) {
 	rz_return_val_if_fail(type, false);
 	if (type->kind != RZ_TYPE_KIND_POINTER) {
 		return false;
@@ -194,7 +300,13 @@ RZ_API bool rz_type_is_void_ptr(RzType *type) {
 	return ptr->kind == RZ_TYPE_KIND_IDENTIFIER && ptr->identifier.kind == RZ_TYPE_IDENTIFIER_KIND_UNSPECIFIED && !strcmp(ptr->identifier.name, "void");
 }
 
-RZ_API bool rz_type_is_default(RzTypeDB *typedb, RzType *type) {
+/**
+ * \brief Checks if the RzType is default
+ *
+ * \param typedb Type Database instance
+ * \param type RzType type pointer
+ */
+RZ_API bool rz_type_is_default(const RzTypeDB *typedb, RZ_NONNULL const RzType *type) {
 	rz_return_val_if_fail(type, false);
 	if (type->kind != RZ_TYPE_KIND_IDENTIFIER) {
 		return false;
@@ -202,18 +314,29 @@ RZ_API bool rz_type_is_default(RzTypeDB *typedb, RzType *type) {
 	if (type->identifier.kind != RZ_TYPE_IDENTIFIER_KIND_UNSPECIFIED) {
 		return false;
 	}
-	return !strcmp(type->identifier.name, RZ_TYPE_DEFAULT) && !type->identifier.is_const;
+	return !strcmp(type->identifier.name, typedb->target->default_type) && !type->identifier.is_const;
 }
 
-RZ_API RZ_OWN RzType *rz_type_new_default(RzTypeDB *typedb) {
+/**
+ * \brief Creates a new instance of the default RzType type
+ *
+ * \param typedb Type Database instance
+ */
+RZ_API RZ_OWN RzType *rz_type_new_default(const RzTypeDB *typedb) {
 	rz_return_val_if_fail(typedb, NULL);
-	RzBaseType *btype = rz_type_db_get_base_type(typedb, RZ_TYPE_DEFAULT);
+	RzBaseType *btype = rz_type_db_get_base_type(typedb, typedb->target->default_type);
 	if (!btype) {
 		return NULL;
 	}
-	return rz_type_identifier_of_base_type(typedb, btype);
+	return rz_type_identifier_of_base_type(typedb, btype, false);
 }
 
+/**
+ * \brief If the type is unsigned it sets the sign
+ *
+ * \param typedb Type Database instance
+ * \param type RzType type pointer
+ */
 RZ_API bool rz_type_atomic_set_sign(RzTypeDB *typedb, RzType *type, bool sign) {
 	rz_return_val_if_fail(type, false);
 	if (type->kind != RZ_TYPE_KIND_IDENTIFIER) {
