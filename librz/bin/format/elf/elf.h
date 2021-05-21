98cd56ee4db39ba0079df7ca30e22f044ba9de87
Alexis Ehret <08a@riseup.net>
2021-05-21 11:49:41 +0200

    Add documentation rz_bin_elf_has_relro ##doc

--- librz/bin/format/elf/elf.h
+++ librz/bin/format/elf/elf.h
@@ -270,7 +270,7 @@ int Elf_(rz_bin_elf_has_relro)(RZ_NONNULL ELFOBJ *bin);
 
 RZ_OWN RzBinSymbol *Elf_(rz_bin_elf_convert_symbol)(RZ_NONNULL ELFOBJ *bin, RZ_NONNULL RzBinElfSymbol *symbol, const char *namefmt);
 
-int Elf_(rz_bin_elf_has_nx)(struct Elf_(rz_bin_elf_obj_t) *bin);
+bool Elf_(rz_bin_elf_has_nx)(RZ_NONNULL ELFOBJ *bin);
 
 const ut8 *Elf_(rz_bin_elf_grab_regstate)(struct Elf_(rz_bin_elf_obj_t) * bin, size_t *size);
 ut64 Elf_(rz_bin_elf_get_sp_val)(struct Elf_(rz_bin_elf_obj_t) * bin);
