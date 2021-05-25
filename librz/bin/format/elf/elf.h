20fd0f0167fc2f98b4de4ca813e4619f5d40c9ee
Alexis Ehret <08a@riseup.net>
2021-05-25 11:21:38 +0200

    Add documentation rz_bin_elf_get_cpu ##doc

--- librz/bin/format/elf/elf.h
+++ librz/bin/format/elf/elf.h
@@ -255,7 +255,7 @@ RZ_OWN char *Elf_(rz_bin_elf_get_abi)(RZ_NONNULL ELFOBJ *bin);
 RZ_OWN char *Elf_(rz_bin_elf_get_arch)(RZ_NONNULL ELFOBJ *bin);
 ut64 Elf_(rz_bin_elf_get_baddr)(RZ_NONNULL ELFOBJ *bin);
 ut64 Elf_(rz_bin_elf_get_boffset)(RZ_NONNULL ELFOBJ *bin);
-char *Elf_(rz_bin_elf_get_cpu)(ELFOBJ *bin);
+RZ_OWN char *Elf_(rz_bin_elf_get_cpu)(RZ_NONNULL ELFOBJ *bin);
 RZ_OWN char *Elf_(rz_bin_elf_get_data_encoding)(RZ_NONNULL ELFOBJ *bin);
 ut64 Elf_(rz_bin_elf_get_entry_offset)(RZ_NONNULL ELFOBJ *bin);
 ut64 Elf_(rz_bin_elf_get_fini_offset)(RZ_NONNULL ELFOBJ *bin);
