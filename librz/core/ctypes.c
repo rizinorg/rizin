2d61154337831e2781f8472876ec60955879f93f
Anton Kochkov <anton.kochkov@gmail.com>
2021-06-10 12:53:09 +0800

    Save parser state between `to` commands

--- librz/core/ctypes.c
+++ librz/core/ctypes.c
@@ -1022,7 +1022,7 @@ RZ_IPI void rz_types_open_file(RzCore *core, const char *path) {
 		char *tmp = rz_core_editor(core, "*.h", "");
 		if (tmp) {
 			char *error_msg = NULL;
-			int result = rz_type_parse_string(core->analysis->typedb, tmp, &error_msg);
+			int result = rz_type_parse_string_stateless(typedb->parser, tmp, &error_msg);
 			if (result && error_msg) {
 				eprintf("%s", error_msg);
 				free(error_msg);
@@ -1031,7 +1031,7 @@ RZ_IPI void rz_types_open_file(RzCore *core, const char *path) {
 		}
 	} else {
 		char *error_msg = NULL;
-		int result = rz_type_parse_file(typedb, path, dir, &error_msg);
+		int result = rz_type_parse_file_stateless(typedb->parser, path, dir, &error_msg);
 		if (result && error_msg) {
 			eprintf("%s", error_msg);
 			free(error_msg);
@@ -1046,7 +1046,7 @@ RZ_IPI void rz_types_open_editor(RzCore *core, const char *typename) {
 	char *tmp = rz_core_editor(core, "*.h", str);
 	if (tmp) {
 		char *error_msg = NULL;
-		int result = rz_type_parse_string(typedb, tmp, &error_msg);
+		int result = rz_type_parse_string_stateless(typedb->parser, tmp, &error_msg);
 		if (result) {
 			// TODO: remove previous types and save new edited types
 			//rz_type_db_purge(typedb);
