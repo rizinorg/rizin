e9ae39273d4bba577af1eddd547ae7941806ee78
pparmin <philipp.armingeon@googlemail.com>
2021-06-13 19:11:02 +0200

    Add newshell support

--- librz/core/p/core_plugin_example.c
+++ librz/core/p/core_plugin_example.c
@@ -18,28 +18,46 @@ mv core_plugin_example.so ~/.config/rizin/plugins
 #undef RZ_IPI
 #define RZ_IPI static
 
-static int rz_cmd_example_init(void *user /* RzCmd* */, const char *unused /* unused during init */) {
-	RzCmd *cmd = (RzCmd *)user;
-	(void)cmd;
+static const RzCmdDescHelp example_usage = {
+	.summary = "Example core plugin",
+};
+
+static bool rz_cmd_example_init(RzCore *core) {
+	RzCmd *rcmd = core->rcmd;
+	RzCmdDesc *root_cd = rz_cmd_get_root(rcmd);
+	if (!root_cd) {
+		rz_warn_if_reached();
+		return false;
+	}
 	/* Here you can initialize any aspect of the
 	 * core plugin (like allocate memory or register
 	 * the core plugin on rzshell or create a socket) */
 	eprintf("This init was called!\n");
+
+	RzCmdDesc *example = rz_cmd_desc_argv_new(rcmd, root_cd, "example",
+		rz_cmd_example_handler, &example_usage);
+	if (!example) {
+		rz_warn_if_reached();
+		return false;
+	}
 	return true;
 }
 
-static int rz_cmd_example_call(void *user /* RzCmd* */, const char *args) {
-	/* This call is used by the old shell */
-	eprintf("This command was called!\n");
-	return false;
-}
-
-static int rz_cmd_example_fini(void *user /* RzCmd* */, const char *unused /* unused during fini */) {
+static bool rz_cmd_example_fini(RzCore *core) {
 	/* Here you can end any aspect of the core
 	 * plugin (like free allocated memory, or
 	 * end sockets, etc..) */
 	eprintf("This fini was called!\n");
 	return true;
+}
+
+RZ_IPI RzCmdStatus rz_cmd_example_handler(RzCore *core, int argc,
+	const char **argv) {
+	if (argc < 2) {
+		return RZ_CMD_STATUS_WRONG_ARGS;
+	}
+	eprintf("This example was called!\n")
+	return RZ_CMD_STATUS_OK;
 }
 
 RzCorePlugin rz_core_plugin_example = {
@@ -49,7 +67,6 @@ RzCorePlugin rz_core_plugin_example = {
 	.author = "somebody",
 	.version = "1.0",
 	.init = rz_cmd_example_init,
-	.call = rz_cmd_example_call,
 	.fini = rz_cmd_example_fini,
 };
 
