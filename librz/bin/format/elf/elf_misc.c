8c2589ba33dd5fbfb3880e2b53ecc4b4396d39e7
Alexis Ehret <08a@riseup.net>
2021-06-18 12:51:55 +0200

    Fix typo in licensing from librz/bin/format/elf/*

--- librz/bin/format/elf/elf_misc.c
+++ librz/bin/format/elf/elf_misc.c
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2021 08A <08A@riseup.net>
 // SPDX-FileCopyrightText: 2021 RizinOrg <info@rizin.re>
-// SPDX-License-Identifier: LPGL-3.0-only
+// SPDX-License-Identifier: LGPL-3.0-only
 
 #include "elf.h"
 
