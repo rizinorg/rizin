425bca132c91050956342e98eb726708a7de2dd8
Aswin C <realc@protonmail.com>
2021-06-20 20:13:57 +0530

    Throw a warning if the file is manually mapped
    * Throw a warning if `file.offset` is changed

--- librz/core/canalysis.c
+++ librz/core/canalysis.c
@@ -7171,7 +7171,8 @@ RZ_API bool rz_core_analysis_everything(RzCore *core, bool experimental, char *d
 		rz_config_set(core->config, "dbg.backend", dh_orig);
 		rz_core_task_yield(&core->tasks);
 	}
-	rz_analysis_add_io_registers_map(core->io, core->analysis);
+	ut64 file_offset = rz_config_get_i(core->config, "file.offset");
+	rz_analysis_add_io_registers_map(core->io, core->analysis, file_offset);
 	return true;
 }
 
@@ -7263,10 +7264,14 @@ RZ_IPI char *rz_core_analysis_all_vars_display(RzCore *core, RzAnalysisFunction 
 	return rz_strbuf_drain(sb);
 }
 
-RZ_API bool rz_analysis_add_io_registers_map(RzIO *io, RzAnalysis *analysis) {
+RZ_API bool rz_analysis_add_io_registers_map(RzIO *io, RzAnalysis *analysis, ut64 file_offset) {
 	ut64 rom_size = analysis->arch_target->profile->rom_size;
 	ut64 rom_address = analysis->arch_target->profile->rom_address;
 	if (rz_io_map_is_mapped(io, rom_address)) {
+		return false;
+	}
+	if (file_offset) {
+		eprintf("Warning: Not mapping the ROM section due to the mapping at 0x%llx \n", file_offset);
 		return false;
 	}
 	RzIOMap *rom_map = rz_io_map_add(io, io->desc->fd, RZ_PERM_RX, 0LL, rom_address, rom_size);
