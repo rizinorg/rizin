98432de976a0719b4cedf7878896097b72099388
Alexis Ehret <08a@riseup.net>
2021-06-24 17:19:55 +0200

    Remove strtab from ELFOBJ and fix dynstr creation ##refactor

--- librz/bin/format/elf/elf_strtab.c
+++ librz/bin/format/elf/elf_strtab.c
@@ -5,7 +5,7 @@
 #include "elf_strtab.h"
 #include "elf.h"
 
-RZ_OWN RzBinElfStrtab *Elf_(rz_bin_elf_new_strtab)(RZ_NONNULL ELFOBJ *bin, ut64 offset, ut64 size) {
+RZ_OWN RzBinElfStrtab *Elf_(rz_bin_elf_strtab_new)(RZ_NONNULL ELFOBJ *bin, ut64 offset, ut64 size) {
 	rz_return_val_if_fail(bin, NULL);
 
 	if (!size || !Elf_(rz_bin_elf_check_array)(bin, offset, size, sizeof(ut8))) {
@@ -21,25 +21,25 @@ RZ_OWN RzBinElfStrtab *Elf_(rz_bin_elf_new_strtab)(RZ_NONNULL ELFOBJ *bin, ut64 
 	result->size = size;
 	result->data = RZ_NEWS(char, size);
 	if (!result->data) {
-		Elf_(rz_bin_elf_free_strtab)(result);
+		Elf_(rz_bin_elf_strtab_free)(result);
 		return NULL;
 	}
 
 	if (rz_buf_read_at(bin->b, offset, (ut8 *)result->data, size) < 0) {
-		Elf_(rz_bin_elf_free_strtab)(result);
+		Elf_(rz_bin_elf_strtab_free)(result);
 		return NULL;
 	}
 
 	if (result->data[0] != '\0' || result->data[size - 1] != '\0') {
 		RZ_LOG_WARN("String table 0x%" PFMT64x "should start and end by a NULL byte", offset);
-		Elf_(rz_bin_elf_free_strtab)(result);
+		Elf_(rz_bin_elf_strtab_free)(result);
 		return NULL;
 	}
 
 	return result;
 }
 
-RZ_OWN char *Elf_(rz_bin_elf_strtab_dup)(RZ_NONNULL RzBinElfStrtab *strtab, ut64 index) {
+RZ_OWN char *Elf_(rz_bin_elf_strtab_get_dup)(RZ_NONNULL RzBinElfStrtab *strtab, ut64 index) {
 	rz_return_val_if_fail(strtab, NULL);
 
 	if (Elf_(rz_bin_elf_strtab_has_index)(strtab, index)) {
@@ -59,8 +59,8 @@ bool Elf_(rz_bin_elf_strtab_has_index)(RZ_NONNULL RzBinElfStrtab *strtab, ut64 i
 	return index < strtab->size;
 }
 
-bool Elf_(rz_bin_elf_strtab_cpy)(RZ_NONNULL RzBinElfStrtab *strtab, char *dst, ut64 index) {
-	rz_return_val_if_fail(strtab, false);
+bool Elf_(rz_bin_elf_strtab_get)(RZ_NONNULL RzBinElfStrtab *strtab, RZ_NONNULL RZ_OUT char *dst, ut64 index) {
+	rz_return_val_if_fail(strtab && dst, false);
 
 	if (!Elf_(rz_bin_elf_strtab_has_index)(strtab, index)) {
 		return false;
@@ -74,7 +74,7 @@ bool Elf_(rz_bin_elf_strtab_cpy)(RZ_NONNULL RzBinElfStrtab *strtab, char *dst, u
 	return true;
 }
 
-void Elf_(rz_bin_elf_free_strtab)(RzBinElfStrtab *ptr) {
+void Elf_(rz_bin_elf_strtab_free)(RzBinElfStrtab *ptr) {
 	if (!ptr) {
 		return;
 	}
