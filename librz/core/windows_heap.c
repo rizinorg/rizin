b2c2ce1f2d3b03196a9ed5c0a5246f6015a9a9e8
Pulak Malhotra <56169176+PulakIIIT@users.noreply.github.com>
2021-07-19 12:43:00 +0530

    Clang format

--- librz/core/windows_heap.c
+++ librz/core/windows_heap.c
@@ -1209,7 +1209,7 @@ static RzTable *__new_heapblock_tbl(void) {
 }
 
 static void w32_list_heaps(RzCore *core, RzOutputMode mode) {
-        init_func();
+	init_func();
 	ULONG pid = core->dbg->pid;
 	PDEBUG_BUFFER db = InitHeapInfo(core->dbg, PDI_HEAPS | PDI_HEAP_BLOCKS);
 	if (!db) {
@@ -1355,7 +1355,7 @@ static void w32_list_heaps_blocks(RzCore *core, RzOutputMode mode, bool flag) {
 //};
 
 static void cmd_debug_map_heap_block_win(RzCore *core, const char *addr, RzOutputMode mode, bool flag) {
-        init_func();
+	init_func();
 	ut64 off = 0;
 	if (!addr) {
 		w32_list_heaps_blocks(core, mode, flag);
