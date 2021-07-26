b9bc178fa786aebeb2fd6521ee4459b164dd4f4e
Heersin <teablearcher@gmail.com>
2021-07-26 14:34:14 +0800

    Add SPDX head

--- librz/core/cil.c
+++ librz/core/cil.c
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2009-2020 pancake <pancake@nopcode.org>
+// SPDX-FileCopyrightText: 2009-2020 nibble <nibble.ds@gmail.com>
+// SPDX-FileCopyrightText: 2021-2021 heersin <teablearcher@gmail.com>
+// SPDX-License-Identifier: LGPL-3.0-only
 #include <string.h>
 
 #include <rz_types.h>
