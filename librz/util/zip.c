d37b86c0aa1a8a35f1c55cc25f11e4babb92b767
DMaroo <dhruvmaru007@gmail.com>
2021-08-13 12:51:44 +0530

    Add function definitions for rz_deflate and rz_deflatew
     * TODO: integrate the functions in project.c to save rzdb in compressed
       formats

--- librz/util/zip.c
+++ librz/util/zip.c
@@ -43,8 +43,8 @@ RZ_API ut8 *rz_inflate_ignore_header(const ut8 *src, int srcLen, int *srcConsume
  * \brief inflate zlib compressed or gzipped.
  * \param src source compressed bytes
  * \param srcLen source bytes length
- * \param srcConsumed comsumed source bytes length
- * \param dstLen uncompressed uncompressed bytes length
+ * \param srcConsumed consumed source bytes length
+ * \param dstLen uncompressed bytes length
  * \param controls the size of the history buffer (or “window size”), and what header and trailer format is expected.
  * \return ptr to uncompressed
  */
@@ -87,8 +87,7 @@ RZ_API ut8 *rz_inflatew(const ut8 *src, int srcLen, int *srcConsumed, int *dstLe
 		}
 		err = inflate(&stream, Z_NO_FLUSH);
 		if (err < 0) {
-			eprintf("inflate error: %d %s\n",
-				err, gzerr(-err));
+			RZ_LOG_ERROR("inflate error: %d %s\n", err, gzerr(-err));
 			goto err_exit;
 		}
 	} while (err != Z_STREAM_END);
@@ -106,5 +105,84 @@ RZ_API ut8 *rz_inflatew(const ut8 *src, int srcLen, int *srcConsumed, int *dstLe
 err_exit:
 	inflateEnd(&stream);
 	free(dst);
+	return NULL;
+}
+
+/**
+ * @brief deflate uncompressed data to zlib or gzipped, use MAX_WBITS as the window size logarithm.
+ * @see rz_deflatew()
+ */
+RZ_API ut8 *rz_deflate(const ut8 *src, int srcLen, int *srcConsumed, int *dstLen) {
+	return rz_deflatew(src, srcLen, srcConsumed, dstLen, MAX_WBITS + 32);
+}
+
+/**
+ * \brief compress/deflate data to zlib or gzip.
+ * \param src source uncompressed bytes
+ * \param srcLen source bytes length
+ * \param srcConsumed consumed source bytes length
+ * \param dstLen compressed bytes length
+ * \param controls the size of the history buffer (or “window size”), and what header and trailer format is expected.
+ * \return ptr to compressed
+ */
+RZ_API ut8 *rz_deflatew(const ut8 *src, int srcLen, int *srcConsumed, int *dstLen, int wbits) {
+	int err = 0;
+	int out_size = 0;
+	ut8 *dst = NULL;
+	ut8 *tmp_ptr;
+	z_stream stream;
+
+	if (srcLen <= 0) {
+		return NULL;
+	}
+
+	memset(&stream, 0, sizeof(z_stream));
+
+	stream.avail_in = srcLen;
+	stream.next_in = (Bytef *)src;
+	stream.zalloc = Z_NULL;
+	stream.zfree = Z_NULL;
+	stream.opaque = Z_NULL;
+
+	if (deflateInit2(&stream, Z_DEFAULT_COMPRESSION, Z_DEFLATED, wbits, 8, Z_DEFAULT_STRATEGY) != Z_OK) {
+		return NULL;
+	}
+
+	do {
+		if (stream.avail_out == 0) {
+			tmp_ptr = realloc(dst, stream.total_out + srcLen);
+			if (!tmp_ptr) {
+				goto err_exit;
+			}
+			dst = tmp_ptr;
+			out_size += srcLen;
+			if (out_size > MAXOUT) {
+				goto err_exit;
+			}
+			stream.next_out = dst + stream.total_out;
+			stream.avail_out = srcLen;
+		}
+		err = deflate(&stream, Z_NO_FLUSH);
+		if (err < 0) {
+			RZ_LOG_ERROR("deflate error: %d %s\n", err, gzerr(-err));
+			goto err_exit;
+		}
+	} while (err != Z_STREAM_END);
+
+	if (dstLen) {
+		*dstLen = stream.total_out;
+	}
+	if (srcConsumed) {
+		*srcConsumed = (const ut8 *)stream.next_in - (const ut8 *)src;
+	}
+
+	deflateEnd(&stream);
+	return dst;
+
+err_exit:
+	deflateEnd(&stream);
+	free(dst);
+	return NULL;
+
 	return NULL;
 }
