radareorg/radare2

Reset certificate search properly (#18664) ##search

libr/core/cmd_search.c
@@ -3391,6 +3391,7 @@ reread:
 			{
 				RSearchKeyword *kw;
 				kw = r_search_keyword_new_hex ("308200003082", "ffff0000ffff", NULL);
+				r_search_reset (core->search, R_SEARCH_KEYWORD);
 				if (kw) {
 					r_search_kw_add (core->search, kw);
 					// eprintf ("Searching %d byte(s)...\n", kw->keyword_length);

rizinorg/rizin

librz/core/cmd_search.c
@@ -3282,6 +3282,7 @@ reread:
 		{
 			RzSearchKeyword *kw;
 			kw = rz_search_keyword_new_hex("308200003082", "ffff0000ffff", NULL);
+			rz_search_reset(core->search, RZ_SEARCH_KEYWORD);
 			if (kw) {
 				rz_search_kw_add(core->search, kw);
 				// eprintf ("Searching %d byte(s)...\n", kw->keyword_length);