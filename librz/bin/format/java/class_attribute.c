e3e4d5cc122380ec519b16404a0319a121747b3d
Giovanni <561184+wargio@users.noreply.github.com>
2021-08-19 16:01:20 +0200

    Fix double free in class_attribute.c (#1486)
    * return when fail in java_attribute_set_localvariabletable
    * return when fail in java_attribute_set_linenumbertable

--- librz/bin/format/java/class_attribute.c
+++ librz/bin/format/java/class_attribute.c
@@ -208,6 +208,7 @@ bool java_attribute_set_linenumbertable(Attribute *attr, RzBuffer *buf) {
 				!rz_buf_read_be16(buf, &alnt->table[i].line_number)) {
 				free(alnt->table);
 				free(alnt);
+				return false;
 			}
 		}
 	}
@@ -243,6 +244,7 @@ bool java_attribute_set_localvariabletable(Attribute *attr, RzBuffer *buf) {
 				!rz_buf_read_be16(buf, &alvt->table[i].index)) {
 				free(alvt->table);
 				free(alvt);
+				return false;
 			}
 		}
 	}
