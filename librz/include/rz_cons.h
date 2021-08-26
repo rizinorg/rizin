4091c38f6709850255074e0c32fb367c09618ca1
GustavoLCR <gugulcr@gmail.com>
2021-08-26 02:16:43 -0300

    Always try to set console codepage to UTF8

--- librz/include/rz_cons.h
+++ librz/include/rz_cons.h
@@ -521,6 +521,7 @@ typedef struct rz_cons_t {
 #elif __WINDOWS__
 	DWORD term_raw, term_buf, term_xterm;
 	UINT old_cp;
+	UINT old_ocp;
 #endif
 	RNum *num;
 	/* Pager (like more or less) to use if the output doesn't fit on the
