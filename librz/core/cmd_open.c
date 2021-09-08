fdb2b4228d98c018c0b6d75bdb338a2e0f1e3811
Rot127 <unisono@quyllur.org>
2021-09-08 10:44:48 +0200

    Replace 'obr' with 'obR'

--- librz/core/cmd_open.c
+++ librz/core/cmd_open.c
@@ -80,8 +80,7 @@ static const char *help_msg_ob[] = {
 	"obj", "", "List opened binary files and objid (JSON format)",
 	"obn", " [name]", "Select binfile by name",
 	"obo", " [fd]", "Switch to open binfile by fd number",
-	"obr", " [baddr]", "Rebase current bin object",
-	"obR", " [baddr]", "Reload the current buffer for setting of the bin (use once only)",
+	"obr", " [baddr]", "Reload the current buffer for setting of the bin (use once only)",
 	NULL
 };
 
@@ -326,25 +325,13 @@ static void cmd_open_bin(RzCore *core, const char *input) {
 		free(v);
 		break;
 	}
-	case 'R': // "obR"
+	case 'r': // "obr"
 		// XXX: this will reload the bin using the buffer.
 		// An assumption is made that assumes there is an underlying
 		// plugin that will be used to load the bin (e.g. malloc://)
 		// TODO: Might be nice to reload a bin at a specified offset?
 		core_bin_reload(core, NULL, input[2] ? rz_num_math(core->num, input + 3) : 0);
 		rz_core_block_read(core);
-		break;
-	case 'r':; // "obr"
-		ut64 baddr = rz_bin_get_baddr(core->bin);
-		ut64 addr = rz_num_math(core->num, input + 2); // mapaddr
-		ut32 cur_bf_id = core->bin->cur->id;
-		char cmd[64] = "";
-
-		sprintf(cmd, "oba 0x%llx 0x%llx", baddr, addr);
-		rz_core_cmd0(core, cmd);
-		sprintf(cmd, "ob-%d", cur_bf_id);
-		rz_core_cmd0(core, cmd);
-		rz_core_bin_apply_all_info(core, rz_bin_cur(core->bin));
 		break;
 	case 'f':
 		if (input[2] == ' ') {
