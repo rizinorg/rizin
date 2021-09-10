fc4d5cd81eca21869dae5118b733fbdc916bfe4a
Anton Kochkov <anton.kochkov@gmail.com>
2021-09-10 15:18:13 +0800

    Fixing Coverity detected leaks in types (#1644)
    * Fix CID 356334
    * Fix CID 356331
    * Fix CID 356330

--- librz/type/type.c
+++ librz/type/type.c
@@ -832,9 +832,12 @@ static char *type_as_string(const RzTypeDB *typedb, RZ_NONNULL const RzType *typ
 		free(typestr);
 		break;
 	}
-	case RZ_TYPE_KIND_CALLABLE:
-		rz_strbuf_append(buf, rz_type_callable_as_string(typedb, type->callable));
+	case RZ_TYPE_KIND_CALLABLE: {
+		char *callstr = rz_type_callable_as_string(typedb, type->callable);
+		rz_strbuf_append(buf, callstr);
+		free(callstr);
 		break;
+	}
 	}
 
 	char *result = rz_strbuf_drain(buf);
@@ -891,9 +894,12 @@ static char *type_as_string_decl(const RzTypeDB *typedb, RZ_NONNULL const RzType
 		free(typestr);
 		break;
 	}
-	case RZ_TYPE_KIND_CALLABLE:
-		rz_strbuf_append(buf, rz_type_callable_as_string(typedb, type->callable));
+	case RZ_TYPE_KIND_CALLABLE: {
+		char *callstr = rz_type_callable_as_string(typedb, type->callable);
+		rz_strbuf_append(buf, callstr);
+		free(callstr);
 		break;
+	}
 	}
 	char *result = rz_strbuf_drain(buf);
 	return result;
@@ -971,9 +977,12 @@ static char *type_as_string_identifier_decl(const RzTypeDB *typedb, RZ_NONNULL c
 		free(typestr);
 		break;
 	}
-	case RZ_TYPE_KIND_CALLABLE:
-		rz_strbuf_append(buf, rz_type_callable_as_string(typedb, type->callable));
+	case RZ_TYPE_KIND_CALLABLE: {
+		char *callstr = rz_type_callable_as_string(typedb, type->callable);
+		rz_strbuf_append(buf, callstr);
+		free(callstr);
 		break;
+	}
 	}
 	return rz_strbuf_drain(buf);
 }
