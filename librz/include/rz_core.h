709a1814d8302fd3fd905fd60d4a3ee4510ad7c3
Florian Märkl <info@florianmaerkl.de>
2021-09-14 16:25:52 +0200

    Expose rz_core_load_theme() as API (#1680)

--- librz/include/rz_core.h
+++ librz/include/rz_core.h
@@ -416,6 +416,7 @@ RZ_API bool rz_core_plugin_fini(RzCore *core);
 //#define rz_core_ncast(x) (RzCore*)(size_t)(x)
 RZ_API RzList *rz_core_list_themes(RzCore *core);
 RZ_API char *rz_core_get_theme(void);
+RZ_API bool rz_core_load_theme(RzCore *core, const char *name);
 RZ_API void rz_core_theme_nextpal(RzCore *core, int mode);
 RZ_API const char *rz_core_get_section_name(RzCore *core, ut64 addr);
 RZ_API RzCons *rz_core_get_cons(RzCore *core);
