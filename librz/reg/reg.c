a7f789b3e50090e9ba9e5aa59c108b9107152db9
Dhruv Maroo <dhruvmaru007@gmail.com>
2021-09-27 14:42:28 +0530

    Add `sys` and `pauth` register profiles

--- librz/reg/reg.c
+++ librz/reg/reg.c
@@ -7,7 +7,7 @@
 RZ_LIB_VERSION(rz_reg);
 
 static const char *types[RZ_REG_TYPE_LAST + 1] = {
-	"gpr", "drx", "fpu", "mmx", "xmm", "ymm", "flg", "seg", NULL
+	"gpr", "drx", "fpu", "mmx", "xmm", "ymm", "flg", "seg", "sys", "pauth", NULL
 };
 
 // Take the 32bits name of a register, and return the 64 bit name of it.
