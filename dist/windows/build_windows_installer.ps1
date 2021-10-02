d3795462db4bf818701f861a0322cc37a545a0a0
Riccardo Schirone <ret2libc@users.noreply.github.com>
2021-10-02 13:53:13 +0200

    Move files to generate windows installer under dist/windows (#1765)

---
+++ dist/windows/build_windows_installer.ps1
@@ -0,0 +1,29 @@
+$name=$args[0]
+$bits=$args[1]
+$end=$args.Length
+$meson_options=$args[2..$end]
+$scriptpath=$PSScriptRoot
+$builddir=$(Join-Path -Path $env:TEMP -ChildPath "build-win-installer")
+$installdir=$(Join-Path -Path $env:TEMP -ChildPath "rizin-win-installer")
+
+pushd $PSScriptRoot\..\..
+
+$version=$(python sys\version.py)
+echo $version
+
+$env:Path += ";$env:ProgramFiles (x86)\Microsoft Visual Studio\Installer"
+$env:Path += ";$env:ProgramFiles\7-Zip"
+$env:Path += ";$env:ProgramFiles (x86)\Inno Setup 6"
+
+dist\windows\vsdevenv.ps1 $bits
+
+meson --buildtype=release --prefix=$installdir $builddir $meson_options
+ninja -C $builddir
+ninja -C $builddir install
+7z a dist\windows\Output\rizin-$name-$version.zip $installdir
+iscc dist\windows\rizin.iss /DRizinLocation=$installdir\* /DLicenseLocation=$PWD\COPYING.LESSER /DMyAppVersion=$version
+
+rm -r $builddir
+rm -r $installdir
+
+popd
