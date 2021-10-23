3ac15b2665704b6aaaf2d9eec212bcc3b09f2401
billow <billow.fun@gmail.com>
2021-10-23 08:09:05 +0800

    fix adjust_offset

--- librz/util/str_search.c
+++ librz/util/str_search.c
@@ -116,6 +116,12 @@ static ut64 adjust_offset(RzStrEnc str_type, const ut8 *buf, const ut64 str_star
 			}
 		}
 		break;
+	case RZ_STRING_ENC_IBM037:
+	case RZ_STRING_ENC_IBM290:
+	case RZ_STRING_ENC_EBCDIC_ES:
+	case RZ_STRING_ENC_EBCDIC_UK:
+	case RZ_STRING_ENC_EBCDIC_US:
+		return 1;
 	default:
 		break;
 	}
@@ -420,8 +426,9 @@ RZ_API int rz_scan_strings(RzBuffer *buf_to_scan, RzList *list, const RzUtilStrS
 						needle++;
 						continue;
 					}
+				} else {
+					str_type = RZ_STRING_ENC_8BIT;
 				}
-				str_type = RZ_STRING_ENC_8BIT;
 			}
 		} else if (type == RZ_STRING_ENC_UTF8) {
 			str_type = RZ_STRING_ENC_8BIT; // initial assumption
