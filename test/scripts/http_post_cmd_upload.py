66902aee218d3076d64855a231d2727255c6f79d
ogre2007@github.com <r3le@ya.ru>
2021-10-29 12:40:38 +0300

    Pylint/black checkers fix

--- test/scripts/http_post_cmd_upload.py
+++ test/scripts/http_post_cmd_upload.py
@@ -10,33 +10,29 @@ usage:
 """
 
 import subprocess
-import sys
 import os
 
 PORT = 28080
-URL = f'http://localhost:{PORT}'
-TMP_DIR = '/tmp'
-TARGET = '/bin/true'
-SAVED_NAME = 'rz_http_test'
+URL = f"http://localhost:{PORT}"
+TMP_DIR = "/tmp"
+TARGET = "/bin/true"
+SAVED_NAME = "rz_http_test"
 
 
 def start_rizin(cmd):
-    #print(' '.join(cmd))
-    return subprocess.Popen(
-        cmd,
-        stderr=subprocess.PIPE,
-        universal_newlines=True)
+    # print(' '.join(cmd))
+    return subprocess.Popen(cmd, stderr=subprocess.PIPE, universal_newlines=True)
 
 
 def curl_run(cmd):
-    cmd = ['curl', '-s'] + cmd
+    cmd = ["curl", "-s"] + cmd
     # print(cmd)
-    result = subprocess.run(cmd, stdout=subprocess.PIPE)
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
     return result
 
 
 def curl_upload(url, file):
-    cmd = [url, f'-F upload=@{file}']
+    cmd = [url, f"-F upload=@{file}"]
     return curl_run(cmd)
 
 
@@ -46,18 +42,23 @@ def curl_get(url, path):
 
 
 def curl_post(url, path, data):
-    cmd = [url + path, f'-d {data}']
+    cmd = [url + path, f"-d {data}"]
     return curl_run(cmd)
 
 
 def main():
     # start rizin
-    popen = start_rizin(['rizin', '-q', f'-e http.port={PORT}',
-                         '-e http.upload=1',
-                         f'-e http.root={TMP_DIR}',
-                         f'-e http.uproot={TMP_DIR}',
-                         '-cRh'
-                         ])
+    popen = start_rizin(
+        [
+            "rizin",
+            "-q",
+            f"-e http.port={PORT}",
+            "-e http.upload=1",
+            f"-e http.root={TMP_DIR}",
+            f"-e http.uproot={TMP_DIR}",
+            "-cRh",
+        ]
+    )
     for output in iter(popen.stderr.readline, ""):
         # print(output)
         # Exit loop once rizin is ready for connections
@@ -65,28 +66,28 @@ def main():
             break
 
     # upload the binary via new /upload path
-    up = curl_upload(URL + '/upload/' + SAVED_NAME, TARGET)
-    # print(up.stdout)
+    curl_upload(URL + "/upload/" + SAVED_NAME, TARGET)
 
     # open the file via new POST-cmd
-    curl_post(URL, '/cmd/', f'o {TMP_DIR}/{SAVED_NAME}')
+    curl_post(URL, "/cmd/", f"o {TMP_DIR}/{SAVED_NAME}")
 
     # analyze the file via new POST-cmd
-    post_cmd = curl_post(URL, '/cmd/', f'ii')
+    post_cmd = curl_post(URL, "/cmd/", f"ii")
     # print(post_cmd.stdout)
 
     # analyze the file by old GET-cmd
-    get_cmd = curl_get(URL, '/cmd/ii')
+    get_cmd = curl_get(URL, "/cmd/ii")
     # print(get_cmd.stdout)
 
     # compare results
     if post_cmd.stdout == get_cmd.stdout:
-        print('New and old cmd results equal')
+        print("New and old cmd results equal")
 
-    if post_cmd.stdout.find(b'strncmp') > 0:
-        print('Test succeeded')
+    if post_cmd.stdout.find(b"strncmp") > 0:
+        print("Test succeeded")
     else:
-        print('Something goes wrong')
+        print("Something goes wrong")
+    os.remove(TMP_DIR + "/" + SAVED_NAME)
     popen.kill()
 
 
