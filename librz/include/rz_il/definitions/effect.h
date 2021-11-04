6d2c547ca12e3b7937d8033f55fd03a1930c02e3
Giovanni <561184+wargio@users.noreply.github.com>
2021-11-04 19:48:10 +0100

    [RzIL] Refactoring, added bool xor and added bitv carry/borrow (#1930)

--- librz/include/rz_il/definitions/effect.h
+++ librz/include/rz_il/definitions/effect.h
@@ -18,33 +18,35 @@ typedef enum {
 	EFFECT_TYPE_NON, // perform none effect will not affect data / control, used for passing info
 	EFFECT_TYPE_DATA,
 	EFFECT_TYPE_CTRL,
-} EFFECT_TYPE;
+} RzEffectType;
 
 typedef enum {
 	EFFECT_NOTATION_NON = 0x0,
 	EFFECT_NOTATION_GOTO_SYS = 0x1,
 	EFFECT_NOTATION_GOTO_HOOK = 0x2
-} EFFECT_NOTATION;
+} RzEffectNotation;
 
 typedef enum {
 	EFFECT_LABEL_ADDR,
 	EFFECT_LABEL_SYSCALL,
 	EFFECT_LABEL_HOOK
 	// more
-} EFFECT_LABEL_TYPE;
+} RzILEffectLabelType;
 
 typedef enum {
 	DATA_EFF_NON,
 	DATA_EFF_ASSIGN,
 	DATA_EFF_INC
 	// maybe more
-} DATA_EFF_OPERATION;
+} RzILDataEffOperation;
 
 struct rzil_effect_label_t {
 	char *label_id; ///< Label name
-	RzILBitVector *addr; ///< RzILBitVector address if EFFECT_LABEL_ADDR
-		///< Function pointer if EFFECT_LABEL_SYSCALL / EFFECT_LABEL_HOOK
-	EFFECT_LABEL_TYPE type; ///< type of label
+	union {
+		RzILBitVector *addr; ///< RzILBitVector address if EFFECT_LABEL_ADDR
+		void *hook; ///< Function pointer if EFFECT_LABEL_SYSCALL / EFFECT_LABEL_HOOK
+	};
+	RzILEffectLabelType type; ///< type of label
 };
 
 struct rzil_control_effect_t {
@@ -54,7 +56,7 @@ struct rzil_control_effect_t {
 struct rzil_data_effect_t {
 	const char *var_name; ///< Name of variable, const one
 	RzILVal *val;
-	DATA_EFF_OPERATION operation; ///< operation to value and variable
+	RzILDataEffOperation operation; ///< operation to value and variable
 };
 
 typedef struct rzil_control_effect_t RzILCtrlEffect;
@@ -68,7 +70,7 @@ typedef struct rzil_effect_union_t RzILEffect;
  */
 struct rzil_effect_union_t {
 	ut8 effect_type; ///< effect type
-	EFFECT_NOTATION notation; ///< Marks for carring additional info
+	RzEffectNotation notation; ///< Marks for carring additional info
 	RzILEffect *next_eff; ///< pointer to next effect, used in packed effect
 	union {
 		RzILCtrlEffect *ctrl_eff; ///< pointer to ctrl effect
@@ -78,12 +80,12 @@ struct rzil_effect_union_t {
 
 // a chain of effects
 // should use something like rz_vector / rz_list
-RZ_API RzILEffect *rz_il_effect_new(EFFECT_TYPE type);
+RZ_API RzILEffect *rz_il_effect_new(RzEffectType type);
 RZ_API RzILDataEffect *rz_il_effect_data_new(void);
 RZ_API RzILCtrlEffect *rz_il_effect_ctrl_new(void);
 RZ_API RzILEffect *rz_il_wrap_ctrl_effect(RzILCtrlEffect *eff);
 RZ_API RzILEffect *rz_il_wrap_data_effect(RzILDataEffect *eff);
-RZ_API RzILEffectLabel *rz_il_effect_label_new(const char *name, EFFECT_LABEL_TYPE type);
+RZ_API RzILEffectLabel *rz_il_effect_label_new(const char *name, RzILEffectLabelType type);
 RZ_API void rz_il_effect_free(RzILEffect *effect);
 RZ_API void rz_il_effect_ctrl_free(RzILCtrlEffect *eff);
 RZ_API void rz_il_effect_data_free(RzILDataEffect *eff);
