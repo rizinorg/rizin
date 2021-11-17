a60a6629831e65114af2812bea25214862ae9740
Giovanni <561184+wargio@users.noreply.github.com>
2021-11-17 11:51:04 +0100

    Fix #1881 - Fix windows build (#1984)

--- dist/windows/build_windows_installer.ps1
+++ dist/windows/build_windows_installer.ps1
@@ -3,13 +3,15 @@ $bits=$args[1]
 $end=$args.Length
 $meson_options=$args[2..$end]
 $scriptpath=$PSScriptRoot
-$builddir=$(Join-Path -Path $env:TEMP -ChildPath "build-win-installer")
-$installdir=$(Join-Path -Path $env:TEMP -ChildPath "rizin-win-installer")
+$builddir=$(Join-Path -Path $env:TEMP -ChildPath "build-win-installer-$name-$bits")
+$installdir=$(Join-Path -Path $env:TEMP -ChildPath "rizin-win-installer-$name-$bits")
 
 pushd $PSScriptRoot\..\..
 
 $version=$(python sys\version.py)
 echo $version
+echo $builddir
+echo $installdir
 
 $env:Path += ";$env:ProgramFiles (x86)\Microsoft Visual Studio\Installer"
 $env:Path += ";$env:ProgramFiles\7-Zip"
@@ -18,7 +20,7 @@ $env:Path += ";$env:ProgramFiles (x86)\Inno Setup 6"
 dist\windows\vsdevenv.ps1 $bits
 
 meson --buildtype=release --prefix=$installdir $builddir $meson_options
-ninja -C $builddir
+ninja -C $builddir -j1
 ninja -C $builddir install
 7z a dist\windows\Output\rizin-$name-$version.zip $installdir
 iscc dist\windows\rizin.iss /DRizinLocation=$installdir\* /DLicenseLocation=$PWD\COPYING.LESSER /DMyAppVersion=$version
