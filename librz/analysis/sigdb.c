f3db4a814480130ac68bf5797563d583e7c239ca
wargio <wargio@libero.it>
2021-12-20 12:57:58 +0100

    Added missing license

--- librz/analysis/sigdb.c
+++ librz/analysis/sigdb.c
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2021 RizinOrg <info@rizin.re>
+// SPDX-FileCopyrightText: 2021 deroad <wargio@libero.it>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #include <rz_analysis.h>
 
 static void analysis_sigdb_signature_free(RzAnalysisSignature *sig) {
