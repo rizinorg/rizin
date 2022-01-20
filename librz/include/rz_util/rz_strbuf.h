c4bc9c77220a23189d6f2e7fdc2168c09cb53f73
Dhruv Maroo <dhruvmaru007@gmail.com>
2022-01-20 22:09:17 +0530

    Add `rz_strbuf_clear` for clearing/emptying RzStrBuf

--- librz/include/rz_util/rz_strbuf.h
+++ librz/include/rz_util/rz_strbuf.h
@@ -18,6 +18,7 @@ RZ_API RzStrBuf *rz_strbuf_new(const char *s);
 RZ_API const char *rz_strbuf_set(RzStrBuf *sb, const char *s); // return = the string or NULL on fail
 RZ_API bool rz_strbuf_slice(RzStrBuf *sb, size_t from, size_t len);
 RZ_API bool rz_strbuf_setbin(RzStrBuf *sb, const ut8 *s, size_t len);
+RZ_API bool rz_strbuf_clear(RZ_NONNULL RzStrBuf *sb);
 RZ_API ut8 *rz_strbuf_getbin(RzStrBuf *sb, int *len);
 RZ_API const char *rz_strbuf_setf(RzStrBuf *sb, const char *fmt, ...) RZ_PRINTF_CHECK(2, 3); // return = the string or NULL on fail
 RZ_API const char *rz_strbuf_vsetf(RzStrBuf *sb, const char *fmt, va_list ap); // return = the string or NULL on fail
