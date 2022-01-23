e5201bfdcc115c23db74bb80df4c00e038ac3655
GustavoLCR <gugulcr@gmail.com>
2022-01-22 23:49:36 -0300

    Move to API documentation to `pdb_downloader.c`
    * Add `RZ_(NONNULL/NULLABLE)` qualifiers
    * Add `rz_return_val_if_fail()` checks

--- librz/bin/pdb/pdb_downloader.h
+++ librz/bin/pdb/pdb_downloader.h
@@ -31,29 +31,8 @@ typedef struct SPDBDownloader {
 	char *(*download)(struct SPDBDownloader *pdb_downloader);
 } SPDBDownloader;
 
-/**
- * \brief initialization of pdb downloader by SPDBDownloaderOpt
- *
- * \param opt PDB options
- * \param pdb_downloader PDB downloader that will be init
- */
-void init_pdb_downloader(SPDBDownloaderOpt *opt, SPDBDownloader *pdb_downloader);
-/**
- * \brief deinitialization of PDB downloader
- *
- * \param pdb_downloader PDB downloader that will be deinit
- */
-void deinit_pdb_downloader(SPDBDownloader *pdb_downloader);
-/**
- * \brief download PDB file
- */
-RZ_API int rz_bin_pdb_download(RzBin *bin, PJ *pj, int isradjson, SPDBOptions *options);
-/**
- * \brief downloads file from symbol server
- * \param options options for downloading file
- * \return char* is the path that file was downloaded to or NULL in case of failure
- */
-RZ_API char *rz_bin_symserver_download(const SPDBDownloaderOpt *options);
+RZ_API int rz_bin_pdb_download(RZ_NONNULL RzBin *bin, RZ_NULLABLE PJ *pj, int isradjson, RZ_NONNULL SPDBOptions *options);
+RZ_API RZ_OWN char *rz_bin_symserver_download(RZ_NONNULL const SPDBDownloaderOpt *options);
 
 #ifdef __cplusplus
 }
