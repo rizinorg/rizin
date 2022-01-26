rizinorg/rizin

commit e2edd9d1ef79842e772e21f07b4992438614083d

Fix many pointer/int conversion warnings on arm32 (#2246)
This is -Wint-to-pointer-cast and -Wpointer-to-int-cast of gcc 10 on
arm32

librz/bin/format/mach0/mach0.c
@@ -2751,12 +2751,12 @@ static int walk_exports(struct MACH0_(obj_t) * bin, RExportsIterator iterator, v
 			RZ_FREE(next);
 			goto beach;
 		}
-		if (UT64_ADD_OVFCHK(tr, (ut64)trie) || tr + (ut64)trie >= (ut64)end) {
+		if (SZT_ADD_OVFCHK((size_t)tr, (size_t)trie) || (size_t)tr + trie >= end) {
 			RZ_LOG_ERROR("malformed export trie\n");
 			RZ_FREE(next);
 			goto beach;
 		}
-		next->node = tr + trie;
+		next->node = (size_t)tr + trie;
 		{
 			// avoid loops
 			RzListIter *it;

librz/analysis/block.c
@@ -857,34 +857,34 @@ static bool automerge_predecessor_successor_cb(ut64 addr, void *user) {
 		return true;
 	}
 	bool found;
-	RzAnalysisBlock *pred = ht_up_find(ctx->predecessors, (ut64)block, &found);
+	RzAnalysisBlock *pred = ht_up_find(ctx->predecessors, (ut64)(size_t)block, &found);
 	if (found) {
 		if (pred) {
 			// only one predecessor found so far, but we are the second so there are multiple now
-			ht_up_update(ctx->predecessors, (ut64)block, NULL);
+			ht_up_update(ctx->predecessors, (ut64)(size_t)block, NULL);
 		} // else: already found multiple predecessors, nothing to do
 	} else {
 		// no predecessor found yet, this is the only one until now
-		ht_up_insert(ctx->predecessors, (ut64)block, ctx->cur_pred);
+		ht_up_insert(ctx->predecessors, (ut64)(size_t)block, ctx->cur_pred);
 	}
 	return true;
 }
 
 static bool automerge_get_predecessors_cb(void *user, const ut64 k, const void *v) {
 	AutomergeCtx *ctx = user;
-	const RzAnalysisFunction *fcn = (const RzAnalysisFunction *)k;
+	const RzAnalysisFunction *fcn = (const RzAnalysisFunction *)(size_t)k;
 	RzListIter *it;
 	RzAnalysisBlock *block;
 	rz_list_foreach (fcn->bbs, it, block) {
 		bool already_visited;
-		ht_up_find(ctx->visited_blocks, (ut64)block, &already_visited);
+		ht_up_find(ctx->visited_blocks, (ut64)(size_t)block, &already_visited);
 		if (already_visited) {
 			continue;
 		}
 		ctx->cur_pred = block;
 		ctx->cur_succ_count = 0;
 		rz_analysis_block_successor_addrs_foreach(block, automerge_predecessor_successor_cb, ctx);
-		ht_up_insert(ctx->visited_blocks, (ut64)block, (void *)ctx->cur_succ_count);
+		ht_up_insert(ctx->visited_blocks, (ut64)(size_t)block, (void *)ctx->cur_succ_count);
 	}
 	return true;
 }
@@ -912,7 +912,7 @@ RZ_API void rz_analysis_block_automerge(RzList *blocks) {
 		RzListIter *fit;
 		RzAnalysisFunction *fcn;
 		rz_list_foreach (block->fcns, fit, fcn) {
-			ht_up_insert(relevant_fcns, (ut64)fcn, NULL);
+			ht_up_insert(relevant_fcns, (ut64)(size_t)fcn, NULL);
 		}
 		ht_up_insert(ctx.blocks, block->addr, block);
 	}
@@ -923,11 +923,11 @@ RZ_API void rz_analysis_block_automerge(RzList *blocks) {
 	// Now finally do the merging
 	RzListIter *tmp;
 	rz_list_foreach_safe (blocks, it, tmp, block) {
-		RzAnalysisBlock *predecessor = ht_up_find(ctx.predecessors, (ut64)block, NULL);
+		RzAnalysisBlock *predecessor = ht_up_find(ctx.predecessors, (ut64)(size_t)block, NULL);
 		if (!predecessor) {
 			continue;
 		}
-		size_t pred_succs_count = (size_t)ht_up_find(ctx.visited_blocks, (ut64)predecessor, NULL);
+		size_t pred_succs_count = (size_t)ht_up_find(ctx.visited_blocks, (ut64)(size_t)predecessor, NULL);
 		if (pred_succs_count != 1) {
 			// we can only merge this predecessor if it has exactly one successor
 			continue;
@@ -939,7 +939,7 @@ RZ_API void rz_analysis_block_automerge(RzList *blocks) {
 		RzListIter *bit;
 		RzAnalysisBlock *clock;
 		for (bit = it->n; bit && (clock = bit->data, 1); bit = bit->n) {
-			RzAnalysisBlock *fixup_pred = ht_up_find(ctx.predecessors, (ut64)clock, NULL);
+			RzAnalysisBlock *fixup_pred = ht_up_find(ctx.predecessors, (ut64)(size_t)clock, NULL);
 			if (fixup_pred == block) {
 				rz_list_push(fixup_candidates, clock);
 			}
@@ -950,10 +950,10 @@ RZ_API void rz_analysis_block_automerge(RzList *blocks) {
 			// Update number of successors of the predecessor
 			ctx.cur_succ_count = 0;
 			rz_analysis_block_successor_addrs_foreach(predecessor, count_successors_cb, &ctx);
-			ht_up_update(ctx.visited_blocks, (ut64)predecessor, (void *)ctx.cur_succ_count);
+			ht_up_update(ctx.visited_blocks, (ut64)(size_t)predecessor, (void *)(size_t)ctx.cur_succ_count);
 			rz_list_foreach (fixup_candidates, bit, clock) {
 				// Make sure all previous pointers to block now go to predecessor
-				ht_up_update(ctx.predecessors, (ut64)clock, predecessor);
+				ht_up_update(ctx.predecessors, (ut64)(size_t)clock, predecessor);
 			}
 			// Remove it from the list
 			rz_list_split_iter(blocks, it);

librz/bin/format/le/le.c
@@ -633,7 +633,7 @@ RzList *rz_bin_le_get_relocs(rz_bin_le_obj_t *bin) {
 			break;
 		}
 		case INTERNALENTRY:
-			rel->addend = (ut64)rz_list_get_n(entries, ordinal - 1);
+			rel->addend = (ut64)(size_t)rz_list_get_n(entries, ordinal - 1);
 			break;
 		}
 		if (header.target & F_TARGET_ADDITIVE) {

librz/core/tui/panels.c
@@ -2502,7 +2502,7 @@ void __fix_layout_w(RzCore *core) {
 	for (; i < panels->n_panels - 1; i++) {
 		RzPanel *p = __get_panel(panels, i);
 		int64_t t = p->view->pos.x + p->view->pos.w;
-		rz_list_append(list, (void *)(t));
+		rz_list_append(list, (void *)(size_t)t);
 	}
 	RzListIter *iter;
 	for (i = 0; i < panels->n_panels; i++) {
@@ -2516,14 +2516,15 @@ void __fix_layout_w(RzCore *core) {
 		bool found = false;
 		void *num = NULL;
 		rz_list_foreach (list, iter, num) {
-			if ((int64_t)num - 1 == tx) {
+			int64_t numi = (int64_t)(size_t)num;
+			if (numi - 1 == tx) {
 				found = true;
 				break;
 			}
-			int sub = (int64_t)num - tx;
+			int sub = numi - tx;
 			if (min > RZ_ABS(sub)) {
 				min = RZ_ABS(sub);
-				target_num = (int64_t)num;
+				target_num = numi;
 			}
 		}
 		if (!found) {
@@ -2543,7 +2544,7 @@ void __fix_layout_h(RzCore *core) {
 	for (; i < panels->n_panels - 1; i++) {
 		RzPanel *p = __get_panel(panels, i);
 		int64_t t = p->view->pos.y + p->view->pos.h;
-		rz_list_append(list, (void *)(t));
+		rz_list_append(list, (void *)(size_t)t);
 	}
 	RzListIter *iter;
 	for (i = 0; i < panels->n_panels; i++) {
@@ -2558,14 +2559,15 @@ void __fix_layout_h(RzCore *core) {
 		bool found = false;
 		void *num = NULL;
 		rz_list_foreach (list, iter, num) {
-			if ((int64_t)num - 1 == ty) {
+			int64_t numi = (int64_t)(size_t)num;
+			if (numi - 1 == ty) {
 				found = true;
 				break;
 			}
-			int sub = (int64_t)num - ty;
+			int sub = numi - ty;
 			if (min > RZ_ABS(sub)) {
 				min = RZ_ABS(sub);
-				target_num = (int64_t)num;
+				target_num = numi;
 			}
 		}
 		if (!found) {

librz/util/file.c
@@ -924,7 +924,7 @@ static RzMmap *file_mmap(RzMmap *m) {
 	m->len = lseek(m->fd, (off_t)0, SEEK_END);
 	if (m->len) {
 		bool is_write = (m->perm & O_WRONLY) || (m->perm & O_RDWR);
-		m->buf = mmap((void *)m->base,
+		m->buf = mmap((void *)(size_t)m->base,
 			m->len,
 			is_write ? PROT_READ | PROT_WRITE : PROT_READ,
 			MAP_SHARED, m->fd, 0);

test/unit/test_analysis_block.c
@@ -483,7 +483,7 @@ bool test_rz_analysis_block_query() {
 
 bool addr_list_cb(ut64 addr, void *user) {
 	RzList *list = user;
-	rz_list_push(list, (void *)addr);
+	rz_list_push(list, (void *)(size_t)addr);
 	return true;
 }
 

test/unit/test_il_vm.c
@@ -32,8 +32,8 @@ static bool test_rzil_vm_global_vars() {
 	// find vars from vm
 	RzILVar *find_var_r1 = rz_il_vm_get_var(vm, RZ_IL_VAR_KIND_GLOBAL, "r1");
 	RzILVar *find_var_r2 = rz_il_vm_get_var(vm, RZ_IL_VAR_KIND_GLOBAL, "r2");
-	mu_assert_eq(var_r1, find_var_r1, "Store and find r1");
-	mu_assert_eq(var_r2, find_var_r2, "Store and find r2");
+	mu_assert_ptreq(var_r1, find_var_r1, "Store and find r1");
+	mu_assert_ptreq(var_r2, find_var_r2, "Store and find r2");
 
 	// initial contents
 	RzILVal *val_r1 = rz_il_vm_get_var_value(vm, RZ_IL_VAR_KIND_GLOBAL, "r1");
@@ -74,7 +74,7 @@ static bool test_rzil_vm_labels() {
 
 	// find label
 	RzILEffectLabel *find_blackhole = rz_il_vm_find_label_by_name(vm, "blackhole");
-	mu_assert_eq(blackhole, find_blackhole, "Find Label");
+	mu_assert_ptreq(blackhole, find_blackhole, "Find Label");
 
 	RzBitVector *find_addr = rz_il_hash_find_addr_by_lblname(vm, "blackhole");
 	is_equal_bv = rz_bv_cmp(find_addr, addr) == 0 ? true : false;
@@ -83,7 +83,7 @@ static bool test_rzil_vm_labels() {
 	// create label lazy (without giving an address)
 	RzILEffectLabel *lazy = rz_il_vm_create_label_lazy(vm, "lazy");
 	RzILEffectLabel *find_lazy = rz_il_vm_find_label_by_name(vm, "lazy");
-	mu_assert_eq(lazy, find_lazy, "Find lazy label");
+	mu_assert_ptreq(lazy, find_lazy, "Find lazy label");
 
 	RzBitVector *lazy_addr = rz_il_hash_find_addr_by_lblname(vm, "lazy");
 	mu_assert_null(lazy_addr, "Lazy label have NULL address");

test/unit/test_uleb128.c
@@ -85,7 +85,7 @@ bool test_leb128_correctness(void) {
 	const ut8 *data = (ut8 *)"\xc5\x00";
 	const ut8 *buf = rz_leb128(data, 2, &val);
 	mu_assert_eq(val, 69, "leb128 decoded");
-	mu_assert_eq(buf, data + 2, "leb128 decoded");
+	mu_assert_ptreq(buf, data + 2, "leb128 decoded");
 	mu_end;
 }
 

commit 97a4f0cdcde667cf0d9019091864298a4e168f76

Simplify and fix macho trie checks
Fix regression introduced in e2edd9d1ef79842e772e21f07b4992438614083d
Build with clang+optimization on linux, then
valgrind rizin test/bins/fuzzed/65940f6c970bb373444e0d0aab817edc
to detect the original issues.

librz/bin/format/mach0/mach0.c
@@ -2751,12 +2751,12 @@ static int walk_exports(struct MACH0_(obj_t) * bin, RExportsIterator iterator, v
 			RZ_FREE(next);
 			goto beach;
 		}
-		if (SZT_ADD_OVFCHK((size_t)tr, (size_t)trie) || (size_t)tr + trie >= end) {
+		if (tr >= size) {
 			RZ_LOG_ERROR("malformed export trie\n");
 			RZ_FREE(next);
 			goto beach;
 		}
-		next->node = (size_t)tr + trie;
+		next->node = trie + (size_t)tr;
 		{
 			// avoid loops
 			RzListIter *it;