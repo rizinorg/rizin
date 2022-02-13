1886edd0170dceaca29ecabd929e79e3aab29be0
Giovanni <561184+wargio@users.noreply.github.com>
2022-02-13 16:49:35 +0100

    [COFF] Add SH, MIPS and ARM support (#2312)
    * Added SH and ARM support
    * Added MIPS support
    * Added R4000 support

--- librz/bin/format/coff/coff.c
+++ librz/bin/format/coff/coff.c
@@ -13,6 +13,9 @@
 RZ_API bool rz_coff_supported_arch(const ut8 *buf) {
 	ut16 arch = *(ut16 *)buf;
 	switch (arch) {
+	case COFF_FILE_MACHINE_MIPS16:
+	case COFF_FILE_MACHINE_MIPSFPU:
+	case COFF_FILE_MACHINE_MIPSFPU16:
 	case COFF_FILE_MACHINE_AMD64:
 	case COFF_FILE_MACHINE_I386:
 	case COFF_FILE_MACHINE_H8300:
@@ -20,6 +23,12 @@ RZ_API bool rz_coff_supported_arch(const ut8 *buf) {
 	case COFF_FILE_MACHINE_R4000:
 	case COFF_FILE_MACHINE_AMD29KBE:
 	case COFF_FILE_MACHINE_AMD29KLE:
+	case COFF_FILE_MACHINE_SH3:
+	case COFF_FILE_MACHINE_SH3DSP:
+	case COFF_FILE_MACHINE_SH4:
+	case COFF_FILE_MACHINE_SH5:
+	case COFF_FILE_MACHINE_THUMB:
+	case COFF_FILE_MACHINE_ARM:
 	case COFF_FILE_MACHINE_ARM64:
 	case COFF_FILE_MACHINE_ARMNT:
 		return true;
