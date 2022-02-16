3c310d0a4503c015dbbd0acad0fc7305d3481616
wargio <wargio@libero.it>
2022-02-16 20:37:59 +0100

    Added install_sigdb and rizin_sigdb for installing sigdb

--- librz/core/cconfig.c
+++ librz/core/cconfig.c
@@ -3797,7 +3797,9 @@ RZ_API int rz_core_config_init(RzCore *core) {
 		"pe", "pilot", "srec", "w32run", "zip", "all", "none", NULL);
 	SETB("flirt.sig.deflate", false, "enables/disables FLIRT zlib compression when creating a signature file (available only for .sig files)");
 	SETI("flirt.node.optimize", RZ_FLIRT_NODE_OPTIMIZE_MAX, "FLIRT optimization option when creating a signature file (none: 0, normal: 1, smallest: 2)");
-	SETPREF("flirt.sigdb.path", "", "Rizin sigdb location on the filesystem.");
+	char *home_sigdb = rz_path_system(RZ_SIGDB);
+	SETPREF("flirt.sigdb.path", home_sigdb, "Rizin sigdb location on the filesystem.");
+	free(home_sigdb);
 
 	rz_config_lock(cfg, true);
 	return true;
