b5354df57ce0d3a95e1fe2890d957889cc70ac08
wargio <wargio@libero.it>
2022-02-17 22:18:07 +0100

    bump copyright

--- librz/analysis/arch/avr/avr_il.c
+++ librz/analysis/arch/avr/avr_il.c
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2021 RizinOrg <info@rizin.re>
-// SPDX-FileCopyrightText: 2021 deroad <wargio@libero.it>
+// SPDX-FileCopyrightText: 2021-2022 RizinOrg <info@rizin.re>
+// SPDX-FileCopyrightText: 2021-2022 deroad <wargio@libero.it>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #include "avr_il.h"
--- librz/analysis/arch/avr/avr_il.h
+++ librz/analysis/arch/avr/avr_il.h
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2021 RizinOrg <info@rizin.re>
-// SPDX-FileCopyrightText: 2021 deroad <wargio@libero.it>
+// SPDX-FileCopyrightText: 2021-2022 RizinOrg <info@rizin.re>
+// SPDX-FileCopyrightText: 2021-2022 deroad <wargio@libero.it>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #ifndef RZIL_ANALYSIS_AVR_H
