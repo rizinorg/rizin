9b2f23b2e3a2c7fc4983bdcaf2e93ea1908dadf2
Giovanni <561184+wargio@users.noreply.github.com>
2022-02-21 13:17:26 +0100

    RzIL: add various ops to opbuilder (#2343)

--- librz/include/rz_il/rz_il_opbuilder_begin.h
+++ librz/include/rz_il/rz_il_opbuilder_begin.h
@@ -33,7 +33,7 @@
 #ifndef RZ_IL_OPBUILDER_BEGIN_H
 #define RZ_IL_OPBUILDER_BEGIN_H
 
-#include "rz_il_opcodes.h"
+#include <rz_il/rz_il_opcodes.h>
 
 #define ITE(c, t, f) rz_il_op_new_ite(c, t, f)
 
@@ -54,45 +54,68 @@
 #define INV(x)    rz_il_op_new_bool_inv(x)
 #define XOR(x, y) rz_il_op_new_bool_xor(x, y)
 #define AND(x, y) rz_il_op_new_bool_and(x, y)
-
-#define ADD(x, y)        rz_il_op_new_add(x, y)
-#define SUB(x, y)        rz_il_op_new_sub(x, y)
-#define SHIFTL0(v, dist) rz_il_op_new_shiftl(IL_FALSE, v, dist)
-#define SHIFTR0(v, dist) rz_il_op_new_shiftr(IL_FALSE, v, dist)
-#define LOGAND(x, y)     rz_il_op_new_log_and(x, y)
-#define LOGOR(x, y)      rz_il_op_new_log_or(x, y)
-#define LOGXOR(x, y)     rz_il_op_new_log_xor(x, y)
-
-#define IS_ZERO(x) rz_il_op_new_is_zero(x)
-#define MSB(x)     rz_il_op_new_msb(x)
-#define LSB(x)     rz_il_op_new_lsb(x)
-#define ULT(x, y)  rz_il_op_new_ult(x, y)
-#define ULE(x, y)  rz_il_op_new_ule(x, y)
-
-#define LOAD(addr)       rz_il_op_new_load(0, addr)
-#define LOADW(n, addr)   rz_il_op_new_loadw(0, addr, n)
-#define STORE(addr, val) rz_il_op_new_store(0, addr, val)
-
-#define VARG(name)    rz_il_op_new_var(name, RZ_IL_VAR_KIND_GLOBAL)
-#define VARL(name)    rz_il_op_new_var(name, RZ_IL_VAR_KIND_LOCAL)
-#define VARLP(name)   rz_il_op_new_var(name, RZ_IL_VAR_KIND_LOCAL_PURE)
-#define SETG(name, v) rz_il_op_new_set(name, false, v)
-#define SETL(name, v) rz_il_op_new_set(name, true, v)
+#define OR(x, y)  rz_il_op_new_bool_or(x, y)
 
 #define UNSIGNED(n, x)    rz_il_op_new_unsigned(n, x)
+#define SIGNED(n, x)      rz_il_op_new_signed(n, x)
 #define APPEND(high, low) rz_il_op_new_append(high, low)
 #define DUP(op)           rz_il_op_pure_dup(op)
 
-#define SEQ2(e0, e1)                         rz_il_op_new_seq(e0, e1)
-#define SEQ3(e0, e1, e2)                     rz_il_op_new_seqn(3, e0, e1, e2)
-#define SEQ4(e0, e1, e2, e3)                 rz_il_op_new_seqn(4, e0, e1, e2, e3)
-#define SEQ5(e0, e1, e2, e3, e4)             rz_il_op_new_seqn(5, e0, e1, e2, e3, e4)
-#define SEQ6(e0, e1, e2, e3, e4, e5)         rz_il_op_new_seqn(6, e0, e1, e2, e3, e4, e5)
-#define SEQ7(e0, e1, e2, e3, e4, e5, e6)     rz_il_op_new_seqn(7, e0, e1, e2, e3, e4, e5, e6)
-#define SEQ8(e0, e1, e2, e3, e4, e5, e6, e7) rz_il_op_new_seqn(8, e0, e1, e2, e3, e4, e5, e6, e7)
+#define ADD(x, y)          rz_il_op_new_add(x, y)
+#define SUB(x, y)          rz_il_op_new_sub(x, y)
+#define MUL(x, y)          rz_il_op_new_mul(x, y)
+#define DIV(x, y)          rz_il_op_new_div(x, y)
+#define SDIV(x, y)         rz_il_op_new_sdiv(x, y)
+#define SHIFTL(f, v, dist) rz_il_op_new_shiftl(f, v, dist)
+#define SHIFTR(f, v, dist) rz_il_op_new_shiftr(f, v, dist)
+#define SHIFTL0(v, dist)   SHIFTL(IL_FALSE, v, dist)
+#define SHIFTR0(v, dist)   SHIFTR(IL_FALSE, v, dist)
+#define SHIFTRA(v, dist)   SHIFTR(MSB(DUP(v)), v, dist)
+#define LOGAND(x, y)       rz_il_op_new_log_and(x, y)
+#define LOGOR(x, y)        rz_il_op_new_log_or(x, y)
+#define LOGXOR(x, y)       rz_il_op_new_log_xor(x, y)
+#define LOGNOT(x)          rz_il_op_new_log_not(x)
+#define NEG(x)             rz_il_op_new_neg(x)
+
+#define NON_ZERO(x) rz_il_op_new_non_zero(x)
+#define IS_ZERO(x)  rz_il_op_new_is_zero(x)
+#define MSB(x)      rz_il_op_new_msb(x)
+#define LSB(x)      rz_il_op_new_lsb(x)
+#define EQ(x, y)    rz_il_op_new_eq(x, y)
+#define ULT(x, y)   rz_il_op_new_ult(x, y)
+#define ULE(x, y)   rz_il_op_new_ule(x, y)
+#define UGT(x, y)   rz_il_op_new_ugt(x, y)
+#define UGR(x, y)   rz_il_op_new_ugr(x, y)
+#define SLT(x, y)   rz_il_op_new_slt(x, y)
+#define SLE(x, y)   rz_il_op_new_sle(x, y)
+#define SGT(x, y)   rz_il_op_new_sgt(x, y)
+#define SGE(x, y)   rz_il_op_new_sge(x, y)
+
+#define LOAD(addr)        rz_il_op_new_load(0, addr)
+#define LOADW(n, addr)    rz_il_op_new_loadw(0, addr, n)
+#define STORE(addr, val)  rz_il_op_new_store(0, addr, val)
+#define STOREW(addr, val) rz_il_op_new_storew(0, addr, val)
+
+#define VARG(name)         rz_il_op_new_var(name, RZ_IL_VAR_KIND_GLOBAL)
+#define VARL(name)         rz_il_op_new_var(name, RZ_IL_VAR_KIND_LOCAL)
+#define VARLP(name)        rz_il_op_new_var(name, RZ_IL_VAR_KIND_LOCAL_PURE)
+#define SETG(name, v)      rz_il_op_new_set(name, false, v)
+#define SETL(name, v)      rz_il_op_new_set(name, true, v)
+#define LET(name, v, body) rz_il_op_new_let(name, v, body)
+
+#define SEQ2(e0, e1)                             rz_il_op_new_seq(e0, e1)
+#define SEQ3(e0, e1, e2)                         rz_il_op_new_seqn(3, e0, e1, e2)
+#define SEQ4(e0, e1, e2, e3)                     rz_il_op_new_seqn(4, e0, e1, e2, e3)
+#define SEQ5(e0, e1, e2, e3, e4)                 rz_il_op_new_seqn(5, e0, e1, e2, e3, e4)
+#define SEQ6(e0, e1, e2, e3, e4, e5)             rz_il_op_new_seqn(6, e0, e1, e2, e3, e4, e5)
+#define SEQ7(e0, e1, e2, e3, e4, e5, e6)         rz_il_op_new_seqn(7, e0, e1, e2, e3, e4, e5, e6)
+#define SEQ8(e0, e1, e2, e3, e4, e5, e6, e7)     rz_il_op_new_seqn(8, e0, e1, e2, e3, e4, e5, e6, e7)
+#define SEQ9(e0, e1, e2, e3, e4, e5, e6, e7, e8) rz_il_op_new_seqn(9, e0, e1, e2, e3, e4, e5, e6, e7, e8)
 
 #define NOP             rz_il_op_new_nop()
 #define BRANCH(c, t, f) rz_il_op_new_branch(c, t, f)
+#define REPEAT(c, b)    rz_il_op_new_repeat(c, b)
 #define JMP(tgt)        rz_il_op_new_jmp(tgt)
+#define GOTO(lbl)       rz_il_op_new_goto(lbl)
 
 #endif
