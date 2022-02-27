808e5014c28c4d78bc58455b6ddce7b4cd526821
wargio <wargio@libero.it>
2022-02-27 13:41:43 +0100

    removed rz_print_mute

--- librz/include/rz_util/rz_print.h
+++ librz/include/rz_util/rz_print.h
@@ -136,7 +136,6 @@ RZ_API char *rz_print_hexpair(RzPrint *p, const char *str, int idx);
 RZ_API void rz_print_hex_from_base2(RzPrint *p, char *bin_str);
 RZ_API RzPrint *rz_print_new(void);
 RZ_API RzPrint *rz_print_free(RzPrint *p);
-RZ_API bool rz_print_mute(RzPrint *p, int x);
 RZ_API void rz_print_set_flags(RzPrint *p, int _flags);
 RZ_API void rz_print_unset_flags(RzPrint *p, int flags);
 RZ_API void rz_print_addr(RzPrint *p, ut64 addr);
