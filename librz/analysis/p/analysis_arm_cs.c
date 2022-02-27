2fc9386d491f8bd3b432674167c4f84724a55fd1
Florian Märkl <info@florianmaerkl.de>
2022-02-27 16:46:20 +0100

    Fix separation of fpu and gpr in arm64 reg profile (#2368)
    d/s/h/b regs are in a different file than x/w regs.

--- librz/analysis/p/analysis_arm_cs.c
+++ librz/analysis/p/analysis_arm_cs.c
@@ -1825,73 +1825,39 @@ static char *get_reg_profile(RzAnalysis *analysis) {
 			"=CF	cf\n"
 			"=SN	%s\n" // x8 on linux or android, x16 for the rest
 
-			/* 8bit sub-registers */
-			"gpr	b0	.8	0	0\n"
-			"gpr	b1	.8	8	0\n"
-			"gpr	b2	.8	16	0\n"
-			"gpr	b3	.8	24	0\n"
-			"gpr	b4	.8	32	0\n"
-			"gpr	b5	.8	40	0\n"
-			"gpr	b6	.8	48	0\n"
-			"gpr	b7	.8	56	0\n"
-			"gpr	b8	.8	64	0\n"
-			"gpr	b9	.8	72	0\n"
-			"gpr	b10	.8	80	0\n"
-			"gpr	b11	.8	88	0\n"
-			"gpr	b12	.8	96	0\n"
-			"gpr	b13	.8	104	0\n"
-			"gpr	b14	.8	112	0\n"
-			"gpr	b15	.8	120	0\n"
-			"gpr	b16	.8	128	0\n"
-			"gpr	b17	.8	136	0\n"
-			"gpr	b18	.8	144	0\n"
-			"gpr	b19	.8	152	0\n"
-			"gpr	b20	.8	160	0\n"
-			"gpr	b21	.8	168	0\n"
-			"gpr	b22	.8	176	0\n"
-			"gpr	b23	.8	184	0\n"
-			"gpr	b24	.8	192	0\n"
-			"gpr	b25	.8	200	0\n"
-			"gpr	b26	.8	208	0\n"
-			"gpr	b27	.8	216	0\n"
-			"gpr	b28	.8	224	0\n"
-			"gpr	b29	.8	232	0\n"
-			"gpr	b30	.8	240	0\n"
-			"gpr	bsp	.8	248	0\n"
-
-			/* 16bit sub-registers */
-			"gpr	h0	.16	0	0\n"
-			"gpr	h1	.16	8	0\n"
-			"gpr	h2	.16	16	0\n"
-			"gpr	h3	.16	24	0\n"
-			"gpr	h4	.16	32	0\n"
-			"gpr	h5	.16	40	0\n"
-			"gpr	h6	.16	48	0\n"
-			"gpr	h7	.16	56	0\n"
-			"gpr	h8	.16	64	0\n"
-			"gpr	h9	.16	72	0\n"
-			"gpr	h10	.16	80	0\n"
-			"gpr	h11	.16	88	0\n"
-			"gpr	h12	.16	96	0\n"
-			"gpr	h13	.16	104	0\n"
-			"gpr	h14	.16	112	0\n"
-			"gpr	h15	.16	120	0\n"
-			"gpr	h16	.16	128	0\n"
-			"gpr	h17	.16	136	0\n"
-			"gpr	h18	.16	144	0\n"
-			"gpr	h19	.16	152	0\n"
-			"gpr	h20	.16	160	0\n"
-			"gpr	h21	.16	168	0\n"
-			"gpr	h22	.16	176	0\n"
-			"gpr	h23	.16	184	0\n"
-			"gpr	h24	.16	192	0\n"
-			"gpr	h25	.16	200	0\n"
-			"gpr	h26	.16	208	0\n"
-			"gpr	h27	.16	216	0\n"
-			"gpr	h28	.16	224	0\n"
-			"gpr	h29	.16	232	0\n"
-			"gpr	h30	.16	240	0\n"
-
+			/* 64bit */
+			"gpr	x0	.64	0	0\n"
+			"gpr	x1	.64	8	0\n"
+			"gpr	x2	.64	16	0\n"
+			"gpr	x3	.64	24	0\n"
+			"gpr	x4	.64	32	0\n"
+			"gpr	x5	.64	40	0\n"
+			"gpr	x6	.64	48	0\n"
+			"gpr	x7	.64	56	0\n"
+			"gpr	x8	.64	64	0\n"
+			"gpr	x9	.64	72	0\n"
+			"gpr	x10	.64	80	0\n"
+			"gpr	x11	.64	88	0\n"
+			"gpr	x12	.64	96	0\n"
+			"gpr	x13	.64	104	0\n"
+			"gpr	x14	.64	112	0\n"
+			"gpr	x15	.64	120	0\n"
+			"gpr	x16	.64	128	0\n"
+			"gpr	x17	.64	136	0\n"
+			"gpr	x18	.64	144	0\n"
+			"gpr	x19	.64	152	0\n"
+			"gpr	x20	.64	160	0\n"
+			"gpr	x21	.64	168	0\n"
+			"gpr	x22	.64	176	0\n"
+			"gpr	x23	.64	184	0\n"
+			"gpr	x24	.64	192	0\n"
+			"gpr	x25	.64	200	0\n"
+			"gpr	x26	.64	208	0\n"
+			"gpr	x27	.64	216	0\n"
+			"gpr	x28	.64	224	0\n"
+			"gpr	x29	.64	232	0\n"
+			"gpr	x30	.64	240	0\n"
+			"gpr	tmp	.64	288	0\n"
 			/* 32bit sub-registers */
 			"gpr	w0	.32	0	0\n"
 			"gpr	w1	.32	8	0\n"
@@ -1927,117 +1893,151 @@ static char *get_reg_profile(RzAnalysis *analysis) {
 			"gpr	wsp	.32	248	0\n"
 			"gpr	wzr	.32	?	0\n"
 
-			/* 32bit float sub-registers */
-			"gpr	s0	.32	0	0\n"
-			"gpr	s1	.32	8	0\n"
-			"gpr	s2	.32	16	0\n"
-			"gpr	s3	.32	24	0\n"
-			"gpr	s4	.32	32	0\n"
-			"gpr	s5	.32	40	0\n"
-			"gpr	s6	.32	48	0\n"
-			"gpr	s7	.32	56	0\n"
-			"gpr	s8	.32	64	0\n"
-			"gpr	s9	.32	72	0\n"
-			"gpr	s10	.32	80	0\n"
-			"gpr	s11	.32	88	0\n"
-			"gpr	s12	.32	96	0\n"
-			"gpr	s13	.32	104	0\n"
-			"gpr	s14	.32	112	0\n"
-			"gpr	s15	.32	120	0\n"
-			"gpr	s16	.32	128	0\n"
-			"gpr	s17	.32	136	0\n"
-			"gpr	s18	.32	144	0\n"
-			"gpr	s19	.32	152	0\n"
-			"gpr	s20	.32	160	0\n"
-			"gpr	s21	.32	168	0\n"
-			"gpr	s22	.32	176	0\n"
-			"gpr	s23	.32	184	0\n"
-			"gpr	s24	.32	192	0\n"
-			"gpr	s25	.32	200	0\n"
-			"gpr	s26	.32	208	0\n"
-			"gpr	s27	.32	216	0\n"
-			"gpr	s28	.32	224	0\n"
-			"gpr	s29	.32	232	0\n"
-			"gpr	s30	.32	240	0\n"
-			/* 64bit */
-			"gpr	x0	.64	0	0\n" // x0
-			"gpr	x1	.64	8	0\n" // x0
-			"gpr	x2	.64	16	0\n" // x0
-			"gpr	x3	.64	24	0\n" // x0
-			"gpr	x4	.64	32	0\n" // x0
-			"gpr	x5	.64	40	0\n" // x0
-			"gpr	x6	.64	48	0\n" // x0
-			"gpr	x7	.64	56	0\n" // x0
-			"gpr	x8	.64	64	0\n" // x0
-			"gpr	x9	.64	72	0\n" // x0
-			"gpr	x10	.64	80	0\n" // x0
-			"gpr	x11	.64	88	0\n" // x0
-			"gpr	x12	.64	96	0\n" // x0
-			"gpr	x13	.64	104	0\n" // x0
-			"gpr	x14	.64	112	0\n" // x0
-			"gpr	x15	.64	120	0\n" // x0
-			"gpr	x16	.64	128	0\n" // x0
-			"gpr	x17	.64	136	0\n" // x0
-			"gpr	x18	.64	144	0\n" // x0
-			"gpr	x19	.64	152	0\n" // x0
-			"gpr	x20	.64	160	0\n" // x0
-			"gpr	x21	.64	168	0\n" // x0
-			"gpr	x22	.64	176	0\n" // x0
-			"gpr	x23	.64	184	0\n" // x0
-			"gpr	x24	.64	192	0\n" // x0
-			"gpr	x25	.64	200	0\n" // x0
-			"gpr	x26	.64	208	0\n" // x0
-			"gpr	x27	.64	216	0\n"
-			"gpr	x28	.64	224	0\n"
-			"gpr	x29	.64	232	0\n"
-			"gpr	x30	.64	240	0\n"
-			"gpr	tmp	.64	288	0\n"
-			/* 64bit double */
-			"gpr	d0	.64	0	0\n" // x0
-			"gpr	d1	.64	8	0\n" // x0
-			"gpr	d2	.64	16	0\n" // x0
-			"gpr	d3	.64	24	0\n" // x0
-			"gpr	d4	.64	32	0\n" // x0
-			"gpr	d5	.64	40	0\n" // x0
-			"gpr	d6	.64	48	0\n" // x0
-			"gpr	d7	.64	56	0\n" // x0
-			"gpr	d8	.64	64	0\n" // x0
-			"gpr	d9	.64	72	0\n" // x0
-			"gpr	d10	.64	80	0\n" // x0
-			"gpr	d11	.64	88	0\n" // x0
-			"gpr	d12	.64	96	0\n" // x0
-			"gpr	d13	.64	104	0\n" // x0
-			"gpr	d14	.64	112	0\n" // x0
-			"gpr	d15	.64	120	0\n" // x0
-			"gpr	d16	.64	128	0\n" // x0
-			"gpr	d17	.64	136	0\n" // x0
-			"gpr	d18	.64	144	0\n" // x0
-			"gpr	d19	.64	152	0\n" // x0
-			"gpr	d20	.64	160	0\n" // x0
-			"gpr	d21	.64	168	0\n" // x0
-			"gpr	d22	.64	176	0\n" // x0
-			"gpr	d23	.64	184	0\n" // x0
-			"gpr	d24	.64	192	0\n" // x0
-			"gpr	d25	.64	200	0\n" // x0
-			"gpr	d26	.64	208	0\n" // x0
-			"gpr	d27	.64	216	0\n"
-			"gpr	d28	.64	224	0\n"
-			"gpr	d29	.64	232	0\n"
-			"gpr	d30	.64	240	0\n"
-			"gpr	dsp	.64	248	0\n"
-			/*  foo */
+			/* aliases */
 			"gpr	fp	.64	232	0\n" // fp = x29
 			"gpr	lr	.64	240	0\n" // lr = x30
 			"gpr	sp	.64	248	0\n"
 			"gpr	pc	.64	256	0\n"
 			"gpr	zr	.64	?	0\n"
 			"gpr	xzr	.64	?	0\n"
+			/* flags */
 			"flg	pstate	.64	280	0   _____tfiae_____________j__qvczn\n" // x0
 			//"flg	cpsr	.32	280	0\n" //	_____tfiae_____________j__qvczn\n"
 			"flg	vf	.1	280.28	0	overflow\n" // set if overflows
 			"flg	cf	.1	280.29	0	carry\n" // set if last op carries
 			"flg	zf	.1	280.30	0	zero\n" // set if last op is 0
-			"flg	nf	.1	280.31	0	sign\n"; // msb bit of last op
+			"flg	nf	.1	280.31	0	sign\n" // msb bit of last op
+
+			/* 64bit double */
+			"fpu	d0	.64	0	0\n"
+			"fpu	d1	.64	8	0\n"
+			"fpu	d2	.64	16	0\n"
+			"fpu	d3	.64	24	0\n"
+			"fpu	d4	.64	32	0\n"
+			"fpu	d5	.64	40	0\n"
+			"fpu	d6	.64	48	0\n"
+			"fpu	d7	.64	56	0\n"
+			"fpu	d8	.64	64	0\n"
+			"fpu	d9	.64	72	0\n"
+			"fpu	d10	.64	80	0\n"
+			"fpu	d11	.64	88	0\n"
+			"fpu	d12	.64	96	0\n"
+			"fpu	d13	.64	104	0\n"
+			"fpu	d14	.64	112	0\n"
+			"fpu	d15	.64	120	0\n"
+			"fpu	d16	.64	128	0\n"
+			"fpu	d17	.64	136	0\n"
+			"fpu	d18	.64	144	0\n"
+			"fpu	d19	.64	152	0\n"
+			"fpu	d20	.64	160	0\n"
+			"fpu	d21	.64	168	0\n"
+			"fpu	d22	.64	176	0\n"
+			"fpu	d23	.64	184	0\n"
+			"fpu	d24	.64	192	0\n"
+			"fpu	d25	.64	200	0\n"
+			"fpu	d26	.64	208	0\n"
+			"fpu	d27	.64	216	0\n"
+			"fpu	d28	.64	224	0\n"
+			"fpu	d29	.64	232	0\n"
+			"fpu	d30	.64	240	0\n"
+			"fpu	dsp	.64	248	0\n"
+			/* 32bit float sub-registers */
+			"fpu	s0	.32	0	0\n"
+			"fpu	s1	.32	8	0\n"
+			"fpu	s2	.32	16	0\n"
+			"fpu	s3	.32	24	0\n"
+			"fpu	s4	.32	32	0\n"
+			"fpu	s5	.32	40	0\n"
+			"fpu	s6	.32	48	0\n"
+			"fpu	s7	.32	56	0\n"
+			"fpu	s8	.32	64	0\n"
+			"fpu	s9	.32	72	0\n"
+			"fpu	s10	.32	80	0\n"
+			"fpu	s11	.32	88	0\n"
+			"fpu	s12	.32	96	0\n"
+			"fpu	s13	.32	104	0\n"
+			"fpu	s14	.32	112	0\n"
+			"fpu	s15	.32	120	0\n"
+			"fpu	s16	.32	128	0\n"
+			"fpu	s17	.32	136	0\n"
+			"fpu	s18	.32	144	0\n"
+			"fpu	s19	.32	152	0\n"
+			"fpu	s20	.32	160	0\n"
+			"fpu	s21	.32	168	0\n"
+			"fpu	s22	.32	176	0\n"
+			"fpu	s23	.32	184	0\n"
+			"fpu	s24	.32	192	0\n"
+			"fpu	s25	.32	200	0\n"
+			"fpu	s26	.32	208	0\n"
+			"fpu	s27	.32	216	0\n"
+			"fpu	s28	.32	224	0\n"
+			"fpu	s29	.32	232	0\n"
+			"fpu	s30	.32	240	0\n"
+			/* 16bit sub-registers */
+			"fpu	h0	.16	0	0\n"
+			"fpu	h1	.16	8	0\n"
+			"fpu	h2	.16	16	0\n"
+			"fpu	h3	.16	24	0\n"
+			"fpu	h4	.16	32	0\n"
+			"fpu	h5	.16	40	0\n"
+			"fpu	h6	.16	48	0\n"
+			"fpu	h7	.16	56	0\n"
+			"fpu	h8	.16	64	0\n"
+			"fpu	h9	.16	72	0\n"
+			"fpu	h10	.16	80	0\n"
+			"fpu	h11	.16	88	0\n"
+			"fpu	h12	.16	96	0\n"
+			"fpu	h13	.16	104	0\n"
+			"fpu	h14	.16	112	0\n"
+			"fpu	h15	.16	120	0\n"
+			"fpu	h16	.16	128	0\n"
+			"fpu	h17	.16	136	0\n"
+			"fpu	h18	.16	144	0\n"
+			"fpu	h19	.16	152	0\n"
+			"fpu	h20	.16	160	0\n"
+			"fpu	h21	.16	168	0\n"
+			"fpu	h22	.16	176	0\n"
+			"fpu	h23	.16	184	0\n"
+			"fpu	h24	.16	192	0\n"
+			"fpu	h25	.16	200	0\n"
+			"fpu	h26	.16	208	0\n"
+			"fpu	h27	.16	216	0\n"
+			"fpu	h28	.16	224	0\n"
+			"fpu	h29	.16	232	0\n"
+			"fpu	h30	.16	240	0\n"
+			/* 8bit sub-registers */
+			"fpu	b0	.8	0	0\n"
+			"fpu	b1	.8	8	0\n"
+			"fpu	b2	.8	16	0\n"
+			"fpu	b3	.8	24	0\n"
+			"fpu	b4	.8	32	0\n"
+			"fpu	b5	.8	40	0\n"
+			"fpu	b6	.8	48	0\n"
+			"fpu	b7	.8	56	0\n"
+			"fpu	b8	.8	64	0\n"
+			"fpu	b9	.8	72	0\n"
+			"fpu	b10	.8	80	0\n"
+			"fpu	b11	.8	88	0\n"
+			"fpu	b12	.8	96	0\n"
+			"fpu	b13	.8	104	0\n"
+			"fpu	b14	.8	112	0\n"
+			"fpu	b15	.8	120	0\n"
+			"fpu	b16	.8	128	0\n"
+			"fpu	b17	.8	136	0\n"
+			"fpu	b18	.8	144	0\n"
+			"fpu	b19	.8	152	0\n"
+			"fpu	b20	.8	160	0\n"
+			"fpu	b21	.8	168	0\n"
+			"fpu	b22	.8	176	0\n"
+			"fpu	b23	.8	184	0\n"
+			"fpu	b24	.8	192	0\n"
+			"fpu	b25	.8	200	0\n"
+			"fpu	b26	.8	208	0\n"
+			"fpu	b27	.8	216	0\n"
+			"fpu	b28	.8	224	0\n"
+			"fpu	b29	.8	232	0\n"
+			"fpu	b30	.8	240	0\n"
+			"fpu	bsp	.8	248	0\n";
 		return rz_str_newf(p, snReg);
 	} else {
 		p =
