72d124a6e5b8478fb82e0cdf204a98bfdbf52a86
wargio <wargio@libero.it>
2022-03-25 13:24:29 +0100

    added comments to understand plugin behavior

--- librz/bin/bobj.c
+++ librz/bin/bobj.c
@@ -481,9 +481,14 @@ RZ_API int rz_bin_object_set_items(RzBinFile *bf, RzBinObject *o) {
 		}
 	}
 	if (bin->filter_rules & RZ_BIN_REQ_STRINGS) {
-		o->strings = p->strings
-			? p->strings(bf)
-			: rz_bin_file_strings(bf, minlen, true);
+		if (p->strings) {
+			o->strings = p->strings(bf);
+		} else {
+			// when a bin plugin does not provide it's own strings
+			// we always take all the strings found in the binary
+			// the method also converts the paddrs to vaddrs
+			o->strings = rz_bin_file_strings(bf, bin->minstrlen, true);
+		}
 		if (bin->debase64) {
 			rz_bin_object_filter_strings(o);
 		}
@@ -838,6 +843,9 @@ RZ_API const RzList *rz_bin_object_reset_strings(RzBin *bin, RzBinFile *bf, RzBi
 	if (plugin && plugin->strings) {
 		obj->strings = plugin->strings(bf);
 	} else {
+		// when a bin plugin does not provide it's own strings
+		// we always take all the strings found in the binary
+		// the method also converts the paddrs to vaddrs
 		obj->strings = rz_bin_file_strings(bf, bin->minstrlen, true);
 	}
 	if (bin->debase64) {
