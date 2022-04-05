037f2916feb179820447d83e1b85081534f36fde
Anton Kochkov <anton.kochkov@gmail.com>
2022-04-05 12:08:54 +0800

    Fix OOB in ASN.1 stringify

--- librz/util/astr.c
+++ librz/util/astr.c
@@ -60,7 +60,7 @@ RZ_API RASN1String *rz_asn1_stringify_string(const ut8 *buffer, ut32 length) {
 	if (!str) {
 		return NULL;
 	}
-	rz_str_filter(str, length);
+	rz_str_filter(str, length - 1);
 	return rz_asn1_create_string(str, true, length);
 }
 
