7438993bb1c9dedb11d83c27a226e6dab5b4b9f1
Rot127 <unisono@quyllur.org>
2022-04-14 12:46:29 -0400

    Add `set_cr0` and `set_carry` functions.

--- librz/analysis/arch/ppc/ppc_il.h
+++ librz/analysis/arch/ppc/ppc_il.h
@@ -4,9 +4,11 @@
 #ifndef PPC_IL_H
 #define PPC_IL_H
 
+#include <rz_reg.h>
 #include <rz_analysis.h>
 #include <rz_il.h>
 #include <rz_types.h>
+#include <capstone.h>
 
 #define PPC_BYTE  8
 #define PPC_HWORD 16
@@ -16,5 +18,8 @@
 
 RZ_IPI RzAnalysisILConfig *rz_ppc_cs_64_il_config(bool big_endian);
 RZ_IPI RzAnalysisILConfig *rz_ppc_cs_32_il_config(bool big_endian);
+
+RZ_OWN RzILOpEffect *set_carry_add_sub(RZ_OWN RzILOpBitVector *a, RZ_OWN RzILOpBitVector *b, cs_mode mode, bool add);
+RZ_OWN RzILOpEffect *set_cr0(RZ_NONNULL RzILOpPure *val);
 
 #endif /* PPC_IL_H */
---
+++ librz/analysis/arch/ppc/ppc_il_flag_ops.c
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: 2022 Rot127 <unisono@quyllur.org>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include "ppc_il.h"
+#include <rz_types.h>
+
+#include <rz_il/rz_il_opbuilder_begin.h>
+
+#define EXTEND(n, v) ITE(MSB(v), SIGNED(n, v), UNSIGNED(n, v))
+
+/**
+ * \brief Set "ca" and "ca32" bit if after an add or sub operation on \p a and \p b the M+1 bit is set
+ *
+ * In 32bit mode the "ca32" bit is set as well. Otherwise only the "ca" bit.
+ *
+ * \param a Value a.
+ * \param b Value b.
+ * \param mode Capstone mode.
+ * \param add True: \p a + \p b False: \p a - \p b
+ * \return RZ_OWN* Effect which sets the carry bits.
+ */
+RZ_OWN RzILOpEffect *set_carry_add_sub(RZ_OWN RzILOpBitVector *a, RZ_OWN RzILOpBitVector *b, cs_mode mode, bool add) {
+	bool mode_64 = (mode & CS_MODE_64);
+	ut32 bits = mode_64 ? 64 : 32;
+	RzILOpBitVector *r;
+	if (add) {
+		r = ADD(EXTEND(bits + 1, a), EXTEND(bits + 1, b));
+	} else {
+		r = SUB(EXTEND(bits + 1, a), EXTEND(bits + 1, b));
+	}
+	RzILOpBool *c = ITE(MSB(r), IL_TRUE, IL_FALSE);
+	return mode_64 ? SETG("ca", c) : SEQ2(SETG("ca", c), SETG("ca32", c));
+}
+
+/**
+ * \brief Set the cr0 register depending how \p val compares to 0.
+ *
+ * \param val Value which is compared to 0.
+ * \return RzILOpEffect* Set cr0 effect.
+ */
+RZ_OWN RzILOpEffect *set_cr0(RZ_NONNULL RzILOpPure *val) {
+	rz_return_val_if_fail(val, NULL);
+	RzILOpBool *so_bit = VARG("so");
+	RzILOpPure *n = U64(0);
+	RzILOpEffect *cond_geq = BRANCH(SGT(val, n),
+		SETG("cr0", LOGOR(SHIFTL0(so_bit, U8(3)), UN(4, 0b010))), // val > 0
+		SETG("cr0", LOGOR(SHIFTL0(so_bit, U8(3)), UN(4, 0b100))) // val == 0
+	);
+	RzILOpEffect *cond_l = BRANCH(SLT(val, n),
+		SETG("cr0", LOGOR(SHIFTL0(so_bit, U8(3)), UN(4, 0b001))), // val < 0
+		cond_geq);
+	return cond_l;
+}
+
+#include <rz_il/rz_il_opbuilder_end.h>
