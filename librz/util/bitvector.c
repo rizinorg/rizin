c80ac8a5215cb4def60b7b672fe0a0eb7dd419da
Heersin <teablearcher@gmail.com>
2022-04-17 17:03:35 +0800

    Fix bug in copy_nbits

--- librz/util/bitvector.c
+++ librz/util/bitvector.c
@@ -202,7 +202,7 @@ RZ_API ut32 rz_bv_copy_nbits(RZ_NONNULL const RzBitVector *src, ut32 src_start_p
 	}
 
 	// normal case here
-	for (ut32 i = 0; i < max_nbit; ++i) {
+	for (ut32 i = 0; i < nbit; ++i) {
 		bool c = rz_bv_get(src, src_start_pos + i);
 		rz_bv_set(dst, dst_start_pos + i, c);
 	}
