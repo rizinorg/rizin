4a921a831cdc07a6de8cabe86d499d3d72f7578e
Peiwei Hu <jlu.hpw@foxmail.com>
2022-07-08 08:29:20 +0800

    Add psc command to generate a C/C++ string

--- librz/core/cmd/cmd_print.c
+++ librz/core/cmd/cmd_print.c
@@ -4888,6 +4888,29 @@ static void core_print_raw_buffer(RzStrStringifyOpt *opt) {
 	}
 }
 
+#define STRING_CHUNK 16
+RZ_IPI RzCmdStatus rz_print_string_c_cpp_handler(RzCore *core, int argc, const char **argv, RzOutputMode mode) {
+	ut64 value;
+	size_t size = core->blocksize;
+	RzStrBuf *sb = rz_strbuf_new(NULL);
+
+	if (!sb) {
+		RZ_LOG_ERROR("Fail to allocate the memory\n");
+		return RZ_CMD_STATUS_ERROR;
+	}
+	rz_strbuf_appendf(sb, "#define STRING_SIZE %" PFMTSZd "\nconst char s[STRING_SIZE] = \"", size);
+	for (size_t pos = 0; pos < size; pos++) {
+		if (pos && !(pos % STRING_CHUNK)) {
+			rz_strbuf_appendf(sb, "\"\n                            \"");
+		}
+		value = rz_read_ble(core->block + pos, false, 8);
+		rz_strbuf_appendf(sb, "\\x%02" PFMT64x, value);
+	}
+	rz_strbuf_append(sb, "\";");
+	rz_cons_printf("%s\n", rz_strbuf_drain(sb));
+	return RZ_CMD_STATUS_OK;
+}
+
 RZ_IPI int rz_cmd_print(void *data, const char *input) {
 	RzCore *core = (RzCore *)data;
 	st64 l;
