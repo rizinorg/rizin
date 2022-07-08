9550bff5ad8284b67dceac720d59582b6f29dc6a
Peiwei Hu <jlu.hpw@foxmail.com>
2022-07-08 09:22:11 +0800

    fixup! Add psc command to generate a C/C++ string

--- librz/core/cmd/cmd_print.c
+++ librz/core/cmd/cmd_print.c
@@ -4889,26 +4889,40 @@ static void core_print_raw_buffer(RzStrStringifyOpt *opt) {
 }
 
 #define STRING_CHUNK 16
-RZ_IPI RzCmdStatus rz_print_string_c_cpp_handler(RzCore *core, int argc, const char **argv, RzOutputMode mode) {
+/**
+ * Return a C/C++ string defination with block size as the length
+ * \param core RzCore
+ * \return a string defination or NULL if the error happens
+ */
+RZ_API char *string_c_cpp(RzCore *core) {
 	ut64 value;
 	size_t size = core->blocksize;
 	RzStrBuf *sb = rz_strbuf_new(NULL);
 
 	if (!sb) {
 		RZ_LOG_ERROR("Fail to allocate the memory\n");
-		return RZ_CMD_STATUS_ERROR;
+		return NULL;
 	}
 	rz_strbuf_appendf(sb, "#define STRING_SIZE %" PFMTSZd "\nconst char s[STRING_SIZE] = \"", size);
 	for (size_t pos = 0; pos < size; pos++) {
 		if (pos && !(pos % STRING_CHUNK)) {
+			// newline and padding for long string
 			rz_strbuf_appendf(sb, "\"\n                            \"");
 		}
 		value = rz_read_ble(core->block + pos, false, 8);
 		rz_strbuf_appendf(sb, "\\x%02" PFMT64x, value);
 	}
 	rz_strbuf_append(sb, "\";");
-	rz_cons_printf("%s\n", rz_strbuf_drain(sb));
-	return RZ_CMD_STATUS_OK;
+	return rz_strbuf_drain(sb);
+}
+
+RZ_IPI RzCmdStatus rz_print_string_c_cpp_handler(RzCore *core, int argc, const char **argv, RzOutputMode mode) {
+	char *str = string_c_cpp(core);
+	if (str) {
+		rz_cons_println(str);
+		return RZ_CMD_STATUS_OK;
+	}
+	return RZ_CMD_STATUS_ERROR;
 }
 
 RZ_IPI int rz_cmd_print(void *data, const char *input) {
