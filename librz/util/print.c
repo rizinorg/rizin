2346ca84d0b75e1bc983c074e09af630f400c822
billow <billow.fun@gmail.com>
2022-07-23 21:34:32 +0800

    fix

--- librz/util/print.c
+++ librz/util/print.c
@@ -1843,7 +1843,7 @@ RZ_API RZ_OWN char *rz_print_jsondump_str(RZ_NONNULL RzPrint *p, RZ_NONNULL cons
 		return NULL;
 	}
 	pj_a(j);
-	for (int i = 0 ; i < len; i += bytesize) {
+	for (int i = 0; i < len; i += bytesize) {
 		ut64 word = rz_read_ble(buf + i, p->big_endian, wordsize);
 		pj_n(j, word);
 	}
