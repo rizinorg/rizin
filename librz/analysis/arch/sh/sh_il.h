ff6383a0e934842862473bad1035aa856876e19c
Dhruv Maroo <dhruvmaru007@gmail.com>
2022-07-26 20:42:41 +0530

    Add const qualifier for `SHOp *op`

--- librz/analysis/arch/sh/sh_il.h
+++ librz/analysis/arch/sh/sh_il.h
@@ -7,7 +7,7 @@
 #include <rz_analysis.h>
 #include "../../../asm/arch/sh/disassembler.h"
 
-RZ_IPI bool rz_sh_il_opcode(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RzAnalysisOp *aop, ut64 pc, RZ_BORROW RZ_NONNULL SHOp *op);
+RZ_IPI bool rz_sh_il_opcode(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RzAnalysisOp *aop, ut64 pc, RZ_BORROW RZ_NONNULL const SHOp *op);
 RZ_IPI RzAnalysisILConfig *rz_sh_il_config(RZ_NONNULL RzAnalysis *analysis);
 
 #endif /* SH_RZIL_H */
