37046f486395097c40bbea74af30d4b77ef3a7b0
Dhruv Maroo <dhruvmaru007@gmail.com>
2022-07-27 19:33:26 +0530

    Remove global variable `privilege_check` and instead use `ctx`
      * Store the IL context in an `SHILContext` struct and pass it around
        to store information to be used outside the lifter

--- librz/analysis/arch/sh/sh_il.h
+++ librz/analysis/arch/sh/sh_il.h
@@ -7,7 +7,15 @@
 #include <rz_analysis.h>
 #include "../../../asm/arch/sh/disassembler.h"
 
-RZ_IPI bool rz_sh_il_opcode(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RzAnalysisOp *aop, ut64 pc, RZ_BORROW RZ_NONNULL const SHOp *op);
+/**
+ * \brief To store the context of the IL lifter ; Used to pass around information outside effects
+ * Other context variables *may* be added in the future when the rest of the instructions are lifted
+ */
+typedef struct sh_il_context_t {
+	bool privilege_check; ///< Set to true whenever the privilege mode is calculated (used to add a `SETL` effect for the privilege bit, in case it is used)
+} SHILContext;
+
+RZ_IPI bool rz_sh_il_opcode(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RzAnalysisOp *aop, ut64 pc, RZ_BORROW RZ_NONNULL const SHOp *op, RZ_NULLABLE SHILContext *ctx);
 RZ_IPI RzAnalysisILConfig *rz_sh_il_config(RZ_NONNULL RzAnalysis *analysis);
 
 #endif /* SH_RZIL_H */
