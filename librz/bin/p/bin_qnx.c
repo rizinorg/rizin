a3d50c1ea185f3f642f2d8180715f82d98840784
wargio <wargio@libero.it>
2022-08-22 20:18:17 +0200

    fix #2964 - double-free in bin_qnx.c

--- librz/bin/p/bin_qnx.c
+++ librz/bin/p/bin_qnx.c
@@ -191,7 +191,21 @@ static RzBinInfo *info(RzBinFile *bf) {
 static RzList *relocs(RzBinFile *bf) {
 	rz_return_val_if_fail(bf && bf->o, NULL);
 	QnxObj *qo = bf->o->bin_obj;
-	return rz_list_clone(qo->fixups);
+	RzBinReloc *reloc = NULL;
+	RzListIter *it = NULL;
+	RzList *relocs = rz_list_newf(free);
+	if (!relocs) {
+		return NULL;
+	}
+
+	rz_list_foreach (qo->fixups, it, reloc) {
+		RzBinReloc *copy = RZ_NEW0(RzBinReloc);
+		copy->vaddr = reloc->vaddr;
+		copy->paddr = reloc->paddr;
+		copy->type = reloc->type;
+		rz_list_append(relocs, copy);
+	}
+	return relocs;
 }
 
 static void header(RzBinFile *bf) {
