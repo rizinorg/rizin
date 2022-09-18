rizinorg/rizin

commit 59b38e6efaf00b9b9869e0ec5baba4f1b9605f37

Add /*<type>*/ comments everywhere (#2986)
Adds /*<type>*/ comments and a linter check from rz-bindgen to enforce
their existence and consistency

Also includes the following fixes made when adding the annotations:
* removed unused intern_table arguments in pyc_dis.c, pyc_dis.h, asm_pyc.c
* removed unused classes argument from place_nodes in agraph.c
* removed unused recurse and recurse_bb functions in canalysis.c
* removed unused vars field from RzPrint struct
* removed unused RzAnalysisType* structs from rz_analysis.h
* removed unused list field from RzEgg struct
* fixed bug in bp_plugin.c where duplication-checking logic iterates over the wrong list
* removed unused q_regs field from RzDebug struct
* removed unused backtrace field from RzDebugPlugin struct
* removed unused classes_list field from RzBinNXOObj struct
* removed unused methods_list and classes_list fields from RzBinZimgObj struct

librz/core/tui/visual.c
@@ -1209,7 +1209,7 @@ static void add_comment(RzCore *core, ut64 addr, const char *prompt) {
 	rz_cons_set_raw(true);
 }
 
-static int follow_ref(RzCore *core, RzList *xrefs, int choice, bool xref_to) {
+static int follow_ref(RzCore *core, RzList /*<RzAnalysisXRef *>*/ *xrefs, int choice, bool xref_to) {
 	RzAnalysisXRef *xrefi = rz_list_get_n(xrefs, choice);
 	if (xrefi) {
 		if (core->print->cur_enabled) {

librz/include/rz_core.h
@@ -216,9 +216,9 @@ typedef struct rz_core_tasks_t {
 	RzCoreTaskBreak break_cb;
 	void *break_cb_user;
 	int task_id_next;
-	RzList *tasks;
-	RzList *tasks_queue;
-	RzList *oneshot_queue;
+	RzList /*<RzCoreTask *>*/ *tasks;
+	RzList /*<RzCoreTask *>*/ *tasks_queue;
+	RzList /*<OneShot *>*/ *oneshot_queue;
 	int oneshots_enqueued;
 	struct rz_core_task_t *current_task;
 	struct rz_core_task_t *main_task;
@@ -235,8 +235,8 @@ typedef struct rz_core_tasks_t {
  * Once a new seek is performed, all redos are lost.
  */
 typedef struct rz_core_seek_history_t {
-	RzVector undos; ///< Stack of RzCoreSeekItems, allowing to "go back in time"
-	RzVector redos; ///< Stack of RzCoreSeekItems, allowing to re-do an action that was undone.
+	RzVector /*<RzCoreSeekItem>*/ undos; ///< Stack of RzCoreSeekItems, allowing to "go back in time"
+	RzVector /*<RzCoreSeekItem>*/ redos; ///< Stack of RzCoreSeekItems, allowing to re-do an action that was undone.
 	bool saved_set; ///< When true, the \p saved field is set
 	RzCoreSeekItem saved_item; ///< Position to save in history
 } RzCoreSeekHistory;
@@ -481,11 +481,11 @@ RZ_API bool rz_core_write_seq_at(RzCore *core, ut64 addr, ut64 from, ut64 to, ut
 RZ_API bool rz_core_shift_block(RzCore *core, ut64 addr, ut64 b_size, st64 dist);
 RZ_API void rz_core_autocomplete(RZ_NULLABLE RzCore *core, RzLineCompletion *completion, RzLineBuffer *buf, RzLinePromptType prompt_type);
 RZ_API RzLineNSCompletionResult *rz_core_autocomplete_rzshell(RzCore *core, RzLineBuffer *buf, RzLinePromptType prompt_type);
-RZ_API void rz_core_print_scrollbar(RzCore *core);
-RZ_API void rz_core_print_scrollbar_bottom(RzCore *core);
+RZ_IPI void rz_core_print_scrollbar(RzCore *core);
+RZ_IPI void rz_core_print_scrollbar_bottom(RzCore *core);
 RZ_API void rz_core_help_vars_print(RzCore *core);
-RZ_API bool rz_core_prevop_addr(RzCore *core, ut64 start_addr, int numinstrs, ut64 *prev_addr);
-RZ_API ut64 rz_core_prevop_addr_force(RzCore *core, ut64 start_addr, int numinstrs);
+RZ_IPI bool rz_core_prevop_addr(RzCore *core, ut64 start_addr, int numinstrs, ut64 *prev_addr);
+RZ_IPI ut64 rz_core_prevop_addr_force(RzCore *core, ut64 start_addr, int numinstrs);
 RZ_API RzBinReloc *rz_core_getreloc(RzCore *core, ut64 addr, int size);
 RZ_API RzBinReloc *rz_core_get_reloc_to(RzCore *core, ut64 addr);
 RZ_API ut64 rz_core_get_asmqjmps(RzCore *core, const char *str);
@@ -800,7 +800,7 @@ typedef struct rz_core_disasm_options {
 	int invbreak;
 	int cbytes;
 	RzAnalysisFunction *function; ///< Disassemble a function
-	RzPVector *vec; ///< Not print, but append as RzPVector<RzAnalysisDisasmText>
+	RzPVector /*<RzAnalysisDisasmText *>*/ *vec; ///< Not print, but append as RzPVector<RzAnalysisDisasmText>
 } RzCoreDisasmOptions;
 
 #define RZ_CORE_MAX_DISASM (1024 * 1024 * 8)
@@ -1068,7 +1068,7 @@ RZ_API bool rz_core_hack(RzCore *core, const char *op);
 RZ_API bool rz_core_dump(RzCore *core, const char *file, ut64 addr, ut64 size, int append);
 RZ_API void rz_core_diff_show(RzCore *core, RzCore *core2, bool json);
 RZ_API bool rz_core_diff_show_function(RzCore *core, RzCore *core2, ut64 addr, bool json);
-RZ_API RZ_OWN char *rz_core_clippy(RzCore *core, const char *msg);
+RZ_API RZ_OWN char *rz_core_clippy(RZ_NONNULL RzCore *core, RZ_NONNULL const char *msg);
 
 // TODO MOVE SOMEWHERE ELSE
 typedef char *(*PrintItemCallback)(void *user, void *p, bool selected);
@@ -1103,7 +1103,7 @@ typedef struct {
 	ut64 from;
 	ut64 to;
 	ut64 step;
-	RzVector blocks;
+	RzVector /*<RzCoreAnalysisStatsItem>*/ blocks;
 } RzCoreAnalysisStats;
 
 RZ_API char *rz_core_analysis_hasrefs(RzCore *core, ut64 value, int mode);
@@ -1113,8 +1113,8 @@ RZ_API void rz_core_analysis_stats_free(RzCoreAnalysisStats *s);
 RZ_API ut64 rz_core_analysis_stats_get_block_from(RZ_NONNULL const RzCoreAnalysisStats *s, size_t i);
 RZ_API ut64 rz_core_analysis_stats_get_block_to(RZ_NONNULL const RzCoreAnalysisStats *s, size_t i);
 
-RZ_API int rz_line_hist_offset_up(RzLine *line);
-RZ_API int rz_line_hist_offset_down(RzLine *line);
+RZ_IPI int rz_line_hist_offset_up(RzLine *line);
+RZ_IPI int rz_line_hist_offset_down(RzLine *line);
 
 RZ_API RZ_OWN char *rz_core_syscall_as_string(RzCore *core, st64 num, ut64 addr);
 

commit 6f0957e3be05b86af70711697fa342c594e0de23

Expose rz_core_prevop_addr* as RZ_API + docs
This was sort of a regression from
59b38e6efaf00b9b9869e0ec5baba4f1b9605f37, where it was switched to
RZ_IPI in the header to be consistent with the source. However we need
RZ_API because it is used at least in Cutter.

librz/core/tui/visual.c
@@ -1076,10 +1076,15 @@ static ut64 prevop_addr(RzCore *core, ut64 addr) {
 	return target > 4 ? target - 4 : 0;
 }
 
-//  Returns true if we can use analysis to find the previous operation address,
-//  sets prev_addr to the value of the instruction numinstrs back.
-//  If we can't use the analysis, then set prev_addr to UT64_MAX and return false;
-RZ_IPI bool rz_core_prevop_addr(RzCore *core, ut64 start_addr, int numinstrs, ut64 *prev_addr) {
+/**
+ * Search of the numinstrs-th instruction before start_addr.
+ *
+ * Sets prev_addr to the value of the instruction numinstrs back.
+ * If we can't use the analysis, then sets prev_addr to UT64_MAX and returns false
+ *
+ * \return if analysis was able to find the previous instruction address
+ */
+RZ_API bool rz_core_prevop_addr(RzCore *core, ut64 start_addr, int numinstrs, ut64 *prev_addr) {
 	RzAnalysisBlock *bb;
 	int i;
 	// Check that we're in a bb, otherwise this prevop stuff won't work.
@@ -1099,9 +1104,11 @@ RZ_IPI bool rz_core_prevop_addr(RzCore *core, ut64 start_addr, int numinstrs, ut
 	return false;
 }
 
-//  Like rz_core_prevop_addr(), but also uses fallback from prevop_addr() if
-//  no analysis info is available.
-RZ_IPI ut64 rz_core_prevop_addr_force(RzCore *core, ut64 start_addr, int numinstrs) {
+/**
+ * Like rz_core_prevop_addr(), but also uses heuristics as fallback if
+ * no concrete analysis info is available.
+ */
+RZ_API ut64 rz_core_prevop_addr_force(RzCore *core, ut64 start_addr, int numinstrs) {
 	int i;
 	for (i = 0; i < numinstrs; i++) {
 		start_addr = prevop_addr(core, start_addr);

librz/include/rz_core.h
@@ -484,8 +484,8 @@ RZ_API RzLineNSCompletionResult *rz_core_autocomplete_rzshell(RzCore *core, RzLi
 RZ_IPI void rz_core_print_scrollbar(RzCore *core);
 RZ_IPI void rz_core_print_scrollbar_bottom(RzCore *core);
 RZ_API void rz_core_help_vars_print(RzCore *core);
-RZ_IPI bool rz_core_prevop_addr(RzCore *core, ut64 start_addr, int numinstrs, ut64 *prev_addr);
-RZ_IPI ut64 rz_core_prevop_addr_force(RzCore *core, ut64 start_addr, int numinstrs);
+RZ_API bool rz_core_prevop_addr(RzCore *core, ut64 start_addr, int numinstrs, ut64 *prev_addr);
+RZ_API ut64 rz_core_prevop_addr_force(RzCore *core, ut64 start_addr, int numinstrs);
 RZ_API RzBinReloc *rz_core_getreloc(RzCore *core, ut64 addr, int size);
 RZ_API RzBinReloc *rz_core_get_reloc_to(RzCore *core, ut64 addr);
 RZ_API ut64 rz_core_get_asmqjmps(RzCore *core, const char *str);