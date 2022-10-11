d6a3a2660a7db627e0faa189997d3417de6104c5
wargio <wargio@libero.it>
2022-10-12 01:00:06 +0200

    Memory optimized threading

--- librz/analysis/similarity.c
+++ librz/analysis/similarity.c
@@ -21,49 +21,92 @@
 
 #define iob_read_at(addr, buf, size) (analysis->iob.read_at(analysis->iob.io, addr, buf, size))
 
+typedef ut8 *(*AllocateBuffer)(RzAnalysis *analysis, void *data, ut8 **buffer, ut32 *buf_sz);
+
 typedef struct shared_context_t {
-	RzList *list_b;
+	const RzList *list_b;
 	RzThreadQueue *queue;
 	RzThreadQueue *matches;
 	RzThreadQueue *unmatch;
+	AllocateBuffer alloc;
+	RzThreadLock *lock_a;
+	RzThreadLock *lock_b;
+	RzAnalysis *analysis_a;
+	RzAnalysis *analysis_b;
 } SharedContext;
 
-typedef struct signature_data_t {
-	size_t size; ///< Signature size
-	ut8 *data; ///< Signature bytes
-	void *info; ///< Info linked to the signature (RzAnalysisBlock or RzAnalysisFunction)
-} SignatureData;
+static bool shared_context_init(SharedContext *context, RzAnalysis *analysis_a, RzAnalysis *analysis_b, RzList /*<void *>*/ *list_a, RzList /*<void *>*/ *list_b, AllocateBuffer alloc_cb) {
+	RzThreadLock *lock_a = rz_th_lock_new(true);
+	RzThreadLock *lock_b = analysis_a == analysis_b ? lock_a : rz_th_lock_new(true);
+	RzThreadQueue *queue = rz_th_queue_new2(rz_list_clone(list_a));
+	RzThreadQueue *matches = rz_th_queue_new(RZ_THREAD_QUEUE_UNLIMITED, NULL);
+	RzThreadQueue *unmatch = rz_th_queue_new(RZ_THREAD_QUEUE_UNLIMITED, NULL);
+	if (!lock_a || !lock_b || !queue || !matches || !unmatch) {
+		rz_th_lock_free(lock_a);
+		rz_th_lock_free(lock_b);
+		rz_th_queue_free(queue);
+		rz_th_queue_free(matches);
+		return false;
+	}
+	context->queue = queue;
+	context->list_b = list_b;
+	context->matches = matches;
+	context->unmatch = unmatch;
+	context->alloc = alloc_cb;
+	context->lock_a = lock_a;
+	context->lock_b = lock_b;
+	context->analysis_a = analysis_a;
+	context->analysis_b = analysis_b;
+	return true;
+}
 
-typedef SignatureData *(*SignatureNewCb)(RzAnalysis *analysis, void *data);
+static void shared_context_fini(SharedContext *context) {
+	rz_th_queue_free(context->queue);
+	rz_th_queue_free(context->matches);
+	rz_th_queue_free(context->unmatch);
+	rz_th_lock_free(context->lock_a);
+	if (context->lock_a != context->lock_b) {
+		rz_th_lock_free(context->lock_b);
+	}
+}
 
-static SignatureData *signature_data_bb_new(RzAnalysis *analysis, RzAnalysisBlock *bb) {
-	rz_return_val_if_fail(analysis && bb, NULL);
-	SignatureData *sim = NULL;
+static bool shared_context_alloc_a(SharedContext *context, void *ptr, ut8 **buffer, ut32 *buf_sz) {
+	rz_th_lock_enter(context->lock_a);
+	bool res = context->alloc(context->analysis_a, ptr, buffer, buf_sz);
+	rz_th_lock_leave(context->lock_a);
+	return res;
+}
+
+static bool shared_context_alloc_b(SharedContext *context, void *ptr, ut8 **buffer, ut32 *buf_sz) {
+	rz_th_lock_enter(context->lock_b);
+	bool res = context->alloc(context->analysis_b, ptr, buffer, buf_sz);
+	rz_th_lock_leave(context->lock_b);
+	return res;
+}
+
+static bool basic_block_data_new(RzAnalysis *analysis, RzAnalysisBlock *bb, ut8 **buffer, ut32 *buf_sz) {
+	rz_return_val_if_fail(analysis && bb && buffer && buf_sz, false);
 	ut8 *data = NULL;
 
 	if (bb->size < 1 ||
 		!(data = malloc(bb->size)) ||
-		!(sim = RZ_NEW0(SignatureData)) ||
 		!iob_read_at(bb->addr, data, bb->size)) {
 		goto fail;
 	}
 
-	sim->size = bb->size;
-	sim->data = data;
-	sim->info = bb;
-	return sim;
+	*buf_sz = bb->size;
+	*buffer = data;
+	return true;
 
 fail:
 	free(data);
-	free(sim);
-	return NULL;
+	return false;
 }
 
-static SignatureData *signature_data_fcn_new(RzAnalysis *analysis, RzAnalysisFunction *fcn) {
-	rz_return_val_if_fail(analysis && fcn, NULL);
+static bool function_data_new(RzAnalysis *analysis, RzAnalysisFunction *fcn, ut8 **buffer, ut32 *buf_sz) {
+	rz_return_val_if_fail(analysis && fcn && buffer && buf_sz, false);
 	size_t size = 0, current = 0;
 	ut8 *data = NULL;
-	SignatureData *sim = NULL;
 	RzAnalysisBlock *bb = NULL;
 	RzListIter *iter = NULL;
 
@@ -71,7 +114,7 @@ static SignatureData *signature_data_fcn_new(RzAnalysis *analysis, RzAnalysisFun
 		size += bb->size;
 	}
 
-	if (size < 1 || !(data = malloc(size)) || !(sim = RZ_NEW0(SignatureData))) {
+	if (size < 1 || !(data = malloc(size))) {
 		goto fail;
 	}
 
@@ -83,111 +126,43 @@ static SignatureData *signature_data_fcn_new(RzAnalysis *analysis, RzAnalysisFun
 		current += bb->size;
 	}
 
-	sim->size = size;
-	sim->data = data;
-	sim->info = fcn;
-	return sim;
+	*buf_sz = size;
+	*buffer = data;
+	return true;
 
 fail:
 	free(data);
-	free(sim);
-	return NULL;
+	return false;
 }
 
-static void signature_data_free(SignatureData *sim) {
-	if (!sim) {
-		return;
-	}
-	free(sim->data);
-	free(sim);
-}
-
-static RzList /*<SignatureData *>*/ *signature_data_from_list(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RzList /*<void *>*/ *list_src, SignatureNewCb callback_new) {
-	RzList *list = rz_list_newf((RzListFree)signature_data_free);
-	if (!list) {
-		RZ_LOG_ERROR("analysis: Cannot allocate RzList for basic blocks signatures\n");
-		return NULL;
-	}
-	void *ptr;
-	RzListIter *iter;
-	SignatureData *sim;
-
-	rz_list_foreach (list_src, iter, ptr) {
-		sim = callback_new(analysis, ptr);
-		if (!sim || !rz_list_append(list, sim)) {
-			signature_data_free(sim);
-			rz_list_free(list);
-			return NULL;
-		}
-	}
-
-	return list;
-}
-
-static bool shared_context_init(SharedContext *context, RzAnalysis *analysis_a, RzAnalysis *analysis_b, RzList /*<void *>*/ *list_a, RzList /*<void *>*/ *list_b, SignatureNewCb alloc_cb) {
-	RzThreadQueue *queue = NULL;
-	RzList *copy_a = signature_data_from_list(analysis_a, list_a, alloc_cb);
-	RzList *copy_b = signature_data_from_list(analysis_b, list_b, alloc_cb);
-	RzThreadQueue *matches = rz_th_queue_new(RZ_THREAD_QUEUE_UNLIMITED, NULL);
-	RzThreadQueue *unmatch = rz_th_queue_new(RZ_THREAD_QUEUE_UNLIMITED, NULL);
-	if (!copy_a || !copy_b || !matches || !unmatch || !(queue = rz_th_queue_new2(copy_a))) {
-		rz_list_free(copy_a);
-		rz_list_free(copy_b);
-		rz_th_queue_free(matches);
-		rz_th_queue_free(unmatch);
-		return false;
-	}
-
-	context->queue = queue;
-	context->list_b = copy_b;
-	context->matches = matches;
-	context->unmatch = unmatch;
-	return true;
-}
-
-static void shared_context_fini(SharedContext *context) {
-	rz_list_free(context->list_b);
-	rz_th_queue_free(context->matches);
-	rz_th_queue_free(context->unmatch);
-	rz_th_queue_free(context->queue);
-}
-
-#define signature_fast_compare(sim_a, sim_b) \
-	(sim_a->size == sim_b->size && !memcmp(sim_a->data, sim_b->data, sim_b->size))
-
-#define signature_levenshtein_distance(sim_a, sim_b, similarity) \
-	rz_diff_levenshtein_distance(sim_a->data, sim_a->size, sim_b->data, sim_b->size, NULL, similarity)
-
-static double calculate_similarity(SignatureData *sig_a, SignatureData *sig_b) {
-	if (signature_fast_compare(sig_a, sig_b)) {
+static double calculate_similarity(const ut8 *buf_a, ut32 size_a, const ut8 *buf_b, ut32 size_b) {
+	if (size_a == size_b && !memcmp(buf_a, buf_b, size_b)) {
 		return 1.0;
 	}
 	double similarity = 0.0;
-	if (!signature_levenshtein_distance(sig_a, sig_b, &similarity)) {
+	if (!rz_diff_levenshtein_distance(buf_a, size_a, buf_b, size_b, NULL, &similarity)) {
 		return 0.0;
 	}
 	return similarity;
 }
 
-#undef signature_fast_compare
-#undef signature_levenshtein_distance
-
-static double analysis_similarity_generic(RzAnalysis *analysis_a, void *ptr_a, RzAnalysis *analysis_b, void *ptr_b, SignatureNewCb callback_new) {
-	SignatureData *sig_a = NULL, *sig_b = NULL;
+static double analysis_similarity_generic(RzAnalysis *analysis_a, void *ptr_a, RzAnalysis *analysis_b, void *ptr_b, AllocateBuffer callback_new) {
+	ut8 *buf_a = NULL, *buf_b = NULL;
+	ut32 size_a = 0, size_b = 0;
 	double similarity = 0.0;
 	bool ret = true;
 
-	if (!(sig_a = callback_new(analysis_a, ptr_a)) ||
-		!(sig_b = callback_new(analysis_b, ptr_b))) {
+	if (!callback_new(analysis_a, ptr_a, &buf_a, &size_a) ||
+		!callback_new(analysis_b, ptr_b, &buf_b, &size_b)) {
 		ret = false;
 		goto fail;
 	}
 
-	similarity = calculate_similarity(sig_a, sig_b);
+	similarity = calculate_similarity(buf_a, size_a, buf_b, size_b);
 
 fail:
-	signature_data_free(sig_a);
-	signature_data_free(sig_b);
+	free(buf_a);
+	free(buf_b);
 	return ret ? similarity : -1.0;
 }
 
@@ -202,7 +177,7 @@ fail:
  */
 RZ_API double rz_analysis_similarity_basic_block(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RzAnalysisBlock *bb_a, RZ_NONNULL RzAnalysisBlock *bb_b) {
 	rz_return_val_if_fail(analysis && bb_a && bb_b, -1.0);
-	return analysis_similarity_generic(analysis, bb_a, analysis, bb_b, (SignatureNewCb)signature_data_bb_new);
+	return analysis_similarity_generic(analysis, bb_a, analysis, bb_b, (AllocateBuffer)basic_block_data_new);
 }
 
 /**
@@ -216,7 +191,7 @@ RZ_API double rz_analysis_similarity_basic_block(RZ_NONNULL RzAnalysis *analysis
  */
 RZ_API double rz_analysis_similarity_function(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RzAnalysisFunction *fcn_a, RZ_NONNULL RzAnalysisFunction *fcn_b) {
 	rz_return_val_if_fail(analysis && fcn_a && fcn_b, -1.0);
-	return analysis_similarity_generic(analysis, fcn_a, analysis, fcn_b, (SignatureNewCb)signature_data_fcn_new);
+	return analysis_similarity_generic(analysis, fcn_a, analysis, fcn_b, (AllocateBuffer)function_data_new);
 }
 
 /**
@@ -231,7 +206,7 @@ RZ_API double rz_analysis_similarity_function(RZ_NONNULL RzAnalysis *analysis, R
  */
 RZ_API double rz_analysis_similarity_basic_block_2(RZ_NONNULL RzAnalysis *analysis_a, RZ_NONNULL RzAnalysisBlock *bb_a, RZ_NONNULL RzAnalysis *analysis_b, RZ_NONNULL RzAnalysisBlock *bb_b) {
 	rz_return_val_if_fail(analysis_a && analysis_b && bb_a && bb_b, -1.0);
-	return analysis_similarity_generic(analysis_a, bb_a, analysis_b, bb_b, (SignatureNewCb)signature_data_bb_new);
+	return analysis_similarity_generic(analysis_a, bb_a, analysis_b, bb_b, (AllocateBuffer)basic_block_data_new);
 }
 
 /**
@@ -246,17 +221,17 @@ RZ_API double rz_analysis_similarity_basic_block_2(RZ_NONNULL RzAnalysis *analys
  */
 RZ_API double rz_analysis_similarity_function_2(RZ_NONNULL RzAnalysis *analysis_a, RZ_NONNULL RzAnalysisFunction *fcn_a, RZ_NONNULL RzAnalysis *analysis_b, RZ_NONNULL RzAnalysisFunction *fcn_b) {
 	rz_return_val_if_fail(analysis_a && analysis_b && fcn_a && fcn_b, -1.0);
-	return analysis_similarity_generic(analysis_a, fcn_a, analysis_b, fcn_b, (SignatureNewCb)signature_data_fcn_new);
+	return analysis_similarity_generic(analysis_a, fcn_a, analysis_b, fcn_b, (AllocateBuffer)function_data_new);
 }
 
-static RZ_OWN RzAnalysisMatchPair *match_pair_new(SignatureData *sig_a, SignatureData *sig_b, double similarity) {
+static RZ_OWN RzAnalysisMatchPair *match_pair_new(const void *pair_a, const void *pair_b, double similarity) {
 	RzAnalysisMatchPair *result = RZ_NEW0(RzAnalysisMatchPair);
 	if (!result) {
 		return NULL;
 	}
 
-	result->pair_a = sig_a->info;
-	result->pair_b = sig_b->info;
+	result->pair_a = pair_a;
+	result->pair_b = pair_b;
 	result->similarity = similarity;
 	return result;
 }
@@ -267,7 +242,7 @@ static int comparePairAddresses(const RzAnalysisMatchPair *ma, const RzAnalysisM
 	return (a && b && a->addr && b->addr ? (a->addr > b->addr) - (a->addr < b->addr) : 0);
 }
 
-static RZ_OWN RzAnalysisMatchResult *analysis_match_result_new(RZ_NONNULL RzAnalysis *analysis_a, RZ_NONNULL RzAnalysis *analysis_b, RZ_NONNULL RzList /*<void *>*/ *list_a, RZ_NONNULL RzList /*<void *>*/ *list_b, RzThreadFunction thread_cb, SignatureNewCb alloc_cb) {
+static RZ_OWN RzAnalysisMatchResult *analysis_match_result_new(RZ_NONNULL RzAnalysis *analysis_a, RZ_NONNULL RzAnalysis *analysis_b, RZ_NONNULL RzList /*<void *>*/ *list_a, RZ_NONNULL RzList /*<void *>*/ *list_b, RzThreadFunction thread_cb, AllocateBuffer alloc_cb) {
 	size_t pool_size = 1;
 	RzListIter *iter;
 	RzAnalysisMatchPair *pair = NULL;
@@ -349,12 +324,10 @@ static void *analysis_match_basic_blocks(SharedContext *shared) {
  */
 RZ_API RZ_OWN RzAnalysisMatchResult *rz_analysis_match_basic_blocks(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RzAnalysisFunction *fcn_a, RZ_NONNULL RzAnalysisFunction *fcn_b) {
 	rz_return_val_if_fail(analysis && fcn_a && fcn_b, NULL);
-	return analysis_match_result_new(analysis, analysis, fcn_a->bbs, fcn_b->bbs, (RzThreadFunction)analysis_match_basic_blocks, (SignatureNewCb)signature_data_bb_new);
+	return analysis_match_result_new(analysis, analysis, fcn_a->bbs, fcn_b->bbs, (RzThreadFunction)analysis_match_basic_blocks, (AllocateBuffer)basic_block_data_new);
 }
 
-static bool function_cmp(SignatureData *sig_a, SignatureData *sig_b) {
-	RzAnalysisFunction *fcn_a = sig_a->info;
-	RzAnalysisFunction *fcn_b = sig_b->info;
+static bool function_cmp(RzAnalysisFunction *fcn_a, RzAnalysisFunction *fcn_b) {
 	if (RZ_STR_ISEMPTY(fcn_b->name) ||
 		!strncmp(fcn_b->name, "fcn.", strlen("fcn.")) ||
 		RZ_STR_ISEMPTY(fcn_a->name) ||
@@ -368,34 +341,49 @@ static bool function_cmp(SignatureData *sig_a, SignatureData *sig_b) {
 static void *analysis_match_functions(SharedContext *shared) {
 	double max_similarity = 0.0, calc_similarity = 0.0;
 	const RzListIter *iter = NULL;
-	SignatureData *sig_a, *sig_b, *match = NULL;
+	RzAnalysisFunction *fcn_a = NULL, *fcn_b = NULL, *match_b = NULL;
 	RzAnalysisMatchPair *pair = NULL;
+	ut32 size_a = 0, size_b = 0;
+	ut8 *buf_a = NULL, *buf_b = NULL;
 
-	while ((sig_a = rz_th_queue_pop(shared->queue, false))) {
-		match = NULL;
+	while ((fcn_a = rz_th_queue_pop(shared->queue, false))) {
+		if (!shared_context_alloc_a(shared, fcn_a, &buf_a, &size_a)) {
+			RZ_LOG_ERROR("analysis_match: cannot allocate buffer for function %s (A)\n", fcn_a->name);
+			rz_th_queue_push(shared->unmatch, fcn_a, true);
+			continue;
+		}
+
+		match_b = NULL;
 		max_similarity = 0.0;
-		rz_list_foreach (shared->list_b, iter, sig_b) {
-			calc_similarity = calculate_similarity(sig_a, sig_b);
+		rz_list_foreach (shared->list_b, iter, fcn_b) {
+			if (!shared_context_alloc_b(shared, fcn_b, &buf_b, &size_b)) {
+				RZ_LOG_ERROR("analysis_match: cannot allocate buffer for function %s (B)\n", fcn_b->name);
+				continue;
+			}
 
-			if (function_cmp(sig_a, sig_b)) {
+			calc_similarity = calculate_similarity(buf_a, size_a, buf_b, size_b);
+			free(buf_b);
+
+			if (function_cmp(fcn_a, fcn_b)) {
 				max_similarity = calc_similarity;
-				match = sig_b;
+				match_b = fcn_b;
 				break;
 			} else if (calc_similarity < RZ_ANALYSIS_SIMILARITY_THRESHOLD && calc_similarity <= max_similarity) {
 				continue;
 			}
 			max_similarity = calc_similarity;
-			match = sig_b;
+			match_b = fcn_b;
 			if (max_similarity >= 1.0) {
 				break;
 			}
 		}
+		free(buf_a);
 
-		if (match && (pair = match_pair_new(sig_a, match, max_similarity))) {
+		if (match_b && (pair = match_pair_new(fcn_a, match_b, max_similarity))) {
 			rz_th_queue_push(shared->matches, pair, true);
 			continue;
 		}
-		rz_th_queue_push(shared->unmatch, sig_a->info, true);
+		rz_th_queue_push(shared->unmatch, fcn_a, true);
 	}
 
 	return NULL;
@@ -412,7 +400,7 @@ static void *analysis_match_functions(SharedContext *shared) {
  */
 RZ_API RZ_OWN RzAnalysisMatchResult *rz_analysis_match_functions(RZ_NONNULL RzAnalysis *analysis, RzList /*<RzAnalysisFunction *>*/ *list_a, RzList /*<RzAnalysisFunction *>*/ *list_b) {
 	rz_return_val_if_fail(analysis && list_a && list_b, NULL);
-	return analysis_match_result_new(analysis, analysis, list_a, list_b, (RzThreadFunction)analysis_match_functions, (SignatureNewCb)signature_data_fcn_new);
+	return analysis_match_result_new(analysis, analysis, list_a, list_b, (RzThreadFunction)analysis_match_functions, (AllocateBuffer)function_data_new);
 }
 
 /**
@@ -427,7 +415,7 @@ RZ_API RZ_OWN RzAnalysisMatchResult *rz_analysis_match_functions(RZ_NONNULL RzAn
  */
 RZ_API RZ_OWN RzAnalysisMatchResult *rz_analysis_match_basic_blocks_2(RZ_NONNULL RzAnalysis *analysis_a, RZ_NONNULL RzAnalysisFunction *fcn_a, RZ_NONNULL RzAnalysis *analysis_b, RZ_NONNULL RzAnalysisFunction *fcn_b) {
 	rz_return_val_if_fail(analysis_a && analysis_b && fcn_a && fcn_b, NULL);
-	return analysis_match_result_new(analysis_a, analysis_b, fcn_a->bbs, fcn_b->bbs, (RzThreadFunction)analysis_match_basic_blocks, (SignatureNewCb)signature_data_bb_new);
+	return analysis_match_result_new(analysis_a, analysis_b, fcn_a->bbs, fcn_b->bbs, (RzThreadFunction)analysis_match_basic_blocks, (AllocateBuffer)basic_block_data_new);
 }
 
 /**
@@ -442,5 +430,5 @@ RZ_API RZ_OWN RzAnalysisMatchResult *rz_analysis_match_basic_blocks_2(RZ_NONNULL
  */
 RZ_API RZ_OWN RzAnalysisMatchResult *rz_analysis_match_functions_2(RZ_NONNULL RzAnalysis *analysis_a, RzList /*<RzAnalysisFunction *>*/ *list_a, RZ_NONNULL RzAnalysis *analysis_b, RzList /*<RzAnalysisFunction *>*/ *list_b) {
 	rz_return_val_if_fail(analysis_a && analysis_b && list_a && list_b, NULL);
-	return analysis_match_result_new(analysis_a, analysis_b, list_a, list_b, (RzThreadFunction)analysis_match_functions, (SignatureNewCb)signature_data_fcn_new);
+	return analysis_match_result_new(analysis_a, analysis_b, list_a, list_b, (RzThreadFunction)analysis_match_functions, (AllocateBuffer)function_data_new);
 }
