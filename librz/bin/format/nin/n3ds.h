e2ee0c3fd602d1bba9d4ec0a0992e6fac1d4460d
Giovanni <561184+wargio@users.noreply.github.com>
2022-10-20 19:56:17 +0200

    Rewritten bin_nin3ds.c and add test (#3110)

--- librz/bin/format/nin/n3ds.h
+++ librz/bin/format/nin/n3ds.h
@@ -1,33 +1,42 @@
+// SPDX-FileCopyrightText: 2022 deroad <wargio@libero.it>
 // SPDX-FileCopyrightText: 2016 Alberto Ortega
 // SPDX-License-Identifier: LGPL-3.0-only
 
 /*
 https://www.3dbrew.org/wiki/FIRM
 More formats to support: https://www.3dbrew.org/wiki/Category:File_formats
+https://github.com/LumaTeam/Luma3DS/blob/master/arm9/source/3dsheaders.h
 */
 
 #ifndef NIN_N3DS_H
 #define NIN_N3DS_H
 
 #include <rz_types_base.h>
-RZ_PACKED(
-	struct n3ds_firm_sect_hdr {
-		ut32 offset;
-		ut32 address;
-		ut32 size;
-		ut32 type; /* ('0'=ARM9/'1'=ARM11) */
-		ut8 sha256[0x20];
-	});
 
-RZ_PACKED(
-	struct n3ds_firm_hdr {
-		ut8 magic[4];
-		ut8 reserved1[4];
-		ut32 arm11_ep;
-		ut32 arm9_ep;
-		ut8 reserved2[0x30];
-		struct n3ds_firm_sect_hdr sections[4];
-		ut8 rsa2048[0x100];
-	});
+enum {
+	N3DS_COPY_MODE_NDMA = 0,
+	N3DS_COPY_MODE_XDMA = 1,
+	N3DS_COPY_MODE_MEMCPY = 2,
+};
+
+typedef struct n3ds_firm_sect_hdr_t {
+	ut32 offset;
+	ut32 address;
+	ut32 size;
+	ut32 copy_mode;
+	ut8 sha256[0x20];
+	// ----- additional rizin info ----- //
+	ut32 type; ///< This is not part of the real section header
+} N3DSFirmSectHdr;
+
+typedef struct n3ds_firm_hdr_t {
+	ut8 magic[4];
+	ut8 reserved1[4];
+	ut32 arm11_ep;
+	ut32 arm9_ep;
+	ut8 reserved2[0x30];
+	N3DSFirmSectHdr sections[4];
+	ut8 rsa2048[0x100];
+} N3DSFirmHdr;
 
 #endif /* NIN_N3DS_H */
