e7e2156eed6c1232b76a4894b17e1b2d4810daac
Giovanni <561184+wargio@users.noreply.github.com>
2022-12-05 02:51:36 +0100

    Do not load sigdb from system and home when running rz-test (#3222)

--- binrz/rz-test/run.c
+++ binrz/rz-test/run.c
@@ -116,6 +116,8 @@ static RzSubprocessOutput *run_rz_test(RzTestRunConfig *config, ut64 timeout_ms,
 	rz_pvector_push(&args, "-escr.utf8=0");
 	rz_pvector_push(&args, "-escr.color=0");
 	rz_pvector_push(&args, "-escr.interactive=0");
+	rz_pvector_push(&args, "-eflirt.sigdb.load.system=false");
+	rz_pvector_push(&args, "-eflirt.sigdb.load.home=false");
 	rz_pvector_push(&args, "-N");
 	RzListIter *it;
 	void *extra_arg, *file_arg;
