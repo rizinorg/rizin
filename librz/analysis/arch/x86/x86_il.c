157359741595b6f30a01ceeb46eff51d30c58f6f
Dhruv Maroo <dhruvmaru007@gmail.com>
2022-12-07 05:50:31 +0530

    Fix Coverity issues for x86 RzIL uplifting (#3227)
    * Fix Coverity issue 403157
    
    * Out of bounds check should also check for equality
    
    * Add `port_label` in `rz_x86_il_config`
    
    * Fix typo while parsing register in x86 assembler
    
    * Check return type `getToken` when parsing registers in x86 assembler

--- librz/analysis/arch/x86/x86_il.c
+++ librz/analysis/arch/x86/x86_il.c
@@ -2587,6 +2587,9 @@ IL_LIFTER(mul) {
 	}
 
 	rz_warn_if_reached();
+	rz_il_op_pure_free(op);
+	rz_il_op_effect_free(true_cond);
+	rz_il_op_effect_free(false_cond);
 	return NULL;
 }
 
@@ -3806,7 +3809,7 @@ static x86_il_ins x86_ins[X86_INS_ENDING] = {
 
 RZ_IPI bool rz_x86_il_opcode(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RzAnalysisOp *aop, ut64 pc, RZ_BORROW RZ_NONNULL const X86ILIns *ins) {
 	rz_return_val_if_fail(analysis && aop && ins, false);
-	if (ins->mnem > X86_INS_ENDING) {
+	if (ins->mnem >= X86_INS_ENDING) {
 		RZ_LOG_ERROR("RzIL: x86: Invalid instruction type %d", ins->mnem);
 		return false;
 	}
@@ -3847,12 +3850,14 @@ RZ_IPI RzAnalysisILConfig *rz_x86_il_config(RZ_NONNULL RzAnalysis *analysis) {
 	RzILEffectLabel *int_label = rz_il_effect_label_new("int", EFFECT_LABEL_SYSCALL);
 	int_label->hook = label_int;
 	rz_analysis_il_config_add_label(r, int_label);
+
 	RzILEffectLabel *halt_label = rz_il_effect_label_new("halt", EFFECT_LABEL_SYSCALL);
 	halt_label->hook = label_halt;
+	rz_analysis_il_config_add_label(r, halt_label);
+
 	RzILEffectLabel *port_label = rz_il_effect_label_new("port", EFFECT_LABEL_SYSCALL);
 	port_label->hook = label_port;
-
-	rz_analysis_il_config_add_label(r, halt_label);
+	rz_analysis_il_config_add_label(r, port_label);
 
 	return r;
 }
--- librz/asm/p/asm_x86_nz.c
+++ librz/asm/p/asm_x86_nz.c
@@ -4738,7 +4738,11 @@ static Register parseReg(RzAsm *a, const char *str, size_t *pos, ut32 *type) {
 	// Get token (especially the length)
 	size_t nextpos, length;
 	const char *token;
-	getToken(str, pos, &nextpos);
+	if (getToken(str, pos, &nextpos) != TT_WORD) {
+		/* We expect a word, anything else means invalid register */
+		return X86R_UNDEFINED;
+	}
+
 	token = str + *pos;
 	length = nextpos - *pos;
 	*pos = nextpos;
@@ -4763,7 +4767,7 @@ static Register parseReg(RzAsm *a, const char *str, size_t *pos, ut32 *type) {
 	}
 	// Debug registers
 	if (length == 3 && token[0] == 'd') {
-		for (i = 0; cregs[i]; i++) {
+		for (i = 0; dregs[i]; i++) {
 			if (!rz_str_ncasecmp(dregs[i], token, length)) {
 				*type = (OT_DEBUGREG & OT_REG(i)) | OT_DWORD;
 				return i;
