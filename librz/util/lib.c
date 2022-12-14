cbad6dfe5ce1d2f7c3a2dd889fa94dc72f3197ea
Riccardo Schirone <rschirone91@gmail.com>
2022-12-14 22:29:21 +0100

    Remove RZ_HOMD_OLD_PLUGINS after transition period ended

--- librz/util/lib.c
+++ librz/util/lib.c
@@ -290,16 +290,6 @@ RZ_API int rz_lib_open(RzLib *lib, const char *file) {
 		return -1;
 	}
 
-	// TODO: remove after 0.4.0 is released
-	if (strstr(file, RZ_HOME_OLD_PLUGINS)) {
-		char *oldhomeplugins = rz_path_home_prefix(RZ_HOME_OLD_PLUGINS);
-		char *homeplugins = rz_path_home_prefix(RZ_PLUGINS);
-		const char *basename = rz_file_basename(file);
-		RZ_LOG_WARN("Loading plugins from '%s' is deprecated, please install plugin '%s' in '%s' instead.\n", oldhomeplugins, basename, homeplugins);
-		free(homeplugins);
-		free(oldhomeplugins);
-	}
-
 	void *handler = rz_lib_dl_open(file);
 	if (!handler) {
 		RZ_LOG_INFO("Cannot open library: '%s'\n", file);
