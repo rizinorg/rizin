a76f9ed5efb015fa1a83f690d8fd14840dce9f4a
Riccardo Schirone <562321+ret2libc@users.noreply.github.com>
2022-12-15 02:45:35 +0100

    Remove RZ_HOMD_OLD_PLUGINS after transition period ended (#3246)

--- librz/main/rz-bin.c
+++ librz/main/rz-bin.c
@@ -709,8 +709,6 @@ RZ_API int rz_main_rz_bin(int argc, const char **argv) {
 	bin = core.bin;
 	if (!(tmp = rz_sys_getenv("RZ_BIN_NOPLUGINS"))) {
 		char *homeplugindir = rz_path_home_prefix(RZ_PLUGINS);
-		// TODO: remove after 0.4.0 is released
-		char *oldhomeplugindir = rz_path_home_prefix(RZ_HOME_OLD_PLUGINS);
 		char *plugindir = rz_path_system(RZ_PLUGINS);
 		RzLib *l = rz_lib_new(NULL, NULL);
 		rz_lib_add_handler(l, RZ_LIB_TYPE_DEMANGLER, "demangler plugins",
@@ -725,10 +723,8 @@ RZ_API int rz_main_rz_bin(int argc, const char **argv) {
 			rz_lib_opendir(l, path, false);
 		}
 		rz_lib_opendir(l, homeplugindir, false);
-		rz_lib_opendir(l, oldhomeplugindir, false);
 		rz_lib_opendir(l, plugindir, false);
 		free(homeplugindir);
-		free(oldhomeplugindir);
 		free(plugindir);
 		free(path);
 		rz_lib_free(l);
