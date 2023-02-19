rizinorg/rizin

commit b46e7bd4f269296a85f3c9b44064607750ae83cd

PPC uplifting to RzIL (#2823)
* Print a warning if config for VM was NULL.
* Add warning if register is not added to VM due to overlap.
* Update PPC register profile.
* Add vector and float registers. As well as some control and system registers.
* Enable to write values 4bit registers.
* PPC: Uplift most common instructions.
* Print warning if reserved SPR instruction is encountered.
* Update PC/LR addresses and add ca32 writes.
* Write cache needs to get flushed so load and store tests don't share the same memory.
* Add missing T/F branch mnemonics.
* Add Move to/from CR/CR0-7
* Fix TA address calculation for branch instructions.
* Add branch tests for branch mnemonics.
* Add XNOP
* NOP cache touch instructions.
* Add undocumented ATTN instruction to the not_implemented group.
* Add isel instruction.
* Implement CRCLR, CRSET, CROR.
* Add CNTLZ instructions.
* Add mcrf instructions.
* Add inacive test for cmpb.
* Add Load bytes reverse instructions.
* Add test and add address alignment to dcbz.
* Add eqv test
* Correct DIV and MUL operations
* Add div tests.
* Add tests and correct MT/MFXER
* Remove register ca32, ov32
* Remove explici setting of cr register because QEMU does not do it. Otherwise we get a mismatch in the trace
* Simplify carry set for add and sub. Sub instructions are exclusivly defined with addition. Hence no sub case needed.
* Unify BD and fix branch instructions. Fix: Check the single bit not the cr reg
* Document Conditional branches and replace NOPs with EMPTY
* Add mulli instruction to double word instructions.
* Fix ca set for shift instructions:
* ca value had to be determined before the shift happened. The wrong ca value was calculated if the src and target reg were the same.
* Replace NOP wit EMPTY.
* Use MSB isntead of SLT.
* Brought fixup of ADD and SUB instructions: The add and sub instructions had several issues which let to incorrect execution.

librz/analysis/arch/ppc/ppc_il.h
@@ -1,0 +1,179 @@
+// SPDX-FileCopyrightText: 2022 Rot127 <unisono@quyllur.org>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#ifndef PPC_IL_H
+#define PPC_IL_H
+
+#include "ppc_analysis.h"
+#include <rz_reg.h>
+#include <rz_analysis.h>
+#include <rz_il.h>
+#include <rz_types.h>
+#include <capstone.h>
+
+#define PPC_BYTE  8
+#define PPC_HWORD 16
+#define PPC_WORD  32
+#define PPC_DWORD 64
+#define PPC_QWORD 128
+
+/**
+ * \brief This value varies from implementation to implementation.
+ * Should be replaced with a plugin specific config option.
+ */
+#define DCACHE_LINE_SIZE 128
+
+/**
+ * \brief Writes to the info log that an unimplemented instruction was encountered and returns an EMPTY() effect.
+ */
+#define NOT_IMPLEMENTED \
+	do { \
+		RZ_LOG_INFO("IL instruction not implemented."); \
+		return NULL; \
+	} while (0)
+
+/**
+ * \brief Unsigned value \p i which is PPC_ARCH_BITS (32 or 64) wide.
+ */
+#define UA(i) (IN_64BIT_MODE ? U64(i) : U32(i))
+
+/**
+ * \brief Signed value \p i which is PPC_ARCH_BITS (32 or 64) wide.
+ */
+#define SA(i) (IN_64BIT_MODE ? S64(i) : S32(i))
+
+/**
+ * \brief Extend value \p v with sign bits to a width of \p n.
+ */
+#define EXTEND(n, v) LET("v", v, ITE(MSB(VARLP("v")), SIGNED(n, VARLP("v")), UNSIGNED(n, VARLP("v"))))
+
+/**
+ * \brief Extend value with sign bits to a width of 32/64 bit.
+ */
+#define EXTS(v) SIGNED(PPC_ARCH_BITS, v)
+
+/**
+ * \brief Extend value with 0s to a width of 32/64 bit.
+ */
+#define EXTZ(v) UNSIGNED(PPC_ARCH_BITS, v)
+
+/**
+ * \brief Rotates a 64bit value. Rotate \p x left by \p y bits.
+ * \p y should be U8, \p x should be U32 and U64 respectively.
+ */
+#define ROTL64(x, y) LET("rotl64_x", x, \
+	LET("rotl64_y", y, \
+		(LOGOR(SHIFTL0(VARLP("rotl64_x"), VARLP("rotl64_y")), SHIFTR0(VARLP("rotl64_x"), SUB(U8(64), UNSIGNED(8, VARLP("rotl64_y"))))))))
+
+/**
+ * \brief Rotates a 32bit value. If the the VM is in 64bit mode "ROTL64(x||x, y)" is executed instead.
+ */
+#define ROTL32(x, y) LET("rotl32_x", x, \
+	LET("rotl32_y", y, \
+		(IN_64BIT_MODE ? ROTL64(APPEND(VARLP("rotl32_x"), VARLP("rotl32_x")), VARLP("rotl32_y")) \
+			       : LOGOR(SHIFTL0(VARLP("rotl32_x"), VARLP("rotl32_y")), SHIFTR0(VARLP("rotl32_x"), SUB(U8(32), UNSIGNED(8, VARLP("rotl32_y"))))))))
+
+/**
+ * \brief Returns a Pure of width \p w with the \p i bit set.
+ * Please note: The left most bit is bit 0.
+ */
+#define BIT_I(w, i) SHIFTR0(SHIFTR(IL_TRUE, UN(w, 0), U8(1)), i)
+
+/**
+ * \brief Sets bit \p i (=IL_TRUE) in the local variable with width \p w of the name \p vn.
+ * Please note: The left most bit is bit 0.
+ */
+#define SET_BIT(vn, w, i) SETL(vn, LOGOR(VARL(vn), BIT_I(w, i)))
+
+/**
+ * \brief Unsets bit \p i (=IL_FALSE) in the local variable with width \p w of the name \p vn.
+ * Please note: The left most bit is bit 0.
+ */
+#define UNSET_BIT(vn, w, i) SETL(vn, LOGAND(VARL(vn), LOGNOT(BIT_MASK(w, i, i))))
+
+/**
+ * \brief Returns a Pure of length \p l with bit \p i to \p j (inclusive \p j) set to IL_TRUE.
+ * \p i marks the left bit. \p j the right. \p i and \p j need to be U8.
+ * Please note: The left most bit is bit 0.
+ */
+#define BIT_MASK(l, i, j) LOGNOT(LOGOR(SHIFTR(IL_TRUE, UN(l, 0), i), SHIFTL(IL_TRUE, UN(l, 0), SUB(U8(l - 1), j))))
+
+/**
+ * \brief Sets bit \p i to \p j of the variable \p v to the value stored in \p s.
+ * Both variables must be of width w.
+ * \p i and \p j should be U8
+ * Please note: The left most bit is bit 0.
+ */
+#define SET_RANGE(v, i, j, s, w) LOGOR(LOGAND(LOGNOT(BIT_MASK(w, i, j)), v), LOGAND(SHIFTL0(s, SUB(U8(w), j)), BIT_MASK(w, i, j)))
+
+/**
+ * \brief Tests bit i in Pure value v with a width of w.
+ * Returns IL_TRUE if bit i is set. IL_FALSE otherwise.
+ * Please note: The left most bit is bit 0.
+ */
+#define BIT_IS_SET(val, w, i) NON_ZERO(LOGAND(val, BIT_I(w, i)))
+
+/**
+ * \brief Implements the mask generation from the Reference Manual.
+ *
+ * if mstart ≤ mstop then
+ *      mask[mstart:mstop] = ones
+ *      mask[all other bits] = zeros
+ * else
+ *      mask[mstart:63] = ones
+ *      mask[0:mstop] = ones
+ *      mask[all other bits] = zeros
+ *
+ * The algorithm implemented here is:
+ *
+ * ```
+ * m = 0
+ * while (mstart != mstop) {
+ *     m[mstart] = 1
+ *     mstart = (mstart + 1) % 64
+ * }
+ * m[mstop] = 1
+ * mask = (PPC_ARCH_BITS) m
+ * ```
+ *
+ * All computations are on 64 bit numbers.
+ * In case of a 32bit CPU the result will be casted to 32bit.
+ * The local variable "mask" will hold the mask
+ */
+#define SET_MASK(mstart, mstop) \
+	SEQ6(SETL("mstart", mstart), \
+		SETL("mstop", mstop), \
+		SETL("m", U64(0)), \
+		REPEAT(INV(EQ(VARL("mstart"), VARL("mstop"))), \
+			SEQ2(SET_BIT("m", 64, VARL("mstart")), \
+				SETL("mstart", MOD(ADD(VARL("mstart"), U8(1)), U8(64))))), \
+		SET_BIT("m", 64, VARL("mstop")), \
+		SETL("mask", CAST(PPC_ARCH_BITS, IL_FALSE, VARL("m"))))
+
+RZ_IPI RzAnalysisILConfig *rz_ppc_cs_64_il_config(bool big_endian);
+RZ_IPI RzAnalysisILConfig *rz_ppc_cs_32_il_config(bool big_endian);
+
+RZ_IPI RzILOpEffect *rz_ppc_cs_get_il_op(RZ_BORROW csh handle, RZ_BORROW cs_insn *insn, const cs_mode mode);
+
+RZ_IPI RZ_OWN RzILOpEffect *ppc_set_carry_add_sub(RZ_OWN RzILOpBitVector *a, RZ_OWN RzILOpBitVector *b, RZ_OWN RZ_NULLABLE RzILOpBitVector *c, cs_mode mode);
+RZ_IPI RZ_OWN RzILOpEffect *ppc_cmp_set_cr(RZ_BORROW RzILOpPure *left, RZ_BORROW RzILOpPure *right, const bool signed_cmp, const char *crX, const cs_mode mode);
+
+RZ_IPI bool ppc_is_x_form(ut32 insn_id);
+RZ_IPI st32 ppc_get_mem_acc_size(ut32 insn_id);
+RZ_IPI bool ppc_updates_ra_with_ea(ut32 insn_id);
+RZ_IPI bool ppc_is_algebraic(ut32 insn_id);
+RZ_IPI bool ppc_sets_lr(ut32 insn_id);
+RZ_IPI bool ppc_is_conditional(ut32 insn_id);
+RZ_IPI bool ppc_moves_to_spr(ut32 insn_id);
+RZ_IPI bool ppc_is_mul_div_d(const ut32 id, const cs_mode mode);
+RZ_IPI bool ppc_is_mul_div_u(const ut32 id);
+RZ_IPI bool ppc_decrements_ctr(RZ_BORROW cs_insn *insn, const cs_mode mode);
+RZ_IPI ut32 ppc_fmx_to_mask(const ut8 fmx);
+
+RZ_IPI RZ_OWN RzILOpEffect *ppc_set_xer(RzILOpPure *val, cs_mode mode);
+RZ_IPI RZ_OWN RzILOpPure *ppc_get_xer(cs_mode mode);
+RZ_IPI RZ_OWN RzILOpPure *ppc_get_branch_ta(RZ_BORROW cs_insn *insn, const cs_mode mode);
+RZ_IPI RZ_OWN RzILOpPure *ppc_get_branch_cond(const csh handle, RZ_BORROW cs_insn *insn, const cs_mode mode);
+RZ_IPI RZ_OWN RzILOpEffect *ppc_sync_crx_cr(const bool to_cr, const ut32 cr_mask);
+
+#endif /* PPC_IL_H */

librz/analysis/arch/ppc/ppc_analysis.h
@@ -1,0 +1,20 @@
+// SPDX-FileCopyrightText: 2022 Rot127 <unisono@quyllur.org>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#ifndef PPC_ANALYSIS_H
+#define PPC_ANALYSIS_H
+
+#define INSOPS   insn->detail->ppc.op_count
+#define INSOP(n) insn->detail->ppc.operands[n]
+#define OP_CNT   insn->detail->ppc.op_count
+#define IMM(x)   (ut64)(insn->detail->ppc.operands[x].imm)
+
+#define PPC_IN_BE_MODE (mode & CS_MODE_BIG_ENDIAN)
+// Capstone does not extract the BO or BI fields of instructions. So we do it manually.
+#define PPC_READ_BO_FIELD (((rz_read_ble32(insn->bytes, PPC_IN_BE_MODE)) & 0x03e00000) >> 21)
+#define PPC_READ_BI_FIELD (((rz_read_ble32(insn->bytes, PPC_IN_BE_MODE)) & 0x001f0000) >> 16)
+#define IN_BE_MODE        (mode & CS_MODE_BIG_ENDIAN)
+#define IN_64BIT_MODE     (mode & CS_MODE_64)
+#define PPC_ARCH_BITS     (IN_64BIT_MODE ? 64 : 32)
+
+#endif /* PPC_ANALYSIS_H */

librz/analysis/arch/ppc/ppc_il_flag_ops.c
@@ -1,0 +1,64 @@
+// SPDX-FileCopyrightText: 2022 Rot127 <unisono@quyllur.org>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include "ppc_il.h"
+#include "ppc_analysis.h"
+#include "rz_il/rz_il_opcodes.h"
+#include <rz_types.h>
+
+#include <rz_il/rz_il_opbuilder_begin.h>
+
+/**
+ * \brief Set "ca" bit if, after an add operation on \p a and \p b , the M+1 bit is set.
+ * If \p c is given the ca bit is set if the result of ( \p b + \p c ) or ( \p a + ( \p b + \p c )) has bit M+1 set.
+ *
+ * \param a Value a.
+ * \param b Value b.
+ * \param c Value c. Is optional and can be NULL
+ * \param mode Capstone mode.
+ * \return RzILOpEffect* Effect which sets the carry bits.
+ */
+RZ_IPI RZ_OWN RzILOpEffect *ppc_set_carry_add_sub(RZ_OWN RzILOpBitVector *a, RZ_OWN RzILOpBitVector *b, RZ_OWN RZ_NULLABLE RzILOpBitVector *c, cs_mode mode) {
+	rz_return_val_if_fail(a && b, NULL);
+	ut32 bits = PPC_ARCH_BITS;
+	RzILOpPure *r0, *r2;
+	if (c) {
+		r2 = ADD(UNSIGNED(bits + 1, a), VARLP("r1"));
+		r0 = LET("r1", ADD(UNSIGNED(bits + 1, b), UNSIGNED(bits + 1, c)), OR(MSB(VARLP("r1")), MSB(r2)));
+	} else {
+		r0 = MSB(ADD(UNSIGNED(bits + 1, a), UNSIGNED(bits + 1, b)));
+	}
+	// For ISA v3 CPU register ca32 should be handled here as well.
+	return SETG("ca", r0);
+}
+
+/**
+ * \brief Compares two values and sets the given cr field.
+ *
+ * \param left The left operand of the comparison.
+ * \param right The right operand of the comparison.
+ * \param signed_cmp True: Signed comparison. False: Unsigned comparison.
+ * \param crX The cr field to set.
+ * \param mode Capstone mode.
+ * \return RzILOpEffect* Sequence of effects which set the cr field accordingly.
+ */
+RZ_IPI RZ_OWN RzILOpEffect *ppc_cmp_set_cr(RZ_BORROW RzILOpPure *left, RZ_BORROW RzILOpPure *right, const bool signed_cmp, const char *crX, const cs_mode mode) {
+	rz_return_val_if_fail(left && right && crX, NULL);
+
+	RzILOpEffect *set_so = SETL("so_flag", BOOL_TO_BV(VARG("so"), 1));
+	RzILOpEffect *set_left = SETL("l", DUP(left));
+	RzILOpEffect *set_right = SETL("r", DUP(right));
+	RzILOpPure *cmp_gt = signed_cmp ? SGT(VARL("l"), VARL("r")) : UGT(VARL("l"), VARL("r"));
+	RzILOpPure *cmp_lt = signed_cmp ? SLT(VARL("l"), VARL("r")) : ULT(VARL("l"), VARL("r"));
+
+	RzILOpEffect *cond_geq = BRANCH(cmp_gt,
+		SETG(crX, APPEND(UN(3, 0b010), VARL("so_flag"))), // left > right
+		SETG(crX, APPEND(UN(3, 0b001), VARL("so_flag"))) // left == right
+	);
+	RzILOpEffect *cond_l = BRANCH(cmp_lt,
+		SETG(crX, APPEND(UN(3, 0b100), VARL("so_flag"))), // left < right
+		cond_geq);
+	return SEQ4(set_left, set_right, set_so, cond_l);
+}
+
+#include <rz_il/rz_il_opbuilder_end.h>

librz/analysis/il/analysis_il.c
@@ -286,6 +286,7 @@ RZ_API bool rz_analysis_il_vm_setup(RzAnalysis *analysis) {
 	rz_return_val_if_fail(analysis, false);
 	rz_analysis_il_vm_cleanup(analysis);
 	if (!analysis->cur || !analysis->cur->il_config) {
+		RZ_LOG_WARN("Could not set up VM. Analysis plugin or RZIL config was NULL.\n");
 		return false;
 	}
 	analysis->il_vm = rz_analysis_il_vm_new(analysis, analysis->reg);

librz/analysis/meson.build
@@ -161,6 +161,9 @@ rz_analysis_sources = [
   'arch/avr/avr_il.c',
   'arch/arm/arm_il32.c',
   'arch/arm/arm_il64.c',
+  'arch/ppc/ppc_il.c',
+  'arch/ppc/ppc_il_ops.c',
+  'arch/ppc/ppc_il_flag_ops.c',
   'arch/sh/sh_il.c',
   '../asm/arch/amd29k/amd29k.c',
   '../asm/arch/avr/disassembler.c',

commit e1a65044139443c43a14c8304d095d9d6cddae56

fix capstone include paths
capstone include paths were adjusted in
2b8104bc5e763ed841d6dbffacbeaf02e86b8421, and this wasn't picked up in
the "PPC uplifting" commit, leading to a build failure with system
capstone.

Also fix the path of the ppc.h include, which seems to mistakenly use
capstone's ppc.h when building with bundled capstone.

Fixes: b46e7bd4f2 ("PPC uplifting to RzIL (#2823)")
Signed-off-by: John Helmert III <ajak@gentoo.org>

librz/analysis/arch/ppc/ppc_il.h
@@ -9,7 +9,7 @@
 #include <rz_analysis.h>
 #include <rz_il.h>
 #include <rz_types.h>
-#include <capstone.h>
+#include <capstone/capstone.h>
 
 #define PPC_BYTE  8
 #define PPC_HWORD 16

librz/analysis/arch/ppc/ppc_il_ops.c
@@ -1,13 +1,13 @@
 // SPDX-FileCopyrightText: 2022 Rot127 <unisono@quyllur.org>
 // SPDX-License-Identifier: LGPL-3.0-only
 
-#include "ppc.h"
+#include "opcode/ppc.h"
 #include "ppc_il.h"
 #include "ppc_analysis.h"
 #include "rz_types_base.h"
 #include <rz_il/rz_il_opcodes.h>
 #include <rz_util/rz_assert.h>
-#include <capstone.h>
+#include <capstone/capstone.h>
 #include <rz_il/rz_il_opbuilder_begin.h>
 
 static RzILOpEffect *load_op(RZ_BORROW csh handle, RZ_BORROW cs_insn *insn, const cs_mode mode) {