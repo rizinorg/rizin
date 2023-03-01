04fe43d013a854c2744ec4f68a94c27ff2daea72
Anton Kochkov <anton.kochkov@gmail.com>
2023-03-01 21:53:04 +0800

    Fix C++ use for rz_* allocation functions

--- librz/include/rz_util/rz_alloc.h
+++ librz/include/rz_util/rz_alloc.h
@@ -5,9 +5,16 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 RZ_API RZ_OWN void *rz_mem_alloc(size_t sz);
 RZ_API void rz_mem_free(void *);
 RZ_API RZ_OWN void *rz_malloc_aligned(size_t size, size_t alignment);
 RZ_API void rz_free_aligned(void *p);
 
+#ifdef __cplusplus
+}
 #endif
+#endif //  RZ_ALLOC_H
