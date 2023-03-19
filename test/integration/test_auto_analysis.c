aef345a5c95c47eb1363ffd0b9419ba87680b931
brightprogrammer <bshock665@gmail.com>
2023-03-19 08:11:52 +0530

    Fix indentation problem

--- test/integration/test_auto_analysis.c
+++ test/integration/test_auto_analysis.c
@@ -12,7 +12,7 @@ bool test_auto_analysis() {
 	RzCore *core = rz_core_new();
 	mu_assert_notnull(core, "new RzCore instance");
 
-    // load test file
+	// load test file
 	ut64 loadaddr = 0;
 	const char *fpath = "bins/elf/ls";
 	rz_core_bin_load(core, fpath, loadaddr);
@@ -20,34 +20,34 @@ bool test_auto_analysis() {
 	// aa
 	rz_core_perform_auto_analysis(core, RZ_CORE_ANALYSIS_SIMPLE);
 	RzList *simple_fcn_list = rz_analysis_function_list(core->analysis);
-    mu_assert_notnull(simple_fcn_list, "simple function list");
-    ut32 simple_fcn_list_length = rz_list_length(simple_fcn_list);
+	mu_assert_notnull(simple_fcn_list, "simple function list");
+	ut32 simple_fcn_list_length = rz_list_length(simple_fcn_list);
 	eprintf("functions count = %d\n", simple_fcn_list_length);
 
-    // aaa
+	// aaa
 	rz_core_perform_auto_analysis(core, RZ_CORE_ANALYSIS_DEEP);
 	RzList *deep_fcn_list = rz_analysis_function_list(core->analysis);
-    mu_assert_notnull(deep_fcn_list, "simple function list");
-    ut32 deep_fcn_list_length = rz_list_length(deep_fcn_list);
+	mu_assert_notnull(deep_fcn_list, "simple function list");
+	ut32 deep_fcn_list_length = rz_list_length(deep_fcn_list);
 	eprintf("functions count = %d\n", deep_fcn_list_length);
 
-    // more function should be detected in deep analysis
-    // not true for all binaries but for most it must be true
-    mu_assert("deep analysis success", deep_fcn_list_length >= simple_fcn_list_length);
+	// more function should be detected in deep analysis
+	// not true for all binaries but for most it must be true
+	mu_assert("deep analysis success", deep_fcn_list_length >= simple_fcn_list_length);
 
-    // aaa
+	// aaa
 	rz_core_perform_auto_analysis(core, RZ_CORE_ANALYSIS_EXPERIMENTAL);
 	RzList *experimental_fcn_list = rz_analysis_function_list(core->analysis);
-    mu_assert_notnull(experimental_fcn_list, "simple function list");
-    ut32 experimental_fcn_list_length = rz_list_length(experimental_fcn_list);
+	mu_assert_notnull(experimental_fcn_list, "simple function list");
+	ut32 experimental_fcn_list_length = rz_list_length(experimental_fcn_list);
 	eprintf("functions count = %d\n", experimental_fcn_list_length);
 
-    // again more in experimental
-    mu_assert("deep analysis success", experimental_fcn_list_length >= deep_fcn_list_length);
+	// again more in experimental
+	mu_assert("deep analysis success", experimental_fcn_list_length >= deep_fcn_list_length);
 
-    // free core and end test
+	// free core and end test
 	rz_core_free(core);
-    mu_end;
+	mu_end;
 }
 
 int all_tests() {
