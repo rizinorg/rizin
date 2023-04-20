823f3d051d075e095368e8e9413fef820d1aec81
brightprogrammer <bshock665@gmail.com>
2023-04-20 22:06:28 +0530

    Add memory maps for different devices in PIC16F family

---
+++ librz/analysis/arch/pic/pic16f_memmaps/memmaps.h
@@ -0,0 +1,8 @@
+#ifndef PIC16F_MEMMAPS_H_
+#define PIC16F_MEMMAPS_H_
+
+#include "pic16f882.h"
+#include "pic16f883_4.h"
+#include "pic16f886_7.h"
+
+#endif // PIC16F_MEMMAPS_H_
--- librz/analysis/arch/pic/pic_il.h
+++ librz/analysis/arch/pic/pic_il.h
@@ -4,12 +4,19 @@
 #ifndef PIC_IL_H_
 #define PIC_IL_H_
 
-#include "../../../asm/arch/pic/pic_midrange.h"
-#include "../../../asm/arch/pic/pic_baseline.h"
+#include <rz_il.h>
 
-// midrange
-RZ_IPI RzAnalysisILConfig *rz_pic_midrange_il_vm_config(RZ_NONNULL RzAnalysis *analysis);
-RZ_IPI RzILOpEffect *rz_pic_midrange_il_op(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RZ_BORROW RzAnalysisOp *op, ut16 instr);
+// PIC16F family
+typedef enum pic16f_device_type_t {
+	PIC16F882,
+	PIC16F883,
+	PIC16F884,
+	PIC16F886,
+	PIC16F887
+} Pic16fDeviceType;
+
+RZ_IPI RzAnalysisILConfig *rz_pic16f_il_vm_config(RZ_NONNULL RzAnalysis *analysis, Pic16DeviceType device_type);
+RZ_IPI RzILOpEffect *rz_pic16f_il_op(RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RZ_BORROW RzAnalysisOp *op, Pic16deviceType device_type, ut16 instr);
 
 // baseline
 /* RZ_IPI RzAnalysisILConfig *rz_pic_baseline_il_vm_config(RZ_NONNULL RzAnalysis *analysis); */
