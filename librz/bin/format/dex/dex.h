30369b824258351860aa9e36ae79cfe516d1a343
wargio <wargio@libero.it>
2023-04-24 18:14:39 +0800

    Fix tests and plugins

--- librz/bin/format/dex/dex.h
+++ librz/bin/format/dex/dex.h
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2021 RizinOrg <info@rizin.re>
-// SPDX-FileCopyrightText: 2021 deroad <wargio@libero.it>
+// SPDX-FileCopyrightText: 2021-2023 RizinOrg <info@rizin.re>
+// SPDX-FileCopyrightText: 2021-2023 deroad <wargio@libero.it>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #ifndef RZ_DEX_H
