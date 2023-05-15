331cf64bf96108d458b28882f63261f1459ffbdb
billow <billow.fun@gmail.com>
2023-05-16 00:53:10 +0800

    Fix `ds_print_dwarf`

--- librz/core/disasm.c
+++ librz/core/disasm.c
@@ -3819,8 +3819,7 @@ static void ds_print_dwarf(RzDisasmState *ds) {
 			return;
 		}
 		ds_align_comment(ds);
-		theme_print_color(pal_comment);
-		rz_cons_printf("; %s", line);
+		theme_printf(pal_comment, "; %s", line);
 		free(ds->osl);
 		ds->osl = ds->sl;
 		ds->sl = NULL;
@@ -4729,7 +4728,7 @@ static void ds_print_esil_analysis(RzDisasmState *ds) {
 	RzCore *core = ds->core;
 	RzAnalysisEsil *esil = core->analysis->esil;
 	const char *pc;
-	int (*hook_mem_write)(RzAnalysisEsil *esil, ut64 addr, const ut8 *buf, int len) = NULL;
+	int (*hook_mem_write)(RzAnalysisEsil * esil, ut64 addr, const ut8 *buf, int len) = NULL;
 	int i, nargs;
 	ut64 at = rz_core_pava(core, ds->at);
 	RzConfigHold *hc = rz_config_hold_new(core->config);
