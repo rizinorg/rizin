75625a68dc1c787dbf0725f3480ab62bc73c9254
wargio <wargio@libero.it>
2023-06-03 19:42:49 +0800

    [SPDX] librz/include/rz_il.h

--- librz/include/rz_il.h
+++ librz/include/rz_il.h
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2021-2023 RizinOrg <info@rizin.re>
+// SPDX-FileCopyrightText: 2021 Heersin <teablearcher@gmail.com>
+// SPDX-FileCopyrightText: 2021 Florian Märkl <info@florianmaerkl.de>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #ifndef RZ_IL_H
 #define RZ_IL_H
 
