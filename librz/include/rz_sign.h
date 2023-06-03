fa5ba55636de2ed818f7bae7b77ec8abeb26b161
wargio <wargio@libero.it>
2023-06-03 19:40:29 +0800

    [SPDX] librz/include/rz_sign.h

--- librz/include/rz_sign.h
+++ librz/include/rz_sign.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 RizinOrg <info@rizin.re>
+// SPDX-FileCopyrightText: 2022 deroad <wargio@libero.it>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #ifndef RZ_SIGN_H
 #define RZ_SIGN_H
 
