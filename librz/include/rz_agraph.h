08d2cf8047feaa873debc2038b0a580a312072e2
wargio <wargio@libero.it>
2023-06-03 17:59:07 +0800

    [SPDX] librz/include/rz_agraph.h

--- librz/include/rz_agraph.h
+++ librz/include/rz_agraph.h
@@ -1,3 +1,10 @@
+// SPDX-FileCopyrightText: 2022 RizinOrg <info@rizin.re>
+// SPDX-FileCopyrightText: 2022 wargio <wargio@libero.it>
+// SPDX-FileCopyrightText: 2022 wingdeans <wingdeans@protonmail.com>
+// SPDX-FileCopyrightText: 2020-2021 Anton Kochkov <anton.kochkov@gmail.com>
+// SPDX-FileCopyrightText: 2019-2020 pancake <pancake@nopcode.org>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #ifndef RZ_AGRAPH_H
 #define RZ_AGRAPH_H
 
