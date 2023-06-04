8071063e0caa469fecb89d22fd22d604cff39f6e
wargio <wargio@libero.it>
2023-06-04 13:44:48 +0800

    [SPDX] librz/include/rz_util/rz_uleb128.h

--- librz/include/rz_util/rz_uleb128.h
+++ librz/include/rz_util/rz_uleb128.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2020 Riccardo Schirone <sirmy15@gmail.com>
+// SPDX-FileCopyrightText: 2016 Maijin <maijin21@gmail.com>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #ifndef RZ_ULEB128_H
 #define RZ_ULEB128_H
 
