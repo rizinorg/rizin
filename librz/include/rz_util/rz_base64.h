fad011ac76277079c37b4900e3ba5b4b6871d893
wargio <wargio@libero.it>
2023-06-04 13:31:42 +0800

    [SPDX] librz/include/rz_util/rz_base64.h

--- librz/include/rz_util/rz_base64.h
+++ librz/include/rz_util/rz_base64.h
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2020 Riccardo Schirone <sirmy15@gmail.com>
+// SPDX-FileCopyrightText: 2017 pancake <pancake@nopcode.org>
+// SPDX-FileCopyrightText: 2016 Maijin <maijin21@gmail.com>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #ifndef RZ_BASE64_H
 #define RZ_BASE64_H
 
