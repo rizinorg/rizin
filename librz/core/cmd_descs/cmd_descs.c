3ac8a36c7bf40e04e2ebf0bcc6c642d7892d9814
Anton Kochkov <anton.kochkov@gmail.com>
2023-06-09 17:59:04 +0800

    Remove tiny graphs (#3567)

--- librz/core/cmd_descs/cmd_descs.c
+++ librz/core/cmd_descs/cmd_descs.c
@@ -3965,7 +3965,6 @@ static const RzCmdDescDetailEntry ag_Formats_detail_entries[] = {
 	{ .text = "json", .arg_str = NULL, .comment = "json" },
 	{ .text = "json_disasm", .arg_str = NULL, .comment = "json formatted disassembly" },
 	{ .text = "sdb", .arg_str = NULL, .comment = "SDB key-value" },
-	{ .text = "tiny", .arg_str = NULL, .comment = "Tiny ascii art" },
 	{ .text = "interactive", .arg_str = NULL, .comment = "Interactive ascii art" },
 	{ 0 },
 };
