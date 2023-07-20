c0850845f11f8ee95ce2b54b046b77b86f97cc1e
Heersin <teablearcher@gmail.com>
2023-07-20 11:30:17 +0800

    RzIL: asm tests for ARM32 VFP and NEON (#3662)

--- librz/analysis/arch/arm/arm_accessors32.h
+++ librz/analysis/arch/arm/arm_accessors32.h
@@ -11,15 +11,17 @@
 
 #define REGID(x)   insn->detail->arm.operands[x].reg
 #define IMM(x)     (ut32)(insn->detail->arm.operands[x].imm)
+#define FPIMM(x)   (double)(insn->detail->arm.operands[x].fp)
 #define INSOP(x)   insn->detail->arm.operands[x]
 #define REGBASE(x) insn->detail->arm.operands[x].mem.base
 // s/index/base|reg/
 #define HASMEMINDEX(x)   (insn->detail->arm.operands[x].mem.index != ARM_REG_INVALID)
 #define ISMEMINDEXSUB(x) insn->detail->arm.operands[x].subtracted
 #define MEMDISP(x)       insn->detail->arm.operands[x].mem.disp
-#define ISIMM(x)         (insn->detail->arm.operands[x].type == ARM_OP_IMM)
+#define ISIMM(x)         (insn->detail->arm.operands[x].type == ARM_OP_IMM || insn->detail->arm.operands[x].type == ARM_OP_FP)
 #define ISREG(x)         (insn->detail->arm.operands[x].type == ARM_OP_REG)
 #define ISMEM(x)         (insn->detail->arm.operands[x].type == ARM_OP_MEM)
+#define ISFPIMM(x)       (insn->detail->arm.operands[x].type == ARM_OP_FP)
 
 #if CS_API_MAJOR > 3
 #define LSHIFT(x)  insn->detail->arm.operands[x].mem.lshift
