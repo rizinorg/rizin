a3441f3ddb6f1a964dff4376d335640a30d13713
Giovanni <561184+wargio@users.noreply.github.com>
2023-08-18 23:25:41 +0800

    Unify crypto plugin api to match all the other plugins. (#3773)

---
+++ librz/core/ccrypto.c
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: 2023 deroad <wargio@libero.it>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include <rz_core.h>
+
+static RzCmdStatus core_crypto_plugin_print(RzCmdStateOutput *state, const RzCryptoPlugin *plugin) {
+	PJ *pj = state->d.pj;
+	switch (state->mode) {
+	case RZ_OUTPUT_MODE_QUIET: {
+		rz_cons_printf("%s ", plugin->name);
+		break;
+	}
+	case RZ_OUTPUT_MODE_JSON: {
+		pj_o(pj);
+		pj_ks(pj, "name", plugin->name);
+		pj_ks(pj, "license", plugin->license);
+		pj_ks(pj, "author", plugin->author);
+		pj_end(pj);
+		break;
+	}
+	case RZ_OUTPUT_MODE_STANDARD: {
+		rz_cons_printf("%-14s %-10s %s\n", plugin->name, plugin->license, plugin->author);
+
+		break;
+	}
+	default: {
+		rz_warn_if_reached();
+		return RZ_CMD_STATUS_NONEXISTINGCMD;
+	}
+	}
+	return RZ_CMD_STATUS_OK;
+}
+
+RZ_API RzCmdStatus rz_core_crypto_plugins_print(RzCrypto *cry, RzCmdStateOutput *state) {
+	rz_return_val_if_fail(cry, RZ_CMD_STATUS_ERROR);
+
+	const RzCryptoPlugin *plugin = NULL;
+	RzCmdStatus status;
+	RzListIter *it;
+	rz_cmd_state_output_array_start(state);
+	if (state->mode == RZ_OUTPUT_MODE_STANDARD) {
+		rz_cons_println("algorithm      license    author");
+	}
+	rz_list_foreach (cry->plugins, it, plugin) {
+		status = core_crypto_plugin_print(state, plugin);
+		if (status != RZ_CMD_STATUS_OK) {
+			return status;
+		}
+	}
+	if (state->mode == RZ_OUTPUT_MODE_QUIET) {
+		rz_cons_newline();
+	}
+	rz_cmd_state_output_array_end(state);
+	return RZ_CMD_STATUS_OK;
+}
