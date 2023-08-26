46856878e6caba06feb8f19b25f524eb22644e1a
Rot127 <unisono@quyllur.org>
2023-08-26 12:40:37 -0500

    Replace ARM64 with version sensitive macros.

--- librz/analysis/arch/arm/arm_accessors64.h
+++ librz/analysis/arch/arm/arm_accessors64.h
@@ -9,21 +9,21 @@
 
 #include <capstone/capstone.h>
 
-#define IMM64(x)   (ut64)(insn->detail->arm64.operands[x].imm)
-#define INSOP64(x) insn->detail->arm64.operands[x]
+#define IMM64(x)   (ut64)(insn->detail->CS_aarch64().operands[x].imm)
+#define INSOP64(x) insn->detail->CS_aarch64().operands[x]
 
-#define REGID64(x)   insn->detail->arm64.operands[x].reg
-#define REGBASE64(x) insn->detail->arm64.operands[x].mem.base
+#define REGID64(x)   insn->detail->CS_aarch64().operands[x].reg
+#define REGBASE64(x) insn->detail->CS_aarch64().operands[x].mem.base
 // s/index/base|reg/
-#define HASMEMINDEX64(x) (insn->detail->arm64.operands[x].mem.index != ARM64_REG_INVALID)
-#define MEMDISP64(x)     (ut64) insn->detail->arm64.operands[x].mem.disp
-#define ISIMM64(x)       (insn->detail->arm64.operands[x].type == ARM64_OP_IMM)
-#define ISREG64(x)       (insn->detail->arm64.operands[x].type == ARM64_OP_REG)
-#define ISMEM64(x)       (insn->detail->arm64.operands[x].type == ARM64_OP_MEM)
+#define HASMEMINDEX64(x) (insn->detail->CS_aarch64().operands[x].mem.index != CS_AARCH64(_REG_INVALID))
+#define MEMDISP64(x)     (ut64) insn->detail->CS_aarch64().operands[x].mem.disp
+#define ISIMM64(x)       (insn->detail->CS_aarch64().operands[x].type == CS_AARCH64(_OP_IMM))
+#define ISREG64(x)       (insn->detail->CS_aarch64().operands[x].type == CS_AARCH64(_OP_REG))
+#define ISMEM64(x)       (insn->detail->CS_aarch64().operands[x].type == CS_AARCH64(_OP_MEM))
 
-#define LSHIFT2_64(x) insn->detail->arm64.operands[x].shift.value
-#define OPCOUNT64()   insn->detail->arm64.op_count
+#define LSHIFT2_64(x) insn->detail->CS_aarch64().operands[x].shift.value
+#define OPCOUNT64()   insn->detail->CS_aarch64().op_count
 
-#define ISWRITEBACK64() (insn->detail->arm64.writeback == true)
+#define ISWRITEBACK64() (insn->detail->CS_aarch64().writeback == true)
 #define ISPREINDEX64()  (((OPCOUNT64() == 2) && (ISMEM64(1)) && (ISWRITEBACK64())) || ((OPCOUNT64() == 3) && (ISMEM64(2)) && (ISWRITEBACK64())))
 #define ISPOSTINDEX64() (((OPCOUNT64() == 3) && (ISIMM64(2)) && (ISWRITEBACK64())) || ((OPCOUNT64() == 4) && (ISIMM64(3)) && (ISWRITEBACK64())))
