7499106ff082ee21f403d751a35b9aa480f69a24
Rot127 <unisono@quyllur.org>
2023-09-07 18:56:52 -0500

    Update Hexagon plugin with generated code.
    Add hexagon il code to rouce files.
    
    Update generated files
    
    Add undocumented ops file.
    
    Fix build: Add missing effects.
    
    Update generated files.
    
    Degrade/remove very common log messages.
    
    Rename: p -> pkt and k -> slot
    
    Reverse renaming of k, since it is indeed the location in the packet and not the slot.
    
    Add slot to HexInsn (needed for RzIL)
    
    Uppdate generated files.
    
    Disassemble a full packet if it is in the buffer.
    
    Allow to get the packet IL ops if they are ready.
    
    Only copy disassembly result if requested.
    
    Fix typo
    
    Fix packet recognition of first and jump target packets.
    
    Remove check for theoretically unreachable effects.
    
    The assumptions made in this line doesn't need to be true.
    For Hexagon every write to a register first happens to a temporary
    register file. Once the whole packet is checked and is valid,
    the operations are performed.
    The last step is to sync the the temporary register file with the default one.
    
    Update generated files (16 passed test_*)
    
    Restructure generated rzil code
    
    Update with optimization by resolving compares and tenaries
    
    All test_* cases succeed
    
    Update source files with new sub-routines
    
    Remove pseudo instructions
    
    Update generated files (all tests pass).
    
    Update source after rebase
    
    Fix: Check rw overlap only for x registers.
    
    Add RzIL tests for hexagon.
    
    Update generated files
    
    Remove unit test for further effect after control effect.
    
    Update generated files.
    
    Fix unit test.
    
    After instructions with invalid decoded registers are decoded as invalid,
    the unit test failed.
    
    Fix syntactical changes in tests.
    
    Use upper case register names in CC
    
    Fix many many mem leaks.
    
    Only generate IL code if requested.
    
    Enable load_align test.
    
    Fix syntax: Remove trailing ; for invalid decodes with parse_bits == 0.
    
    Add angle brakcets type annotations.
    
    Remove many ressource leaks.
    
    Note that not RzAsm and RzAnalysis for Hexagon cannot be freed,
    because there is currrently no reasonable way to make the plugin thread safe.
    
    Add bitmaps
    
    Use RzBitmap instead of manually setting bits.
    
    Fix type annotations.
    
    Triple test timeout for ASAN tests.
    
    Fix type annotation.
    
    Use PVector instead of Vector to save the HexILOp pointer.
    
    Ignore dwarf error for now.
    
    Apparently, mixing allocated and static memory in a vector leaks the allocated memory. rip.
    
    Replace bitmaps with bitvectors.
    
    Ensure packet il stats reset.
    
    Remove warning from test

--- librz/include/rz_util/rz_bits.h
+++ librz/include/rz_util/rz_bits.h
@@ -8,6 +8,104 @@
 extern "C" {
 #endif
 
+#include <rz_util/rz_assert.h>
+#include <rz_types_base.h>
+
+typedef ut64 RzBitmap64;
+typedef ut32 RzBitmap32;
+
+/**
+ * \brief Init the 64bit bitmap \p map.
+ *
+ * \param map The bitmap to edit.
+ */
+static inline void rz_bits_map_init_64(RZ_OUT RzBitmap64 *map) {
+	rz_return_if_fail(map);
+	*map = 0;
+}
+
+/**
+ * \brief Init the 32bit bitmap \p map.
+ *
+ * \param map The bitmap to edit.
+ */
+static inline void rz_bits_map_init_32(RZ_OUT RzBitmap32 *map) {
+	rz_return_if_fail(map);
+	*map = 0;
+}
+
+/**
+ * \brief Set the bit at \p pos in the 64bit bitmap \p map.
+ *
+ * \param map The bitmap to edit.
+ * \param pos Bit index to set.
+ */
+static inline void rz_bits_map_set_64(RZ_OUT RzBitmap64 *map, ut32 pos) {
+	rz_return_if_fail(map && pos < 64);
+	*map = *map | ((1ULL) << pos);
+}
+
+/**
+ * \brief Set the bit at \p pos in the 32bit bitmap \p map.
+ *
+ * \param map The bitmap to edit.
+ * \param pos Bit index to set.
+ */
+static inline void rz_bits_map_set_32(RZ_OUT RzBitmap32 *map, ut32 pos) {
+	rz_return_if_fail(map && pos < 32);
+	*map = *map | ((1UL) << pos);
+}
+
+/**
+ * \brief Unset the bit at \p pos in the 64bit bitmap \p map.
+ *
+ * \param map The bitmap to edit.
+ * \param pos Bit index to unset.
+ */
+static inline void rz_bits_map_unset_64(RZ_OUT RzBitmap64 *map, ut32 pos) {
+	rz_return_if_fail(map && pos < 64);
+	*map = *map & ~((ut64)(1ULL) << pos);
+}
+
+/**
+ * \brief Unset the bit at \p pos in the 32bit bitmap \p map.
+ *
+ * \param map The bitmap to edit.
+ * \param pos Bit index to unset.
+ */
+static inline void rz_bits_map_unset_32(RZ_OUT RzBitmap32 *map, ut32 pos) {
+	rz_return_if_fail(map && pos < 32);
+	*map = *map & ~((ut32)(1UL) << pos);
+}
+
+/**
+ * \brief Get the bit at \p pos in the 64bit bitmap \p map.
+ *
+ * \param map The bitmap to check.
+ * \param pos Bit index to check.
+ *
+ * \return true If bit is set.
+ * \return false If bit is unset.
+ */
+static inline bool rz_bits_map_get_64(const RzBitmap64 *map, ut32 pos) {
+	rz_return_val_if_fail(map && pos < 64, false);
+	return (*map & ((1ULL) << pos)) != 0;
+}
+
+/**
+ * \brief Get the bit at \p pos in the 32bit bitmap \p map.
+ *
+ * \param map The bitmap to check.
+ * \param pos Bit index to check.
+ *
+ * \return true If bit is set.
+ * \return false If bit is unset.
+ */
+static inline bool rz_bits_map_get_32(const RzBitmap32 *map, ut32 pos) {
+	rz_return_val_if_fail(map && pos < 32, false);
+	return (*map & ((1UL) << pos)) != 0;
+}
+
 /**
  * \brief Get the number of leading zeros of a 64-bit integer in binary representation.
  * \param x the 64-bit integer
