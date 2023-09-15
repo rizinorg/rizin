a040c935bae8c4deb9befa09159a64763d48425c
0xsh4dy <rakshitawasthi17@gmail.com>
2023-09-15 10:39:00 +0530

    Add functions for sm4-ECB mode

--- librz/crypto/p/crypto_sm4.h
+++ librz/crypto/p/crypto_sm4.h
@@ -1,9 +1,12 @@
+// SPDX-FileCopyrightText: 2023 0xSh4dy <rakshitawasthi17@gmail.com>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #ifndef CRYPTO_SM4_ALGO_H
 #define CRYPTO_SM4_ALGO_H
 #include<rz_util.h>
 
 #define SM4_KEY_SIZE 16
-struct sm4_state{
+typedef struct sm4_ctx{
     ut8 key[SM4_KEY_SIZE];
-};
+} sm4_state;
 #endif
--- librz/crypto/p/crypto_sm4_ecb.c
+++ librz/crypto/p/crypto_sm4_ecb.c
@@ -1,30 +1,41 @@
+// SPDX-FileCopyrightText: 2023 0xSh4dy <rakshitawasthi17@gmail.com>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #include <rz_lib.h>
 #include <rz_crypto.h>
 #include "crypto_sm4.h"
 
-static void sm4_ecb_encrypt(struct sm4_state* ctx,ut8* in,ut8* out){
-
+static void sm4_setkey_enc(sm4_state *s, const ut8 *key) {
 }
 
-static void sm4_ecb_decrypt(struct sm4_state* ctx,ut8* in,ut8* out){
-
+static void sm4_setkey_dec(sm4_state *s, const ut8 *key) {
 }
 
 static bool sm4_ecb_set_key(RzCrypto *cry, const ut8 *key, int keylen, int mode, int direction) {
+	rz_return_val_if_fail(cry->user && key, false);
+	sm4_state *s = (sm4_state *)cry->user;
+
+	if (keylen != SM4_KEY_SIZE) {
+		return false;
+	}
+	if (direction == RZ_CRYPTO_DIR_ENCRYPT) {
+		sm4_setkey_enc(s, key);
+	} else {
+		sm4_setkey_dec(s, key);
+	}
 	return true;
 }
 
 static int sm4_ecb_get_key_size(RzCrypto *cry) {
-    return 128;
+	return SM4_KEY_SIZE;
 }
 
 static bool sm4_use(const char *algo) {
 	return !strcmp(algo, "sm4-ecb");
 }
 
-
 static bool update(RzCrypto *cry, const ut8 *buf, int len) {
-    return true;
+	return true;
 }
 
 static bool final(RzCrypto *cry, const ut8 *buf, int len) {
@@ -32,14 +43,14 @@ static bool final(RzCrypto *cry, const ut8 *buf, int len) {
 }
 
 static bool sm4_ecb_init(RzCrypto *cry) {
-	rz_return_val_if_fail(cry,false);
+	rz_return_val_if_fail(cry, false);
 
-	cry->user = RZ_NEW0(struct sm4_state);
-    return cry->user!=NULL;
+	cry->user = RZ_NEW0(sm4_state);
+	return cry->user != NULL;
 }
 
 static bool sm4_ecb_fini(RzCrypto *cry) {
-	rz_return_val_if_fail(cry,false);
+	rz_return_val_if_fail(cry, false);
 
 	free(cry->user);
 	return true;
