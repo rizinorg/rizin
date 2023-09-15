60105c7f397eafac49c0dd599d95eb624f6bb6e5
0xsh4dy <rakshitawasthi17@gmail.com>
2023-09-15 11:29:46 +0530

    Fix formatting issues

--- librz/crypto/p/crypto_sm4.h
+++ librz/crypto/p/crypto_sm4.h
@@ -3,10 +3,10 @@
 
 #ifndef CRYPTO_SM4_ALGO_H
 #define CRYPTO_SM4_ALGO_H
-#include<rz_util.h>
+#include <rz_util.h>
 
 #define SM4_KEY_SIZE 16
-typedef struct sm4_ctx{
+typedef struct sm4_ctx {
     ut8 key[SM4_KEY_SIZE];
 } sm4_state;
-#endif
+#endi
