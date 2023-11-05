865f2aeb038df9c586c5e60e33ac33ab4c7445eb
Peiwei Hu <jlu.hpw@foxmail.com>
2023-11-06 01:04:17 +0800

    fixup! remove redundant function info while disassembling

--- librz/core/disasm.c
+++ librz/core/disasm.c
@@ -2182,7 +2182,13 @@ static bool is_sym_dbg_equal(const char *a, const char *b) {
 	if (!rz_str_startswith(b, "sym.") && !rz_str_startswith(b, "dbg.")) {
 		return false;
 	}
-	return !strcmp(a + strlen("sym."), b + strlen("sym."));
+	const size_t sym_n = strlen("sym.");
+	return !strcmp(a + sym_n, b + sym_n);
+}
+
+static inline bool is_flag_overlapped(RzFlagItem *flag, RzAnalysisFunction *f) {
+	const bool name_overlapped = !strcmp(flag->name, f->name) || is_sym_dbg_equal(flag->name, f->name);
+	return f->addr == flag->offset && name_overlapped;
 }
 
 #define printPre (outline || !*comma)
@@ -2209,7 +2215,7 @@ static void ds_show_flags(RzDisasmState *ds, bool overlapped) {
 	RzAnalysisBlock *switch_block = NULL;
 	const char *switch_enum_name = NULL;
 	rz_list_foreach (uniqlist, iter, flag) {
-		if (!overlapped && f && f->addr == flag->offset && (!strcmp(flag->name, f->name) || is_sym_dbg_equal(flag->name, f->name))) {
+		if (!overlapped && f && is_flag_overlapped(flag, f)) {
 			// do not show non-overlapped flags that have the same name as the function
 			continue;
 		}
