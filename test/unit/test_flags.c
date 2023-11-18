b9851b7c24ab5d3cc2d0192d4b8f8859cad2999c
Florian Märkl <info@florianmaerkl.de>
2023-11-18 23:15:50 +0100

    Make rz_flag_set_next() try the addr before incremental numbers as suffix (#3986)
    In binaries with a large number of flags of the same name, like in macho
    with many relocs to the same symbol, using an incremental number as
    suffix to generate unique flag names leads to quadratic runtime. We can
    avoid this by first trying to append the address, which as a nice side
    effect also results in more meaningful names.

--- test/unit/test_flags.c
+++ test/unit/test_flags.c
@@ -131,10 +131,49 @@ bool test_rz_flag_get_at() {
 	mu_end;
 }
 
+bool test_rz_flag_set_next() {
+	RzFlag *flag = rz_flag_new();
+
+	RzFlagItem *fi = rz_flag_set_next(flag, "catastasis", 0x100, 0);
+	mu_assert_notnull(fi, "set flag");
+	mu_assert_streq(fi->name, "catastasis", "flag_name");
+	RzFlagItem *fi2 = rz_flag_set_next(flag, "catastasis", 0xfa1afe1, 0);
+	mu_assert_notnull(fi2, "set flag");
+	mu_assert_streq(fi2->name, "catastasis.fa1afe1", "flag_name");
+	fi = rz_flag_set_next(flag, "catastasis", (ut64)-1, 0);
+	mu_assert_notnull(fi, "set flag");
+	mu_assert_streq(fi->name, "catastasis.ffffffffffffffff", "flag_name");
+	fi = rz_flag_set_next(flag, "catastasis", 0xfa1afe1, 0);
+	mu_assert_ptreq(fi, fi2, "set matching");
+	fi = rz_flag_set_next(flag, "catastasis", 0xfa1afe1, 10);
+	mu_assert_notnull(fi, "set flag");
+	mu_assert_streq(fi->name, "catastasis.0", "flag_name");
+	fi = rz_flag_set_next(flag, "catastasis", 0xfa1afe1, 8);
+	mu_assert_notnull(fi, "set flag");
+	mu_assert_streq(fi->name, "catastasis.1", "flag_name");
+	fi = rz_flag_set_next(flag, "catastasis.fa1afe1", 0x200, 0);
+	mu_assert_notnull(fi, "set flag");
+	mu_assert_streq(fi->name, "catastasis.fa1afe1.200", "flag_name");
+
+	RzList *all = rz_flag_all_list(flag, false);
+	mu_assert_eq(rz_list_length(all), 6, "all flags count");
+	mu_assert_streq(((RzFlagItem *)rz_list_get_n(all, 0))->name, "catastasis", "flag name");
+	mu_assert_streq(((RzFlagItem *)rz_list_get_n(all, 1))->name, "catastasis.fa1afe1.200", "flag name");
+	mu_assert_streq(((RzFlagItem *)rz_list_get_n(all, 2))->name, "catastasis.fa1afe1", "flag name");
+	mu_assert_streq(((RzFlagItem *)rz_list_get_n(all, 3))->name, "catastasis.0", "flag name");
+	mu_assert_streq(((RzFlagItem *)rz_list_get_n(all, 4))->name, "catastasis.1", "flag name");
+	mu_assert_streq(((RzFlagItem *)rz_list_get_n(all, 5))->name, "catastasis.ffffffffffffffff", "flag name");
+	rz_list_free(all);
+
+	rz_flag_free(flag);
+	mu_end;
+}
+
 int all_tests(void) {
 	mu_run_test(test_rz_flag_get_set);
 	mu_run_test(test_rz_flag_by_spaces);
 	mu_run_test(test_rz_flag_get_at);
+	mu_run_test(test_rz_flag_set_next);
 	return tests_passed != tests_run;
 }
 
