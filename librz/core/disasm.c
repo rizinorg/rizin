090c0daabb5832865bb31bfc4471cbbbd1933005
Peiwei Hu <jlu.hpw@foxmail.com>
2023-12-20 11:09:22 +0800

    fixup! fixup! Remove the dependency of core->block in pi/pI

--- librz/core/disasm.c
+++ librz/core/disasm.c
@@ -5661,7 +5661,7 @@ RZ_API int rz_core_print_disasm_instructions_with_buf(RzCore *core, ut64 address
 			RZ_LOG_ERROR("Fail to alloc memory.");
 			return 0;
 		}
-		if (!rz_core_read_at(core, address, buf, RZ_ABS(nb_bytes) + 1)) {
+		if (rz_io_nread_at(core->io, address, buf, RZ_ABS(nb_bytes) + 1) == -1) {
 			RZ_LOG_ERROR("Fail to read from 0x%" PFMT64x ".", address);
 			return 0;
 		}
