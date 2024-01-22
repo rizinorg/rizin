ec36cdbcb37a61fdacab4ddc0f59a1522d34d507
Anton Kochkov <anton.kochkov@gmail.com>
2024-01-22 20:34:42 +0800

    Remove some more of `sdb_fmt()` calls

--- librz/core/cagraph.c
+++ librz/core/cagraph.c
@@ -243,7 +243,9 @@ RZ_IPI bool rz_core_agraph_add_shortcut(RzCore *core, RzAGraph *g, RzANode *an, 
 	if (!shortcut) {
 		return false;
 	}
-	sdb_set(g->db, sdb_fmt("agraph.nodes.%s.shortcut", title), shortcut, 0);
+	char *key = rz_str_newf("agraph.nodes.%s.shortcut", title);
+	sdb_set(g->db, key, shortcut, 0);
+	free(key);
 	// title + "[o{shortcut}]", so w + 3 ?
 	an->shortcut_w = strlen(shortcut) + 3;
 	free(shortcut);
