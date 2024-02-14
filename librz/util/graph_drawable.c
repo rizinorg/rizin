be106ba663fa4d5c5fc426f34c74feba0fe27134
Rot127 <unisono@quyllur.org>
2024-02-13 23:16:15 -0500

    Add meta information of iCFG and CFG nodes to dot graph labels.

--- librz/util/graph_drawable.c
+++ librz/util/graph_drawable.c
@@ -150,33 +150,54 @@ RZ_API RZ_OWN char *rz_graph_drawable_to_dot(RZ_NONNULL RzGraph /*<RzGraphNodeIn
 	rz_list_foreach (nodes, it, node) {
 		RzGraphNodeInfo *print_node = (RzGraphNodeInfo *)node->data;
 		char *url;
-		char *label;
+		RzStrBuf *label = rz_strbuf_new("");
 
 		switch (print_node->type) {
 		default:
 			RZ_LOG_ERROR("Unhandled node type. Graph node either doesn't support dot graph printing or it isn't implemented.\n");
+			rz_strbuf_free(label);
 			return NULL;
 		case RZ_GRAPH_NODE_TYPE_CFG:
-			label = rz_str_newf("0x%" PFMT64x, print_node->cfg.address);
-			url = label;
+			rz_strbuf_appendf(label, "0x%" PFMT64x, print_node->cfg.address);
+			if (print_node->subtype & RZ_GRAPH_NODE_SUBTYPE_CFG_ENTRY) {
+				rz_strbuf_append(label, " (entry)");
+			}
+			if (print_node->subtype & RZ_GRAPH_NODE_SUBTYPE_CFG_CALL) {
+				rz_strbuf_append(label, " (call)");
+			}
+			if (print_node->subtype & RZ_GRAPH_NODE_SUBTYPE_CFG_RETURN) {
+				rz_strbuf_append(label, " (ret)");
+			}
+			if (print_node->subtype & RZ_GRAPH_NODE_SUBTYPE_CFG_COND) {
+				rz_strbuf_append(label, " (cond)");
+			}
+			if (print_node->subtype & RZ_GRAPH_NODE_SUBTYPE_CFG_EXIT) {
+				rz_strbuf_append(label, " (exit)");
+			}
+			url = rz_strbuf_get(label);
 			break;
 		case RZ_GRAPH_NODE_TYPE_ICFG:
-			label = rz_str_newf("0x%" PFMT64x, print_node->icfg.address);
-			url = label;
+			rz_strbuf_appendf(label, "0x%" PFMT64x, print_node->icfg.address);
+			if (print_node->subtype == RZ_GRAPH_NODE_SUBTYPE_ICFG_MALLOC) {
+				rz_strbuf_append(label, " (malloc)");
+			}
+			url = rz_strbuf_get(label);
 			break;
 		case RZ_GRAPH_NODE_TYPE_DEFAULT:
 			url = print_node->def.title;
 			if (print_node->def.body && print_node->def.body[0]) {
 				rz_str_replace_ch(print_node->def.body, '\"', '\'', true);
-				label = rz_str_newf("%s\\n%s", print_node->def.title, print_node->def.body);
+				rz_strbuf_appendf(label, "%s\\n%s", print_node->def.title, print_node->def.body);
 			} else {
-				label = rz_str_dup(print_node->def.title);
+				rz_strbuf_append(label, print_node->def.title);
 			}
 		}
 
 		rz_strbuf_appendf(&buf, "%d [URL=\"%s\", color=\"lightgray\", label=\"%s\"]\n",
-			node->idx, url, label);
-		free(label);
+			node->idx, url, rz_strbuf_get(label));
+		rz_strbuf_free(label);
+		// url sometimes is set to label above and shouldn't be used after label was freed.
+		url = NULL;
 		rz_list_foreach (node->out_nodes, itt, target) {
 			rz_strbuf_appendf(&buf, "%d -> %d\n", node->idx, target->idx);
 		}
