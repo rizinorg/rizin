f24036a3b7629d66114149fe428e579ded9ae709
wargio <wargio@libero.it>
2024-02-15 15:44:46 +0800

    CID 435760: Out-of-bounds read

--- librz/asm/arch/v850/v850_disas.c
+++ librz/asm/arch/v850/v850_disas.c
@@ -761,7 +761,7 @@ static bool decode_formatIX(V850_Inst *inst) {
 	case V850_TST1:
 	case V850_SET1: OPERANDS("%s, [%s]", R2, R1); break;
 	case V850_SASF:
-	case V850_SETF: OPERANDS("%s, %s", conds[get_reg1(inst)], R2); break;
+	case V850_SETF: OPERANDS("%s, %s", conds[get_cond(inst)], R2); break;
 	case V850_BINS: OPERANDS("%s, %d, %d, %s", R1, bins_pos(inst), bins_width(inst), R2); break;
 	case V850_SCH0L:
 	case V850_SCH0R:
