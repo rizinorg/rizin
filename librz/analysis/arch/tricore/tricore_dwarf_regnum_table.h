e690ea37deb627e8b3c0761e2df8d48f93621358
Anton Kochkov <anton.kochkov@gmail.com>
2024-02-21 23:25:16 +0800

    analysis/dwarf: extract register profiles in headers (#4281)

---
+++ librz/analysis/arch/tricore/tricore_dwarf_regnum_table.h
@@ -0,0 +1,62 @@
+// SPDX-FileCopyrightText: 2012-2020 houndthe <cgkajm@gmail.com>
+// SPDX-FileCopyrightText: 2024 Billow <billow.fun@gmail.com>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+/// 4.5.1 DWARF Register Numbers https://www.infineon.com/dgdl/Infineon-TC2xx_EABI-UM-v02_09-EN.pdf?fileId=5546d46269bda8df0169ca1bfc7d24ab
+static const char *map_dwarf_reg_to_tricore_reg(ut32 reg_num) {
+	switch (reg_num) {
+	case 0: return "d0";
+	case 1: return "d1";
+	case 2: return "d2";
+	case 3: return "d3";
+	case 4: return "d4";
+	case 5: return "d5";
+	case 6: return "d6";
+	case 7: return "d7";
+	case 8: return "d8";
+	case 9: return "d9";
+	case 10: return "d10";
+	case 11: return "d11";
+	case 12: return "d12";
+	case 13: return "d13";
+	case 14: return "d14";
+	case 15: return "d15";
+	case 16: return "a0";
+	case 17: return "a1";
+	case 18: return "a2";
+	case 19: return "a3";
+	case 20: return "a4";
+	case 21: return "a5";
+	case 22: return "a6";
+	case 23: return "a7";
+	case 24: return "a8";
+	case 25: return "a9";
+	case 26: return "a10";
+	case 27: return "a11";
+	case 28: return "a12";
+	case 29: return "a13";
+	case 30: return "a14";
+	case 31: return "a15";
+	case 32: return "e0";
+	case 33: return "e2";
+	case 34: return "e4";
+	case 35: return "e6";
+	case 36: return "e8";
+	case 37: return "e10";
+	case 38: return "e12";
+	case 39: return "e14";
+	case 40: return "psw";
+	case 41: return "pcxi";
+	case 42: return "pc";
+	case 43: return "pcx";
+	case 44: return "lcx";
+	case 45: return "isp";
+	case 46: return "icr";
+	case 47: return "pipn";
+	case 48: return "biv";
+	case 49: return "btv";
+	default:
+		rz_warn_if_reached();
+		return "unsupported_reg";
+	}
+}
