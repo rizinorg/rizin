026e1f4da6b10eea6e292fcac8f86587bcf9a09d
Riccardo Schirone <sirmy15@gmail.com>
2024-02-27 10:11:19 +0100

    core: remove plugin from list if init fails

--- librz/core/cplugin.c
+++ librz/core/cplugin.c
@@ -30,6 +30,7 @@ RZ_API bool rz_core_plugin_add(RzCore *core, RZ_NONNULL RzCorePlugin *plugin) {
 	rz_return_val_if_fail(plugin && plugin->init && plugin->name && plugin->author && plugin->license, false);
 	RZ_PLUGIN_CHECK_AND_ADD(core->plugins, plugin, RzCorePlugin);
 	if (!plugin->init(core)) {
+		RZ_PLUGIN_REMOVE(core->plugins, plugin);
 		return false;
 	}
 	return true;
@@ -45,12 +46,13 @@ RZ_API bool rz_core_plugin_del(RzCore *core, RZ_NONNULL RzCorePlugin *plugin) {
 
 RZ_API bool rz_core_plugin_init(RzCore *core) {
 	int i;
+	bool res = true;
 	core->plugins = rz_list_new();
 	for (i = 0; i < RZ_ARRAY_SIZE(core_static_plugins); i++) {
 		if (!rz_core_plugin_add(core, core_static_plugins[i])) {
-			RZ_LOG_ERROR("core: error loading core plugin\n");
-			return false;
+			RZ_LOG_ERROR("core: error loading core plugin '%s'\n", core_static_plugins[i]->name);
+			res = false;
 		}
 	}
-	return true;
+	return res;
 }
