0fff253fd9999fb5390e723d7c8ed48e484583d5
Peiwei Hu <jlu.hpw@foxmail.com>
2024-02-28 22:28:26 -0800

    Fold XREFs line more than 3 when in the graph mode (#4308)

--- librz/core/cgraph.c
+++ librz/core/cgraph.c
@@ -359,8 +359,9 @@ static RZ_OWN RzGraph /*<RzGraphNodeInfo *>*/ *rz_core_graph_function_bbs(RZ_NON
 		goto fail;
 	}
 
-	rz_config_hold_i(hc, "asm.lines", "asm.lines.fcn", "asm.bytes", "asm.debuginfo", "asm.offset", "asm.marks",
+	rz_config_hold_i(hc, "asm.xrefs.max", "asm.lines", "asm.lines.fcn", "asm.bytes", "asm.debuginfo", "asm.offset", "asm.marks",
 		"asm.cmt.right", "asm.cmt.col", "asm.bb.middle", NULL);
+	rz_config_set_i(core->config, "asm.xrefs.max", 3);
 	rz_config_set_i(core->config, "asm.lines", 0);
 	rz_config_set_i(core->config, "asm.lines.fcn", 0);
 	rz_config_set_i(core->config, "asm.bytes", 0);
