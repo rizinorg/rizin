f0749201128629665eb4bc30175d9f4bfef3d3bf
Giridhar Prasath R <giridh1337@gmail.com>
2024-03-20 06:08:37 -0400

    Update debug API header

--- librz/include/rz_debug.h
+++ librz/include/rz_debug.h
@@ -465,8 +465,10 @@ RZ_API bool rz_debug_select(RzDebug *dbg, int pid, int tid);
 RZ_API RzDebugPid *rz_debug_pid_new(const char *path, int pid, int uid, char status, ut64 pc);
 RZ_API RzDebugPid *rz_debug_pid_free(RzDebugPid *pid);
 RZ_API RzList /*<RzDebugPid *>*/ *rz_debug_pids(RzDebug *dbg, int pid);
+#ifdef __linux__
 RZ_API RZ_OWN RzList /*<RzList *>*/ *get_pid_thread_list(RZ_NONNULL RzDebug *dbg, int main_pid);
 RZ_API ut64 get_linux_tls_val(RZ_NONNULL RzDebug *dbg, int tid);
+#endif
 RZ_API RzDebugPid *rz_debug_get_thread(RzList /*<RzList *>*/ *th_list, int tid);
 
 RZ_API bool rz_debug_set_arch(RzDebug *dbg, const char *arch, int bits);
