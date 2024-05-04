ac6276d2200bfd44da976a8de8d1a789d9adff48
Anton Kochkov <anton.kochkov@gmail.com>
2024-05-04 12:59:14 +0800

    hppa: remove GNU plugin

--- librz/arch/p_gnu/arch_hppa.c
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2024 RizinOrg <info@rizin.re>
-// SPDX-FileCopyrightText: 2024 deroad <wargio@libero.it>
-// SPDX-License-Identifier: LGPL-3.0-only
-
-#include <deprecated_arch_helper.h>
-
-#include "asm/asm_hppa_gnu.c"
-
-RZ_ARCH_ASM_ONLY_PLUGIN_DEFINE_DEPRECATED(hppa_gnu);
