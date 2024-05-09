61c8074cbfd6518e6aa9ea88780ab6f27249c409
pelijah <pelijah@users.noreply.github.com>
2024-05-09 14:46:22 +0300

    Add set_s_to_vector() API

--- librz/include/rz_util/set.h
+++ librz/include/rz_util/set.h
@@ -7,6 +7,7 @@
 
 #include <rz_util/ht_sp.h>
 #include <rz_util/ht_up.h>
+#include <rz_vector.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,6 +20,7 @@ RZ_API void set_s_add(RZ_NONNULL SetS *set, const char *str);
 RZ_API bool set_s_contains(RZ_NONNULL SetS *set, const char *str);
 RZ_API void set_s_delete(RZ_NONNULL SetS *set, const char *str);
 RZ_API void set_s_free(RZ_NULLABLE SetS *set);
+RZ_API RZ_OWN RzPVector /*<char *>*/ *set_s_to_vector(RZ_NONNULL SetS *set);
 
 typedef HtUP SetU;
 
