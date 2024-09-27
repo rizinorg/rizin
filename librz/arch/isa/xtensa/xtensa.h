40954cc1cc778f8559f858b1c284dbe27c28d873
billow <billow.fun@gmail.com>
2024-09-27 18:53:14 +0800

    xtensa: add basic file

---
+++ librz/arch/isa/xtensa/xtensa.h
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2024 billow <billow.fun@gmail.com>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#ifndef RIZIN_XTENSA_H
+#define RIZIN_XTENSA_H
+
+#endif // RIZIN_XTENSA_H
