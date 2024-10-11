d0cc273ca5f6011448741e29729bde46736d7109
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2024-10-11 21:11:11 +0800

    Cast `actual` web wider

--- test/scripts/dk_9.py
+++ test/scripts/dk_9.py
@@ -1,11 +1,11 @@
-import rzpipe
 import sys
+import rzpipe
 
 rzp = rzpipe.open("bins/elf/analysis/calls_x64", flags=["-a", "x86", "-d", "-1"])
-rzp.cmd("")  # Eat "PID ?? started" banner
 expected = "child received signal 9"
-# ebfe is infinite loop
-actual = rzp.cmd("wx ebfe; dk 9; dc")
+actual = rzp.cmd("")
+actual += rzp.cmd("wx ebfe; dk 9; dc")  # ebfe is infinite loop
+actual += rzp.cmd("")
 if expected in actual or "Process exited with status=0x9" in actual:
     sys.stderr.write(expected + "\n")
 else:
