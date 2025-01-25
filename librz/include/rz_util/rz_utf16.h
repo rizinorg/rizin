261fddc7e2ee802a2a5299ece12a4e915571a6ad
notxvilka <notxvilka@proton.me>
2025-01-25 22:22:23 +0800

    Update librz/include/rz_util/rz_utf16.h
    Co-authored-by: Khairul Azhar Kasmiran <kazarmy@gmail.com>

--- librz/include/rz_util/rz_utf16.h
+++ librz/include/rz_util/rz_utf16.h
@@ -4,9 +4,9 @@
 /* For RzRune definition */
 #include "rz_utf8.h"
 
-RZ_API size_t rz_utf16_decode(RZ_NONNULL const ut8 *ptr, size_t ptrlen, RZ_NONNULL RZ_OUT RzCodePoint *ch, bool bigendian);
-RZ_API size_t rz_utf16le_decode(RZ_NONNULL const ut8 *ptr, size_t ptrlen, RZ_NONNULL RZ_OUT RzCodePoint *ch);
-RZ_API size_t rz_utf16be_decode(RZ_NONNULL const ut8 *ptr, size_t ptrlen, RZ_NONNULL RZ_OUT RzCodePoint *ch);
-RZ_API size_t rz_utf16le_encode(RZ_NONNULL RZ_OUT ut8 *ptr, RzCodePoint ch);
+RZ_API size_t rz_utf16_decode(RZ_NONNULL const ut8 *buf, size_t buf_len, RZ_NONNULL RZ_OUT RzCodePoint *ch, bool bigendian);
+RZ_API size_t rz_utf16le_decode(RZ_NONNULL const ut8 *buf, size_t buf_len, RZ_NONNULL RZ_OUT RzCodePoint *ch);
+RZ_API size_t rz_utf16be_decode(RZ_NONNULL const ut8 *buf, size_t buf_len, RZ_NONNULL RZ_OUT RzCodePoint *ch);
+RZ_API size_t rz_utf16le_encode(RZ_NONNULL RZ_OUT ut8 *buf, RzCodePoint ch);
 
 #endif //  RZ_UTF16_H
