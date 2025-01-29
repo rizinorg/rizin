df8e22ea33877aa5f672d46629aa48a86167406d
Rot127 <unisono@quyllur.org>
2025-01-29 10:58:19 -0500

    Fix check

--- librz/util/utf32.c
+++ librz/util/utf32.c
@@ -56,7 +56,7 @@ RZ_API int rz_utf32be_decode(const ut8 *ptr, int ptrlen, RzCodePoint *ch) {
  * \param lookahead Number of code points to check.
  * Note: if the buffer can't cover all \p lookahead code points, this returns false.
  *
- * \return True if the buffer \p lookahead valid UTF-32 code points.
+ * \return True if the buffer has \p lookahead valid UTF-32 code points.
  * \return False otherwise.
  */
 RZ_API bool rz_utf32_valid_cp(RZ_NONNULL const ut8 *buf, size_t buf_len, bool big_endian, size_t lookahead) {
