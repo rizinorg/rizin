f3cdf3326a27e306ee37ba598b5c043c279e7c7e
Rot127 <unisono@quyllur.org>
2025-02-01 11:32:51 -0500

    Fix UTF-32 decoding.

--- librz/util/utf32.c
+++ librz/util/utf32.c
@@ -4,35 +4,26 @@
 #include <rz_types.h>
 #include <rz_util.h>
 
-/* Convert an UTF-32 buf into a unicode RzCodePoint */
-RZ_API int rz_utf32_decode(const ut8 *ptr, int ptrlen, RzCodePoint *ch, bool bigendian) {
-	if (ptrlen < 1) {
+/**
+ * \brief Decode bytes from the buffer \p buf into a code point.
+ *
+ * \param buf The buffer to read from.
+ * \param buf_len The buffer size in bytes.
+ * \param ch The code point to write the value to.
+ * \param big_endian If the buffer bytes have big endian order.
+ *
+ * \return The number of bytes converted. For UTF-32 this is always 0 or 4.
+ */
+RZ_API size_t rz_utf32_decode(RZ_NONNULL const ut8 *buf, size_t buf_len, RZ_NULLABLE RZ_OUT RzCodePoint *ch, bool big_endian) {
+	rz_return_val_if_fail(buf, 0);
+	if (buf_len < 4) {
 		return 0;
 	}
-	int low = 0;
-	int high = 3;
-	if (bigendian) {
-		low = 3;
-		high = 0;
+	if (!ch) {
+		return 4;
 	}
-	if (ptrlen > 3) {
-		int sign = bigendian ? -1 : 1;
-		if (ch) {
-			int i;
-			*ch = (ut32)ptr[low];
-			for (i = 1; i < 4; i++) {
-				*ch |= (ut32)ptr[3 - high + i * sign] << 8 * i;
-			}
-		}
-		if (ptr[high] || ptr[high - 1 * sign]) {
-			return 4;
-		}
-		if (ptr[low + 1 * sign]) {
-			return 2;
-		}
-		return 1;
-	}
-	return 0;
+	*ch = rz_read_ble32(buf, big_endian);
+	return 4;
 }
 
 /* Convert an UTF-32LE buf into a unicode RzCodePoint */
