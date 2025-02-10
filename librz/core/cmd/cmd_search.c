a9826867479448d9280fab08aff8d5b1a3863a79
Rot127 <unisono@quyllur.org>
2025-02-10 11:15:54 -0500

    Handle the 'settings' case in code search.

--- librz/core/cmd/cmd_search.c
+++ librz/core/cmd/cmd_search.c
@@ -2967,8 +2967,11 @@ static RzCmdStatus cmd_string_search_generic(RzCore *core, const char *string, c
 		expected = rz_str_enc_string_as_type(rz_config_get(core->config, "str.encoding"));
 	}
 	if (!RZ_STR_EQ(encoding, "guess") && expected == RZ_STRING_ENC_GUESS) {
-		RZ_LOG_ERROR("core: invalid encoding '%s'.\n", encoding);
-		goto invalid_args;
+		if (!RZ_STR_EQ(encoding, "settings")) {
+			RZ_LOG_ERROR("core: invalid encoding '%s'.\n", encoding);
+			goto invalid_args;
+		}
+		// Else we are fine, since the encoding in the settings is set to 'guess'.
 	}
 
 	bool progress = rz_config_get_b(core->config, "search.show_progress");
