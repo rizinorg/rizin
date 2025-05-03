e7f09ba4fcd77c3683e05c11e259f75a988ae145
Karliss <karlis3p70l1ij@gmail.com>
2025-05-03 17:42:54 +0300

    Fix license header for the new test.

--- test/integration/test_analysis_fcn.c
+++ test/integration/test_analysis_fcn.c
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 KarlisS <karlis3p70l1ij@gmail.com>
+// SPDX-FileCopyrightText: 2025 KarlisS <karlis3p70l1ij@gmail.com>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #include <rz_core.h>
