bd6b35b88769c8bf3f03668ee44c1c1fc31203f8
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2025-05-07 21:30:21 +0800

    Add pe fix for bins/fuzzed/crash-71087187fb883c4a4d92b11c8663d47a

--- librz/bin/p/bin_mdmp.c
+++ librz/bin/p/bin_mdmp.c
@@ -42,7 +42,9 @@ static RzPVector /*<RzBinAddr *>*/ *mdmp_entries(RzBinFile *bf) {
 	}
 	rz_list_foreach (obj->pe64_bins, it, pe64_bin) {
 		vec = Pe64_rz_bin_mdmp_pe_get_entrypoint(pe64_bin);
-		rz_pvector_join(ret, vec);
+		if (vec) {
+			rz_pvector_join(ret, vec);
+		}
 		rz_pvector_free(vec);
 	}
 
