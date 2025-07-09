ab177ef0ad8889de1116b2c395b1a4b38179581b
BigFish2086 <a.ibrahim8686@gmail.com>
2025-07-09 15:13:59 +0300

    Extract base36 decoding function from subprojects/rzwinkd/iob_net.c to librz/util/base36.c #5216
    - Also, added Doxygen docs to rz_base36_decode function

---
+++ librz/include/rz_util/rz_base36.h
@@ -0,0 +1,17 @@
+#ifndef RZ_base36_H
+#define RZ_base36_H
+
+#include <rz_types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+RZ_API ut64 rz_base36_decode(const char *str, const size_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //  RZ_base36_H
+
