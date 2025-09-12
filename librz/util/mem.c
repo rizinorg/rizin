4ff7b5191b7f14d41bf0fbab4e689926337255a4
Rot127 <unisono@quyllur.org>
2025-09-12 13:37:13 -0500

    Add helper to swap sequences of 2 bytes.

--- librz/util/mem.c
+++ librz/util/mem.c
@@ -371,3 +371,72 @@ RZ_API RZ_OWN ut8 *rz_mem_copy_offset(const ut8 *buf, size_t buf_size, size_t of
 	}
 	return dst;
 }
+
+/**
+ * \brief Swaps the bytes in 2 byte blocks from the given buffer and returns
+ * the result.
+ * Remainders of less than 2 bytes at the end of the buffer won't be swapped.
+ *
+ * \param buf The input buffer. The buffer pointer must be aligned to 0x2.
+ * \param buf_size The size of the input buffer. Must be greater than 0.
+ *
+ * \return The input buffer with swapped bytes or NULL in case of failure.
+ *
+ * NOTE: This function can be used to change the endianness of 2 byte values
+ * in the given buffer.
+ *
+ * Examples:
+ * ```c
+ * ut8 a[4] = { 0xff, 0x00, 0x99, 0x00 };
+ * ut8 b[4] = { 0x00, 0xff, 0x00, 0x99 };
+ * ut8 *swapped = rz_mem_swap_bytes_2(a);
+ * assert(memcmp(swapped, b, sizeof(a)) == 0);
+ * ```
+ *
+ * ```c
+ * ut8 a[4] = { 0xff, 0x00, 0x99, 0x00, 0x11 };
+ * ut8 b[4] = { 0x00, 0xff, 0x00, 0x99, 0x11 };
+ * ut8 *swapped = rz_mem_swap_bytes_2(a);
+ * assert(memcmp(swapped, b, sizeof(a)) == 0);
+ * ```
+ */
+RZ_API RZ_OWN ut8 *rz_mem_swap_bytes_2(RZ_NONNULL const ut8 *buf, size_t buf_size) {
+	rz_return_val_if_fail(buf && buf_size != 0, NULL);
+	size_t al = rz_mem_ptr_alignment(buf);
+	if (al < 2) {
+		RZ_LOG_ERROR("mem: Given memory is not aligned to 0x2 or greater.\n");
+		return NULL;
+	}
+	ut8 *dst = RZ_NEWS0(ut8, buf_size);
+	if (!dst) {
+		return NULL;
+	}
+
+	const ut64 *src_64 = (const ut64 *)buf;
+	ut64 *dst_64 = (ut64 *)dst;
+	while (al >= 8 && buf_size >= 8) {
+		*dst_64++ = ((*src_64 & 0xff00ff00ff00ff00) >> 8) | ((*src_64 & 0x00ff00ff00ff00ff) << 8);
+		src_64++;
+		buf_size -= 8;
+	}
+	const ut32 *src_32 = (const ut32 *)src_64;
+	ut32 *dst_32 = (ut32 *)dst_64;
+	while (al >= 4 && buf_size >= 4) {
+		*dst_32++ = ((*src_32 & 0xff00ff00) >> 8) | ((*src_32 & 0x00ff00ff) << 8);
+		src_32++;
+		buf_size -= 4;
+	}
+	const ut16 *src_16 = (const ut16 *)src_32;
+	ut16 *dst_16 = (ut16 *)dst_32;
+	while (buf_size >= 2) {
+		*dst_16++ = ((*src_16 & 0xff00) >> 8) | ((*src_16 & 0x00ff) << 8);
+		src_16++;
+		buf_size -= 2;
+	}
+	if (buf_size == 1) {
+		const ut8 *src_8 = (const ut8 *)src_16;
+		ut8 *dst_8 = (ut8 *)dst_16;
+		*dst_8++ = *src_8;
+	}
+	return dst;
+}
