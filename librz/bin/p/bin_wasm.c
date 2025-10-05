fc316ea23badd88b8da6929944e0da941d122346
Rot127 <unisono@quyllur.org>
2025-10-05 10:15:21 -0500

    Enforce UTF-8 for initial string search.

--- librz/bin/p/bin_wasm.c
+++ librz/bin/p/bin_wasm.c
@@ -318,6 +318,15 @@ static RzBuffer *create(RzBin *bin, const ut8 *code, int codelen, const ut8 *dat
 	return buf;
 }
 
+static RzPVector /*<RzBinString *>*/ *strings(RzBinFile *bf) {
+	RzBinStringSearchOpt opt;
+	rz_bin_string_search_opt_init(&opt);
+	opt.mode = RZ_BIN_STRING_SEARCH_MODE_READ_ONLY_SECTIONS;
+	// https://webassembly.github.io/spec/core/bikeshed/#dependencies%E2%91%A0
+	opt.string_encoding = RZ_STRING_ENC_UTF8;
+	return rz_bin_file_strings(bf, &opt);
+}
+
 RzBinPlugin rz_bin_plugin_wasm = {
 	.name = "wasm",
 	.desc = "WebAssembly",
@@ -337,6 +346,7 @@ RzBinPlugin rz_bin_plugin_wasm = {
 	.info = &info,
 	.libs = &libs,
 	.create = &create,
+	.strings = &strings,
 };
 
 #ifndef RZ_PLUGIN_INCORE
