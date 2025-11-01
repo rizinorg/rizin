af26c8e6505a38d2abceab9efcc167ee979d5bfa
Matul <7746matul@gmail.com>
2025-11-01 21:39:13 +0530

    Update rz-find.c
    the changes made by me are here

--- librz/main/rz-find.c
+++ librz/main/rz-find.c
@@ -39,6 +39,7 @@ typedef struct {
 	const char *curfile;
 	const char *comma;
 	const char *exec_command;
+	const char *sys_command;
 } RzfindOptions;
 
 static void rzfind_options_fini(RzfindOptions *ro) {
@@ -160,6 +161,21 @@ static int hit(RzSearchKeyword *kw, void *user, ut64 addr) {
 		free(command);
 		return 1;
 	}
+	if (ro->sys_command) {
+		// Replace {} in the command with the filename
+		char *replaced = rz_str_replace((char *)ro->sys_command, "{}", ro->curfile, 0);
+		if (!replaced) {
+			replaced = rz_str_dup(ro->sys_command);
+		}
+
+		RZ_LOG_INFO("Executing system command: %s\n", replaced);
+		int status = rz_sys_system(replaced);
+		if (status == -1) {
+			RZ_LOG_ERROR("Failed to execute system command: %s\n", replaced);
+		}
+		free(replaced);
+		return 1;
+	}
 	return 1;
 }
 
@@ -201,6 +217,7 @@ static int show_help(const char *argv0, int line) {
 		"-b",    "size",    "Set block size",
 		"-e",    "regex",   "Search for regex matches (can be used multiple times)",
 		"-E",    "cmd",     "Execute command for each file found",
+		"-R",    "cmd",     "Execute a system shell command for each match (use {} for filename)",
 		"-f",    "from",    "Start searching from address 'from'",
 		"-F",    "file",    "Read the contents of the file and use it as keyword",
 		"-h",    "",        "Show this help",
@@ -516,7 +533,7 @@ RZ_API int rz_main_rz_find(int argc, const char **argv) {
 	const char *file = NULL;
 
 	RzGetopt opt;
-	rz_getopt_init(&opt, argc, argv, "a:ie:b:jmM:s:w:S:I:x:Xzf:F:t:E:rqnhvZ");
+	rz_getopt_init(&opt, argc, argv, "a:ie:b:jmM:s:w:S:I:x:Xzf:F:t:E:R:rqnhvZ");
 	while ((c = rz_getopt_next(&opt)) != -1) {
 		switch (c) {
 		case 'a':
@@ -550,6 +567,10 @@ RZ_API int rz_main_rz_find(int argc, const char **argv) {
 			ro.quiet = true;
 			ro.exec_command = opt.arg;
 			break;
+		case 'R':
+    		ro.quiet = true;
+    		ro.sys_command = opt.arg;
+		    break;
 		case 's':
 			ro.mode = RZ_SEARCH_KEYWORD;
 			ro.hexstr = false;
