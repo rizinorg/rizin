283a110a4cf92c717c5f4fbd30f10d87b1672aa9
Rot127 <unisono@quyllur.org>
2025-11-06 12:29:15 -0500

    Move the interpreters into their own logical unit.

--- librz/include/rz_inquiry/rz_interpreter.h
+++ librz/include/rz_inquiry/rz_interpreter.h
@@ -1,6 +1,11 @@
 // SPDX-FileCopyrightText: 2025 RizinOrg <info@rizin.re>
 // SPDX-License-Identifier: LGPL-3.0-only
 
+/**
+ * \file The header file for the RzInterpreter contains declarations for
+ * all RzIL based interpreters.
+ */
+
 #ifndef RZ_INTERPRETER
 #define RZ_INTERPRETER
 
@@ -17,39 +22,39 @@ typedef enum {
 	/**
 	 * \brief Value abstraction into constant and bottom values.
 	 */
-	RZ_INQUIRY_ABSTRACTION_CONST = 1 << 0,
-} RzInquiryAbstraction;
+	RZ_INTERPRETER_ABSTRACTION_CONST = 1 << 0,
+} RzInterpreterAbstraction;
 
 /**
  * \brief An abitrary abstract value.
  */
 typedef struct {
-	RzInquiryAbstraction kind;
+	RzInterpreterAbstraction kind;
 	void *abstr_data;
-} RzInquiryAbstrVal;
+} RzInterpreterAbstrVal;
 
 typedef enum {
-	RZ_INQUIRY_YIELD_KIND_ABSTR_VAL, ///< Yield object is an abstract value.
-	RZ_INQUIRY_YIELD_KIND_CFG_EDGE, ///< Yield object a discovered CFG edge.
-} RzInquiryYieldKind;
+	RZ_INTERPRETER_YIELD_KIND_ABSTR_VAL, ///< Yield object is an abstract value.
+	RZ_INTERPRETER_YIELD_KIND_CFG_EDGE, ///< Yield object a discovered CFG edge.
+} RzInterpreterYieldKind;
 
-typedef void *RzInquiryYield; ///< A yield of an interpreter. Type is implied by the queue.
+typedef void *RzInterpreterYield; ///< A yield of an interpreter. Type is implied by the queue.
 
 // TODO: Could be private
 /**
  * \brief A filter for abstract values to decide if they should be pushed into
  * the yield queue or not.
  */
-typedef bool (*RzInquiryYieldFilter)(RzInquiryYieldKind kind, const RzInquiryYield *yield);
+typedef bool (*RzInterpreterYieldFilter)(RzInterpreterYieldKind kind, const RzInterpreterYield *yield);
 
 /**
  * \brief A queue to push interpretation yields into.
  */
 typedef struct {
-	RzInquiryAbstraction kind;
-	RzInquiryYieldFilter *filter;
+	RzInterpreterAbstraction kind;
+	RzInterpreterYieldFilter *filter;
 	RzThreadQueue /*<RzInquiryYield>*/ *yield_queue;
-} RzInquiryYieldQueue;
+} RzInterpreterYieldQueue;
 
 /**
  * \brief The IL effect scopes pushed over the IL queue.
@@ -57,23 +62,23 @@ typedef struct {
  * more or less instructions.
  */
 typedef enum {
-	RZ_INQUIRY_IL_QUEUE_ELEM_SCOPE_IPKT, ///< RzILOpEffect scope is one atomically execute instruction packet.
-	RZ_INQUIRY_IL_QUEUE_ELEM_SCOPE_BB, ///< RzILOpEffect scope is one basic block (n instruction packets with a branch or termination at the end).
-} RzInquiryILQueueElemScope;
+	RZ_INTERPRETER_IL_QUEUE_ELEM_SCOPE_IPKT, ///< RzILOpEffect scope is one atomically execute instruction packet.
+	RZ_INTERPRETER_IL_QUEUE_ELEM_SCOPE_BB, ///< RzILOpEffect scope is one basic block (n instruction packets with a branch or termination at the end).
+} RzInterpreterILQueueElemScope;
 
 typedef struct {
-	RzInquiryILQueueElemScope effect_scope;
+	RzInterpreterILQueueElemScope effect_scope;
 	RzILOpEffect *il_effect;
-} RzInquiryILQueueElement;
+} RzInterpreterILQueueElement;
 
 typedef struct {
 	RzThreadQueue /*<RzInquiryILQueueElement *>*/ *yield_queue;
-} RzInquiryILQueue;
+} RzInterpreterILQueue;
 
 /**
  * \brief Performs abstract interpretation.
  */
-RZ_API bool rz_inquiry_abstract_interpretation(
+RZ_API bool rz_interpreter_run(
 	RZ_NONNULL RZ_BORROW RzThreadQueue /*<ut64>*/ *request_il,
 	RZ_NONNULL RZ_BORROW RzThreadQueue /*<RzInquiryILQueueElement *>*/ *receive_il,
 	RZ_NONNULL RZ_BORROW RzPVector /*<RzInquiryYieldQueue*>*/ *yield_queues);
