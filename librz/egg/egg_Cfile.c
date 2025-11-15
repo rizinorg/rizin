f188de5613329baa21806268f20563f8b56b31b0
rohahann-tech <rohahann@gmail.com>
2025-11-15 23:15:32 +0530

    Fix: incorrect sflib include path for Windows x86-32 in rz-gg

--- librz/egg/egg_Cfile.c
+++ librz/egg/egg_Cfile.c
@@ -150,6 +150,11 @@ static struct cEnv_t *rz_egg_Cfile_set_cEnv(RZ_BORROW RZ_NONNULL RzPath *sys_pat
 		use_clang = true;
 		cEnv->TEXT = "0.__TEXT.__text";
 	}
+        if (!strcmp(cEnv->TRIPLET, "windows-x86-32")) {
+                free(cEnv->TRIPLET);
+                cEnv->TRIPLET = rz_str_dup("windows-x86");
+        }
+
 
 	buffer = rz_str_newf("%s -fno-stack-protector -nostdinc -include '%s'/'%s'/sflib.h",
 		cEnv->CFLAGS, cEnv->SFLIBPATH, cEnv->TRIPLET);
