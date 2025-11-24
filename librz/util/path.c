87901d8348c163fb5602d359346b8d9ee0797e33
Arya-1-HR <aryagowda177@gmail.com>
2025-11-24 19:30:11 +0530

    del len arg

--- librz/util/path.c
+++ librz/util/path.c
@@ -325,7 +325,7 @@ RZ_API RZ_OWN RzPath *rz_path_new(void) {
  * \param usr_input The raw path string provided by the user. May be NULL or empty.
  * \return A new normalized path string.
  */
-RZ_API RZ_OWN char *rz_path_normalize_nd_expand(char *usr_input, size_t len) {
+RZ_API RZ_OWN char *rz_path_normalize_expand(char *usr_input) {
 	char *input = rz_str_dup(usr_input);
 	if (RZ_STR_ISEMPTY(input)) {
 		free(input);
