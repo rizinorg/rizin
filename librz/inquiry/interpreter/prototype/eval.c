4c562b9de4c2b3376842c441e08b40731874a7f8
Rot127 <unisono@quyllur.org>
2025-12-02 17:19:34 +0100

    Copy bit vectors when copying abstract data.

--- librz/inquiry/interpreter/prototype/eval.c
+++ librz/inquiry/interpreter/prototype/eval.c
@@ -2,14 +2,11 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #include "eval.h"
+#include <rz_util/rz_bitvector.h>
 
 void copy_abstr_data(ProtoIntrprAbstrData *dst, const ProtoIntrprAbstrData *src) {
-	// TODO: For performance it should really just copy the data between bit vectors
-	// Not freeing the old one and allocating a new one with rz_bv_dup().
-	// But this has to wait until we can copy bit vectors between on using an array
-	// and one using an ut64 to store its bits.
-	rz_bv_free(dst->bv);
-	dst->bv = rz_bv_dup(src->bv);
+	rz_bv_cast_inplace(dst->bv, rz_bv_len(src->bv), false);
+	rz_bv_copy(dst->bv, src->bv);
 	dst->is_concrete = src->is_concrete;
 }
 
