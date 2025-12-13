fe41cbc7aeb9e6aa0192de3f134ef507758a1fbd
wargio <deroad@kumo.xn--q9jyb4c>
2025-12-13 21:58:11 +0800

    Add proper xcore support

--- librz/arch/p/analysis/analysis_xcore_cs.c
+++ librz/arch/p/analysis/analysis_xcore_cs.c
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2014-2017 pancake <pancake@nopcode.org>
+// SPDX-FileCopyrightText: 2025 deroad <deroad@kumo.xn--q9jyb4c>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #include <rz_analysis.h>
@@ -8,7 +8,52 @@
 
 #define INSOP(n) insn->detail->xcore.operands[n]
 
-static void opex(RzStrBuf *buf, csh handle, cs_insn *insn) {
+static char *xcore_get_reg_profile(RzAnalysis *analysis) {
+	const char *p =
+		"=PC	pc\n"
+		"=SP	dp\n"
+		"=BP	sp\n"
+		"=SR	sr\n"
+		"=SN	r0\n"
+		"=A0	r4\n"
+		"=A1	r5\n"
+		"=A2	r6\n"
+		"=A3	r7\n"
+		"=A4	r8\n"
+		"=A5	r9\n"
+		"=A6	r10\n"
+		"gpr	r0   .32 0 0\n"
+		"gpr	r1   .32 4 0\n"
+		"gpr	r2   .32 8 0\n"
+		"gpr	r3   .32 12 0\n"
+		"gpr	r4   .32 16 0\n"
+		"gpr	r5   .32 20 0\n"
+		"gpr	r6   .32 24 0\n"
+		"gpr	r7   .32 28 0\n"
+		"gpr	r8   .32 32 0\n"
+		"gpr	r9   .32 36 0\n"
+		"gpr	r10  .32 40 0\n"
+		"gpr	r11  .32 44 0\n"
+		"gpr	cp   .32 48 0\n" // constant pool pointer
+		"gpr	dp   .32 52 0\n" // data pointer
+		"gpr	sp   .32 56 0\n" // stack pointer
+		"gpr	lr   .32 60 0\n" // link register
+		"gpr	pc   .32 64 0\n" // program counter
+		"gpr	sr   .32 68 0\n" // status register
+		"gpr	spc  .32 72 0\n" // saved pc
+		"gpr	ssr  .32 76 0\n" // saved status
+		"gpr	et   .32 80 0\n" // exception type
+		"gpr	ed   .32 84 0\n" // exception data
+		"gpr	sed  .32 88 0\n" // saved exception data
+		"gpr	kep  .32 92 0\n" // kernel entry pointer
+		"gpr	ksp  .32 96 0\n" // kernel stack pointer
+		"gpr	id   .32 100 0\n" // thread id
+		"";
+
+	return rz_str_dup(p);
+}
+
+static void xcore_opex(RzStrBuf *buf, csh handle, cs_insn *insn) {
 	int i;
 	PJ *pj = pj_new();
 	if (!pj) {
@@ -69,9 +114,6 @@ static int analyze_op(RzAnalysis *a, RzAnalysisOp *op, ut64 addr, const ut8 *buf
 	cs_insn *insn;
 	int mode, n, ret;
 	mode = CS_MODE_BIG_ENDIAN;
-	if (!strcmp(a->cpu, "v9")) {
-		mode |= CS_MODE_V9;
-	}
 	if (mode != ctx->omode) {
 		if (ctx->handle) {
 			cs_close(&ctx->handle);
@@ -86,52 +128,118 @@ static int analyze_op(RzAnalysis *a, RzAnalysisOp *op, ut64 addr, const ut8 *buf
 		}
 		cs_option(ctx->handle, CS_OPT_DETAIL, CS_OPT_ON);
 	}
-	// capstone-next
+
 	n = cs_disasm(ctx->handle, (const ut8 *)buf, len, addr, 1, &insn);
 	if (n < 1) {
 		op->type = RZ_ANALYSIS_OP_TYPE_ILL;
-	} else {
-		if (mask & RZ_ANALYSIS_OP_MASK_OPEX) {
-			opex(&op->opex, ctx->handle, insn);
+		if (mask & RZ_ANALYSIS_OP_MASK_DISASM) {
+			op->mnemonic = strdup("invalid");
 		}
-		op->size = insn->size;
-		op->id = insn->id;
-		switch (insn->id) {
-		case XCORE_INS_DRET:
-		case XCORE_INS_KRET:
-		case XCORE_INS_RETSP:
-			op->type = RZ_ANALYSIS_OP_TYPE_RET;
-			break;
-		case XCORE_INS_DCALL:
-		case XCORE_INS_KCALL:
-		case XCORE_INS_ECALLF:
-		case XCORE_INS_ECALLT:
-			op->type = RZ_ANALYSIS_OP_TYPE_CALL;
-			op->jump = INSOP(0).imm;
-			break;
-		/* ??? */
-		case XCORE_INS_BL:
-		case XCORE_INS_BLA:
-		case XCORE_INS_BLAT:
-		case XCORE_INS_BT:
-		case XCORE_INS_BF:
-		case XCORE_INS_BU:
-		case XCORE_INS_BRU:
-			op->type = RZ_ANALYSIS_OP_TYPE_CALL;
-			op->jump = INSOP(0).imm;
-			break;
-		case XCORE_INS_SUB:
-		case XCORE_INS_LSUB:
-			op->type = RZ_ANALYSIS_OP_TYPE_SUB;
-			break;
-		case XCORE_INS_ADD:
-		case XCORE_INS_LADD:
-			op->type = RZ_ANALYSIS_OP_TYPE_ADD;
-			break;
-		}
-		cs_free(insn, n);
+		return -1;
 	}
-	//	cs_close (&handle);
+
+	if (mask & RZ_ANALYSIS_OP_MASK_DISASM) {
+		op->mnemonic = rz_str_newf("%s%s%s", insn->mnemonic, insn->op_str[0] ? " " : "", insn->op_str);
+	}
+
+	if (mask & RZ_ANALYSIS_OP_MASK_OPEX) {
+		xcore_opex(&op->opex, ctx->handle, insn);
+	}
+
+	op->size = insn->size;
+	op->id = insn->id;
+	switch (insn->id) {
+	case XCORE_INS_BAU: /* Branch absolute unconditional register */
+		op->type = RZ_ANALYSIS_OP_TYPE_RJMP;
+		op->fail = addr + op->size;
+		break;
+	case XCORE_INS_BLA: /* Branch and link absolute via register */
+		op->type = RZ_ANALYSIS_OP_TYPE_RCALL;
+		op->fail = addr + op->size;
+		break;
+	case XCORE_INS_BLAT: /* Branch and link absolute via table */
+		op->type = RZ_ANALYSIS_OP_TYPE_MEM | RZ_ANALYSIS_OP_TYPE_UCALL;
+		op->fail = addr + op->size;
+		break;
+	case XCORE_INS_BL:
+		op->type = RZ_ANALYSIS_OP_TYPE_CALL;
+		op->jump = addr + (st64)(INSOP(0).imm * op->size);
+		op->fail = addr + op->size;
+		break;
+	case XCORE_INS_BF:
+		/* fall-thru */
+	case XCORE_INS_BT:
+		op->type = RZ_ANALYSIS_OP_TYPE_CJMP;
+		op->jump = addr + (st64)(INSOP(1).imm * op->size);
+		op->fail = addr + op->size;
+		break;
+	case XCORE_INS_BU:
+		op->type = RZ_ANALYSIS_OP_TYPE_JMP;
+		op->jump = addr + (st64)(INSOP(0).imm * op->size);
+		break;
+	case XCORE_INS_BRU:
+		op->type = RZ_ANALYSIS_OP_TYPE_RJMP;
+		break;
+	case XCORE_INS_DCALL:
+		op->type = RZ_ANALYSIS_OP_TYPE_UCALL;
+		break;
+	case XCORE_INS_DRET:
+		op->type = RZ_ANALYSIS_OP_TYPE_RET;
+		break;
+	case XCORE_INS_ECALLF:
+		op->type = RZ_ANALYSIS_OP_TYPE_UCCALL;
+		op->fail = addr + op->size;
+		break;
+	case XCORE_INS_ECALLT:
+		op->type = RZ_ANALYSIS_OP_TYPE_UCCALL;
+		op->fail = addr + op->size;
+		break;
+	case XCORE_INS_EQ: /* Compare if equal */
+		/* fall-thru */
+	case XCORE_INS_LSS: /* Compare if less than (signed) */
+		/* fall-thru */
+	case XCORE_INS_LSU: /* Compare if less than (unsigned) */
+		op->type = RZ_ANALYSIS_OP_TYPE_CMP;
+		break;
+	case XCORE_INS_KCALL: /* kernel call */
+		op->type = RZ_ANALYSIS_OP_TYPE_RCALL;
+		break;
+	case XCORE_INS_KRET:
+		op->type = RZ_ANALYSIS_OP_TYPE_RET;
+		break;
+	case XCORE_INS_LD16S:
+		/* fall-thru */
+	case XCORE_INS_LD8U:
+		/* fall-thru */
+	case XCORE_INS_LDA16:
+		/* fall-thru */
+	case XCORE_INS_LDAW:
+		/* fall-thru */
+	case XCORE_INS_LDW:
+		/* fall-thru */
+	case XCORE_INS_ST16:
+		/* fall-thru */
+	case XCORE_INS_ST8:
+		/* fall-thru */
+	case XCORE_INS_STW:
+		op->type = RZ_ANALYSIS_OP_TYPE_MEM | RZ_ANALYSIS_OP_TYPE_MOV;
+		break;
+	case XCORE_INS_LDAP:
+		op->type = RZ_ANALYSIS_OP_TYPE_MOV;
+		op->ptr = op->val = addr + (st64)(INSOP(0).imm * op->size);
+		break;
+	case XCORE_INS_LDC:
+		op->type = RZ_ANALYSIS_OP_TYPE_MOV;
+		op->val = (st64)INSOP(0).imm;
+		break;
+	case XCORE_INS_RETSP:
+		op->type = RZ_ANALYSIS_OP_TYPE_RET;
+		break;
+	default:
+		break;
+	}
+	cs_free(insn, n);
+
 	return op->size;
 }
 
@@ -141,6 +249,23 @@ static bool xcore_fini(void *user) {
 		RZ_FREE(ctx);
 	}
 	return true;
+}
+
+static int xcore_archinfo(RzAnalysis *a, RzAnalysisInfoType query) {
+	switch (query) {
+	case RZ_ANALYSIS_ARCHINFO_MIN_OP_SIZE:
+		return 2;
+	case RZ_ANALYSIS_ARCHINFO_MAX_OP_SIZE:
+		return 4;
+	case RZ_ANALYSIS_ARCHINFO_TEXT_ALIGN:
+		return 2;
+	case RZ_ANALYSIS_ARCHINFO_DATA_ALIGN:
+		return 1;
+	case RZ_ANALYSIS_ARCHINFO_CAN_USE_POINTERS:
+		return true;
+	default:
+		return -1;
+	}
 }
 
 RzAnalysisPlugin rz_analysis_plugin_xcore_cs = {
@@ -153,7 +278,8 @@ RzAnalysisPlugin rz_analysis_plugin_xcore_cs = {
 	.op = &analyze_op,
 	.init = xcore_init,
 	.fini = xcore_fini,
-	//.set_reg_profile = &set_reg_profile,
+	.archinfo = xcore_archinfo,
+	.get_reg_profile = &xcore_get_reg_profile,
 };
 
 #ifndef RZ_PLUGIN_INCORE
