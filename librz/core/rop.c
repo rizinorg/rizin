372544f450ad603c06de065fced18a065db7f445
Arya-1-HR <aryagowda177@gmail.com>
2025-12-22 22:25:34 +0530

    long mode update

--- librz/core/rop.c
+++ librz/core/rop.c
@@ -956,34 +956,32 @@ static void rz_rop_gadget_print_long_mode(const RzCore *core, const RzRopGadgetI
 		free(buf);
 		return;
 	}
-	const int DISASM_WIDTH = 50;
+	int desired_width = 50;
+	const bool colorize = rz_config_get_i(core->config, "scr.color");
 	rz_cons_printf("Gadget 0x%" PFMT64x " (size %d bytes)\n", addr, size);
-	rz_cons_printf("%s--%s\n",rz_str_repeat("-", DISASM_WIDTH),rz_str_repeat("-", 40));
-	RzAsmOp asmop = { 0 };
+	rz_cons_printf("%s--%s\n", rz_str_repeat("-", desired_width), rz_str_repeat("-", desired_width));
+	RzAsmOp *asmop = NULL;
+	RzAnalysisOp aop = RZ_EMPTY;
 	int idx = 0;
 	int instr_count = 0;
-	bool use_color = core->print && (core->print->flags & RZ_PRINT_FLAGS_COLOR);
 	while (idx < size) {
 		rz_asm_set_pc(core->rasm, addr + idx);
-		int len = rz_asm_disassemble(core->rasm, &asmop, buf + idx, size - idx);
+		int len = rz_asm_disassemble(core->rasm, asmop, buf + idx, size - idx);
 		if (len < 1)
 			break;
+		rz_analysis_op(core->analysis, &aop, addr + idx, buf + idx, size - idx, RZ_ANALYSIS_OP_MASK_BASIC);
 		char *hex = rz_hex_bin2strdup(buf + idx, len);
-		const char *opcode_str = rz_strbuf_get(&asmop.buf_asm);
-		if (!opcode_str) {
-			opcode_str = "";
+		const char *asm_str = rz_asm_op_get_asm(asmop);
+		RzStrBuf *colored_asm = NULL;
+		if (colorize) {
+			colored_asm = get_colored_asm_str(core, asmop, &aop);
+			asm_str = colored_asm ? rz_strbuf_get(colored_asm) : asm_str;
 		}
-		if (use_color) {
-			rz_cons_printf(Color_CYAN);
-		}
-		rz_cons_printf("  0x%08" PFMT64x, addr + idx);
-		if (use_color) {
-			rz_cons_printf(Color_RESET);
-		}
-		rz_cons_printf("  %-8s  ", hex ? hex : "");
-		rz_cons_printf("%s", opcode_str);
-		int opcode_len = strlen(opcode_str);
-		int padding = (DISASM_WIDTH - 22) - opcode_len;
+		const char *reset_color = colorize ? Color_RESET : "";
+		int asm_len_clean = rz_str_ansi_len(asm_str);
+		int visible_len = 22 + asm_len_clean;
+		rz_cons_printf("  0x%08" PFMT64x "  %-8s  %s%s", addr + idx, hex ? hex : "", asm_str, reset_color);
+		int padding = desired_width - visible_len;
 		if (padding > 0) {
 			rz_cons_printf("%*s", padding, "");
 		}
@@ -1000,13 +998,7 @@ static void rz_rop_gadget_print_long_mode(const RzCore *core, const RzRopGadgetI
 					if (reg_info && reg_info->name) {
 						if (!first)
 							rz_cons_printf(" ");
-						if (use_color) {
-							rz_cons_printf(Color_YELLOW);
-						}
 						rz_cons_printf("%s", reg_info->name);
-						if (use_color) {
-							rz_cons_printf(Color_RESET);
-						}
 						first = false;
 					}
 				}
@@ -1022,13 +1014,7 @@ static void rz_rop_gadget_print_long_mode(const RzCore *core, const RzRopGadgetI
 					if (dep_info && dep_info->name) {
 						if (!first)
 							rz_cons_printf(" ");
-						if (use_color) {
-							rz_cons_printf(Color_GREEN);
-						}
 						rz_cons_printf("%s", dep_info->name);
-						if (use_color) {
-							rz_cons_printf(Color_RESET);
-						}
 						first = false;
 					}
 				}
@@ -1037,11 +1023,15 @@ static void rz_rop_gadget_print_long_mode(const RzCore *core, const RzRopGadgetI
 		} else {
 			rz_cons_newline();
 		}
+		if (colored_asm) {
+			rz_strbuf_free(colored_asm);
+		}
 		free(hex);
+		rz_analysis_op_fini(&aop);
 		idx += len;
 		instr_count++;
 	}
-	rz_asm_op_fini(&asmop);
+	rz_asm_op_fini(asmop);
 	free(buf);
 	rz_cons_newline();
 }
