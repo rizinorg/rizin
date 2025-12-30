b1990c61d52bf1f3367dbfc38100eda153868e16
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2025-12-30 19:35:42 +0800

    Shorten rz_list_safe_next to rz_list_next

--- librz/include/rz_list.h
+++ librz/include/rz_list.h
@@ -51,11 +51,11 @@ typedef int (*RzListComparator)(const void *value, const void *list_data, void *
 #define rz_list_foreach_prev_safe(list, it, tmp, var) \
 	for (it = list->tail; it && (var = it->val, tmp = it->prev, 1); it = tmp)
 
-#define rz_list_empty(x)     (!(x) || !(x)->length)
-#define rz_list_head(x)      ((x) ? (x)->head : NULL)
-#define rz_list_tail(x)      ((x) ? (x)->tail : NULL)
-#define rz_list_prev(x)      ((x)->prev)
-#define rz_list_safe_next(x) ((x) ? (x)->next : NULL)
+#define rz_list_empty(x) (!(x) || !(x)->length)
+#define rz_list_head(x)  ((x) ? (x)->head : NULL)
+#define rz_list_tail(x)  ((x) ? (x)->tail : NULL)
+#define rz_list_prev(x)  ((x)->prev)
+#define rz_list_next(x)  ((x)->next)
 
 #define rz_list_iter_get(x) \
 	x->val; \
