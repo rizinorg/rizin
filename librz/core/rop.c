596e4aca5013d0c0a52407c53275d63d39b76970
Arya-1-HR <aryagowda177@gmail.com>
2026-01-04 20:55:44 +0530

    rz-bindgen linter correction

--- librz/core/rop.c
+++ librz/core/rop.c
@@ -986,7 +986,7 @@ static void print_rop_dependencies(const RzRopGadgetInfo *gadget_info) {
 	}
 }
 
-static void print_rop_long_info(const RzRopGadgetInfo *gadget_info, /*<size_t *>*/ RzVector *lens, /*<ut64 *>*/ RzVector *add, /*<char *>*/ RzPVector *asm_strs, /*<char *>*/ RzPVector *hex_strs, int high_pad, bool utf8, bool colorize) {
+static void print_rop_long_info(const RzRopGadgetInfo *gadget_info, RzVector /*<size_t *>*/ *lens, RzVector /*<ut64 *>*/ *add, RzPVector /*<char *>*/ *asm_strs, RzPVector /*<char *>*/ *hex_strs, int high_pad, bool utf8, bool colorize) {
 	ut32 size = gadget_info->size;
 	size_t instr_count = 0;
 	int pad = 0;
