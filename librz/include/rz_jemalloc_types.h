89ece6353f6e8bf854a90b3022e5babcac62a5b8
bubblepipe <zhouqi008012@gmail.com>
2026-01-06 00:35:54 +0800

    Update librz/include/rz_jemalloc_types.h
    Co-authored-by: Rot127 <45763064+Rot127@users.noreply.github.com>

--- librz/include/rz_jemalloc_types.h
+++ librz/include/rz_jemalloc_types.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 Rizin Contributors
+// SPDX-FileCopyrightText: 2025 bubblepipe <bubblepipe42@gmail.com>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #ifndef RZ_JEMALLOC_TYPES_H
