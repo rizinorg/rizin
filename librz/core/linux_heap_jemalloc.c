f7c6851df97e4e5c11caa96124f8e48333672908
bubblepipe <bubblepipe42@gmail.com>
2026-01-10 02:05:38 +0800

    fix 2026

--- librz/core/linux_heap_jemalloc.c
+++ librz/core/linux_heap_jemalloc.c
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2017 javierptd <javierptd@gmail.com>
-// SPDX-FileCopyrightText: 2025 bubblepipe <bubblepipe42@gmail.com>
+// SPDX-FileCopyrightText: 2026 bubblepipe <bubblepipe42@gmail.com>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #ifndef INCLUDE_HEAP_JEMALLOC_STD_C
