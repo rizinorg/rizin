0f2d6da19423c25cc8d7c28261f977d7e539ac34
Anton Angelov <anton.angelov@protonmail.com>
2026-01-22 16:15:27 +0200

    Update benchmarked function

--- test/bench/bench_il.c
+++ test/bench/bench_il.c
@@ -10,24 +10,28 @@
  * \brief Benchmark for `rz_il_*` functions
  */
 
-// todo: delete
+// todo: delete..
 static bool read_n_bits_into(RzBuffer *mem_buf, RZ_OUT RzBitVector *out_bv, ut32 n_bits, const RzBitVector *key, bool big_endian) {
 	ut64 address = rz_bv_to_ut64(key);
 	size_t n_bytes = rz_bv_len_bytes(out_bv);
 
-	ut8 *data = calloc(n_bytes, 1);
-	if (!data) {
-		return false;
-	}
-	// we ignore bad reads. RzBuffer fills up with its "overflow byte" on failure.
-	rz_buf_read_at(mem_buf, address, data, n_bytes);
-	if (big_endian) {
-		rz_bv_set_from_bytes_be(out_bv, data, 0, n_bits);
-	} else {
-		rz_bv_set_from_bytes_le(out_bv, data, 0, n_bits);
-	}
-	free(data);
+	// ut8 *data = calloc(n_bytes, 1);
+	// if (!data) {
+	// 	return false;
+	// }
+	// // we ignore bad reads. RzBuffer fills up with its "overflow byte" on failure.
+	// rz_buf_read_at(mem_buf, address, data, n_bytes);
+	// if (big_endian) {
+	// 	rz_bv_set_from_bytes_be(out_bv, data, 0, n_bits);
+	// } else {
+	// 	rz_bv_set_from_bytes_le(out_bv, data, 0, n_bits);
+	// }
+	// free(data);
 
+	st64 prev_addr = rz_buf_tell(mem_buf);
+	rz_buf_seek(mem_buf, address, RZ_BUF_SET);
+	rz_bv_set_from_buffer_ble(out_bv, mem_buf, n_bytes, big_endian);
+	rz_buf_seek(mem_buf, prev_addr, RZ_BUF_SET);
 	return true;
 }
 
