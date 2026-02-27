a3f1e40c42bc40ee891a1da359cee76104fd32fd
Rifat Arifoglu <rifatarifoglu38@gmail.com>
2026-02-27 08:44:30 +0000

    Fix wrong SPDX copyright.

--- librz/hash/algorithms/ror13/ror13.h
+++ librz/hash/algorithms/ror13/ror13.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2021 deroad <wargio@libero.it>
+// SPDX-FileCopyrightText: 2026 Rifat <rifatarifoglu38@gmail.com>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #ifndef RZ_HASH_ROR13_H
