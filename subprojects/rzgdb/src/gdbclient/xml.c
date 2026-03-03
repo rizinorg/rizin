5897f30c25329d61011fd876001c74a1fc75186a
Suleif <66480590+suleif0@users.noreply.github.com>
2026-03-03 17:27:08 +0000

    Fix a buffer overflow in gdbr_parse_processes_xml (#5973)
    * Fix a buffer overflow in gdbr_parse_processes_xml, and added a regression test for it

--- subprojects/rzgdb/src/gdbclient/xml.c
+++ subprojects/rzgdb/src/gdbclient/xml.c
@@ -361,7 +361,7 @@ exit_err:
 </osdata>
 */
 static int gdbr_parse_processes_xml(libgdbr_t *g, char *xml_data, ut64 len, int pid, RzList *list) {
-	char pidstr[MAX_PID_CHARS + 1], status[1024], cmdline[1024];
+	char pidstr[MAX_PID_CHARS + 1], status[1024], cmdline[4096];
 	char *itemstr, *column, *column_end, *proc_filename;
 	int ret = -1, ipid, column_data_len;
 	RzDebugPid *pid_info = NULL;
@@ -408,8 +408,7 @@ static int gdbr_parse_processes_xml(libgdbr_t *g, char *xml_data, ut64 len, int 
 
 		column += sizeof("<column name=\"command\">") - 1;
 		column_data_len = column_end - column;
-
-		memcpy(cmdline, column, column_data_len);
+		memcpy(cmdline, column, RZ_MIN(column_data_len, sizeof(cmdline)));
 		cmdline[column_data_len] = '\0';
 
 		// Attempt to read the pid's info from /proc. Non UNIX systems will have the
