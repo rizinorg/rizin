acb7d3022f4db8ed615443d1f3fd5841b9fd4c84
JagathP <jagathp0210@gmail.com>
2026-03-11 20:15:51 +0530

    Added the analysis plugin file and test file for classic bpf

--- librz/arch/p/arch_bpf_cs.c
+++
@@ -1,4 +0,0 @@
-#include <deprecated_arch_helper.h>
-#include "asm/asm_bpf_cs.c"
-
-RZ_ARCH_ASM_ONLY_PLUGIN_DEFINE_DEPRECATED(bpf_cs);
--- librz/arch/p/asm/asm_cbpf_cs.c
+++ librz/arch/p/asm/asm_cbpf_cs.c
@@ -1,10 +1,12 @@
+// SPDX-FileCopyrightText: 2026 Jagath-P jagathp0210@gmail.com
+// SPDX-License-Identifier: LGPL-3.0-only
 #include <rz_asm.h>
 #include <rz_lib.h>
 #include "cs_helper.h"
 
-CAPSTONE_DEFINE_PLUGIN_FUNCTIONS(bpf_asm);
+CAPSTONE_DEFINE_PLUGIN_FUNCTIONS(cbpf_asm);
 
-static int bpf_disassemble(RzAsm *a, RzAsmOp *op, const ut8 *buf, int len) {
+static int cbpf_disassemble(RzAsm *a, RzAsmOp *op, const ut8 *buf, int len) {
 	CapstoneContext *ctx = (CapstoneContext *)a->plugin_data;
 	cs_insn *insn;
 	int n, ret = -1;
@@ -30,16 +32,15 @@ static int bpf_disassemble(RzAsm *a, RzAsmOp *op, const ut8 *buf, int len) {
 		rz_asm_op_set_asm(op, "invalid");
 		ret = -1;
 		cs_free(insn, n);
-		return ret;
+		return 8;
 	}
 	if (insn->size != 8 && insn->size != 16) {
 		cs_free(insn, n);
 		ret = -1;
-		return ret;
+		return 8;
 	}
 	op->size = insn->size;
 	ret = op->size;
-
 	if (insn->op_str[0]) {
 		rz_asm_op_setf_asm(op, "%s %s", insn->mnemonic, insn->op_str);
 	} else {
@@ -49,23 +50,23 @@ static int bpf_disassemble(RzAsm *a, RzAsmOp *op, const ut8 *buf, int len) {
 	return ret;
 }
 
-RzAsmPlugin rz_asm_plugin_bpf_cs = {
-	.name = "bpf",
+RzAsmPlugin rz_asm_plugin_cbpf_cs = {
+	.name = "cbpf",
 	.license = "LGPL3",
-	.desc = "BPF disassembly plugin",
-	.arch = "bpf",
+	.desc = "CBPF disassembly plugin",
+	.arch = "cbpf",
 	.bits = 32,
 	.endian = RZ_SYS_ENDIAN_BIG | RZ_SYS_ENDIAN_LITTLE,
-	.init = &bpf_asm_init,
-	.fini = &bpf_asm_fini,
-	.disassemble = &bpf_disassemble,
-	.mnemonics = &bpf_asm_mnemonics
+	.init = &cbpf_asm_init,
+	.fini = &cbpf_asm_fini,
+	.disassemble = &cbpf_disassemble,
+	.mnemonics = &cbpf_asm_mnemonics
 };
 
 #ifndef RZ_PLUGIN_INCORE
 RZ_API RzLibStruct rizin_plugin = {
 	.type = RZ_LIB_TYPE_ASM,
-	.data = &rz_asm_plugin_bpf_cs,
+	.data = &rz_asm_plugin_cbpf_cs,
 	.version = RZ_VERSION
 };
 #endif
