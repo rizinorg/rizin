cd6c542a9c1943f0c4eb1b7fca7ea5f61ade8119
cheese-cakee <farzanaman99@gmail.com>
2026-03-23 22:02:43 +0530

    Handle incomplete UTF-8 tails

--- librz/util/str.c
+++ librz/util/str.c
@@ -4182,6 +4182,22 @@ RZ_API RzStrEnc rz_str_guess_encoding_from_buffer(RZ_NONNULL const ut8 *buffer, 
  * \param  length The real string length.
  * \return The stringified raw buffer
  */
+static inline bool stringification_has_incomplete_tail(const ut8 *buf, ut32 buflen, ut32 i, RzStrEnc enc) {
+	const size_t remaining = buflen - i;
+	switch (enc) {
+	case RZ_STRING_ENC_UTF8:
+		return rz_utf8_size(buf + i) > remaining;
+	case RZ_STRING_ENC_UTF16LE:
+	case RZ_STRING_ENC_UTF16BE:
+		return remaining < 2;
+	case RZ_STRING_ENC_UTF32LE:
+	case RZ_STRING_ENC_UTF32BE:
+		return remaining < 4;
+	default:
+		return false;
+	}
+}
+
 RZ_API RZ_OWN char *rz_str_stringify_raw_buffer(RzStrStringifyOpt *option, RZ_NULLABLE RZ_OUT ut32 *length) {
 	rz_return_val_if_fail(option && option->buffer && option->encoding != RZ_STRING_ENC_GUESS, NULL);
 	if (option->length < 1) {
@@ -4254,6 +4270,9 @@ RZ_API RZ_OWN char *rz_str_stringify_raw_buffer(RzStrStringifyOpt *option, RZ_NU
 
 		if (rsize == 0) {
 			if (option->stop_at_unprintable) {
+				break;
+			}
+			if (option->json && stringification_has_incomplete_tail(buf, buflen, i, enc)) {
 				break;
 			}
 			switch (enc) {
