3bb03ee63efc7505f00153569c4e2a6f89413d2d
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-04-01 23:32:32 +0800

    Inline rz_list_iter_set_data() and shorten it to rz_list_set_val()

--- librz/arch/p/parse/parse_dalvik_pseudo.c
+++ librz/arch/p/parse/parse_dalvik_pseudo.c
@@ -246,7 +246,7 @@ RzList /*<char *>*/ *dalvik_tokenize(const char *assembly, size_t length) {
 		RzListIter *it;
 		rz_list_foreach (tokens, it, buf) {
 			char *repl = rz_str_replace(buf, ",", comma_replace, 1);
-			rz_list_iter_set_data(it, repl);
+			rz_list_set_val(it, repl);
 		}
 	}
 
