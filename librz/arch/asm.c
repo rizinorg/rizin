015add96c205accd08d9c2c32eb1798e9f18846e
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-04-03 20:33:38 +0800

    Add `const` to suitable rz_asm funcs

--- librz/arch/asm.c
+++ librz/arch/asm.c
@@ -159,7 +159,7 @@ static inline int rz_asm_pseudo_org(RzAsm *a, char *input) {
 	return 0;
 }
 
-static inline int rz_asm_pseudo_intN(RzAsm *a, RzAsmOp *op, char *input, int n) {
+static inline int rz_asm_pseudo_intN(const RzAsm *a, RzAsmOp *op, char *input, int n) {
 	ut16 s;
 	ut32 i;
 	ut64 s64 = rz_num_math(NULL, input);
@@ -186,15 +186,15 @@ static inline int rz_asm_pseudo_intN(RzAsm *a, RzAsmOp *op, char *input, int n) 
 	return n;
 }
 
-static inline int rz_asm_pseudo_int16(RzAsm *a, RzAsmOp *op, char *input) {
+static inline int rz_asm_pseudo_int16(const RzAsm *a, RzAsmOp *op, char *input) {
 	return rz_asm_pseudo_intN(a, op, input, 2);
 }
 
-static inline int rz_asm_pseudo_int32(RzAsm *a, RzAsmOp *op, char *input) {
+static inline int rz_asm_pseudo_int32(const RzAsm *a, RzAsmOp *op, char *input) {
 	return rz_asm_pseudo_intN(a, op, input, 4);
 }
 
-static inline int rz_asm_pseudo_int64(RzAsm *a, RzAsmOp *op, char *input) {
+static inline int rz_asm_pseudo_int64(const RzAsm *a, RzAsmOp *op, char *input) {
 	return rz_asm_pseudo_intN(a, op, input, 8);
 }
 
@@ -380,17 +380,17 @@ RZ_API bool rz_asm_plugin_del(RzAsm *a, RZ_NONNULL RzAsmPlugin *p) {
 	return ht_sp_delete(a->plugins, p->name);
 }
 
-RZ_API const RzAsmPlugin *rz_asm_plugin_current(RZ_NONNULL RzAsm *a) {
+RZ_API const RzAsmPlugin *rz_asm_plugin_current(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, false);
 	return a->cur;
 }
 
-RZ_API RZ_OWN RzIterator *rz_asm_plugin_iterator(RZ_NONNULL RzAsm *a) {
+RZ_API RZ_OWN RzIterator *rz_asm_plugin_iterator(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, false);
 	return ht_sp_as_iter(a->plugins);
 }
 
-RZ_API const RzAsmPlugin *rz_asm_plugin_find(RZ_NONNULL RzAsm *a, RZ_NONNULL const char *name) {
+RZ_API const RzAsmPlugin *rz_asm_plugin_find(RZ_NONNULL const RzAsm *a, RZ_NONNULL const char *name) {
 	rz_return_val_if_fail(a && RZ_STR_ISNOTEMPTY(name), NULL);
 	bool found = false;
 	RzAsmPlugin *plugin = ht_sp_find(a->plugins, name, &found);
@@ -400,7 +400,7 @@ RZ_API const RzAsmPlugin *rz_asm_plugin_find(RZ_NONNULL RzAsm *a, RZ_NONNULL con
 	return NULL;
 }
 
-RZ_API bool rz_asm_is_valid(RzAsm *a, const char *name) {
+RZ_API bool rz_asm_is_valid(const RzAsm *a, const char *name) {
 	if (!name || !*name) {
 		return false;
 	}
@@ -465,7 +465,7 @@ static ut32 asm_get_first_default_bits(RzAsmPlugin *h) {
  *
  * \return     If the get_config callback is set, then must return a non-NULL value
  */
-RZ_API RZ_OWN RzConfig *rz_asm_get_new_config(RZ_NONNULL RzAsm *a) {
+RZ_API RZ_OWN RzConfig *rz_asm_get_new_config(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, NULL);
 	if (!a->cur || !a->cur->get_config) {
 		return NULL;
@@ -481,7 +481,7 @@ RZ_API RZ_OWN RzConfig *rz_asm_get_new_config(RZ_NONNULL RzAsm *a) {
  *
  * \return     On success returns a valid pointer, otherwise NULL.
  */
-RZ_API RZ_OWN char *rz_asm_get_sys_opcode_path(RZ_NONNULL RzAsm *a, RZ_NONNULL const char *path) {
+RZ_API RZ_OWN char *rz_asm_get_sys_opcode_path(RZ_NONNULL const RzAsm *a, RZ_NONNULL const char *path) {
 	rz_return_val_if_fail(a && path, NULL);
 	return rz_path_system(a->sdb_opcodes_path, path);
 }
@@ -554,12 +554,12 @@ RZ_DEPRECATE RZ_API void rz_asm_set_cpu(RzAsm *a, const char *cpu) {
 	a->cpu = rz_str_dup(cpu);
 }
 
-RZ_API const char *rz_asm_get_cpu(RZ_NONNULL RzAsm *a) {
+RZ_API const char *rz_asm_get_cpu(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, "");
 	return a->cpu;
 }
 
-RZ_API const char *rz_asm_get_platforms(RZ_NONNULL RzAsm *a) {
+RZ_API const char *rz_asm_get_platforms(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, "");
 	return rz_str_get(a->platforms);
 }
@@ -578,7 +578,7 @@ RZ_API void rz_asm_set_features(RZ_NONNULL RzAsm *a, RZ_NULLABLE const char *fea
 	a->features = rz_str_dup(features);
 }
 
-RZ_API const char *rz_asm_get_features(RZ_NONNULL RzAsm *a) {
+RZ_API const char *rz_asm_get_features(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, "");
 	return rz_str_get(a->features);
 }
@@ -589,7 +589,7 @@ RZ_API void rz_asm_set_pseudo(RZ_NONNULL RzAsm *a, bool enable) {
 	a->pseudo = enable;
 }
 
-RZ_API bool rz_asm_get_pseudo(RZ_NONNULL RzAsm *a) {
+RZ_API bool rz_asm_get_pseudo(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, "");
 	return a->pseudo;
 }
@@ -599,7 +599,7 @@ RZ_API void rz_asm_set_utf8(RZ_NONNULL RzAsm *a, bool utf8) {
 	a->utf8 = utf8;
 }
 
-RZ_API bool rz_asm_get_utf8(RZ_NONNULL RzAsm *a) {
+RZ_API bool rz_asm_get_utf8(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, false);
 	return a->utf8;
 }
@@ -609,7 +609,7 @@ RZ_API void rz_asm_set_segment_granularity(RZ_NONNULL RzAsm *a, int seggrn) {
 	a->seggrn = seggrn;
 }
 
-RZ_API int rz_asm_get_segment_granularity(RZ_NONNULL RzAsm *a) {
+RZ_API int rz_asm_get_segment_granularity(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, false);
 	return a->seggrn;
 }
@@ -622,7 +622,7 @@ RZ_API void rz_asm_set_pc_align(RZ_NONNULL RzAsm *a, ut32 pc_align) {
 	a->pcalign = pc_align;
 }
 
-RZ_API ut32 rz_asm_get_pc_align(RZ_NONNULL RzAsm *a) {
+RZ_API ut32 rz_asm_get_pc_align(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, 1);
 	if (a->pcalign < 2) {
 		return 1;
@@ -635,7 +635,7 @@ RZ_API void rz_asm_set_syscall(RZ_NONNULL RzAsm *a, RzSyscall *syscall) {
 	a->syscall = syscall;
 }
 
-RZ_API RzSyscall *rz_asm_get_syscall(RZ_NONNULL RzAsm *a) {
+RZ_API RzSyscall *rz_asm_get_syscall(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, NULL);
 	return a->syscall;
 }
@@ -645,7 +645,7 @@ RZ_API void rz_asm_set_invalid_as_hex_flag(RZ_NONNULL RzAsm *a, bool invhex) {
 	a->invhex = invhex;
 }
 
-RZ_API bool rz_asm_get_invalid_as_hex_flag(RZ_NONNULL RzAsm *a) {
+RZ_API bool rz_asm_get_invalid_as_hex_flag(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, false);
 	return a->invhex;
 }
@@ -655,7 +655,7 @@ RZ_API void rz_asm_set_show_immediate_hashtag(RZ_NONNULL RzAsm *a, bool show) {
 	a->immdisp = show;
 }
 
-RZ_API bool rz_asm_get_show_immediate_hashtag(RZ_NONNULL RzAsm *a) {
+RZ_API bool rz_asm_get_show_immediate_hashtag(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, false);
 	return a->immdisp;
 }
@@ -669,7 +669,7 @@ RZ_DEPRECATE RZ_API void rz_asm_set_core(RZ_NONNULL RzAsm *a, RZ_NULLABLE void *
 	a->core = core;
 }
 
-RZ_DEPRECATE RZ_API RZ_BORROW RzBinBind *rz_asm_get_bin_bind(RzAsm *a) {
+RZ_DEPRECATE RZ_API RZ_BORROW const RzBinBind *rz_asm_get_bin_bind(const RzAsm *a) {
 	rz_return_val_if_fail(a, NULL);
 	return &a->binb;
 }
@@ -684,47 +684,47 @@ RZ_DEPRECATE RZ_API int rz_asm_set_bits(RzAsm *a, int bits) {
 	return false;
 }
 
-RZ_DEPRECATE RZ_API int rz_asm_get_bits(RZ_NONNULL RzAsm *a) {
+RZ_DEPRECATE RZ_API int rz_asm_get_bits(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, 0);
 	return a->bits;
 }
 
-RZ_DEPRECATE RZ_API bool rz_asm_is_bits(RzAsm *a, int bits) {
+RZ_DEPRECATE RZ_API bool rz_asm_is_bits(const RzAsm *a, int bits) {
 	rz_return_val_if_fail(a, false);
 	return a->bits == bits;
 }
 
-RZ_DEPRECATE RZ_API int rz_asm_get_plugin_bits(RZ_NONNULL RzAsm *a) {
+RZ_DEPRECATE RZ_API int rz_asm_get_plugin_bits(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, 0);
 	return a->cur ? a->cur->bits : 0;
 }
 
-RZ_DEPRECATE RZ_API const char *rz_asm_get_plugin_cpus(RZ_NONNULL RzAsm *a) {
+RZ_DEPRECATE RZ_API const char *rz_asm_get_plugin_cpus(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, NULL);
 	return a->cur ? rz_str_get(a->cur->cpus) : "";
 }
 
-RZ_DEPRECATE RZ_API const char *rz_asm_get_plugin_platforms(RZ_NONNULL RzAsm *a) {
+RZ_DEPRECATE RZ_API const char *rz_asm_get_plugin_platforms(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, NULL);
 	return a->cur ? rz_str_get(a->cur->platforms) : "";
 }
 
-RZ_DEPRECATE RZ_API const char *rz_asm_get_plugin_features(RZ_NONNULL RzAsm *a) {
+RZ_DEPRECATE RZ_API const char *rz_asm_get_plugin_features(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, NULL);
 	return a->cur ? rz_str_get(a->cur->features) : "";
 }
 
-RZ_API ut32 rz_asm_get_endianness(RzAsm *a) {
+RZ_API ut32 rz_asm_get_endianness(const RzAsm *a) {
 	rz_return_val_if_fail(a && a->cur, RZ_SYS_ENDIAN_NONE);
 	return a->cur->endian;
 }
 
-RZ_API bool rz_asm_is_big_endian_set(RZ_NONNULL RzAsm *a) {
+RZ_API bool rz_asm_is_big_endian_set(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, false);
 	return a->big_endian;
 }
 
-RZ_API bool rz_asm_support_endianness(RzAsm *a, ut32 endian) {
+RZ_API bool rz_asm_support_endianness(const RzAsm *a, ut32 endian) {
 	rz_return_val_if_fail(a && a->cur, false);
 
 	if (a->cur->endian == RZ_SYS_ENDIAN_NONE || a->cur->endian == RZ_SYS_ENDIAN_BI) {
@@ -762,17 +762,17 @@ RZ_API void rz_asm_set_syntax(RZ_NONNULL RzAsm *a, RzAsmSyntax syntax) {
 	a->syntax = syntax;
 }
 
-RZ_API RzAsmSyntax rz_asm_get_syntax(RZ_NONNULL RzAsm *a) {
+RZ_API RzAsmSyntax rz_asm_get_syntax(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, RZ_ASM_SYNTAX_NONE);
 	return a->syntax;
 }
 
-RZ_API bool rz_asm_is_syntax(RZ_NONNULL RzAsm *a, RzAsmSyntax syntax) {
+RZ_API bool rz_asm_is_syntax(RZ_NONNULL const RzAsm *a, RzAsmSyntax syntax) {
 	rz_return_val_if_fail(a, false);
 	return a->syntax == syntax;
 }
 
-RZ_API ut64 rz_asm_get_pc(RZ_NONNULL RzAsm *a) {
+RZ_API ut64 rz_asm_get_pc(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, 0);
 	return a->pc;
 }
@@ -856,14 +856,14 @@ RZ_API int rz_asm_disassemble(RzAsm *a, RzAsmOp *op, const ut8 *buf, int len) {
 
 typedef int (*Ase)(const RzAsm *a, RzAsmOp *op, const char *buf);
 
-static bool assemblerMatches(RzAsm *a, RzAsmPlugin *h) {
+static bool assemblerMatches(const RzAsm *a, RzAsmPlugin *h) {
 	if (!a || !h->arch || !h->assemble || !has_bits(h, a->bits)) {
 		return false;
 	}
 	return (!strncmp(a->cur->arch, h->arch, strlen(a->cur->arch)));
 }
 
-static Ase findAssembler(RzAsm *a, const char *kw) {
+static Ase findAssembler(const RzAsm *a, const char *kw) {
 	Ase ase = NULL;
 	RzIterator *iter = ht_sp_as_iter(a->plugins);
 	RzAsmPlugin **val;
@@ -951,7 +951,7 @@ RZ_API void rz_asm_list_directives(void) {
  *
  * \return     On success true, otherwise false.
  */
-RZ_API bool rz_asm_software_breakpoint(RZ_NONNULL RzAsm *a, RZ_NONNULL RzAsmOp *op) {
+RZ_API bool rz_asm_software_breakpoint(RZ_NONNULL const RzAsm *a, RZ_NONNULL RzAsmOp *op) {
 	rz_return_val_if_fail(a && op, false);
 	memset(op, 0, sizeof(RzAsmOp));
 
@@ -963,7 +963,7 @@ RZ_API bool rz_asm_software_breakpoint(RZ_NONNULL RzAsm *a, RZ_NONNULL RzAsmOp *
 }
 
 // returns instruction size
-RZ_API int rz_asm_assemble(RzAsm *a, RzAsmOp *op, const char *buf) {
+RZ_API int rz_asm_assemble(const RzAsm *a, RzAsmOp *op, const char *buf) {
 	rz_return_val_if_fail(a && op && buf, 0);
 	int ret = 0;
 	char *b = rz_str_dup(buf);
@@ -1443,23 +1443,23 @@ fail:
 	return rz_asm_code_free(acode);
 }
 
-RZ_API int rz_asm_get_offset(RzAsm *a, int type, int idx) { // link to rbin
+RZ_API int rz_asm_get_offset(const RzAsm *a, int type, int idx) { // link to rbin
 	if (a && a->binb.bin && a->binb.get_offset) {
 		return a->binb.get_offset(a->binb.bin, type, idx);
 	}
 	return -1;
 }
 
-RZ_API char *rz_asm_describe(RzAsm *a, const char *str) {
+RZ_API char *rz_asm_describe(const RzAsm *a, const char *str) {
 	return (a && a->pair) ? sdb_get(a->pair, str) : NULL;
 }
 
-RZ_API void rz_asm_describe_iterate(RZ_NONNULL RzAsm *a, RZ_NONNULL SdbForeachCallback cb, RZ_NULLABLE void *user) {
+RZ_API void rz_asm_describe_iterate(RZ_NONNULL const RzAsm *a, RZ_NONNULL SdbForeachCallback cb, RZ_NULLABLE void *user) {
 	rz_return_if_fail(a);
 	sdb_foreach(a->pair, cb, user);
 }
 
-RZ_API RZ_BORROW HtSP /*<RzAsmPlugin *>*/ *rz_asm_get_plugins(RZ_BORROW RZ_NONNULL RzAsm *a) {
+RZ_API RZ_BORROW HtSP /*<RzAsmPlugin *>*/ *rz_asm_get_plugins(RZ_BORROW RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, NULL);
 	return a->plugins;
 }
@@ -1468,12 +1468,12 @@ RZ_API bool rz_asm_set_arch(RzAsm *a, const char *name, int bits) {
 	return rz_asm_use(a, name) ? rz_asm_set_bits(a, bits) : false;
 }
 
-RZ_API const char *rz_asm_get_arch(RZ_NONNULL RzAsm *a) {
+RZ_API const char *rz_asm_get_arch(RZ_NONNULL const RzAsm *a) {
 	rz_return_val_if_fail(a, RZ_SYS_ARCH);
 	return a->cur ? a->cur->name : RZ_SYS_ARCH;
 }
 
-RZ_API bool rz_asm_is_arch(RZ_NONNULL RzAsm *a, RZ_NONNULL const char *name) {
+RZ_API bool rz_asm_is_arch(RZ_NONNULL const RzAsm *a, RZ_NONNULL const char *name) {
 	rz_return_val_if_fail(a && RZ_STR_ISNOTEMPTY(name), false);
 	return a->cur && RZ_STR_EQ(a->cur->name, name);
 }
@@ -1526,7 +1526,7 @@ RZ_API int rz_asm_syntax_from_string(const char *name) {
 	return -1;
 }
 
-RZ_API char *rz_asm_mnemonics(RzAsm *a, int id, bool json) {
+RZ_API char *rz_asm_mnemonics(const RzAsm *a, int id, bool json) {
 	rz_return_val_if_fail(a && a->cur, NULL);
 	if (a->cur->mnemonics) {
 		return a->cur->mnemonics(a, id, json);
@@ -1534,7 +1534,7 @@ RZ_API char *rz_asm_mnemonics(RzAsm *a, int id, bool json) {
 	return NULL;
 }
 
-RZ_API int rz_asm_mnemonics_byname(RzAsm *a, const char *name) {
+RZ_API int rz_asm_mnemonics_byname(const RzAsm *a, const char *name) {
 	rz_return_val_if_fail(a && a->cur, 0);
 	if (a->cur->mnemonics) {
 		int i;
