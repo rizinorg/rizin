e4f5c3cf8bd41ef5472ccfe04f431c1e8c26ab9c
Riccardo Schirone <rschirone91@gmail.com>
2026-04-06 20:55:37 +0200

    librz/core: do not ask for confirmation if non-interactive

--- librz/core/cmd/cmd_debug.c
+++ librz/core/cmd/cmd_debug.c
@@ -3103,8 +3103,10 @@ RZ_IPI RzCmdStatus rz_cmd_debug_pid_attach_handler(RzCore *core, int argc, const
 		return RZ_CMD_STATUS_ERROR;
 	}
 	if (rz_core_is_debug(core) || (core->dbg->cur && core->dbg->cur->pids && core->dbg->pid != -1)) {
-		if (!rz_cons_yesno('n', "core: A debug session is already active. Do you want to attach to another process? (y/N) ")) {
-			return RZ_CMD_STATUS_ERROR;
+		if (rz_cons_is_interactive()) {
+			if (!rz_cons_yesno('n', "core: A debug session is already active. Do you want to attach to another process? (y/N) ")) {
+				return RZ_CMD_STATUS_ERROR;
+			}
 		}
 	}
 	rz_core_debug_attach(core, pid);
