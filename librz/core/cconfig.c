26967d8992ee0e7c50c699c667b6a62bb3212cd4
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-05-06 22:58:23 +0800

    log.level help: Don't show `0:DEBUG` on Release builds

--- librz/core/cconfig.c
+++ librz/core/cconfig.c
@@ -3352,8 +3352,11 @@ RZ_API int rz_core_config_init(RzCore *core) {
 	/* log */
 	// RZ_LOGLEVEL / log.level
 	p = rz_sys_getenv("RZ_LOGLEVEL");
-	SETICB("log.level", p ? atoi(p) : RZ_DEFAULT_LOGLVL, cb_log_config_level, "Target log level/severity"
-										  " (0:DEBUG, 1:VERBOSE, 2:INFO, 3:WARN, 4:ERROR, 5:FATAL)");
+	SETICB("log.level", p ? atoi(p) : RZ_DEFAULT_LOGLVL, cb_log_config_level, "Target log level/severity ("
+#if RZ_BUILD_DEBUG
+										  "0:DEBUG, "
+#endif
+										  "1:VERBOSE, 2:INFO, 3:WARN, 4:ERROR, 5:FATAL)");
 	free(p);
 
 #if RZ_BUILD_DEBUG
