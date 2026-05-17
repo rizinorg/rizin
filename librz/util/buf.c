e1e4cd0513b3b6dfe40fe058a7c3e4ee4951b143
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-05-17 23:27:12 +0800

    Prevent RzBuffer's `Oxff_priv` from overriding `io.0xff`

--- librz/util/buf.c
+++ librz/util/buf.c
@@ -483,6 +483,7 @@ RZ_API RZ_OWN RzBuffer *rz_buf_new_sparse(ut8 Oxff) {
 	RzBuffer *b = new_buffer(RZ_BUFFER_SPARSE, NULL);
 	if (b) {
 		b->Oxff_priv = Oxff;
+		b->Oxff_priv_override = true;
 	}
 
 	return b;
@@ -1245,7 +1246,7 @@ RZ_API st64 rz_buf_read(RZ_NONNULL RzBuffer *b, RZ_NONNULL ut8 RZ_OUT *buf, ut64
 		return -1;
 	}
 
-	if (len > result) {
+	if (len > result && b->Oxff_priv_override) {
 		memset(buf + result, b->Oxff_priv, len - result);
 	}
 
@@ -1401,6 +1402,7 @@ RZ_API void rz_buf_set_overflow_byte(RZ_NONNULL RzBuffer *b, ut8 Oxff) {
 	rz_return_if_fail(b);
 
 	b->Oxff_priv = Oxff;
+	b->Oxff_priv_override = true;
 }
 
 /**
