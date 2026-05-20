1bf9f61374ff805631b3d53355a93c9202967fe8
Rot127 <unisono@quyllur.org>
2026-05-20 20:30:04 +0200

    Add benchmark for rz_vector_swap

--- test/bench/bench_vector.c
+++ test/bench/bench_vector.c
@@ -43,12 +43,31 @@ static void bench_rz_vector_remove_at(RzTable *t_out) {
 	rz_vector_free(v);
 }
 
+static void bench_rz_vector_swap(RzTable *t_out) {
+	RzVector *v = rz_vector_new(sizeof(ut64), NULL, NULL);
+
+	for (size_t i = 0; i < ITERATION_COUNT; ++i) {
+		rz_vector_push(v, &i);
+	}
+
+	{
+		RZ_BENCH_RUN_I("[RzVector] rz_vector_swap", i, t_out, ITERATION_COUNT, {
+			size_t index_a = rz_num_rand32(ITERATION_COUNT - 1);
+			size_t index_b = rz_num_rand32(ITERATION_COUNT - 1);
+			rz_vector_swap(v, index_a, index_b);
+		});
+	}
+
+	rz_vector_free(v);
+}
+
 int main() {
 	RzTable *t = rz_table_new();
 	RZ_BENCH_TABLE_INIT(t);
 
 	// Micro benchmarks
 	bench_rz_vector_remove_at(t);
+	bench_rz_vector_swap(t);
 
 	// Print results
 	RZ_BENCH_TABLE_PRINT_AND_FREE(t);
