5b6e9b58df7d8b6d6ea145e301dbbd098ed9b967
billow <billow.fun@gmail.com>
2026-05-23 22:34:03 +0800

    Update Capstone and add m68k ColdFire support

--- librz/arch/isa/m68k/m68k_cs.h
+++ librz/arch/isa/m68k/m68k_cs.h
@@ -9,12 +9,6 @@
 
 #define M68K_LONGEST_INSTRUCTION 22
 
-#ifdef RZ_CAPSTONE_HAS_M68K_CPU32
-#define M68K_CPUS "68000,68010,68020,68030,68040,68060,cpu32"
-#else
-#define M68K_CPUS "68000,68010,68020,68030,68040,68060"
-#endif
-
 static inline cs_mode rz_m68k_cs_mode(const char *cpu) {
 	if (!cpu) {
 		return CS_MODE_M68K_040;
@@ -22,6 +16,29 @@ static inline cs_mode rz_m68k_cs_mode(const char *cpu) {
 #ifdef RZ_CAPSTONE_HAS_M68K_CPU32
 	if (strstr(cpu, "cpu32") || strstr(cpu, "CPU32")) {
 		return CS_MODE_M68K_CPU32;
+	}
+#endif
+#ifdef RZ_CAPSTONE_HAS_M68K_COLDFIRE
+	if (strstr(cpu, "cfv1") || strstr(cpu, "CFV1")) {
+		return CS_MODE_M68K_CFV1;
+	}
+	if (strstr(cpu, "cfv2") || strstr(cpu, "CFV2")) {
+		return CS_MODE_M68K_CFV2;
+	}
+	if (strstr(cpu, "cfv3") || strstr(cpu, "CFV3")) {
+		return CS_MODE_M68K_CFV3;
+	}
+	if (strstr(cpu, "cfv4e") || strstr(cpu, "CFV4E")) {
+		return CS_MODE_M68K_CFV4E;
+	}
+	if (strstr(cpu, "cfv4") || strstr(cpu, "CFV4")) {
+		return CS_MODE_M68K_CFV4;
+	}
+	if (strstr(cpu, "cfv5") || strstr(cpu, "CFV5")) {
+		return CS_MODE_M68K_CFV5;
+	}
+	if (strstr(cpu, "coldfire") || strstr(cpu, "ColdFire") || strstr(cpu, "COLDFIRE")) {
+		return CS_MODE_M68K_COLDFIRE;
 	}
 #endif
 	if (strstr(cpu, "68000")) {
