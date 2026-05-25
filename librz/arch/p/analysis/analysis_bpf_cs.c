a845484cdeffe67f34568555938e08787af5ae39
JagathP <jagathp0210@gmail.com>
2026-05-25 15:43:13 +0530

    Fix email formatting

--- librz/arch/p/analysis/analysis_bpf_cs.c
+++ librz/arch/p/analysis/analysis_bpf_cs.c
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2026 Jagath-P jagathp0210@gmail.com
+// SPDX-FileCopyrightText: 2026 Jagath-P <jagathp0210@gmail.com>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 #include <rz_analysis.h>
