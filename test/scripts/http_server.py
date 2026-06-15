150ce4392f5f2ec10e71c1df3f675b3b6331b96d
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-06-15 22:35:59 +0800

    Use scripts/http_server.py instead

---
+++ test/scripts/http_server.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+r"""
+This script launches python -m http.server in a subprocess and waits until it's ready to receive
+a new connection.
+
+"""
+
+import http.client
+import subprocess
+
+
+def main():
+    subprocess.run(
+        "python3 -m http.server --bind 127.0.0.1 --directory www &",
+        shell=True,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    while True:
+        try:
+            http.client.HTTPConnection("127.0.0.1", 8000, timeout=5).connect()
+        except:
+            continue
+        break
+
+
+if __name__ == "__main__":
+    main()
