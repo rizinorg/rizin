a2a3d0bc5e1f7e11bb936fb8d9f091e712969208
Anton Kochkov <anton.kochkov@gmail.com>
2026-06-18 08:28:53 +0400

    rz-find: flush output to avoid undeterminism

--- librz/main/rz-find.c
+++ librz/main/rz-find.c
@@ -160,6 +160,8 @@ static int hit(RzSearchKeyword *kw, void *user, ut64 addr) {
 	}
 	if (ro->exec_command) {
 		char *command = rz_str_newf("%s %s", ro->exec_command, ro->curfile);
+		// Flush buffered output before spawning the command
+		fflush(stdout);
 		int status = rz_sys_system(command);
 		if (status == -1) {
 			RZ_LOG_ERROR("Failed to execute command: %s\n", command);
