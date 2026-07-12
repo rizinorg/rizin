9836b05b05c98863667cb0acd5c5c765499dc31a
Florian Märkl <info@florianmaerkl.de>
2026-07-12 10:42:12 +0200

    Rewrite unsafe string handling in i8080 disasm

--- librz/arch/p/asm/asm_i8080.c
+++ librz/arch/p/asm/asm_i8080.c
@@ -9,7 +9,7 @@
 #include <i8080/i8080dis.h>
 
 static int do_disassemble(const RzAsm *a, RzAsmOp *op, const ut8 *buf, int len) {
-	int dlen = i8080_disasm(buf, rz_strbuf_get(&op->buf_asm), len);
+	int dlen = i8080_disasm(buf, len, &op->buf_asm);
 	return op->size = RZ_MAX(0, dlen);
 }
 
