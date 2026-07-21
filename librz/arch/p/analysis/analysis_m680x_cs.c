9e1241ab7fd1d32cefd4eb9ec966f9745f65000c
Naren Sirigere <sirigere.naren@gmail.com>
2026-07-21 19:00:12 +0530

    Share CPU mode selection

--- librz/arch/p/analysis/analysis_m680x_cs.c
+++ librz/arch/p/analysis/analysis_m680x_cs.c
@@ -7,42 +7,6 @@
 
 #include <capstone/m680x.h>
 
-static int m680xmode(const char *str) {
-	if (!str) {
-		return CS_MODE_M680X_6800;
-	}
-	// replace this with the asm.features?
-	if (strstr(str, "6800") || strstr(str, "6802") || strstr(str, "6808")) {
-		return CS_MODE_M680X_6800;
-	} else if (strstr(str, "6801") || strstr(str, "6803")) {
-		return CS_MODE_M680X_6801;
-	} else if (strstr(str, "6805")) {
-		return CS_MODE_M680X_6805;
-	} else if (rz_str_casestr(str, "68hc08")) {
-		return CS_MODE_M680X_6808;
-	} else if (strstr(str, "6809")) {
-		return CS_MODE_M680X_6809;
-	} else if (strstr(str, "6811")) {
-		return CS_MODE_M680X_6811;
-	} else if (rz_str_casestr(str, "cpu12")) {
-		return CS_MODE_M680X_CPU12;
-	} else if (strstr(str, "6301")) {
-		return CS_MODE_M680X_6301;
-	} else if (strstr(str, "6309")) {
-		return CS_MODE_M680X_6309;
-	} else if (rz_str_casestr(str, "hcs08")) {
-		return CS_MODE_M680X_HCS08;
-	}
-#ifdef RZ_CAPSTONE_HAS_M680X_HCS12X
-	else if (rz_str_casestr(str, "rs08")) {
-		return CS_MODE_M680X_RS08;
-	} else if (rz_str_casestr(str, "hcs12x")) {
-		return CS_MODE_M680X_HCS12X;
-	}
-#endif
-	return CS_MODE_M680X_6800;
-}
-
 #define OP(x)   insn->detail->m680x.operands[x]
 #define TYPE(x) OP(x).type
 #define IMM(x)  OP(x).imm
@@ -79,7 +43,7 @@ static int analyze_op(RzAnalysis *a, RzAnalysisOp *op, ut64 addr, const ut8 *buf
 	int n, ret, opsize = -1;
 	cs_insn *insn;
 
-	int mode = m680xmode(rz_analysis_get_cpu(a));
+	int mode = m680x_mode(rz_analysis_get_cpu(a));
 
 	if (mode != ctx->omode || a->bits != ctx->obits) {
 		cs_close(&ctx->handle);
