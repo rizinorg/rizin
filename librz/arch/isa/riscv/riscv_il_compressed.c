f82b9bb234d7a371cc67c48ce410541e20a712fb
مصطفي محمود كمال الدين <48567303+moste00@users.noreply.github.com>
2026-07-25 18:26:55 +0300

    workaround for capstone c.slli bug

--- librz/arch/isa/riscv/riscv_il_compressed.c
+++ librz/arch/isa/riscv/riscv_il_compressed.c
@@ -30,7 +30,19 @@ DEFINE_ALIAS_LIFTER(c_addi, addi)
 DEFINE_ALIAS_LIFTER(c_addi16sp, addi)
 DEFINE_ALIAS_LIFTER(c_addi4spn, addi)
 DEFINE_ALIAS_LIFTER(c_add, add)
-DEFINE_ALIAS_LIFTER(c_slli, slli)
+
+RzILOpEffect *rz_riscv_lift_c_slli(RZ_BORROW RZ_NONNULL RzAnalysis *analysis, RZ_NONNULL RzAnalysisOp *op, RZ_NONNULL cs_insn *insn, ut64 current_addr, size_t size) {
+	if (insn->detail->riscv.op_count == 2 &&
+		insn->detail->riscv.operands[0].type == RISCV_OP_REG &&
+		insn->detail->riscv.operands[0].reg == RISCV_REG_X0 &&
+		insn->detail->riscv.operands[1].type == RISCV_OP_IMM) {
+		// Capstone exposes the c.slli hint form as the same public ID as c.slli.
+		// It should later fix this by having a different public ID for the hint form.
+		return NOP();
+	}
+	return rz_riscv_lift_slli(analysis, op, insn, current_addr, size);
+}
+
 DEFINE_ALIAS_LIFTER(c_andi, andi)
 DEFINE_ALIAS_LIFTER(c_srli, srli)
 DEFINE_ALIAS_LIFTER(c_srai, srai)
@@ -72,4 +84,3 @@ DEFINE_LIFTER_FOR_ONEWAY_JUMP(c_beqz, DECODE_C_BRANCH_ZERO, BRANCH(EQ(rs1, UN(an
 DEFINE_LIFTER_FOR_ONEWAY_JUMP(c_bnez, DECODE_C_BRANCH_ZERO, BRANCH(NE(rs1, UN(analysis->bits, 0)), JMP(imm), JMP(UN(analysis->bits, current_addr + size))))
 
 #include <rz_il/rz_il_opbuilder_end.h>
-
