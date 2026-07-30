6970493f61019853c694dbfc5c8bd94b9291d5cb
Rot127 <unisono@quyllur.org>
2026-07-30 13:14:16 +0200

    Add Xtensa ESP32s3 CPU.
    Moves the asm tests to a properly configured test file.

--- librz/arch/p/asm/asm_xtensa_cs.c
+++ librz/arch/p/asm/asm_xtensa_cs.c
@@ -34,6 +34,7 @@ static char **xtensa_cpu_descriptions() {
 	static char *cpu_desc[] = {
 		"esp32", "Xtensa microcontroller with Wi-Fi and Bluetooth capabilities",
 		"esp32s2", "Xtensa microcontroller with Wi-Fi and USB OTG support",
+		"esp32s3", "Xtensa microcontroller with Wi-Fi, USB OTG support, and HIFI3",
 		"esp8266", "Xtensa microcontroller with Wi-Fi support",
 		NULL
 	};
@@ -46,7 +47,7 @@ RzAsmPlugin rz_asm_plugin_xtensa_cs = {
 	.desc = "Tensilica Xtensa Capstone-based disassembler",
 	.author = "billow",
 	.arch = "xtensa",
-	.cpus = "esp32,esp32s2,esp8266",
+	.cpus = "esp32,esp32s2,esp32s3,esp8266",
 	.bits = 32,
 	.endian = RZ_SYS_ENDIAN_LITTLE | RZ_SYS_ENDIAN_BIG,
 	.disassemble = asm_xtensa_disassemble,
