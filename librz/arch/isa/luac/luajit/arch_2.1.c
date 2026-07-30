2e834543d6f820720aed06f1bd1da23fdd92d441
Arya H R <aryarajappa77@gmail.com>
2026-07-31 00:19:01 +0530

    librz/arch/lua: reduced heap allocations during LuaJIT processing (#6596)
    Co-authored-by: Arya-1-HR <aryagowda177@gmail.com>

--- librz/arch/isa/luac/luajit/arch_2.1.c
+++ librz/arch/isa/luac/luajit/arch_2.1.c
@@ -10,24 +10,24 @@ st32 luajitop_get_value(ut32 instr) {
 	return offset;
 }
 
-char *luajitop_new_str_3arg(char *opname, int a, int b, int c) {
-	return rz_str_newf("%s r%d %d %d", opname, a, b, c);
+void luajitop_setf_asm_3arg(RzAsmOp *op, char *opname, int a, int b, int c) {
+	rz_asm_op_setf_asm(op, "%s r%d %d %d", opname, a, b, c);
 }
 
-char *luajitop_new_str_2arg(char *opname, int a, int b) {
-	return rz_str_newf("%s r%d %d", opname, a, b);
+void luajitop_setf_asm_2arg(RzAsmOp *op, char *opname, int a, int b) {
+	rz_asm_op_setf_asm(op, "%s r%d %d", opname, a, b);
 }
 
-char *luajitop_new_str_1arg(char *opname, int a) {
-	return rz_str_newf("%s %d", opname, a);
+void luajitop_setf_asm_1arg(RzAsmOp *op, char *opname, int a) {
+	rz_asm_op_setf_asm(op, "%s r%d", opname, a);
 }
 
 // For Register to constant value (e.g., KSHORT r1, 8)
-char *luajitop_new_str_reg_const(const char *opname, ut32 a, st32 d) {
-	return rz_str_newf("%s r%d %d", opname, a, d);
+void luajitop_setf_asm_reg_const(RzAsmOp *op, const char *opname, ut32 a, st32 d) {
+	rz_asm_op_setf_asm(op, "%s r%d %d", opname, a, d);
 }
 
 // For Register to Register (e.g., MOV r5, r4)
-char *luajitop_new_str_reg_reg(const char *opname, ut32 a, ut32 d) {
-	return rz_str_newf("%s r%d r%d", opname, a, d);
+void luajitop_setf_asm_reg_reg(RzAsmOp *op, const char *opname, ut32 a, st32 d) {
+	rz_asm_op_setf_asm(op, "%s r%d r%d", opname, a, d);
 }
