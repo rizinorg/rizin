1cee6c6aea44652251fec53e8488f37844cd4828
مصطفي محمود كمال الدين <48567303+moste00@users.noreply.github.com>
2026-08-16 23:48:51 +0300

    minor refactor to use output modes instead of hardcoded j character for json output

--- librz/core/cmd/cmd.c
+++ librz/core/cmd/cmd.c
@@ -1447,9 +1447,9 @@ fail:
 
 DEFINE_HANDLE_TS_FCN_AND_SYMBOL(help_stmt) {
 	size_t node_str_len = strlen(node_string);
-	if (RZ_STR_EQ(node_string, "?+j")) {
+	if (rz_str_startswith(node_string, "?+")) {
 		const char *argv[1] = { node_string };
-		return rz_cmd_catalog_json_handler(state->core, 1, argv);
+		return rz_cmd_catalog_json_handler(state->core, 1, argv, RZ_OUTPUT_MODE_JSON);
 	} else if (node_str_len >= 2 && RZ_STR_EQ(node_string + node_str_len - 2, "?*")) {
 		node_string[node_str_len - 2] = 0;
 		const char *argv[2] = { NULL, node_string };
--- librz/core/cmd/cmd_help.c
+++ librz/core/cmd/cmd_help.c
@@ -117,8 +117,12 @@ error:
 	goto beach;
 }
 
-// "?+j"
-RZ_IPI RzCmdStatus rz_cmd_catalog_json_handler(RzCore *core, int argc, const char **argv) {
+// "?+"
+RZ_IPI RzCmdStatus rz_cmd_catalog_json_handler(RzCore *core, int argc, const char **argv, RzOutputMode mode) {
+	if (mode != RZ_OUTPUT_MODE_JSON) {
+		RZ_LOG_ERROR("Command ?+ does not support output mode %d. Only json is supported.\n", mode);
+		return RZ_CMD_STATUS_ERROR;
+	}
 	PJ *pj = pj_new();
 	if (!pj || !rz_cmd_get_tree_json(core->rcmd, pj)) {
 		pj_free(pj);
