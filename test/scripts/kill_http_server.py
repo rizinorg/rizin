77bb89961fe6af42967ebcfcfaaa344b8096c34b
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-09-15 18:11:56 +0800

    Satisfy `black`

--- test/scripts/kill_http_server.py
+++ test/scripts/kill_http_server.py
@@ -7,7 +7,7 @@ This script kills the http.server launched by http_server.py.
 
 import psutil
 
-for p in psutil.process_iter(['cmdline']):
+for p in psutil.process_iter(["cmdline"]):
     if "python3 -m http.server" in " ".join(p.cmdline()):
         p.kill()
         break
