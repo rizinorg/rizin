8da315e93db7bd0ed44d3d60efd5053d044e3317
609bob <1850029304@qq.com>
2026-09-18 19:55:13 +0800

    fix out-of-bounds access and wrong rgb: offset in rz_str_ansi_trim() in librz/util/str_trim.c

--- librz/util/str_trim.c
+++ librz/util/str_trim.c
@@ -11,6 +11,9 @@
 #include <ctype.h>
 #include <stdarg.h>
 
+#define RZ_ANSI_OSC_RGB_PREFIX_LEN 5
+#define RZ_ANSI_OSC_RGB_LEN 18
+
 // TODO: simplify this horrible loop
 RZ_API void rz_str_trim_path(char *s) {
 	char *src, *dst, *p;
@@ -227,8 +230,9 @@ RZ_API int rz_str_ansi_trim(char *str, int str_len, int n) {
 			if (ch2 == '\\') {
 				i++;
 			} else if (ch2 == ']') {
-				if (i + 2 + 5 + 4 <= str_len && !strncmp(str + 2 + 5, "rgb:", 4)) {
-					i += 18;
+				int rgb_off = strlen("\x1b]") + RZ_ANSI_OSC_RGB_PREFIX_LEN; /* ESC ] + opaque 5-byte prefix */
+				if (i + rgb_off + strlen("rgb:") <= str_len && !strncmp(str + i + rgb_off, "rgb:", 4)) {  
+					i += RZ_ANSI_OSC_RGB_LEN; /* whole OSC rgb: sequence incl. terminator */
 				}
 			} else if (ch2 == '[') {
 				for (++i; (i < str_len) && str[i] && str[i] != 'J' && str[i] != 'm' && str[i] != 'H';
