3e6d6904c15349dc232460ad0374b702aea3b9f6
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-09-19 11:21:46 +0800

    Revert "http_server.py: Double timeout"
    This reverts commit 6eeacec5060deba577d87635f63a2ddf4fceb7de.

--- test/scripts/http_server.py
+++ test/scripts/http_server.py
@@ -21,7 +21,7 @@ def main():
     )
     while True:
         try:
-            http.client.HTTPConnection("127.0.0.1", 9000, timeout=10).connect()
+            http.client.HTTPConnection("127.0.0.1", 9000, timeout=5).connect()
         except ConnectionRefusedError:
             continue
         break
