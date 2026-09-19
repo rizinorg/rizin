0c9274922a1f501273df8a13bd5d64095796a11a
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-09-19 12:44:10 +0800

    kill_http_server.py: Print error message if http.server process not found

--- test/scripts/kill_http_server.py
+++ test/scripts/kill_http_server.py
@@ -6,12 +6,16 @@ This script kills the http.server launched by http_server.py.
 """
 
 import psutil
+import sys
 
 for p in psutil.process_iter(["cmdline"]):
     try:
         if "python3 -m http.server" in " ".join(p.cmdline()):
             p.kill()
             print("Killed http.server")
-            break
+            sys.exit(0)
     except psutil.AccessDenied:
         pass
+
+print("ERROR: http.server process not found")
+sys.exit(1)
