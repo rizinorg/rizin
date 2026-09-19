21a859fb63381470677257f84eb5815503d0f727
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-09-19 12:46:56 +0800

    [to be reverted] macos: Print list of processes

--- test/scripts/kill_http_server.py
+++ test/scripts/kill_http_server.py
@@ -8,8 +8,10 @@ This script kills the http.server launched by http_server.py.
 import psutil
 import sys
 
-for p in psutil.process_iter(["cmdline"]):
+for p in psutil.process_iter(["pid", "name", "exe", "cmdline"]):
     try:
+        if sys.platform == "darwin":
+            print(p.pid, p.name(), p.exe(), p.cmdline())
         if "python3 -m http.server" in " ".join(p.cmdline()):
             p.kill()
             print("Killed http.server")
