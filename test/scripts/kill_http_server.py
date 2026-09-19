558b8e7d4e154491d7591f3082825921683ec16e
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-09-19 13:18:19 +0800

    Revert "[to be reverted] macos: Print list of processes"
    This reverts commit 21a859fb63381470677257f84eb5815503d0f727.

--- test/scripts/kill_http_server.py
+++ test/scripts/kill_http_server.py
@@ -8,10 +8,8 @@ This script kills the http.server launched by http_server.py.
 import sys
 import psutil
 
-for p in psutil.process_iter(["pid", "name", "exe", "cmdline"]):
+for p in psutil.process_iter(["cmdline"]):
     try:
-        if sys.platform == "darwin":
-            print(p.pid, p.name(), p.exe(), p.cmdline())
         if "python3 -m http.server" in " ".join(p.cmdline()):
             p.kill()
             print("Killed http.server")
