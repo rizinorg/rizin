a14cb2f0ba2e9496448638cbe41645a33fb02c99
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-09-21 20:07:42 +0800

    http test: Continue on timeout and confirm psutil import (#6768)
    * kill_http_server.py: Print confirmation
    * http_server.py: Continue on TimeoutError
    * kill_http_server.py: Continue on psutil.AccessDenied
    * Install `psutil` on bsd ci
    * kill_http_server.py: Print error message if http.server process not found
    * kill_http_server.py: Add psutil.NoSuchProcess to exception list

--- test/scripts/kill_http_server.py
+++ test/scripts/kill_http_server.py
@@ -5,9 +5,18 @@ This script kills the http.server launched by http_server.py.
 
 """
 
+import re
+import sys
 import psutil
 
 for p in psutil.process_iter(["cmdline"]):
-    if "python3 -m http.server" in " ".join(p.cmdline()):
-        p.kill()
-        break
+    try:
+        if re.search(r"[Pp]ython3? -m http\.server", " ".join(p.cmdline())):
+            p.kill()
+            print("Killed http.server")
+            sys.exit(0)
+    except (psutil.AccessDenied, psutil.NoSuchProcess):
+        pass
+
+print("ERROR: http.server process not found")
+sys.exit(1)
